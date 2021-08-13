21f58d6a48bbb0959cb31900a0601ce34b47134f
V0rt3x667 <al-g0l@outlook.com>
2021-08-13 02:27:03 +0100

    Add lr-dosbox-svn core and remove lr-dosbox core.

---
+++ scriptmodules/libretrocores/lr-dosbox-svn.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# This file is part of the ArchyPie project.
+#
+# Please see the LICENSE file at the top-level directory of this distribution.
+
+rp_module_id="lr-dosbox-svn"
+rp_module_desc="DOSBox SVN Libretro Core"
+rp_module_help="ROM Extensions: .bat .com .exe .sh\n\nCopy your DOS games to $ROMDIR/pc"
+rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/dosbox-svn/libretro/COPYING"
+rp_module_repo="git https://github.com/libretro/dosbox-svn.git libretro"
+rp_module_section="exp"
+rp_module_flags=""
+
+function sources_lr-dosbox-svn() {
+    gitPullOrClone
+}
+
+function build_lr-dosbox-svn() {
+    local params=()
+    if isPlatform "arm"; then
+        if isPlatform "armv6"; then
+            params+="WITH_DYNAREC=oldarm"
+        else
+            params+="WITH_DYNAREC=arm"
+        fi
+    fi
+    make -C libretro -f Makefile.libretro clean
+    make -C libretro -f Makefile.libretro "${params[@]}"
+    md_ret_require="$md_build/libretro/dosbox_svn_libretro.so"
+}
+
+function install_lr-dosbox-svn() {
+    md_ret_files=(
+        'COPYING'
+        'libretro/dosbox_svn_libretro.so'
+        'README'
+    )
+}
+
+function configure_lr-dosbox-svn() {
+    mkRomDir "pc"
+    ensureSystemretroconfig "pc"
+
+    addEmulator 0 "$md_id" "pc" "$md_inst/dosbox_svn_libretro.so"
+    addSystem "pc"
+}
