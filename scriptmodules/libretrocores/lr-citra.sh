1d806d1380860dd21174d16bbb7dca07b023e2e1
V0rt3x667 <al-g0l@outlook.com>
2022-03-20 16:57:05 +0000

    Disable lr-citra
    Disable lr-citra until upstream fixes issues with compilation failing.

--- scriptmodules/libretrocores/lr-citra.sh
+++ scriptmodules/libretrocores/lr-citra.sh
@@ -10,10 +10,19 @@ rp_module_help="ROM Extensions: .3ds .3dsx .app .cci .cxi\n\nCopy Your Nintendo 
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/citra/master/license.txt"
 rp_module_repo="git https://github.com/libretro/citra.git master"
 rp_module_section="opt"
-rp_module_flags="!all 64bit"
+rp_module_flags="!all"
 
 function depends_lr-citra() {
-    depends_citra
+        local depends=(
+        'boost'
+        'clang'
+        'cmake'
+        'ffmpeg'
+        'fmt'
+        'libfdk-aac'
+        'ninja'
+    )
+    getDepends "${depends[@]}"
 }
 
 function sources_lr-citra() {
@@ -21,17 +30,23 @@ function sources_lr-citra() {
 }
 
 function build_lr-citra() {
-    mkdir build
-    cd build
-    cmake .. \
+    cmake . \
+        -Bbuild \
+        -GNinja \
         -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX="$md_inst" \
+        -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON \
         -DENABLE_LIBRETRO=ON \
+        -DCMAKE_C_COMPILER="clang" \
+        -DCMAKE_CXX_COMPILER="clang++" \
+        -DCMAKE_CXX_FLAGS="${CXXFLAGS} -DFMT_USE_USER_DEFINED_LITERALS=0 -fbracket-depth=649 -fno-lto" \
+        -DUSE_SYSTEM_BOOST="ON" \
         -DENABLE_QT=OFF \
-        -DENABLE_SDL2=ON \
-        -DENABLE_WEB_SERVICE=OFF
-    make clean
-    make
-    md_ret_require="$md_build/build/src/citra_libretro/citra_libretro.so"
+        -DENABLE_SDL2=OFF \
+        -DENABLE_WEB_SERVICE=OFF \
+        -Wno-dev
+    ninja -C build
+    md_ret_require="build/src/citra_libretro/citra_libretro.so"
 }
 
 function install_lr-citra() {
