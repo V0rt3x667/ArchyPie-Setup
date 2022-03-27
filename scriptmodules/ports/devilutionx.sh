725750cbda491acb3bb7a0f40aa944a5ac662c8b
V0rt3x667 <al-g0l@outlook.com>
2022-03-27 17:05:16 +0100

    Fix and update DevilutionX module.
    Fix and update DevilutionX module.

--- scriptmodules/ports/devilutionx.sh
+++ scriptmodules/ports/devilutionx.sh
@@ -18,11 +18,11 @@ function _get_branch_devilutionx() {
 function depends_devilutionx() {
     local depends=(
         'cmake'
-        'fmt'
         'gettext'
         'libpng'
-        'libsodium'
         'ninja'
+        'perl-rename'
+        'sdl2'
         'sdl2_ttf'
     )
     getDepends "${depends[@]}"
@@ -33,24 +33,63 @@ function sources_devilutionx() {
 }
 
 function build_devilutionx() {
+    ver=$(_get_branch_devilutionx)
     cmake . \
         -Bbuild \
         -GNinja \
+        -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_INSTALL_PREFIX="$md_inst" \
         -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON \
+        -DVERSION_NUM="$ver" \
+        -DDEVILUTIONX_SYSTEM_LIBFMT=OFF \
+        -DDEVILUTIONX_SYSTEM_LIBSODIUM=OFF \
         -Wno-dev
+    ninja -C build clean
     ninja -C build
     md_ret_require="$md_build/build/devilutionx"
 }
 
 function install_devilutionx() {
-    ninja -C build install/strip
+    md_ret_files=(
+        'build/devilutionx'
+        'Packaging/resources/assets/'
+        'Packaging/nix/README.txt'
+    )
+}
+
+function _add_games_devilutionx() {
+    local cmd="$1"
+    local game
+    
+    # Lowercase file names
+    pushd "$romdir/ports/diablo"
+    perl-rename 'y/A-Z/a-z/' *
+    popd
+    
+    declare -A games=(
+        ['diablo/diabat.mpq']="Diablo"
+        ['diablo/hellfire.mpq']="Diablo: Hellfire"
+        ['diablo/spawn.mpq']="Diablo (Shareware)"
+    )
+
+    for game in "${!games[@]}"; do
+        local file="$romdir/ports/$game"
+        local grp="${game#*/}"
+        if [[ "$game" == diablo/diabat.mpq && -f "$file" ]]; then
+            addPort "$md_id" "${game#*/}" "${games[$game]}" "$cmd --diablo"
+        elif [[ "${game}" == diablo/hellfire.mpq && -f "$file" ]]; then
+            addPort "$md_id" "${game#*/}" "${games[$game]}" "$cmd --hellfire"
+        elif [[ "${game}" == diablo/spawn.mpq && -f "$file" ]]; then
+            addPort "$md_id" "${game#*/}" "${games[$game]}" "$cmd --spawn"
+        fi
+    done
 }
 
 function configure_devilutionx() {
     mkRomDir "ports/diablo"
+    mkUserDir "$home/.local/share/diasurgical"
 
     moveConfigDir "$home/.local/share/diasurgical/devilution" "$md_conf_root/diablo"
 
-    addPort "$md_id" "diablo" "diablo" "$md_inst/bin/devilutionx --data-dir $romdir/ports/diablo --save-dir $romdir/ports/diablo --ttf-dir $md_inst/emulators/devilutionx/share"
+    [[ "$md_mode" == "install" ]] && _add_games_devilutionx "$md_inst/devilutionx --data-dir $romdir/ports/diablo --save-dir $romdir/ports/diablo --data-dir $md_inst/ports/devilutionx/assets"
 }
