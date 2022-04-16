3414ee3a5f2f3489d38337f066d385a77d35642b
V0rt3x667 <al-g0l@outlook.com>
2022-04-17 00:48:46 +0100

    Update lr-desmume.sh
    Add missing quotes.

--- scriptmodules/libretrocores/lr-desmume.sh
+++ scriptmodules/libretrocores/lr-desmume.sh
@@ -30,7 +30,7 @@ function sources_lr-desmume() {
 function build_lr-desmume() {
     cd desmume/src/frontend/libretro
     make clean
-    make $(_params_lr-desmume)
+    make "$(_params_lr-desmume)"
     md_ret_require="$md_build/desmume/src/frontend/libretro/desmume_libretro.so"
 }
 
