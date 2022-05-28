a6f7cae9ca7c8f27312e4291266486703a50b4a9
V0rt3x667 <al-g0l@outlook.com>
2022-05-28 02:16:31 +0100

    Updated Bombermaaan module.

--- scriptmodules/ports/bombermaaan.sh
+++ scriptmodules/ports/bombermaaan.sh
@@ -7,56 +7,82 @@
 rp_module_id="bombermaaan"
 rp_module_desc="Bombermaaan - Bomberman Clone"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/bjaraujo/Bombermaaan/master/LICENSE.txt"
-rp_module_repo="git https://github.com/bjaraujo/Bombermaaan.git :_get_branch_bombermaaan"
+rp_module_repo="git https://github.com/bjaraujo/Bombermaaan.git master"
 rp_module_section="exp"
 rp_module_flags="sdl1 !mali"
 
-function _get_branch_bombermaaan() {
-    download https://api.github.com/repos/bjaraujo/Bombermaaan/releases/latest - | grep -m 1 tag_name | cut -d\" -f4
-}
-
 function depends_bombermaaan() {
-    getDepends cmake sdl sdl_mixer
+    local depends=(
+        'cmake'
+        'ninja'
+        'sdl_gfx'
+        'sdl_mixer'
+    )
+    getDepends "${depends[@]}"
 }
 
 function sources_bombermaaan() {
     gitPullOrClone
+    # "sdl 1 classic" required, Bombermaaan will not build with sdl12-compat.
+    _sources_sdl
+}
+
+function _sources_sdl() {
+    gitPullOrClone "$md_build/sdl" "https://github.com/libsdl-org/SDL-1.2.git" "main"
+}
+
+function _build_sdl() {
+    cd sdl || return
+    ./autogen.sh
+    ./configure --disable-rpath --disable-static
+    make clean
+    make
 }
 
 function build_bombermaaan() {
-    cd trunk
+    _build_sdl && cd "$md_build" || return
     cmake . \
+        -Strunk \
+        -Btrunk/build \
+        -GNinja \
+        -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_INSTALL_PREFIX="$md_inst" \
-        -DLOAD_RESOURCES_FROM_FILES:BOOL=ON \
+        -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON \
+        -DSDLMAIN_LIBRARY="$md_build/sdl/build/.libs/libSDLmain.a" \
+        -DSDL_LIBRARY="$md_build/sdl/build/.libs/libSDL.so" \
+        -DSDL_INCLUDE_DIR="$md_build/sdl/include" \
+        -DLOAD_RESOURCES_FROM_FILES=ON \
         -Wno-dev
-    make
-    mv bin/Bombermaaan bombermaaan
-    md_ret_require="$md_build/trunk/bombermaaan"
+    ninja -C trunk/build clean
+    ninja -C trunk/build
+    md_ret_require="$md_build/trunk/build/bin/Bombermaaan"
 }
 
 function install_bombermaaan() {
-    md_ret_files=(        
-        'trunk/bombermaaan'
-        'trunk/levels'
-        'trunk/res/images'
-        'trunk/res/sounds'
-    )
+    # md_ret_files=(        
+    #     'trunk/bombermaaan'
+    #     'trunk/levels'
+    #     'trunk/res/images'
+    #     'trunk/res/sounds'
+    # )
+    ninja -C trunk/build install/strip
 }
 
 function configure_bombermaaan() {
-    addPort "$md_id" "bombermaaan" "Bombermaaan" "$md_inst/bombermaaan"
+    addPort "$md_id" "bombermaaan" "Bombermaaan" "$md_inst/Bombermaaan"
 
     isPlatform "dispmanx" && setBackend "$md_id" "dispmanx"
 
     moveConfigDir "$home/.Bombermaaan" "$md_conf_root/bombermaaan"
+    moveConfigFile "$md_inst/config.xml" "$md_conf_root/bombermaaan/config.xml"
 
-    local file="$romdir/ports/Bombermaaan.sh"
-    cat >"$file" << _EOF_
-#!/bin/bash
-pushd "$md_inst"
-"$rootdir/supplementary/runcommand/runcommand.sh" 0 _PORT_ bombermaaan ""
-popd
-_EOF_
-    chown $user:$user "$file"
-    chmod a+x "$file"
+#     local file="$romdir/ports/Bombermaaan.sh"
+#     cat >"$file" << _EOF_
+# #!/bin/bash
+# pushd "$md_inst"
+# "$rootdir/supplementary/runcommand/runcommand.sh" 0 _PORT_ bombermaaan ""
+# popd
+# _EOF_
+#     chown $user:$user "$file"
+#     chmod a+x "$file"
 }
