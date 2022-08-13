c54a96f7ebb98efa8347849c8cc40a3fde41f174
V0rt3x667 <al-g0l@outlook.com>
2022-08-13 14:53:29 +0100

    Revised eduke32.sh
    Revised eduke32.sh. Created a new port called b-engine for build engine based games.

--- scriptmodules/ports/eduke32.sh
+++ scriptmodules/ports/eduke32.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This file is part of the ArchyPie project.
+# This file is part of the ArchPie project.
 #
 # Please see the LICENSE file at the top-level directory of this distribution.
 
@@ -17,6 +17,7 @@ function depends_eduke32() {
         'libpng'
         'libvorbis'
         'libvpx'
+        'sdl2'
         'sdl2_mixer'
     )
     isPlatform "x86" && depends+=('nasm')
@@ -27,12 +28,13 @@ function depends_eduke32() {
 
 function sources_eduke32() {
     gitPullOrClone
+
+    applyPatch "$md_data/01_set_default_config_path.patch"
 }
 
 function build_eduke32() {
     local params=(LTO=1 SDL_TARGET=2 SDL_STATIC=0)
 
-    [[ "$md_id" == "ionfury" ]] && params+=(FURY=1)
     ! isPlatform "x86" && params+=(NOASM=1)
     ! isPlatform "x11" && params+=(HAVE_GTK2=0)
     ! isPlatform "gl3" && params+=(POLYMER=0)
@@ -41,106 +43,87 @@ function build_eduke32() {
     make veryclean
     make "${params[@]}"
 
-    if [[ "$md_id" == "ionfury" ]]; then
-        md_ret_require="$md_build/fury"
-    else
-        md_ret_require="$md_build/eduke32"
-    fi
+    md_ret_require="$md_build/eduke32"
 }
 
 function install_eduke32() {
-    md_ret_files=('mapster32')
-
-    if [[ "$md_id" == "ionfury" ]]; then
-        md_ret_files+=('fury')
-    else
-        md_ret_files+=('eduke32')
-    fi
+    md_ret_files=('eduke32' 'mapster32')
 }
 
-function game_data_eduke32() {
+function _game_data_eduke32() {
     local dest 
-    dest="$romdir/ports/duke3d"
-    if [[ "$md_id" == "eduke32" ]]; then
-        if [[ ! -f "$dest/duke3d.grp" ]]; then
-            mkUserDir "$dest"
-            local temp 
-            temp="$(mktemp -d)"
-            download "$__archive_url/3dduke13.zip" "$temp"
-            unzip -L -o "$temp/3dduke13.zip" -d "$temp" dn3dsw13.shr
-            unzip -L -o "$temp/dn3dsw13.shr" -d "$dest" duke3d.grp duke.rts
-            rm -rf "$temp"
-            chown -R "$user:$user" "$dest"
-        fi
-    fi
-}
-
-function configure_eduke32() {
-    local appname="eduke32"
-    local portname="duke3d"
-    if [[ "$md_id" == "ionfury" ]]; then
-        appname="fury"
-        portname="ionfury"
-    fi
-    local config="$md_conf_root/$portname/settings.cfg"
-
-    mkRomDir "ports/$portname"
-    moveConfigDir "$home/.config/$appname" "$md_conf_root/$portname"
-
-    _add_games_eduke32 "$md_inst/$appname"
-
-    if [[ "$md_mode" == "install" ]]; then
-        game_data_eduke32
-
-        touch "$config"
-        iniConfig " " '"' "$config"
-
-        # enforce vsync for kms targets
-        isPlatform "kms" && iniSet "r_swapinterval" "1"
-
-        # the VC4 & V3D drivers render menu splash colours incorrectly without this
-        isPlatform "mesa" && iniSet "r_useindexedcolortextures" "0"
-
-        chown -R "$user:$user" "$config"
+    dest="$romdir/ports/b-engine/duke3d"
+    if [[ -f "$dest/duke3d.grp" || -f "$dest/DUKE3D.GRP" ]]; then
+        return
+    else
+        downloadAndExtract "$__arpie_url/Duke3D/duke3d_assets_sw.tar.xz" "$dest"
     fi
 }
 
 function _add_games_eduke32() {
-    local binary="$1"
+    local cmd="$1"
+    local dir
     local game
-    local game_args
-    local game_portname
-    local game_launcher
-    local num_games=4
+    declare -A games=(
+        ['duke3d/duke3d.grp']="Duke Nukem 3D"
+        ['duke3d/dukedc.grp']="Duke Nukem 3D: Duke It Out in D.C."
+        ['duke3d/nwinter.grp']="Duke Nukem 3D: Duke: Nuclear Winter"
+        ['duke3d/vacation.grp']="Duke Nukem 3D: Duke Caribbean: Life's a Beach"
+        ['nam/nam.grp']="NAM (AKA Napalm)"
+        ['nam/napalm.grp']="Napalm (AKA NAM)"
+    )
 
-    if [[ "$md_id" == "ionfury" ]]; then
-        num_games=0
-        local game0=('Ion Fury' '' '')
-    else
-        local game0=('Duke Nukem 3D' 'duke3d' '-addon 0')
-        local game1=('Duke Nukem 3D: Duke It Out In D.C.' 'duke3d' '-addon 1')
-        local game2=('Duke Nukem 3D: Duke: Nuclear Winter' 'duke3d' '-addon 2')
-        local game3=('Duke Nukem 3D: Duke Caribbean: Life'\''s a Beach' 'duke3d' '-addon 3')
-        local game4=('NAM' 'nam' '-nam')
-    fi
-
-    for ((game=0;game<=num_games;game++)); do
-        game_launcher="game${game}[0]"
-        game_portname="game${game}[1]"
-        game_args="game${game}[2]"
-
-        if [[ -d "$romdir/ports/${!game_portname}" ]]; then
-           addPort "$md_id" "${!game_portname}" "${!game_launcher}" "pushd $md_conf_root/${!game_portname}; ${binary}.sh %ROM%; popd" "-j$romdir/ports/${!game_portname} ${!game_args}"
+    # Create .sh files for each game found. Uppercase filenames will be converted to lowercase.
+    for game in "${!games[@]}"; do
+        dir="$romdir/ports/b-engine/${game%%/*}"
+        if [[ "$md_mode" == "install" ]]; then
+            pushd "$dir"
+            perl-rename 'y/A-Z/a-z/' [^.-]{*,*/*}
+            popd
+        fi
+        if [[ -f "$dir/${game##*/}" ]]; then
+            if [[ "$game" == "duke3d/duke3d.grp" ]]; then
+                addPort "$md_id" "b-engine" "${games[$game]}" "pushd $md_conf_root/b-engine/$md_id; $md_inst/$md_id.sh %ROM%; popd" "-j $dir -addon 0"
+            elif [[ "$game" == "duke3d/dukedc.grp" ]]; then
+                addPort "$md_id" "b-engine" "${games[$game]}" "pushd $md_conf_root/b-engine/$md_id; $md_inst/$md_id.sh %ROM%; popd" "-j $dir -addon 1"
+            elif [[ "$game" == "duke3d/nwinter.grp" ]]; then
+                addPort "$md_id" "b-engine" "${games[$game]}" "pushd $md_conf_root/b-engine/$md_id; $md_inst/$md_id.sh %ROM%; popd" "-j $dir -addon 2"
+            elif [[ "$game" == "duke3d/vacation.grp" ]]; then
+                addPort "$md_id" "b-engine" "${games[$game]}" "pushd $md_conf_root/b-engine/$md_id; $md_inst/$md_id.sh %ROM%; popd" "-j $dir -addon 3"
+            elif [[ "$game" == "nam/nam.grp" ]]; then
+                addPort "$md_id" "b-engine" "${games[$game]}" "pushd $md_conf_root/b-engine/$md_id; $md_inst/$md_id.sh %ROM%; popd" "-j $dir -nam"
+            elif [[ "$game" == "nam/napalm.grp" ]]; then
+                addPort "$md_id" "b-engine" "${games[$game]}" "pushd $md_conf_root/b-engine/$md_id; $md_inst/$md_id.sh %ROM%; popd" "-j $dir -napalm"
+            fi
         fi
     done
 
     if [[ "$md_mode" == "install" ]]; then
-        # we need to use a dumb launcher script to strip quotes from runcommand's generated arguments
-        cat > "${binary}.sh" << _EOF_
+        # Use a dumb launcher script to strip quotes from runcommand's generated arguments.
+        local binary
+        binary="eduke32"
+
+    cat > "$md_inst/$md_id.sh" << _EOF_
 #!/bin/bash
-# HACK: force vsync for RPI Mesa driver for now
-VC4_DEBUG=always_sync $binary \$*
+$cmd \$*
 _EOF_
-        chmod +x "${binary}.sh"
+    chmod +x "$md_inst/$md_id.sh"
     fi
+}
+
+function configure_eduke32() {
+    if [[ "$md_mode" == "install" ]]; then
+        mkRomDir "ports/b-engine"
+
+        local dirs=('duke3d' 'nam')
+        for dir in "${dirs[@]}"; do
+            mkRomDir "ports/b-engine/$dir"
+        done
+
+        _game_data_eduke32
+    fi
+
+    moveConfigDir "$arpiedir/ports/$md_id" "$md_conf_root/b-engine/$md_id"
+
+    _add_games_eduke32 "$md_inst/eduke32"
 }
