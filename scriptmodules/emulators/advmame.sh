4f95669316afb3c47648e3f7aadb19f59325d2b2
V0rt3x667 <al-g0l@outlook.com>
2022-11-08 01:34:59 +0000

    Update AdvancedMAME

--- scriptmodules/emulators/advmame.sh
+++ scriptmodules/emulators/advmame.sh
@@ -5,48 +5,39 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="advmame"
-rp_module_desc="AdvanceMAME - Arcade Emulator"
-rp_module_help="ROM Extension: .zip\n\nCopy your AdvanceMAME roms to either $romdir/mame-advmame or\n$romdir/arcade"
+rp_module_desc="AdvanceMAME: Arcade Machine Emulator (MAME 0.106)"
+rp_module_help="ROM Extension: .zip\n\nCopy your AdvanceMAME roms to either $romdir/mame-$md_id or\n$romdir/arcade"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/amadvance/advancemame/master/COPYING"
 rp_module_repo="git https://github.com/amadvance/advancemame master"
 rp_module_section="opt"
-rp_module_flags="sdl2 sdl1-videocore"
-
-function _update_hook_advmame() {
-    # if the non split advmame is installed, make directories for 0.94 / 1.4 so they will be updated
-    # when doing update all packages
-    if [[ -d "$md_inst/0.94.0" ]]; then
-        mkdir -p "$rootdir/emulators/advmame-"{0.94,1.4}
-        printMsgs "dialog" "The advmame package has now been split into the following packages.\n\nadvmame-0.94\nadvmame-1.4\nadvmame\n\nIf you have chosen just to update the ArchyPie-Setup script, you will need to update all the advmame packages for them to work correctly.\n\nNote that advmame-0.94.0.rc will be renamed to advmame-0.94.rc and the config for the main advmame will be advmame.rc.\n\nThe advmame package will be the latest version of the software."
-    fi
-}
+rp_module_flags=""
 
 function depends_advmame() {
-    local depends=(autoconf automake)
+    local depends=('sdl2')
     if isPlatform "videocore"; then
-        depends+=(sdl libraspberrypi-firmware)
-    else
-        depends+=(sdl2)
+        depends+=('raspberrypi-firmware')
     fi
     getDepends "${depends[@]}"
 }
 
 function sources_advmame() {
     gitPullOrClone
+    applyPatch "$md_data/01_set_default_config_path.patch"
 }
 
 function build_advmame() {
-    local params=()
+    local params=('--enable-sdl2' '--disable-sdl' '--disable-oss')
     if isPlatform "videocore"; then
-        params+=(--enable-sdl --disable-sdl2 --enable-vc)
+        params+=('--enable-vc')
     else
-        params+=(--enable-sdl2 --disable-sdl --disable-vc)
+        params+=('--disable-vc')
     fi
+
     ./autogen.sh
     ./configure CFLAGS="$CFLAGS -fno-stack-protector" --prefix="$md_inst" "${params[@]}"
     make clean
     make
-    md_ret_require="$md_build/advmame"
+    md_ret_require="$md_build/$md_id"
 }
 
 function install_advmame() {
@@ -54,52 +45,48 @@ function install_advmame() {
 }
 
 function configure_advmame() {
-    mkRomDir "arcade"
-    mkRomDir "arcade/advmame"
-    mkRomDir "mame-advmame"
-
-    moveConfigDir "$home/.advance" "$md_conf_root/mame-advmame"
-
-    # move any old named configs (with 3.2 taking priority)
-    local ver
-    for ver in 3.1 3.2; do
-        if [[ -f "$md_conf_root/mame-advmame/advmame-$ver.rc" ]]; then
-            mv "$md_conf_root/mame-advmame/advmame-$ver.rc" "$md_conf_root/mame-advmame/advmame.rc"
-        fi
-
-        # remove any old emulator.cfg entries
-        delEmulator advmame-$ver mame-advmame
-        delEmulator advmame-$ver arcade
-    done
-
     if [[ "$md_mode" == "install" ]]; then
-        local mame_sub_dir
-        for mame_sub_dir in artwork diff hi inp memcard nvram sample snap sta; do
-            mkRomDir "mame-advmame/$mame_sub_dir"
-            ln -sf "$romdir/mame-advmame/$mame_sub_dir" "$romdir/arcade/advmame"
-            # fix for older broken symlink generation
-            rm -f "$romdir/mame-advmame/$mame_sub_dir/$mame_sub_dir"
+        mkRomDir "arcade"
+        mkRomDir "arcade/$md_id"
+        mkRomDir "mame-$md_id"
+
+        local dirs=(
+            'artwork'
+            'diff'
+            'hi'
+            'inp'
+            'memcard'
+            'nvram'
+            'sample'
+            'snap'
+            'sta'
+        )
+        for dir in "${dirs[@]}"; do
+            mkRomDir "mame-$md_id/$dir"
+            ln -sf "$romdir/mame-$md_id/$dir" "$romdir/arcade/$md_id"
         done
+
+        mkUserDir "$arpdir/$md_id"
+        moveConfigDir "$arpdir/$md_id" "$md_conf_root/mame-$md_id"
     fi
 
-    if [[ "$md_mode" == "install" && ! -f "$md_conf_root/mame-advmame/$md_id.rc" ]]; then
+    if [[ "$md_mode" == "install" && ! -f "$md_conf_root/mame-$md_id/$md_id.rc" ]]; then
+        su "$user" -c "$md_inst/bin/$md_id --default"
 
-        su "$user" -c "$md_inst/bin/advmame --default"
-
-        iniConfig " " "" "$md_conf_root/mame-advmame/$md_id.rc"
+        iniConfig " " "" "$md_conf_root/mame-$md_id/$md_id.rc"
 
         iniSet "misc_quiet" "yes"
-        iniSet "dir_rom" "$romdir/mame-advmame:$romdir/arcade"
-        iniSet "dir_artwork" "$romdir/mame-advmame/artwork"
-        iniSet "dir_sample" "$romdir/mame-advmame/samples"
-        iniSet "dir_diff" "$romdir/mame-advmame/diff"
-        iniSet "dir_hi" "$romdir/mame-advmame/hi"
-        iniSet "dir_image" "$romdir/mame-advmame"
-        iniSet "dir_inp" "$romdir/mame-advmame/inp"
-        iniSet "dir_memcard" "$romdir/mame-advmame/memcard"
-        iniSet "dir_nvram" "$romdir/mame-advmame/nvram"
-        iniSet "dir_snap" "$romdir/mame-advmame/snap"
-        iniSet "dir_sta" "$romdir/mame-advmame/nvram"
+        iniSet "dir_rom" "$romdir/mame-$md_id:$romdir/arcade"
+        iniSet "dir_artwork" "$romdir/mame-$md_id/artwork"
+        iniSet "dir_sample" "$romdir/mame-$md_id/samples"
+        iniSet "dir_diff" "$romdir/mame-$md_id/diff"
+        iniSet "dir_hi" "$romdir/mame-$md_id/hi"
+        iniSet "dir_image" "$romdir/mame-$md_id"
+        iniSet "dir_inp" "$romdir/mame-$md_id/inp"
+        iniSet "dir_memcard" "$romdir/mame-$md_id/memcard"
+        iniSet "dir_nvram" "$romdir/mame-$md_id/nvram"
+        iniSet "dir_snap" "$romdir/mame-$md_id/snap"
+        iniSet "dir_sta" "$romdir/mame-$md_id/nvram"
 
         if isPlatform "videocore"; then
             iniSet "device_video" "fb"
@@ -111,7 +98,7 @@ function configure_advmame() {
             iniSet "display_resizeeffect" "none"
             iniSet "display_resize" "integer"
             iniSet "display_magnify" "1"
-        elif isPlatform "kms" || isPlatform "mali"; then
+        else
             iniSet "device_video" "sdl"
             # need to force keyboard device as auto will choose event driver which doesn't work with sdl
             iniSet "device_keyboard" "sdl"
@@ -119,7 +106,6 @@ function configure_advmame() {
             iniSet "display_magnify" "1"
             # disable threading to get rid of the crash-on-exit when using SDL, preventing config save
             iniSet "misc_smp" "no"
-        else
             iniSet "device_video_output" "overlay"
             iniSet "display_aspectx" 16
             iniSet "display_aspecty" 9
@@ -127,9 +113,9 @@ function configure_advmame() {
         fi
     fi
 
-    addEmulator 1 "$md_id" "arcade" "$md_inst/bin/advmame %BASENAME%"
-    addEmulator 1 "$md_id" "mame-advmame" "$md_inst/bin/advmame %BASENAME%"
+    addEmulator 1 "$md_id" "arcade" "$md_inst/bin/$md_id %BASENAME%"
+    addEmulator 1 "$md_id" "mame-$md_id" "$md_inst/bin/$md_id %BASENAME%"
 
     addSystem "arcade"
-    addSystem "mame-advmame"
+    addSystem "mame-$md_id"
 }
