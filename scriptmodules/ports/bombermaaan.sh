2d7fba3b05add3d6ff4befdef73eda6c5b01561f
V0rt3x667 <al-g0l@outlook.com>
2022-11-30 02:58:00 +0000

    Update bombermaaan.sh

--- scriptmodules/ports/bombermaaan.sh
+++ scriptmodules/ports/bombermaaan.sh
@@ -5,11 +5,11 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="bombermaaan"
-rp_module_desc="Bombermaaan - Bomberman Clone"
+rp_module_desc="Bombermaaan: Bomberman Clone"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/bjaraujo/Bombermaaan/master/LICENSE.txt"
 rp_module_repo="git https://github.com/bjaraujo/Bombermaaan.git master"
 rp_module_section="exp"
-rp_module_flags="sdl1 !mali"
+rp_module_flags="!mali"
 
 function depends_bombermaaan() {
     local depends=(
@@ -17,6 +17,7 @@ function depends_bombermaaan() {
         'dos2unix'
         'ninja'
         'sdl_mixer'
+        'sdl12-compat' 
     )
     getDepends "${depends[@]}"
 }
@@ -24,18 +25,21 @@ function depends_bombermaaan() {
 function sources_bombermaaan() {
     gitPullOrClone
 
-    # line endings need to be converted or patching will fail.
+    # Line Endings Need To Be Converted Or Patching Will Fail.
     find . -type f -exec dos2unix {} \;
 
-    applyPatch "$md_data/01_set_default_config_path.patch"
-    applyPatch "$md_data/02_set_default_fullscreen.patch"
+    # Set Default Config Path
+    sed -e "s|append(\"/.Bombermaaan/\");|append(\"/ArchyPie/configs/${md_id}/\");|g" -i "${md_build}/trunk/src/CGame.cpp"
 
-    # "sdl 1 classic" required, Bombermaaan will not build with sdl12-compat.
+    # Set Fullscreen By Default
+    sed -e "s|DISPLAYMODE_WINDOWED;|DISPLAYMODE_FULL3;|g" -i "${md_build}/trunk/src/COptions.cpp"
+    
+    # "SDL 1 Classic" Is Required To Build Bombermaaan. The sdl12-compat Package Is Used To Run Bombermaaan.
     _sources_sdl
 }
 
 function _sources_sdl() {
-    gitPullOrClone "$md_build/sdl" "https://github.com/libsdl-org/SDL-1.2.git" "main"
+    gitPullOrClone "${md_build}/sdl" "https://github.com/libsdl-org/SDL-1.2.git" "main"
 }
 
 function _build_sdl() {
@@ -47,22 +51,22 @@ function _build_sdl() {
 }
 
 function build_bombermaaan() {
-    _build_sdl && cd "$md_build" || exit
+    _build_sdl && cd "${md_build}" || exit
     cmake . \
         -Strunk \
         -Btrunk/build \
         -GNinja \
         -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$md_inst" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
         -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON \
-        -DSDLMAIN_LIBRARY="$md_build/sdl/build/.libs/libSDLmain.a" \
-        -DSDL_LIBRARY="$md_build/sdl/build/.libs/libSDL.so" \
-        -DSDL_INCLUDE_DIR="$md_build/sdl/include" \
+        -DSDLMAIN_LIBRARY="${md_build}/sdl/build/.libs/libSDLmain.a" \
+        -DSDL_LIBRARY="${md_build}/sdl/build/.libs/libSDL.so" \
+        -DSDL_INCLUDE_DIR="${md_build}/sdl/include" \
         -DLOAD_RESOURCES_FROM_FILES=ON \
         -Wno-dev
     ninja -C trunk/build clean
     ninja -C trunk/build
-    md_ret_require="$md_build/trunk/build/bin/Bombermaaan"
+    md_ret_require="${md_build}/trunk/build/bin/Bombermaaan"
 }
 
 function install_bombermaaan() {
@@ -70,9 +74,7 @@ function install_bombermaaan() {
 }
 
 function configure_bombermaaan() {
-    addPort "$md_id" "bombermaaan" "Bombermaaan" "$md_inst/Bombermaaan"
+    [[ "${md_mode}" == "install" ]] && moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${md_id}/"
 
-    moveConfigDir "$arpiedir/ports/$md_id" "$md_conf_root/$md_id"
-
-    [[ "$md_mode" == "install" ]] && isPlatform "dispmanx" && setBackend "$md_id" "dispmanx"
+    addPort "${md_id}" "${md_id}" "Bombermaaan" "${md_inst}/Bombermaaan"
 }
