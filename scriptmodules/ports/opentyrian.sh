f2bce2358043d6d9ebb651b8b172b483dfb1a5dc
V0rt3x667 <al-g0l@outlook.com>
2022-11-30 23:05:33 +0000

    Update OpenTyrian and add OpenTyrian2000

--- scriptmodules/ports/opentyrian.sh
+++ scriptmodules/ports/opentyrian.sh
@@ -5,14 +5,14 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="opentyrian"
-rp_module_desc="Open Tyrian - Port of the Classic DOS Game Tyrian"
+rp_module_desc="OpenTyrian: Port of the Classic DOS Game Tyrian"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/opentyrian/opentyrian/master/COPYING"
 rp_module_repo="git https://github.com/opentyrian/opentyrian.git :_get_branch_opentyrian"
 rp_module_section="opt"
 rp_module_flags=""
 
 function _get_branch_opentyrian() {
-    download https://api.github.com/repos/opentyrian/opentyrian/releases/latest - | grep -m 1 tag_name | cut -d\" -f4
+    download "https://api.github.com/repos/opentyrian/opentyrian/releases/latest" - | grep -m 1 tag_name | cut -d\" -f4
 }
 
 function depends_opentyrian() {
@@ -25,37 +25,36 @@ function depends_opentyrian() {
 
 function sources_opentyrian() {
     gitPullOrClone
-    # patch to default to fullscreen
-    applyPatch "$md_data/01_fullscreen.patch"
+
+    # Set Config Path & Fullscreen
+    applyPatch "${md_data}/01_set_default_config_path_fullscreen.patch"
 }
 
 function build_opentyrian() {
     rpSwap on 512
     make clean
-    make TYRIAN_DIR="$md_inst"
+    make TYRIAN_DIR="${md_inst}"
     rpSwap off
-    md_ret_require="$md_build/opentyrian"
+    md_ret_require="${md_build}/${md_id}"
 }
 
 function install_opentyrian() {
-    make install prefix="$md_inst"
+    make install prefix="${md_inst}"
 }
 
 function _game_data_opentyrian() {
-    if [[ ! -d "$romdir/ports/$md_id/data" ]]; then
-        cd "$__tmpdir"
-        # get Tyrian 2.1 (freeware game data)
-        downloadAndExtract "$__archive_url/tyrian21.zip" "$romdir/ports/$md_id/data" -j
-        chown -R "$user:$user" "$romdir/ports/opentyrian"
+    if [[ ! -d "${romdir}/ports/${md_id}/data" ]]; then
+        cd "${__tmpdir}" || exit
+        downloadAndExtract "https://www.camanis.net/tyrian/tyrian21.zip" "${romdir}/ports/${md_id}/data" -j
+        chown -R "${user}:${user}" "${romdir}/ports/${md_id}"
     fi
 }
 
 function configure_opentyrian() {
-    addPort "$md_id" "opentyrian" "OpenTyrian" "$md_inst/bin/opentyrian --data $romdir/ports/$md_id/data"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "ports/${md_id}" && _game_data_opentyrian
+        moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${md_id}/"
+    fi
 
-    mkRomDir "ports/opentyrian"
-
-    moveConfigDir "$home/.config/opentyrian" "$md_conf_root/opentyrian"
-
-    [[ "$md_mode" == "install" ]] && _game_data_opentyrian
+    addPort "${md_id}" "${md_id}" "OpenTyrian" "${md_inst}/bin/${md_id} --data ${romdir}/ports/${md_id}/data/"
 }
---
+++ scriptmodules/ports/opentyrian2000.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+# This file is part of the ArchyPie project.
+#
+# Please see the LICENSE file at the top-level directory of this distribution.
+
+rp_module_id="opentyrian2000"
+rp_module_desc="OpenTyrian2000: Port of the Classic DOS Game Tyrian"
+rp_module_licence="GPL2 https://raw.githubusercontent.com/KScl/opentyrian2000/master/COPYING"
+rp_module_repo="git https://github.com/KScl/opentyrian2000.git master"
+rp_module_section="opt"
+rp_module_flags=""
+
+function depends_opentyrian2000() {
+    local depends=(
+        'sdl2_net'
+        'sdl2'
+    )
+    getDepends "${depends[@]}"
+}
+
+function sources_opentyrian2000() {
+    gitPullOrClone
+
+    # Set Config Path & Fullscreen
+    applyPatch "${md_data}/01_set_default_config_path_fullscreen.patch"
+}
+
+function build_opentyrian2000() {
+    rpSwap on 512
+    make clean
+    make TYRIAN_DIR="${md_inst}"
+    rpSwap off
+    md_ret_require="${md_build}/${md_id}"
+}
+
+function install_opentyrian2000() {
+    make install prefix="${md_inst}"
+}
+
+function _game_data_opentyrian2000() {
+    if [[ ! -d "${romdir}/ports/${md_id}/data" ]]; then
+        cd "${__tmpdir}" || exit
+        downloadAndExtract "https://www.camanis.net/tyrian/tyrian2000.zip" "${romdir}/ports/${md_id}/data" -j
+        chown -R "${user}:${user}" "${romdir}/ports/${md_id}"
+    fi
+}
+
+function configure_opentyrian2000() {
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "ports/${md_id}" && _game_data_opentyrian2000
+        moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${md_id}/"
+    fi
+
+    addPort "${md_id}" "${md_id}" "OpenTyrian2000" "${md_inst}/bin/${md_id} --data ${romdir}/ports/${md_id}/data/"
+}
