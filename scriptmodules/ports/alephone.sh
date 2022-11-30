9a047c5c2764777fe59b6a4a8a09790c3d278c03
V0rt3x667 <al-g0l@outlook.com>
2022-11-30 03:01:57 +0000

    Update alephone.sh
    A few minor changes.

--- scriptmodules/ports/alephone.sh
+++ scriptmodules/ports/alephone.sh
@@ -6,7 +6,6 @@
 
 rp_module_id="alephone"
 rp_module_desc="Aleph One: Marathon Game Engine"
-rp_module_help=""
 rp_module_licence="GPL3 https://raw.githubusercontent.com/Aleph-One-Marathon/alephone/master/COPYING"
 rp_module_repo="git https://github.com/Aleph-One-Marathon/alephone.git :_get_branch_alephone"
 rp_module_section="opt"
@@ -39,7 +38,7 @@ function sources_alephone() {
     gitPullOrClone
 
     # Set Default Config Path
-    sed -e "s|/.alephone|/ArchyPie/configs/alephone|g" -i "${md_build}/Source_Files/CSeries/cspaths_sdl.cpp"
+    sed -e "s|/.alephone|/ArchyPie/configs/${md_id}|g" -i "${md_build}/Source_Files/CSeries/cspaths_sdl.cpp"
 }
 
 function build_alephone() {
@@ -47,7 +46,7 @@ function build_alephone() {
     ./configure --prefix="${md_inst}"
     make clean
     make
-    md_ret_require="${md_build}/Source_Files/alephone"
+    md_ret_require="${md_build}/Source_Files/${md_id}"
 }
 
 function install_alephone() {
@@ -59,7 +58,7 @@ function _game_data_alephone() {
     local release_url
 
     version="$(_get_branch_alephone)"
-    release_url="https://github.com/Aleph-One-Marathon/alephone/releases/download/$version"
+    release_url="https://github.com/Aleph-One-Marathon/alephone/releases/download/${version}"
 
     if [[ ! -f "${romdir}/ports/${md_id}/Marathon/Shapes.shps" ]]; then
         downloadAndExtract "${release_url}/Marathon-${version/release-/}-Data.zip" "${romdir}/ports/${md_id}"
@@ -79,7 +78,6 @@ function _game_data_alephone() {
 function configure_alephone() {
     if [[ "${md_mode}" == "install" ]]; then
         mkRomDir "ports/${md_id}" && _game_data_alephone
-        mkUserDir "${arpdir}/${md_id}"
         moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${md_id}/"
     fi
 
