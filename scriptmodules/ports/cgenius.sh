e01ac37179dcd46e922ecad7f8e6f21ad16a3f8d
V0rt3x667 <al-g0l@outlook.com>
2022-12-01 22:12:00 +0000

    Update cgenius.sh

--- scriptmodules/ports/cgenius.sh
+++ scriptmodules/ports/cgenius.sh
@@ -5,13 +5,13 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="cgenius"
-rp_module_desc="Commander Genius - Modern Interpreter for the Commander Keen Games (Vorticon and Galaxy Games)"
+rp_module_desc="Commander Genius: Modern Interpreter for the Commander Keen Games (Vorticon and Galaxy Games)"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/gerstrong/Commander-Genius/master/COPYRIGHT"
 rp_module_repo="git https://gitlab.com/Dringgstein/Commander-Genius.git :_get_branch_cgenius"
 rp_module_section="exp"
 
 function _get_branch_cgenius() {
-    download https://gitlab.com/api/v4/projects/Dringgstein%2FCommander-Genius/releases - | grep -m 1 tag_name | cut -d\" -f8
+    download "https://gitlab.com/api/v4/projects/Dringgstein%2FCommander-Genius/releases" - | grep -m 1 tag_name | cut -d\" -f8
 }
 
 function depends_cgenius() {
@@ -31,19 +31,19 @@ function depends_cgenius() {
 function sources_cgenius() {
     gitPullOrClone
 
-    applyPatch "$md_data/01_set_default_config_path.patch"
+    # Set Default Config Path
+    sed -e "s|/.CommanderGenius|/ArchyPie/configs/${md_id}|g" -i "${md_build}/GsKit/base/utils/FindFile.cpp"
 }
 
 function build_cgenius() {
     local params
-    if isPlatform x11; then
-        params+=("-DUSE_OPENGL=ON")
-    fi 
+    isPlatform "x11" && params+=('-DUSE_OPENGL=ON')
+
     cmake . \
         -Bbuild \
         -GNinja \
         -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$md_inst" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
         -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON \
         -DNOTYPESAVE=ON \
         -DBUILD_COSMOS=1 \
@@ -51,7 +51,7 @@ function build_cgenius() {
         -Wno-dev
     ninja -C build clean
     ninja -C build
-    md_ret_require="$md_build/build/src/CGeniusExe"
+    md_ret_require="${md_build}/build/src/CGeniusExe"
 }
 
 function install_cgenius() {
@@ -69,7 +69,7 @@ function _add_games_cgenius(){
         ['keen1/keen1.exe']="Keen 1: Marooned on Mars (Invasion of the Vorticons)"
         ['keen2/keen2.exe']="Keen 2: The Earth Explodes (Invasion of the Vorticons)"
         ['keen3/keen3.exe']="Keen 3: Keen Must Die! (Invasion of the Vorticons)"
-        ['keen3.5/keen3.5.exe']="Keen Dreams (Lost Episode)"
+        ['kdreams/kdreams.exe']="Keen Dreams (Lost Episode)"
         ['keen4/keen4.exe']="Keen 4: Secret of the Oracle (Goodbye, Galaxy!)"
         ['keen4/keen4e.exe']="Keen 4: Secret of the Oracle (Goodbye, Galaxy!)"
         ['keen5/keen5.exe']="Keen 5: The Armageddon Machine (Goodbye, Galaxy!)"
@@ -77,47 +77,47 @@ function _add_games_cgenius(){
         ['keen6/keen6.exe']="Keen 6: Aliens Ate My Baby Sitter! (Goodbye, Galaxy!)"
     )
 
-    # Create .sh files for each game found. Uppercase filenames will be converted to lowercase.
+    # Create .sh Files For Each Game Found. Uppercase Filenames Will Be Converted to Lowercase.
     for game in "${!games[@]}"; do
-        dir="$romdir/ports/$md_id"
-        pushd "$dir/${game%%/*}" || return
+        dir="${romdir}/ports/${md_id}"
+        pushd "${dir}/${game%%/*}" || return
         perl-rename 'y/A-Z/a-z/' [^.-]{*,*/*}
         popd || return
-        if [[ -f "$dir/$game" ]]; then
-            addPort "$md_id" "cgenius" "${games[$game]}" "$md_inst/$md_id.sh %ROM%" "dir=games/${game%/*}"
+        if [[ -f "${dir}/$game" ]]; then
+            addPort "${md_id}" "${md_id}" "${games[$game]}" "${md_inst}/${md_id}.sh %ROM%" "dir=games/${game%/*}"
         fi
     done
 
-    if [[ "$md_mode" == "install" ]]; then
-        # Create a launcher script to strip quotes from runcommand's generated arguments.
-        cat >"$md_inst/$md_id.sh" << _EOF_
+    if [[ "${md_mode}" == "install" ]]; then
+        # Create a Launcher Script to Strip Quotes from runcommand's Generated Arguments.
+        cat >"${md_inst}/${md_id}.sh" << _EOF_
 #!/bin/bash
 $cmd \$*
 _EOF_
-        chmod +x "$md_inst/$md_id.sh"
+        chmod +x "${md_inst}/${md_id}.sh"
     fi
 }
 
 function configure_cgenius() {
-    if [[ "$md_mode" == "install" ]]; then
-        mkRomDir "ports/$md_id"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "ports/${md_id}"
     fi
 
-    moveConfigDir "$arpiedir/ports/$md_id" "$md_conf_root/$md_id/"
-    moveConfigDir "$arpiedir/ports/$md_id/games" "$romdir/ports/$md_id/"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${md_id}/"
+    moveConfigDir "${arpdir}/${md_id}/games" "${romdir}/ports/${md_id}/"
 
-    if [[ "$md_mode" == "install" ]]; then
+    if [[ "${md_mode}" == "install" ]]; then
         local config
 
-        # Set Default Settings.
+        # Set Fullscreen By Default
         config="$(mktemp)"
-        iniConfig " = " "" "$config"
-        echo "[Video]" > "$config"
+        iniConfig " = " "" "${config}"
+        echo "[Video]" > "${config}"
         iniSet "fullscreen" "true"
 
-        copyDefaultConfig "$config" "$md_conf_root/$md_id/cgenius.cfg"
+        copyDefaultConfig "${config}" "${md_conf_root}/${md_id}/cgenius.cfg"
         rm "$config"
     fi
 
-    _add_games_cgenius "$md_inst/CGeniusExe"
+    _add_games_cgenius "${md_inst}/CGeniusExe"
 }
