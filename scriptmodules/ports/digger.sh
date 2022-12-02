807bdbd6e5d1f5b700f365cb23c9be21fb4e8bf4
V0rt3x667 <al-g0l@outlook.com>
2022-12-02 19:49:01 +0000

    Update digger.sh

--- scriptmodules/ports/digger.sh
+++ scriptmodules/ports/digger.sh
@@ -5,7 +5,7 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="digger"
-rp_module_desc="Digger Remastered"
+rp_module_desc="Digger Reloaded: Port of Digger Remastered"
 rp_module_licence="GPL https://raw.githubusercontent.com/sobomax/digger/master/README.md"
 rp_module_repo="git https://github.com/proyvind/digger.git joystick"
 rp_module_section="exp"
@@ -23,7 +23,9 @@ function depends_digger() {
 function sources_digger() {
     gitPullOrClone
 
-    applyPatch "$md_data/01_set_default_config_path.patch"
+    # Set Default Config Path(s)
+    sed -e "s|/.digger.rc|/ArchyPie/configs/${md_id}/digger.rc|g" -i "${md_build}/def.h"
+    sed -e "s|/.digger.sco|/ArchyPie/configs/${md_id}/digger.sco|g" -i "${md_build}/scores.c"
 }
 
 function build_digger() {
@@ -31,12 +33,12 @@ function build_digger() {
         -Bbuild \
         -GNinja \
         -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$md_inst" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
         -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON \
         -Wno-dev
     ninja -C build clean
     ninja -C build
-    md_ret_require="$md_build/build/digger"
+    md_ret_require="${md_build}/build/${md_id}"
 }
 
 function install_digger() {
@@ -44,7 +46,7 @@ function install_digger() {
 }
 
 function configure_digger() {
-    addPort "$md_id" "digger" "Digger Remastered" "$md_inst/digger /F"
+    addPort "${md_id}" "${md_id}" "Digger Remastered" "${md_inst}/${md_id} /F"
 
-    moveConfigDir "$arpiedir/ports/$md_id" "$md_conf_root/$md_id/"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${md_id}/"
 }
