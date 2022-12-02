35325f5647e14da7a4b2f338164f17b75b724fd5
V0rt3x667 <al-g0l@outlook.com>
2022-12-02 19:00:45 +0000

    Update dhewm3.sh

--- scriptmodules/ports/dhewm3.sh
+++ scriptmodules/ports/dhewm3.sh
@@ -5,14 +5,14 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="dhewm3"
-rp_module_desc="dhewm3 - DOOM 3 Port"
+rp_module_desc="dhewm3: DOOM3 Port"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/dhewm/dhewm3/master/COPYING.txt"
 rp_module_repo="git https://github.com/dhewm/dhewm3.git :_get_branch_dhewm3"
 rp_module_section="opt"
 rp_module_flags="!all 64bit"
 
 function _get_branch_dhewm3() {
-    download https://api.github.com/repos/dhewm/dhewm3/releases/latest - | grep -m 1 tag_name | cut -d\" -f4
+    download "https://api.github.com/repos/${md_id/3/}/${md_id}/releases/latest" - | grep -m 1 tag_name | cut -d\" -f4
 }
 
 function depends_dhewm3() {
@@ -32,7 +32,9 @@ function depends_dhewm3() {
 function sources_dhewm3() {
     gitPullOrClone
 
-    applyPatch "$md_data/01_set_default_config_path.patch"
+    # Set Default Config Path(s)
+    sed -e "s|%s/.local/share/${md_id}|/ArchyPie/configs/${md_id}|g" -i "${md_build}/neo/sys/linux/main.cpp"
+    sed -e "s|%s/.config/${md_id}|/ArchyPie/configs/${md_id}|g" -i "${md_build}/neo/sys/linux/main.cpp"
 }
 
 function build_dhewm3() {
@@ -41,16 +43,16 @@ function build_dhewm3() {
         -GNinja \
         -Bbuild \
         -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$md_inst" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
         -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON \
-        -DCMAKE_EXE_LINKER_FLAGS="${LDFLAGS} -Wl,-rpath='$md_inst/lib'" \
+        -DCMAKE_EXE_LINKER_FLAGS="${LDFLAGS} -Wl,-rpath='${md_inst}/lib'" \
         -DREPRODUCIBLE_BUILD=1 \
         -DD3XP=1 \
         -DDEDICATED=1 \
         -Wno-dev
     ninja -C build clean
     ninja -C build
-    md_ret_require="$md_build/build/dhewm3"
+    md_ret_require="${md_build}/build/${md_id}"
 }
 
 function install_dhewm3() {
@@ -59,13 +61,16 @@ function install_dhewm3() {
 
 function _game_data_dhewm3() {
     local url
-    url="https://files.holarse-linuxgaming.de/native/Spiele/Doom%203/Demo/doom3-linux-1.1.1286-demo.x86.run"
-    if [[ ! -f "$romdir/ports/doom3/base/pak000.pk4" ]] || [[ ! -f "$romdir/ports/doom3/demo/demo00.pk4" ]]; then
-        download "$url" "$romdir/ports/doom3"
-        chmod +x "$romdir/ports/doom3/doom3-linux-1.1.1286-demo.x86.run"
-        cd "$romdir/ports/doom3" || return
-        ./doom3-linux-1.1.1286-demo.x86.run --tar xf demo/ && rm "$romdir/ports/doom3/doom3-linux-1.1.1286-demo.x86.run"
-        chown "$user:$user" "$romdir/ports/doom3/demo"
+    local portname
+    portname="doom3"
+    url="https://files.holarse-linuxgaming.de/native/Spiele/Doom%203/Demo/${portname}-linux-1.1.1286-demo.x86.run"
+
+    if [[ ! -f "${romdir}/ports/${portname}/base/pak000.pk4" ]] && [[ ! -f "${romdir}/ports/${portname}/demo/demo00.pk4" ]]; then
+        download "$url" "${romdir}/ports/${portname}"
+        chmod +x "${romdir}/ports/${portname}/${portname}-linux-1.1.1286-demo.x86.run"
+        cd "${romdir}/ports/${portname}" || return
+        ./${portname}-linux-1.1.1286-demo.x86.run --tar xf demo/ && rm "${romdir}/ports/${portname}/${portname}-linux-1.1.1286-demo.x86.run"
+        chown -R "${user}:${user}" "${romdir}/ports/${portname}/demo"
     fi
 }
 
@@ -73,31 +78,41 @@ function _add_games_dhewm3() {
     local cmd="$1"
     local dir
     local game
-    declare -A games=(
-        ['base/pak000.pk4']="Doom III"
-        ['demo/demo00.pk4']="Doom III (Demo)"
-        ['d3xp/pak000.pk4']="Doom III: Resurrection of Evil"
-    )
+    local portname
 
-    # Create .sh files for each game found. Uppercase filenames will be converted to lowercase.
+    declare -A games=(
+        ['base/pak000.pk4']="Doom3"
+        ['demo/demo00.pk4']="Doom3 (Demo)"
+        ['d3xp/pak000.pk4']="Doom3: Resurrection of Evil"
+    )
+    portname="doom3"
+
+    # Create .sh Files For Each Game Found. Uppercase Filenames Will Be Converted to Lowercase.
     for game in "${!games[@]}"; do
-        dir="$romdir/ports/doom3"
-        pushd "$dir/${game%%/*}" || return
+        dir="${romdir}/ports/${portname}"
+        pushd "${dir}/${game%%/*}" || return
         perl-rename 'y/A-Z/a-z/' [^.-]{*,*/*}
         popd || return
-        if [[ -f "$dir/$game" ]]; then
-            addPort "$md_id" "doom3" "${games[$game]}" "$cmd" "${game%%/*}"
+        if [[ -f "${dir}/${game}" ]]; then
+            addPort "${md_id}" "${portname}" "${games[$game]}" "${cmd}" "${game%%/*}"
         fi
     done
 }
 
 function configure_dhewm3() {
-    if [[ "$md_mode" == "install" ]]; then
-        mkRomDir "ports/doom3" && _game_data_dhewm3
+    local portname
+    portname="doom3"
+
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "ports/${portname}" 
+        mkRomDir "ports/${portname}/base"
+        mkRomDir "ports/${portname}/d3xp"
+        
+        _game_data_dhewm3
     fi
 
-    moveConfigDir "$arpiedir/ports/$md_id" "$md_conf_root/doom3/$md_id/"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${portname}/${md_id}/"
 
-    local basedir="$romdir/ports/doom3"
-    _add_games_dhewm3 "$md_inst/bin/dhewm3 +set fs_basepath $basedir +set r_fullscreen 1 +set fs_game %ROM%"
+    local basedir="${romdir}/ports/${portname}"
+    _add_games_dhewm3 "${md_inst}/bin/${md_id} +set fs_basepath ${basedir} +set r_fullscreen 1 +set fs_game %ROM%"
 }
