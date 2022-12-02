b0c97b6ebba2abf62f6012a4a150936a84006a60
V0rt3x667 <al-g0l@outlook.com>
2022-12-02 03:37:20 +0000

    Update cannonball.sh

--- scriptmodules/ports/cannonball.sh
+++ scriptmodules/ports/cannonball.sh
@@ -5,8 +5,8 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="cannonball"
-rp_module_desc="Cannonball - An Enhanced OutRun Engine"
-rp_module_help="You need to unzip your OutRun set B from latest MAME (outrun.zip) to $romdir/ports/cannonball. They should match the file names listed in the roms.txt file found in the roms folder. You will also need to rename the epr-10381a.132 file to epr-10381b.132 before it will work."
+rp_module_desc="Cannonball: An Enhanced OutRun Engine"
+rp_module_help="Unzip the OutRun Revision B ROM Set from MAME (outrun.zip) to ${romdir}/ports/cannonball and rename ROM epr-10381a.132 to epr-10381b.132"
 rp_module_licence="NONCOM https://raw.githubusercontent.com/djyt/cannonball/master/docs/license.txt"
 rp_module_repo="git https://github.com/djyt/cannonball.git master"
 rp_module_section="opt"
@@ -18,16 +18,19 @@ function depends_cannonball() {
         'ninja'
         'sdl2'
     )
-    isPlatform "rpi" && depends+=('raspberrypi-firmware')
-    isPlatform "mesa" && depends+=('mesa')
-
     getDepends "${depends[@]}"
 }
 
 function sources_cannonball() {
     gitPullOrClone
 
-    applyPatch "$md_data/01_set_config_&_rom_paths.patch"
+    # Set Default Config Path(s)
+    sed -e "s|<rompath>roms/</rompath>|<rompath>${romdir}/ports/${md_id}/</rompath>|g" -i "${md_build}/res/config.xml"
+    sed -e "s|<respath>./res/</respath>|<respath>${md_inst}/res/</respath>|g" -i "${md_build}/res/config.xml"
+    sed -e "s|<savepath>./</savepath>|<savepath>${romdir}/ports/${md_id}/hiscores/${md_id}/</savepath>|g" -i "${md_build}/res/config.xml"
+    
+    # Set Fullscreen By Default
+    sed -e "s|<mode>0</mode>|<mode>1</mode>|g" -i "${md_build}/res/config.xml"
 }
 
 function build_cannonball() {
@@ -43,13 +46,13 @@ function build_cannonball() {
         -Bbuild \
         -GNinja \
         -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$md_inst" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
         -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON \
-        -DTARGET="$target" \
+        -DTARGET="${target}" \
         -Wno-dev
     ninja -C build clean
     ninja -C build
-    md_ret_require="$md_build/build/cannonball"
+    md_ret_require="${md_build}/build/${md_id}"
 }
 
 function install_cannonball() {
@@ -62,17 +65,16 @@ function install_cannonball() {
 }
 
 function configure_cannonball() {
-    if [[ "$md_mode" == "install" ]]; then
-        mkRomDir "ports/$md_id"
-        mkRomDir "ports/$md_id/hiscores/$md_id"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "ports/${md_id}"
+        mkRomDir "ports/${md_id}/hiscores"
+        mkRomDir "ports/${md_id}/hiscores/${md_id}"
     fi
 
-    addPort "$md_id" "cannonball" "Cannonball: OutRun Engine" "pushd $md_inst; $md_inst/cannonball; popd"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${md_id}/"
+        
+    copyDefaultConfig "${md_inst}/config.xml" "${md_conf_root}/${md_id}/config.xml"
+    copyDefaultConfig "${md_inst}/roms.txt" "${romdir}/ports/${md_id}/roms.txt"
 
-    moveConfigDir "$arpiedir/ports/$md_id" "$md_conf_root/$md_id/"
-
-    if [[ "$md_mode" == "install" ]]; then
-        copyDefaultConfig "$md_inst/config.xml" "$md_conf_root/$md_id/config.xml"
-        copyDefaultConfig "$md_inst/roms.txt" "$romdir/ports/$md_id/roms.txt"
-    fi
+    addPort "${md_id}" "${md_id}" "Cannonball: OutRun Engine" "${md_inst}/${md_id} -cfgfile ${arpdir}/${md_id}/config.xml"
 }
