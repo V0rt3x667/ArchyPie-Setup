b7ad12b92aa725c724d02bf9c70c3e49288aaa36
V0rt3x667 <al-g0l@outlook.com>
2022-12-04 06:41:53 +0000

    Update gemrb.sh

--- scriptmodules/ports/gemrb.sh
+++ scriptmodules/ports/gemrb.sh
@@ -5,14 +5,14 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="gemrb"
-rp_module_desc="GemRB - Port of Bioware's Infinity Engine"
+rp_module_desc="GemRB: Port of Bioware's Infinity Engine"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/gemrb/gemrb/master/COPYING"
 rp_module_repo="git https://github.com/gemrb/gemrb.git :_get_branch_gemrb"
 rp_module_section="exp"
-rp_module_flags="!mali dispmanx"
+rp_module_flags="!mali"
 
 function _get_branch_gemrb() {
-    download https://api.github.com/repos/gemrb/gemrb/releases/latest - | grep -m 1 tag_name | cut -d\" -f4
+    download "https://api.github.com/repos/gemrb/gemrb/releases/latest" - | grep -m 1 tag_name | cut -d\" -f4
 }
 
 function depends_gemrb() {
@@ -36,13 +36,13 @@ function sources_gemrb() {
 
 function build_gemrb() {
     local params
-    isPlatform "gl" && params+=(-DOPENGL_BACKEND=OpenGL)
+    ! isPlatform "wayland" && isPlatform "gl" && params+=('-DOPENGL_BACKEND=OpenGL')
     
     cmake . \
         -GNinja \
         -Bbuild \
         -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$md_inst" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
         -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON \
         -DFREETYPE_INCLUDE_DIRS=/usr/include/freetype2/ \
         -DSDL_BACKEND=SDL2 \
@@ -51,7 +51,7 @@ function build_gemrb() {
         -Wno-dev
     ninja -C build clean
     ninja -C build
-    md_ret_require="$md_build/build/gemrb/gemrb"
+    md_ret_require="${md_build}/build/${md_id}/${md_id}"
 }
 
 function install_gemrb() {
@@ -59,20 +59,24 @@ function install_gemrb() {
 }
 
 function configure_gemrb() {
-    mkRomDir "ports/baldurs1"
-    mkRomDir "ports/baldurs2"
-    mkRomDir "ports/icewind1"
-    mkRomDir "ports/icewind2"
-    mkRomDir "ports/planescape"
+    if [[ "${md_mode}" == "install" ]]; then
+        local dirs=(
+            'baldursgate1'
+            'baldursgate2'
+            'icewind1'
+            'icewind2'
+            'planescape'
+        )
+        for dir in "${dirs[@]}"; do
+            mkRomDir "ports/${md_id}/${dir}"
+            mkUserDir "${arpdir}/${md_id}/${dir}"
+        done
+    fi
 
-    addPort "$md_id" "baldursgate1" "Baldur's Gate" "$md_inst/bin/gemrb -C $md_conf_root/baldursgate1/GemRB.cfg"
-    addPort "$md_id" "baldursgate2" "Baldur's Gate II: Shadows of Amn" "$md_inst/bin/gemrb -C $md_conf_root/baldursgate2/GemRB.cfg"
-    addPort "$md_id" "icewind1" "Icewind Dale" "$md_inst/bin/gemrb -C $md_conf_root/icewind1/GemRB.cfg"
-    addPort "$md_id" "icewind2" "Icewind Dale II" "$md_inst/bin/gemrb -C $md_conf_root/icewind2/GemRB.cfg"
-    addPort "$md_id" "planescape" "Planescape Torment" "$md_inst/bin/gemrb -C $md_conf_root/planescape/GemRB.cfg"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${md_id}/"
 
-    # Create Baldurs Gate 1 configuration
-    cat > "$md_conf_root/baldursgate1/GemRB.cfg" << _EOF_
+    # Create Baldurs Gate 1 Configuration
+    cat > "${md_conf_root}/${md_id}/baldursgate1/GemRB.cfg" << _EOF_
 GameType=bg1
 GameName=Baldur's Gate
 Width=640
@@ -84,13 +88,13 @@ AudioDriver = openal
 GUIEnhancements = 15
 DrawFPS=0
 CaseSensitive=1
-GamePath=$romdir/ports/baldurs1/
-CD1=$romdir/ports/baldurs1/
-CachePath=$romdir/ports/baldurs1/cache/
+GamePath=${romdir}/ports/${md_id}/baldursgate1/
+CD1=${romdir}/ports/${md_id}/baldursgate1/
+CachePath=${romdir}/ports/${md_id}/baldursgate1/cache/
 _EOF_
 
-    # Create Baldurs Gate 2 configuration
-    cat > "$md_conf_root/baldursgate2/GemRB.cfg" << _EOF_
+    # Create Baldurs Gate 2 Configuration
+    cat > "${md_conf_root}/${md_id}/baldursgate2/GemRB.cfg" << _EOF_
 GameType=bg2
 GameName=Baldur's Gate II: Shadows of Amn
 Width=640
@@ -102,18 +106,18 @@ AudioDriver = openal
 GUIEnhancements = 15
 DrawFPS=0
 CaseSensitive=1
-GamePath=$romdir/ports/baldurs2/
-CD1=$romdir/ports/baldurs2/data/
-CD2=$romdir/ports/baldurs2/data/
-CD3=$romdir/ports/baldurs2/data/
-CD4=$romdir/ports/baldurs2/data/
-CD5=$romdir/ports/baldurs2/data/
-CD6=$romdir/ports/baldurs2/data/
-CachePath=$romdir/ports/baldurs2/cache
+GamePath=${romdir}/ports/${md_id}/baldursgate2/
+CD1=${romdir}/ports/${md_id}/baldursgate2/data/
+CD2=${romdir}/ports/${md_id}/baldursgate2/data/
+CD3=${romdir}/ports/${md_id}/baldursgate2/data/
+CD4=${romdir}/ports/${md_id}/baldursgate2/data/
+CD5=${romdir}/ports/${md_id}/baldursgate2/data/
+CD6=${romdir}/ports/${md_id}/baldursgate2/data/
+CachePath=${romdir}/ports/${md_id}/baldursgate2/cache
 _EOF_
 
-    # Create Icewind 1 configuration
-    cat > "$md_conf_root/icewind1/GemRB.cfg" << _EOF_
+    # Create Icewind 1 Configuration
+    cat > "${md_conf_root}/${md_id}/icewind1/GemRB.cfg" << _EOF_
 GameType=auto
 GameName=Icewind Dale
 Width=640
@@ -125,15 +129,15 @@ AudioDriver = openal
 GUIEnhancements = 15
 DrawFPS=0
 CaseSensitive=1
-GamePath=$romdir/ports/icewind1/
-CD1=$romdir/ports/icewind1/Data/
-CD2=$romdir/ports/icewind1/CD2/Data/
-CD3=$romdir/ports/icewind1/CD3/Data/
-CachePath=$romdir/ports/icewind1/cache/
+GamePath=${romdir}/ports/${md_id}/icewind1/
+CD1=${romdir}/ports/${md_id}/icewind1/Data/
+CD2=${romdir}/ports/${md_id}/icewind1/CD2/Data/
+CD3=${romdir}/ports/${md_id}/icewind1/CD3/Data/
+CachePath=${romdir}/ports/${md_id}/icewind1/cache/
 _EOF_
 
-    # Create Icewind2 configuration
-    cat > "$md_conf_root/icewind2/GemRB.cfg" << _EOF_
+    # Create Icewind2 Configuration
+    cat > "${md_conf_root}/${md_id}/icewind2/GemRB.cfg" << _EOF_
 GameType=iwd2
 GameName=Icewind Dale II
 Width=800
@@ -145,13 +149,13 @@ AudioDriver = openal
 GUIEnhancements = 15
 DrawFPS=0
 CaseSensitive=1
-GamePath=$romdir/ports/icewind2/
-CD1=$romdir/ports/icewind2/data/
-CachePath=$romdir/ports/icewind2/cache/
+GamePath=${romdir}/ports/${md_id}/icewind2/
+CD1=${romdir}/ports/${md_id}/icewind2/data/
+CachePath=${romdir}/ports/${md_id}/icewind2/cache/
 _EOF_
 
-    #create Planescape configuration
-    cat > "$md_conf_root/planescape/GemRB.cfg" << _EOF_
+    # Create Planescape Configuration
+    cat > "${md_conf_root}/${md_id}/planescape/GemRB.cfg" << _EOF_
 GameType=pst
 GameName=Planescape Torment
 Width=640
@@ -163,19 +167,16 @@ AudioDriver = openal
 GUIEnhancements = 15
 DrawFPS=0
 CaseSensitive=1
-GamePath=$romdir/ports/planescape/
-CD1=$romdir/ports/planescape/data/
-CachePath=$romdir/ports/planescape/cache/
+GamePath=${romdir}/ports/${md_id}/planescape/
+CD1=${romdir}/ports/${md_id}/planescape/data/
+CachePath=${romdir}/${md_id}/ports/planescape/cache/
 _EOF_
 
-    local dirs=(
-        'baldursgate1'
-        'baldursgate2'
-        'icewind1'
-        'icewind2'
-        'planescape'
-    )
-    for dir in "${dirs[@]}"; do
-        chown "$user:$user" "$md_conf_root/$dir/GemRB.cfg"
-    done
+    chown -R "${user}:${user}" "${md_conf_root}/${md_id}"
+
+    addPort "${md_id}" "baldursgate1" "Baldur's Gate" "${md_inst}/bin/${md_id} -C ${md_conf_root}/${md_id}/baldursgate1/GemRB.cfg"
+    addPort "${md_id}" "baldursgate2" "Baldur's Gate II: Shadows of Amn" "${md_inst}/bin/${md_id} -C ${md_conf_root}/${md_id}/baldursgate2/GemRB.cfg"
+    addPort "${md_id}" "icewind1" "Icewind Dale" "${md_inst}/bin/${md_id} -C ${md_conf_root}/${md_id}/icewind1/GemRB.cfg"
+    addPort "${md_id}" "icewind2" "Icewind Dale II" "${md_inst}/bin/${md_id} -C ${md_conf_root}/${md_id}/icewind2/GemRB.cfg"
+    addPort "${md_id}" "planescape" "Planescape Torment" "${md_inst}/bin/${md_id} -C ${md_conf_root}/${md_id}/planescape/GemRB.cfg"
 }
