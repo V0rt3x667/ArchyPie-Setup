084f59d3643504605fc29e3eb3287b694d5d73d3
V0rt3x667 <al-g0l@outlook.com>
2022-12-08 01:58:33 +0000

    Update archypie_packages.sh

--- archypie_packages.sh
+++ archypie_packages.sh
@@ -4,59 +4,57 @@
 #
 # Please see the LICENSE file at the top-level directory of this distribution.
 
-__version="4.8.2"
+__version="4.9.0"
 
-[[ "$__debug" -eq 1 ]] && set -x
+[[ "${__debug}" -eq 1 ]] && set -x
 
-# main archypie install location
+# Main ArchyPie Install Location
 rootdir="/opt/archypie"
 
-# if __user is set, try and install for that user, else use SUDO_USER
-if [[ -n "$__user" ]]; then
-    user="$__user"
-    if ! id -u "$__user" &>/dev/null; then
-        echo "User $__user not exist"
+# If "__user" is set, try and install for that user, else use "SUDO_USER"
+if [[ -n "${__user}" ]]; then
+    user="${__user}"
+    if ! id -u "${__user}" &>/dev/null; then
+        echo "User ${__user} Does Not Exist!"
         exit 1
     fi
 else
-    user="$SUDO_USER"
-    [[ -z "$user" ]] && user="$(id -un)"
+    user="${SUDO_USER}"
+    [[ -z "${user}" ]] && user="$(id -un)"
 fi
 
-home="$(eval echo ~"$user")"
-datadir="$home/ArchyPie"
-biosdir="$datadir/BIOS"
-romdir="$datadir/roms"
-arpdir="$datadir/configs"
-emudir="$rootdir/emulators"
-configdir="$rootdir/configs"
+home="$(eval echo ~"${user}")"
+datadir="${home}/ArchyPie"
+biosdir="${datadir}/BIOS"
+romdir="${datadir}/roms"
+arpdir="${datadir}/configs"
+emudir="${rootdir}/emulators"
+configdir="${rootdir}/configs"
 
 scriptdir="$(dirname "$0")"
-scriptdir="$(cd "$scriptdir" && pwd)"
+scriptdir="$(cd "${scriptdir}" && pwd)"
 
-__logdir="$scriptdir/logs"
-__tmpdir="$scriptdir/tmp"
-__builddir="$__tmpdir/build"
-__swapdir="$__tmpdir"
+__logdir="${scriptdir}/logs"
+__tmpdir="${scriptdir}/tmp"
+__builddir="${__tmpdir}/build"
+__swapdir="${__tmpdir}"
 
-# check, if sudo is used
+# Check if "sudo" is Used
 if [[ "$(id -u)" -ne 0 ]]; then
     echo "Script must be run under sudo from the user you want to install for. Try 'sudo $0'"
     exit 1
 fi
 
-__backtitle="ArchyPie Setup. Installation Folder: $rootdir User: $user"
+__backtitle="ArchyPie Setup - Installation Folder: ${rootdir} User: ${user}"
 
-source "$scriptdir/scriptmodules/system.sh"
-source "$scriptdir/scriptmodules/helpers.sh"
-source "$scriptdir/scriptmodules/inifuncs.sh"
-source "$scriptdir/scriptmodules/packages.sh"
+source "${scriptdir}/scriptmodules/system.sh"
+source "${scriptdir}/scriptmodules/helpers.sh"
+source "${scriptdir}/scriptmodules/inifuncs.sh"
+source "${scriptdir}/scriptmodules/packages.sh"
 
 setup_env
 
 rp_registerAllModules
-
-ensureFBMode 320 240
 
 rp_ret=0
 if [[ $# -gt 0 ]]; then
@@ -68,15 +66,15 @@ else
 fi
 
 if [[ "${#__ERRMSGS[@]}" -gt 0 ]]; then
-    # override return code if ERRMSGS is set - eg in the case of calling basic_install from setup
-    # we won't get the return code, as we don't handle return codes when calling non packaging functions
-    # as it would require all modules functions to handle errors differently, and make things more complicated
+    # Override return code if ERRMSGS is set, for example in the case of calling basic_install from the setup menu.
+    # We won't get the return code, as we don't handle return codes when calling non packaging functions
+    # as it would require all modules functions to handle errors differently, and make things more complicated.
     [[ "$rp_ret" -eq 0 ]] && rp_ret=1
-    printMsgs "console" "Errors:\n${__ERRMSGS[@]}"
+    printMsgs "console" "Errors:\n" "${__ERRMSGS[@]}"
 fi
 
 if [[ "${#__INFMSGS[@]}" -gt 0 ]]; then
-    printMsgs "console" "Info:\n${__INFMSGS[@]}"
+    printMsgs "console" "Info:\n" "${__INFMSGS[@]}"
 fi
 
 exit $rp_ret
