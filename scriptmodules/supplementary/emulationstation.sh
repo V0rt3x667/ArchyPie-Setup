aa9c5bcbc4e2f55a362b24f21e340c227b20ad3f
V0rt3x667 <al-g0l@outlook.com>
2022-12-08 02:36:15 +0000

    Update emulationstation.sh

--- scriptmodules/supplementary/emulationstation.sh
+++ scriptmodules/supplementary/emulationstation.sh
@@ -12,12 +12,12 @@ rp_module_section="core"
 rp_module_flags="frontend"
 
 function _get_input_cfg_emulationstation() {
-    echo "$configdir/all/emulationstation/es_input.cfg"
+    echo "${configdir}/all/emulationstation/es_input.cfg"
 }
 
 function _update_hook_emulationstation() {
     # Make sure the input configuration scripts and launch script are always up to date
-    if rp_isInstalled "$md_id"; then
+    if rp_isInstalled "${md_id}"; then
         copy_inputscripts_emulationstation
         install_launch_emulationstation
     fi
@@ -28,7 +28,7 @@ function _sort_systems_emulationstation() {
     cp "/etc/emulationstation/es_systems.cfg" "/etc/emulationstation/es_systems.cfg.bak"
     xmlstarlet sel -D -I \
         -t -m "/" -e "systemList" \
-        -m "//system" -s A:T:U "$field" -c "." \
+        -m "//system" -s A:T:U "${field}" -c "." \
         "/etc/emulationstation/es_systems.cfg.bak" >"/etc/emulationstation/es_systems.cfg"
 }
 
@@ -43,36 +43,36 @@ function _add_system_emulationstation() {
 
     local conf="/etc/emulationstation/es_systems.cfg"
     mkdir -p "/etc/emulationstation"
-    if [[ ! -f "$conf" ]]; then
-        echo "<systemList />" >"$conf"
+    if [[ ! -f "${conf}" ]]; then
+        echo "<systemList />" >"${conf}"
     fi
 
-    cp "$conf" "$conf.bak"
-    if [[ $(xmlstarlet sel -t -v "count(/systemList/system[name='$name'])" "$conf") -eq 0 ]]; then
+    cp "${conf}" "${conf}.bak"
+    if [[ $(xmlstarlet sel -t -v "count(/systemList/system[name='${name}'])" "${conf}") -eq 0 ]]; then
         xmlstarlet ed -L -s "/systemList" -t elem -n "system" -v "" \
-            -s "/systemList/system[last()]" -t elem -n "name" -v "$name" \
-            -s "/systemList/system[last()]" -t elem -n "fullname" -v "$fullname" \
-            -s "/systemList/system[last()]" -t elem -n "path" -v "$path" \
-            -s "/systemList/system[last()]" -t elem -n "extension" -v "$extension" \
-            -s "/systemList/system[last()]" -t elem -n "command" -v "$command" \
-            -s "/systemList/system[last()]" -t elem -n "platform" -v "$platform" \
-            -s "/systemList/system[last()]" -t elem -n "theme" -v "$theme" \
-            "$conf"
+            -s "/systemList/system[last()]" -t elem -n "name" -v "${name}" \
+            -s "/systemList/system[last()]" -t elem -n "fullname" -v "${fullname}" \
+            -s "/systemList/system[last()]" -t elem -n "path" -v "${path}" \
+            -s "/systemList/system[last()]" -t elem -n "extension" -v "${extension}" \
+            -s "/systemList/system[last()]" -t elem -n "command" -v "${command}" \
+            -s "/systemList/system[last()]" -t elem -n "platform" -v "${platform}" \
+            -s "/systemList/system[last()]" -t elem -n "theme" -v "${theme}" \
+            "${conf}"
     else
         xmlstarlet ed -L \
-            -u "/systemList/system[name='$name']/fullname" -v "$fullname" \
-            -u "/systemList/system[name='$name']/path" -v "$path" \
-            -u "/systemList/system[name='$name']/extension" -v "$extension" \
-            -u "/systemList/system[name='$name']/command" -v "$command" \
-            -u "/systemList/system[name='$name']/platform" -v "$platform" \
-            -u "/systemList/system[name='$name']/theme" -v "$theme" \
-            "$conf"
+            -u "/systemList/system[name='${name}']/fullname" -v "${fullname}" \
+            -u "/systemList/system[name='${name}']/path" -v "${path}" \
+            -u "/systemList/system[name='${name}']/extension" -v "${extension}" \
+            -u "/systemList/system[name='${name}']/command" -v "${command}" \
+            -u "/systemList/system[name='${name}']/platform" -v "${platform}" \
+            -u "/systemList/system[name='${name}']/theme" -v "${theme}" \
+            "${conf}"
     fi
 
     # Alert the user if they have a custom es_systems.cfg which doesn't contain the system we are adding
-    local conf_local="$configdir/all/emulationstation/es_systems.cfg"
-    if [[ -f "$conf_local" ]] && [[ "$(xmlstarlet sel -t -v "count(/systemList/system[name='$name'])" "$conf_local")" -eq 0 ]]; then
-        md_ret_info+=("You have a custom override of the EmulationStation system config in:\n\n$conf_local\n\nYou will need to copy the updated $system config from $conf to your custom config for $system to show up in EmulationStation.")
+    local conf_local="${configdir}/all/emulationstation/es_systems.cfg"
+    if [[ -f "$conf_local" ]] && [[ "$(xmlstarlet sel -t -v "count(/systemList/system[name='${name}'])" "$conf_local")" -eq 0 ]]; then
+        md_ret_info+=("You have a custom override of the EmulationStation system config in:\n\n$conf_local\n\nYou will need to copy the updated $system config from ${conf} to your custom config for $system to show up in EmulationStation.")
     fi
 
     _sort_systems_emulationstation "name"
@@ -82,7 +82,7 @@ function _del_system_emulationstation() {
     local fullname="$1"
     local name="$2"
     if [[ -f /etc/emulationstation/es_systems.cfg ]]; then
-        xmlstarlet ed -L -P -d "/systemList/system[name='$name']" /etc/emulationstation/es_systems.cfg
+        xmlstarlet ed -L -P -d "/systemList/system[name='${name}']" /etc/emulationstation/es_systems.cfg
     fi
 }
 
@@ -94,33 +94,33 @@ function _add_rom_emulationstation() {
     local desc="$5"
     local image="$6"
 
-    local config_dir="$configdir/all/emulationstation"
+    local config_dir="${configdir}/all/emulationstation"
 
-    mkUserDir "$config_dir"
-    mkUserDir "$config_dir/gamelists"
-    mkUserDir "$config_dir/gamelists/$system_name"
-    local config="$config_dir/gamelists/$system_name/gamelist.xml"
+    mkUserDir "${config_dir}"
+    mkUserDir "${config_dir}/gamelists"
+    mkUserDir "${config_dir}/gamelists/${system_name}"
+    local config="${config_dir}/gamelists/${system_name}/gamelist.xml"
 
-    if [[ ! -f "$config" ]]; then
-        echo "<gameList />" >"$config"
+    if [[ ! -f "${config}" ]]; then
+        echo "<gameList />" >"${config}"
     fi
 
-    if [[ $(xmlstarlet sel -t -v "count(/gameList/game[path='$path'])" "$config") -eq 0 ]]; then
+    if [[ $(xmlstarlet sel -t -v "count(/gameList/game[path='${path}'])" "${config}") -eq 0 ]]; then
         xmlstarlet ed -L -s "/gameList" -t elem -n "game" -v "" \
-            -s "/gameList/game[last()]" -t elem -n "path" -v "$path" \
-            -s "/gameList/game[last()]" -t elem -n "name" -v "$name" \
-            -s "/gameList/game[last()]" -t elem -n "desc" -v "$desc" \
-            -s "/gameList/game[last()]" -t elem -n "image" -v "$image" \
-            "$config"
+            -s "/gameList/game[last()]" -t elem -n "path" -v "${path}" \
+            -s "/gameList/game[last()]" -t elem -n "name" -v "${name}" \
+            -s "/gameList/game[last()]" -t elem -n "desc" -v "${desc}" \
+            -s "/gameList/game[last()]" -t elem -n "image" -v "${image}" \
+            "${config}"
     else
         xmlstarlet ed -L \
-            -u "/gameList/game[name='$name']/path" -v "$path" \
-            -u "/gameList/game[name='$name']/name" -v "$name" \
-            -u "/gameList/game[name='$name']/desc" -v "$desc" \
-            -u "/gameList/game[name='$name']/image" -v "$image" \
-            "$config"
+            -u "/gameList/game[name='${name}']/path" -v "${path}" \
+            -u "/gameList/game[name='${name}']/name" -v "${name}" \
+            -u "/gameList/game[name='${name}']/desc" -v "${desc}" \
+            -u "/gameList/game[name='${name}']/image" -v "${image}" \
+            "${config}"
     fi
-    chown "$user:$user" "$config"
+    chown "${user}:${user}" "${config}"
 }
 
 function depends_emulationstation() {
@@ -158,7 +158,7 @@ function build_emulationstation() {
     fi
     
     if isPlatform "x11"; then
-        gl_ver=$(sudo -u "$user" glxinfo | grep -oP "OpenGL version string: \K(\d+)")
+        gl_ver=$(sudo -u "${user}" glxinfo | grep -oP "OpenGL version string: \K(\d+)")
         [[ "$gl_ver" -gt 1 ]] && params+=('-DUSE_GL21=On')
     fi
     
@@ -166,14 +166,14 @@ function build_emulationstation() {
     cmake . \
         -GNinja \
         -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$md_inst" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
         -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON \
         "${params[@]}" \
         -Wno-dev
     ninja clean
     ninja
     rpSwap off
-    md_ret_require="$md_build/emulationstation"
+    md_ret_require="${md_build}/emulationstation"
 }
 
 function install_emulationstation() {
@@ -193,31 +193,31 @@ function init_input_emulationstation() {
     es_config="$(_get_input_cfg_emulationstation)"
 
     # If there is no ES config (or empty file) create it with initial inputList element
-    if [[ ! -s "$es_config" ]]; then
-        echo "<inputList />" >"$es_config"
+    if [[ ! -s "${es_config}" ]]; then
+        echo "<inputList />" >"${es_config}"
     fi
 
     # Add or update our inputconfiguration.sh inputAction
-    if [[ $(xmlstarlet sel -t -v "count(/inputList/inputAction[@type='onfinish'])" "$es_config") -eq 0 ]]; then
+    if [[ $(xmlstarlet sel -t -v "count(/inputList/inputAction[@type='onfinish'])" "${es_config}") -eq 0 ]]; then
         xmlstarlet ed -L -S \
             -s "/inputList" -t elem -n "inputActionTMP" -v "" \
             -s "//inputActionTMP" -t attr -n "type" -v "onfinish" \
-            -s "//inputActionTMP" -t elem -n "command" -v "$md_inst/scripts/inputconfiguration.sh" \
-            -r "//inputActionTMP" -v "inputAction" "$es_config"
+            -s "//inputActionTMP" -t elem -n "command" -v "${md_inst}/scripts/inputconfiguration.sh" \
+            -r "//inputActionTMP" -v "inputAction" "${es_config}"
     else
         xmlstarlet ed -L \
-            -u "/inputList/inputAction[@type='onfinish']/command" -v "$md_inst/scripts/inputconfiguration.sh" \
-            "$es_config"
+            -u "/inputList/inputAction[@type='onfinish']/command" -v "${md_inst}/scripts/inputconfiguration.sh" \
+            "${es_config}"
     fi
 
-    chown "$user:$user" "$es_config"
+    chown "${user}:${user}" "${es_config}"
 }
 
 function copy_inputscripts_emulationstation() {
-    mkdir -p "$md_inst/scripts"
+    mkdir -p "${md_inst}/scripts"
 
-    cp -r "$scriptdir/scriptmodules/$md_type/emulationstation/"* "$md_inst/scripts/"
-    chmod +x "$md_inst/scripts/inputconfiguration.sh"
+    cp -r "${scriptdir}/scriptmodules/${md_type}/emulationstation/"* "${md_inst}/scripts/"
+    chmod +x "${md_inst}/scripts/inputconfiguration.sh"
 }
 
 function install_launch_emulationstation() {
@@ -242,7 +242,7 @@ export TTY="\${TTY:8:1}"
 
 clear
 tput civis
-"$md_inst/emulationstation.sh" "\$@"
+"${md_inst}/emulationstation.sh" "\$@"
 if [[ \$? -eq 139 ]]; then
     dialog --cr-wrap --no-collapse --msgbox "EmulationStation crashed!\n\nIf this is your first boot of ArchyPie, make sure you are using the correct image for your system.\n\\nCheck your rom file/folder permissions and if running on a Raspberry Pi, make sure your gpu_split is set high enough and/or switch back to using carbon theme.\n\nFor more help please use the ArchyPie forum." 20 60 >/dev/tty
 fi
@@ -252,11 +252,11 @@ _EOF_
 
     if isPlatform "x11" || isPlatform "wayland"; then
         mkdir -p /usr/share/{icons,applications}
-        cp "$scriptdir/scriptmodules/$md_type/emulationstation/retropie.svg" "/usr/share/icons/"
+        cp "${scriptdir}/scriptmodules/${md_type}/emulationstation/retropie.svg" "/usr/share/icons/"
         cat > /usr/share/applications/archypie.desktop << _EOF_
 [Desktop Entry]
 Type=Application
-Exec=$TERM_PROGRAM emulationstation
+Exec=${TERM_PROGRAM} emulationstation
 Hidden=false
 NoDisplay=false
 X-GNOME-Autostart-enabled=true
@@ -283,16 +283,16 @@ function remove_emulationstation() {
 }
 
 function configure_emulationstation() {
-    moveConfigDir "$home/.emulationstation" "$configdir/all/emulationstation"
+    moveConfigDir "${home}/.emulationstation" "${configdir}/all/emulationstation"
 
-    [[ "$md_mode" == "remove" ]] && return
+    [[ "${md_mode}" == "remove" ]] && return
 
     # Remove other EmulationStation if it's installed, so we don't end up with
     # both packages interfering, but leave configs alone so switching is easy
-    if [[ "$md_id" == "emulationstation-dev" ]]; then
-        rmDirExists "$rootdir/$md_type/emulationstation"
+    if [[ "${md_id}" == "emulationstation-dev" ]]; then
+        rmDirExists "${rootdir}/${md_type}/emulationstation"
     else
-        rmDirExists "$rootdir/$md_type/emulationstation-dev"
+        rmDirExists "${rootdir}/${md_type}/emulationstation-dev"
     fi
 
     init_input_emulationstation
@@ -303,7 +303,6 @@ function configure_emulationstation() {
 
     mkdir -p "/etc/emulationstation"
 
-    # Ensure we have a default theme
     rp_callModule esthemes install_theme
 
     addAutoConf "es_swap_a_b" 0
@@ -321,28 +320,28 @@ function gui_emulationstation() {
     local options
     while true; do
         local options=(
-            1 "Clear/Reset Emulation Station input configuration"
+            1 "Clear/Reset Emulation Station input Configuration"
         )
 
-        if [[ "$disable" -eq 0 ]]; then
+        if [[ "${disable}" -eq 0 ]]; then
             options+=(2 "Auto Configuration (Currently: Enabled)")
         else
             options+=(2 "Auto Configuration (Currently: Disabled)")
         fi
 
-        if [[ "$es_swap" -eq 0 ]]; then
+        if [[ "${es_swap}" -eq 0 ]]; then
             options+=(3 "Swap A/B Buttons in ES (Currently: Default)")
         else
             options+=(3 "Swap A/B Buttons in ES (Currently: Swapped)")
         fi
 
-        local cmd=(dialog --backtitle "$__backtitle" --default-item "$default" --menu "Choose an option" 22 76 16)
+        local cmd=(dialog --backtitle "${__backtitle}" --default-item "${default}" --menu "Choose An Option" 22 76 16)
         local choice 
         choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-        [[ -z "$choice" ]] && break
-        default="$choice"
+        [[ -z "${choice}" ]] && break
+        default="${choice}"
 
-        case "$choice" in
+        case "${choice}" in
             1)
                 if dialog --defaultno --yesno "Are you sure you want to reset the EmulationStation controller configuration? This will wipe all controller configs for ES and it will prompt to reconfigure on next start." 22 76 2>&1 >/dev/tty; then
                     clear_input_emulationstation
@@ -351,15 +350,15 @@ function gui_emulationstation() {
                 ;;
             2)
                 disable="$((disable ^ 1))"
-                setAutoConf "disable" "$disable"
+                setAutoConf "disable" "${disable}"
                 ;;
             3)
                 es_swap="$((es_swap ^ 1))"
-                setAutoConf "es_swap_a_b" "$es_swap"
+                setAutoConf "es_swap_a_b" "${es_swap}"
                 local ra_swap="false"
                 getAutoConf "es_swap_a_b" && ra_swap="true"
-                iniSet "menu_swap_ok_cancel_buttons" "$ra_swap" "$configdir/all/retroarch.cfg"
-                printMsgs "dialog" "You will need to reconfigure you controller in Emulation Station for the changes to take effect."
+                iniSet "menu_swap_ok_cancel_buttons" "${ra_swap}" "${configdir}/all/retroarch.cfg"
+                printMsgs "dialog" "You will need to reconfigure you controller in EmulationStation for the changes to take effect."
                 ;;
         esac
     done
