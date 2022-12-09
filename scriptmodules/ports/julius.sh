fda5843af5557e9b07344cce07073d34428a474b
V0rt3x667 <al-g0l@outlook.com>
2022-12-09 00:45:24 +0000

    Update julius.sh

--- scriptmodules/ports/julius.sh
+++ scriptmodules/ports/julius.sh
@@ -5,20 +5,20 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="julius"
-rp_module_desc="Julius - Caesar III Port"
-rp_module_licence="AGPL https://raw.githubusercontent.com/bvschaik/julius/master/LICENSE.txt"
+rp_module_desc="Julius: Caesar III Port"
+rp_module_licence="AGPL3 https://raw.githubusercontent.com/bvschaik/julius/master/LICENSE.txt"
 rp_module_repo="git https://github.com/bvschaik/julius.git :_get_branch_julius"
 rp_module_section="opt"
 rp_module_flags="!all 64bit"
 
 function _get_branch_julius() {
-    download https://api.github.com/repos/bvschaik/julius/releases/latest - | grep -m 1 tag_name | cut -d\" -f4
+    download "https://api.github.com/repos/bvschaik/${md_id}/releases/latest" - | grep -m 1 tag_name | cut -d\" -f4
 }
 
 function depends_julius() {
     local depends=(
-        'clang'
         'cmake'
+        'libpng'
         'ninja'
         'sdl2'
         'sdl2_mixer'
@@ -34,14 +34,13 @@ function build_julius() {
     cmake . \
         -Bbuild \
         -GNinja \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$md_inst" \
-        -DCMAKE_C_COMPILER=clang \
-        -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
         -Wno-dev
     ninja -C build clean
     ninja -C build
-    md_ret_require="$md_build/build/julius"
+    md_ret_require="${md_build}/build/${md_id}"
 }
 
 function install_julius() {
@@ -49,9 +48,12 @@ function install_julius() {
 }
 
 function configure_julius() {
-    mkRomDir "ports/caesar3"
+    local portname
+    portname="caesar3"
 
-    moveConfigDir "$home/.config/julius" "$md_conf_root/caesar3"
+    [[ "${md_mode}" == "install" ]] && mkRomDir "ports/${portname}"
 
-    addPort "$md_id" "caesar3" "Caesar III" "$md_inst/bin/julius $romdir/ports/caesar3"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${portname}/${md_id}"
+
+    addPort "${md_id}" "${portname}" "Caesar III" "${md_inst}/bin/${md_id} ${romdir}/ports/${portname}"
 }
