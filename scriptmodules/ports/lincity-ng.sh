9face298145dedc787b6cb38fa615282a1b0dbbf
V0rt3x667 <al-g0l@outlook.com>
2022-12-10 00:14:49 +0000

    Update lincity-ng.sh

--- scriptmodules/ports/lincity-ng.sh
+++ scriptmodules/ports/lincity-ng.sh
@@ -5,7 +5,7 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="lincity-ng"
-rp_module_desc="LinCity NG - Open-source City Building Game"
+rp_module_desc="LinCity NG: A City Simulation Game"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/lincity-ng/lincity-ng/master/COPYING"
 rp_module_repo="git https://github.com/lincity-ng/lincity-ng.git master"
 rp_module_section="opt"
@@ -14,44 +14,39 @@ rp_module_flags="!mali"
 function depends_lincity-ng() {
     local depends=(
         'ftjam'
-        'glu'
-        'libglvnd'
-        'mesa'
+        'libxml2'
         'physfs'
         'sdl2_gfx'
         'sdl2_image'
         'sdl2_mixer'
         'sdl2_ttf'
         'sdl2'
+        'zlib'
     )
     getDepends "${depends[@]}"
 }
 
 function sources_lincity-ng() {
     gitPullOrClone
+
+    # Set Default Config Path(s)
+    sed -e "s|LC_SAVE_DIR \".lincity-ng\"|LC_SAVE_DIR \"ArchyPie/configs/${md_id}\"|g" -i "${md_build}/src/${md_id/-ng/}/loadsave.h"
 }
 
 function build_lincity-ng() {
     ./autogen.sh
-    ./configure --prefix="$md_inst"
+    ./configure --prefix="${md_inst}"
     jam
-    md_ret_require="$md_build/lincity-ng"
+    md_ret_require="${md_build}/${md_id}"
 }
 
 function install_lincity-ng() {
-    jam -sprefix="$md_inst" install
+    jam -sprefix="${md_inst}" install
 }
 
 function configure_lincity-ng() {
-    local binary="XINIT:$md_inst/bin/lincity-ng -g -f"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${portname}/${md_id}/"
+    #moveConfigDir "${home}/.lincity-ng" "${md_conf_root}/lincity-ng"
 
-    addPort "$md_id" "lincity-ng" "LinCity-NG" "$binary"
-
-    moveConfigDir "$home/.lincity-ng" "$md_conf_root/lincity-ng"
-    # fix for wrong config location
-    if [[ -d "/lincity-ng" ]]; then
-        cp -R /lincity-ng "$md_conf_root/"
-        rm -rf /lincity-ng
-        chown "$user:$user" "$md_conf_root/lincity-ng"
-    fi
+    addPort "${md_id}" "${md_id}" "LinCity-NG" "${md_inst}/bin/${md_id} -g -f"
 }
