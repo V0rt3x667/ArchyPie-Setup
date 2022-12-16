a24154997ab1fa0a4a7dbe4f03e8fc27e747c3ca
V0rt3x667 <al-g0l@outlook.com>
2022-12-16 21:13:36 +0000

    Update openblok.sh

--- scriptmodules/ports/openblok.sh
+++ scriptmodules/ports/openblok.sh
@@ -5,13 +5,17 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="openblok"
-rp_module_desc="OpenBlok - A Block Dropping Game"
+rp_module_desc="OpenBlok: A Block Dropping Game"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/mmatyas/openblok/master/LICENSE.md"
-rp_module_repo="git https://github.com/mmatyas/openblok.git master"
+rp_module_repo="git https://github.com/mmatyas/openblok.git :_get_branch_openblok"
 rp_module_section="exp"
 rp_module_flags=""
 
-function depends_openblok() { 
+function _get_branch_openblok() {
+    download "https://api.github.com/repos/mmatyas/${md_id}/releases/latest" - | grep -m 1 tag_name | cut -d\" -f4
+}
+
+function depends_openblok() {
     local depends=(
         'cmake'
         'gcc11'
@@ -33,16 +37,15 @@ function build_openblok() {
     cmake . \
         -Bbuild \
         -GNinja \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$md_inst" \
-        -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON \
-        -DCMAKE_C_COMPILER=gcc-11 \
-        -DCMAKE_CXX_COMPILER=g++-11 \
-        -DINSTALL_PORTABLE=ON \
-        -DENABLE_MP3=OFF
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
+        -DCMAKE_C_COMPILER="gcc-11" \
+        -DCMAKE_CXX_COMPILER="g++-11" \
+        -DINSTALL_PORTABLE="ON"
     ninja -C build clean
     ninja -C build
-    md_ret_require="$md_build/build/src/openblok"
+    md_ret_require="${md_build}/build/src/${md_id}"
 }
 
 function install_openblok() {
@@ -50,6 +53,19 @@ function install_openblok() {
 }
 
 function configure_openblok() {
-    moveConfigDir "$home/.local/share/openblok" "$md_conf_root/openblok"
-    addPort "$md_id" "openblok" "OpenBlok" "$md_inst/openblok"
+    moveConfigDir "$home/.local/share/${md_id}" "$md_conf_root/${md_id}"
+
+    if [[ "${md_mode}" == "install" ]]; then
+        local config
+        config="$(mktemp)"
+
+        iniConfig " = " "" "${config}"
+        echo "[system]" > "${config}"
+        iniSet "fullscreen" "on"
+
+        copyDefaultConfig "${config}" "${md_conf_root}/${md_id}/game.cfg"
+        rm "${config}"
+    fi
+
+    addPort "${md_id}" "${md_id}" "OpenBlok" "${md_inst}/${md_id}"
 }
