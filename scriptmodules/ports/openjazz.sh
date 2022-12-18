84a5d0531382d2f08a3967ea2d06eb4afcf572e2
V0rt3x667 <al-g0l@outlook.com>
2022-12-18 10:34:42 +0000

    Update openjazz.sh

--- scriptmodules/ports/openjazz.sh
+++ scriptmodules/ports/openjazz.sh
@@ -5,9 +5,9 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="openjazz"
-rp_module_desc="OpenJazz - Open-Source Version of the Classic Jazz Jackrabbit Games"
+rp_module_desc="OpenJazz: Open-Source Version of the Classic Jazz Jackrabbit Games"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/AlisterT/openjazz/master/COPYING"
-rp_module_help="For registered version, replace the shareware files by adding your full version game files to $romdir/ports/jazz."
+rp_module_help="For Registered Version, Replace the Shareware Files by Adding the Full Version Game Files to: ${romdir}/ports/jazz."
 rp_module_repo="git https://github.com/AlisterT/openjazz.git master"
 rp_module_section="opt"
 rp_module_flags=""
@@ -15,7 +15,7 @@ rp_module_flags=""
 function depends_openjazz() {
     local depends=(
         'sdl_net'
-        'sdl'
+        'sdl12-compat'
     )
     getDepends "${depends[@]}"
 }
@@ -26,8 +26,8 @@ function sources_openjazz() {
 
 function build_openjazz() {
     make clean
-    make PREFIX="$md_inst" USE_SDL_NET=1
-    md_ret_require="$md_build/OpenJazz"
+    make PREFIX="${md_inst}" USE_SDL_NET=1
+    md_ret_require="${md_build}/OpenJazz"
 }
 
 function install_openjazz() {
@@ -39,18 +39,19 @@ function install_openjazz() {
 }
 
 function _game_data_openjazz() {
-    if [[ ! -f "$romdir/ports/jazz/JAZZ.EXE" ]]; then
-        downloadAndExtract "https://image.dosgamesarchive.com/games/jazz.zip" "$romdir/ports/jazz"
-        chown -R "$user:$user" "$romdir/ports/jazz"
+    if [[ ! -f "${romdir}/ports/jazz/JAZZ.EXE" ]]; then
+        downloadAndExtract "https://image.dosgamesarchive.com/games/jazz.zip" "${romdir}/ports/jazz"
+        chown -R "${user}:${user}" "${romdir}/ports/jazz"
     fi
 }
 
 function configure_openjazz() {
-    addPort "$md_id" "$md_id" "Jazz Jackrabbit" "$md_inst/OpenJazz -f DATAPATH $romdir/ports/jazz"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "ports/jazz"
+        _game_data_openjazz
+    fi
 
-    mkRomDir "ports/jazz"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${md_id}"
 
-    moveConfigDir "$home/.openjazz" "$md_conf_root/openjazz"
-
-    [[ "$md_mode" == "install" ]] && _game_data_openjazz
+    addPort "${md_id}" "${md_id}" "Jazz Jackrabbit" "pushd ${md_conf_root}/${md_id}; ${md_inst}/OpenJazz -f DATAPATH ${romdir}/ports/jazz; popd"
 }
