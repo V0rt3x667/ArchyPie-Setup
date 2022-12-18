8ce9b4e6a77b745a75e70bf228eebd811428f270
V0rt3x667 <al-g0l@outlook.com>
2022-12-18 16:11:10 +0000

    Update openttd.sh

--- scriptmodules/ports/openttd.sh
+++ scriptmodules/ports/openttd.sh
@@ -5,14 +5,14 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="openttd"
-rp_module_desc="OpenTTD - Game Engine for Transport Tycoon Deluxe"
+rp_module_desc="OpenTTD: Game Engine for Transport Tycoon Deluxe"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/OpenTTD/OpenTTD/master/COPYING.md"
 rp_module_repo="git https://github.com/OpenTTD/OpenTTD.git :_get_branch_openttd"
 rp_module_section="opt"
-rp_module_flags="sdl2 !mali"
+rp_module_flags="!mali"
 
 function _get_branch_openttd() {
-    download https://api.github.com/repos/OpenTTD/OpenTTD/releases/latest - | grep -m 1 tag_name | cut -d\" -f4
+    download "https://api.github.com/repos/${md_id}/${md_id}/releases/latest" - | grep -m 1 tag_name | cut -d\" -f4
 }
 
 function depends_openttd() {
@@ -34,22 +34,25 @@ function depends_openttd() {
 
 function sources_openttd() {
     gitPullOrClone
-    sed -i '/sse/d;/SSE/d' "$md_build/CMakeLists.txt"
+
+    # Set Default Config Path(s)
+    sed -e "s|PATHSEP \".config\" PATHSEP;|PATHSEP \"/ArchyPie/configs/${md_id}\" PATHSEP;|g" -i "${md_build}/src/fileio.cpp"
+    sed -e "s|PATHSEP \".local\" PATHSEP \"share\" PATHSEP;|PATHSEP \"/ArchyPie/configs/${md_id}\" PATHSEP;|g" -i "${md_build}/src/fileio.cpp"
 }
 
 function build_openttd() {
     cmake . \
         -GNinja \
         -Bbuild \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$md_inst" \
-        -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
         -DCMAKE_INSTALL_BINDIR="." \
         -DCMAKE_INSTALL_DATADIR="data" \
         -Wno-dev
     ninja -C build clean
     ninja -C build
-    md_ret_require="$md_build/build/openttd"
+    md_ret_require="${md_build}/build/${md_id}"
 }
 
 function install_openttd() {
@@ -57,14 +60,21 @@ function install_openttd() {
 }
 
 function configure_openttd() {
-    addPort "$md_id" "openttd" "OpenTTD" "$md_inst/openttd"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${portname}/${md_id}"
 
-    [[ "$md_mode" == "remove" ]] && return
+    if [[ "${md_mode}" == "install" ]]; then
+        local config
+        config="$(mktemp)"
 
-    isPlatform "dispmanx" && setBackend "$md_id" "dispmanx"
+        iniConfig " = " "" "${config}"
+        echo "[misc]" > "${config}"
+        iniSet "fullscreen" "true"
 
-    local dir
-    for dir in .config .local/share; do
-        moveConfigDir "$home/$dir/openttd" "$md_conf_root/openttd"
-    done
+        mkdir "${md_conf_root}/${md_id}/${md_id}"
+        chown -R "${user}:${user}" "${md_conf_root}/${md_id}/${md_id}"
+        copyDefaultConfig "${config}" "${md_conf_root}/${md_id}/${md_id}/openttd.cfg"
+        rm "${config}"
+    fi
+
+    addPort "${md_id}" "${md_id}" "OpenTTD" "${md_inst}/${md_id}"
 }
