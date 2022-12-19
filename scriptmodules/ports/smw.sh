a940fed82a67d405ea16e05cb0dbc48e44fc7cd4
V0rt3x667 <al-g0l@outlook.com>
2022-12-19 00:01:47 +0000

    Update smw.sh

--- scriptmodules/ports/smw.sh
+++ scriptmodules/ports/smw.sh
@@ -5,11 +5,11 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="smw"
-rp_module_desc="Super Mario War - Fan-made Multiplayer Super Mario Bros. Style Deathmatch Game"
+rp_module_desc="Super Mario War: Fan-made Multiplayer Super Mario Bros. Style Deathmatch Game"
 rp_module_licence="NONCOM"
 rp_module_repo="git https://github.com/mmatyas/supermariowar.git master"
 rp_module_section="opt"
-rp_module_flags="sdl2 !mali"
+rp_module_flags="!mali"
 
 function depends_smw() {
     local depends=(
@@ -26,34 +26,33 @@ function depends_smw() {
 
 function sources_smw() {
     gitPullOrClone
+
+    # Set Default Config Path(s)
+    sed -e "s|std::string result(\".smw/\");|std::string result(\"ArchyPie/configs/${md_id}/\");|g" -i "${md_build}/src/common/path.cpp"
 }
 
 function build_smw() {
     cmake . \
         -Bbuild \
         -GNinja \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$md_inst" \
-        -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON \
-        -DBUILD_STATIC_LIBS=Off \
-        -DSMW_BINDIR="$md_inst" \
-        -DSMW_DATADIR="$md_inst/data"
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
+        -DBUILD_STATIC_LIBS="OFF" \
+        -DSMW_BINDIR="${md_inst}" \
+        -DSMW_DATADIR="${md_inst}/data"
     ninja -C build clean
     ninja -C build
-    md_ret_require="$md_build/build/Binaries/Release/smw"
+    md_ret_require="${md_build}/build/Binaries/Release/${md_id}"
 }
 
 function install_smw() {
     ninja -C build install/strip
-    chmod a+x "$md_inst"/{smw,smw-leveledit,smw-worldedit}
+    chmod a+x "${md_inst}"/{smw,smw-leveledit,smw-worldedit}
 }
 
 function configure_smw() {
-    addPort "$md_id" "smw" "Super Mario War" "$md_inst/smw"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${md_id}"
 
-    [[ "$md_mode" == "remove" ]] && return
-
-    isPlatform "dispmanx" && setBackend "$md_id" "dispmanx"
-
-    moveConfigFile "$home/.smw.options.bin" "$md_conf_root/smw/.smw.options.bin"
+    addPort "${md_id}" "${md_id}" "Super Mario War" "${md_inst}/${md_id}"
 }
