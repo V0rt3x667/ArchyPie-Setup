b13fedeb97a5308ba551ef79cdcfdc86a3453e24
V0rt3x667 <al-g0l@outlook.com>
2022-12-20 18:35:40 +0000

    Update supertux.sh

--- scriptmodules/ports/supertux.sh
+++ scriptmodules/ports/supertux.sh
@@ -5,20 +5,20 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="supertux"
-rp_module_desc="SuperTux - Classic 2D Jump'n'Run Sidescroller Game"
+rp_module_desc="SuperTux: Classic 2D Jump'n'Run Sidescroller Game"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/SuperTux/supertux/master/LICENSE.txt"
 rp_module_repo="git https://github.com/SuperTux/supertux.git :_get_branch_supertux"
 rp_module_section="opt"
 rp_module_flags="!mali"
 
 function _get_branch_supertux() {
-    download https://api.github.com/repos/SuperTux/supertux/releases/latest - | grep -m 1 tag_name | cut -d\" -f4
+    download "https://api.github.com/repos/${md_id}/${md_id}/releases/latest" - | grep -m 1 tag_name | cut -d\" -f4
 }
 
 function depends_supertux() {
     local depends=(
-        'boost'
         'boost-libs'
+        'boost'
         'cmake'
         'curl'
         'freetype2'
@@ -38,22 +38,24 @@ function depends_supertux() {
 
 function sources_supertux() {
     gitPullOrClone
-    sed '1i#include <memory>' -i external/partio_zip/zip_manager.cpp
+
+    # Fix Build On GCC12
+    sed "1i#include <memory>" -i "${md_build}/external/partio_zip/zip_manager.cpp"
 }
 
 function build_supertux() {
     cmake . \
         -Bbuild \
         -GNinja \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$md_inst" \
-        -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON \
-        -DINSTALL_SUBDIR_BIN=bin \
-        -DUSE_SYSTEM_PHYSFS=ON \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
+        -DINSTALL_SUBDIR_BIN="bin" \
+        -DUSE_SYSTEM_PHYSFS="ON" \
         -Wno-dev
     ninja -C build clean
     ninja -C build
-    md_ret_require="$md_build/build/supertux2"
+    md_ret_require="${md_build}/build/supertux2"
 }
 
 function install_supertux() {
@@ -61,7 +63,7 @@ function install_supertux() {
 }
 
 function configure_supertux() {
-    addPort "$md_id" "supertux" "SuperTux" "$md_inst/bin/supertux2 --fullscreen"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${md_id}/"
 
-    moveConfigDir "$home/.local/share/supertux2" "$md_conf_root/supertux"
+    addPort "${md_id}" "${md_id}" "SuperTux" "${md_inst}/bin/supertux2 --userdir ${md_conf_root}/${md_id}/ --fullscreen"
 }
