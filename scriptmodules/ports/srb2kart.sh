42ae6af4b12e4b6fd1abb27b63ef12986542a601
V0rt3x667 <al-g0l@outlook.com>
2022-12-20 16:42:10 +0000

    Update srb2kart.sh

--- scriptmodules/ports/srb2kart.sh
+++ scriptmodules/ports/srb2kart.sh
@@ -5,13 +5,13 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="srb2kart"
-rp_module_desc="Sonic Robo Blast 2 Kart - Kart Racing Mod Based on Sonic Robo Blast 2"
+rp_module_desc="Sonic Robo Blast 2 Kart: Kart Racing Mod Based On Sonic Robo Blast 2"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/STJr/Kart-Public/master/LICENSE"
 rp_module_repo="git https://github.com/STJr/Kart-Public.git :_get_branch_srb2kart"
 rp_module_section="opt"
 
 function _get_branch_srb2kart() {
-    download https://api.github.com/repos/STJr/Kart-Public/releases/latest - | grep -m 1 tag_name | cut -d\" -f4
+    download "https://api.github.com/repos/STJr/Kart-Public/releases/latest" - | grep -m 1 tag_name | cut -d\" -f4
 }
 
 function depends_srb2kart() {
@@ -23,30 +23,32 @@ function depends_srb2kart() {
         'sdl2_mixer'
         'sdl2'
     )
-    isPlatform x86 && depends+=('yasm')
+    isPlatform "x86" && depends+=('yasm')
     getDepends "${depends[@]}"
 }
 
 function sources_srb2kart() {
-    gitPullOrClone
-
     local ver
     ver="$(_get_branch_srb2kart)"
-    downloadAndExtract "https://github.com/STJr/Kart-Public/releases/download/$ver/srb2kart-${ver//./}-Installer.exe" "$md_build/assets/installer"
-    rm "$md_build"/assets/installer/{*.bat,*.dat,*.dll,*.exe,*.txt}
+
+    gitPullOrClone
+    downloadAndExtract "https://github.com/STJr/Kart-Public/releases/download/${ver}/AssetsLinuxOnly.zip" "${md_build}/assets/installer"
+
+    # Set Default Config Path(s)
+    sed -e "s|DEFAULTDIR \".srb2kart\"|DEFAULTDIR \"${md_id}\"|g" -i "${md_build}/src/doomdef.h"
 }
 
 function build_srb2kart() {
     cmake . \
         -Bbuild \
         -GNinja \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$md_inst" \
-        -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
         -Wno-dev
     ninja -C build clean
     ninja -C build
-    md_ret_require="$md_build/build/bin/srb2kart"
+    md_ret_require="${md_build}/build/bin/${md_id}"
 }
 
 function install_srb2kart() {
@@ -54,7 +56,7 @@ function install_srb2kart() {
 }
 
 function configure_srb2kart() {
-  addPort "$md_id" "srb2kart" "Sonic Robo Blast 2 Kart" "$md_inst/srb2kart"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${md_id}/"
 
-  moveConfigDir "$home/.srb2kart" "$md_conf_root/srb2kart"
+    addPort "${md_id}" "${md_id}" "Sonic Robo Blast 2 Kart" "${md_inst}/${md_id} -home ${md_conf_root}"
 }
