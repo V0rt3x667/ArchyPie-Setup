82029ed635a781ecfd4545c86fc8708a5c3954c2
V0rt3x667 <al-g0l@outlook.com>
2022-12-24 21:37:40 +0000

    Update tyrquake.sh

--- scriptmodules/ports/tyrquake.sh
+++ scriptmodules/ports/tyrquake.sh
@@ -5,7 +5,7 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="tyrquake"
-rp_module_desc="TyrQuake - Quake Port"
+rp_module_desc="TyrQuake: Quake Port"
 rp_module_licence="GPL2 https://disenchant.net/git/tyrquake.git/plain/gnu.txt"
 rp_module_repo="git git://disenchant.net/tyrquake master"
 rp_module_section="opt"
@@ -15,52 +15,63 @@ function depends_tyrquake() {
     if isPlatform "gl" || isPlatform "mesa"; then
         depends+=('libglvnd')
     fi
-
     getDepends "${depends[@]}"
 }
 
 function sources_tyrquake() {
     gitPullOrClone
+
+    # Set Default Config Path(s)
+    sed -e "s|(va(\"%s/.tyrquake\", home),|(va(\"%s/ArchyPie/configs/${md_id}\", home),|g" -i "${md_build}/common/common.c"
 }
 
 function build_tyrquake() {
-    local params=(USE_SDL=Y USE_XF86DGA=N)
+    local params=('USE_SDL=Y' 'USE_XF86DGA=N')
     make clean
     make "${params[@]}" bin/tyr-quake bin/tyr-glquake
     md_ret_require=(
-        "$md_build/bin/tyr-quake"
-        "$md_build/bin/tyr-glquake"
+        "${md_build}/bin/tyr-glquake"
+        "${md_build}/bin/tyr-quake"
     )
 }
 
 function install_tyrquake() {
     md_ret_files=(
-        'changelog.txt'
-        'readme.txt'
-        'readme-id.txt'
-        'gnu.txt'
         'bin'
+        'changelog.txt'
+        'gnu.txt'
+        'readme-id.txt'
+        'readme.txt'
     )
 }
 
-function _add_games_tyrquake() {
-    local params=("-basedir $romdir/ports/quake" "-game %QUAKEDIR%" "-fullscreen")
-    local binary="$md_inst/bin/tyr-quake"
+function configure_tyrquake() {
+    local portname
+    portname="quake"
 
-    isPlatform "kms" && params+=("-width %XRES%" "-height %YRES%" "+set vid_vsync 2")
-    if isPlatform "gl" || isPlatform "mesa"; then
-        binary="$md_inst/bin/tyr-glquake"
+    if [[ "$md_mode" == "install" ]]; then
+        local dirs=(
+            'dopa'
+            'hipnotic'
+            'id1'
+            'rogue'
+        )
+        mkRomDir "ports/${portname}"
+        for dir in "${dirs[@]}"; do
+            mkRomDir "ports/${portname}/${dir}"
+        done
+        _game_data_lr-tyrquake
     fi
 
-    _add_games_lr-tyrquake "$binary ${params[*]}"
-}
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${portname}/${md_id}/"
 
-function configure_tyrquake() {
-    mkRomDir "ports/quake"
+    local params=("-basedir ${romdir}/ports/${portname}" '-game %QUAKEDIR%' '-fullscreen')
+    local binary="${md_inst}/bin/tyr-quake"
 
-    [[ "$md_mode" == "install" ]] && game_data_lr-tyrquake
+    isPlatform "kms" && params+=('-width %XRES%' '-height %YRES%' '+set vid_vsync 2')
+    if isPlatform "gl" || isPlatform "mesa"; then
+        binary="${md_inst}/bin/tyr-glquake"
+    fi
 
-    _add_games_tyrquake
-
-    moveConfigDir "$home/.tyrquake" "$md_conf_root/quake/tyrquake"
+    _add_games_lr-tyrquake "${binary} ${params[*]}"
 }
