6847f6e2e62200e28cd1435286c175ee83600309
V0rt3x667 <al-g0l@outlook.com>
2022-12-27 18:44:42 +0000

    Update yquake2.sh

--- scriptmodules/ports/yquake2.sh
+++ scriptmodules/ports/yquake2.sh
@@ -5,14 +5,14 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="yquake2"
-rp_module_desc="Yamagi Quake II - Quake II Client Including Ground Zero, The Reckoning & Capture The Flag"
+rp_module_desc="Yamagi Quake II: Quake II Client Including Ground Zero, The Reckoning & Capture The Flag"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/yquake2/yquake2/master/LICENSE"
 rp_module_repo="git https://github.com/yquake2/yquake2.git :_get_branch_yquake2"
 rp_module_section="exp"
 rp_module_flags=""
 
 function _get_branch_yquake2() {
-    download https://api.github.com/repos/yquake2/yquake2/tags - | grep -m 1 name | cut -d\" -f4
+    download "https://api.github.com/repos/yquake2/yquake2/tags" - | grep -m 1 name | cut -d\" -f4
 }
 
 function depends_yquake2() {
@@ -38,12 +38,15 @@ function sources_yquake2() {
         'yquake2'
     )
     for repo in "${repos[@]}"; do
-        if [[ "$repo" == yquake2 ]]; then
-            gitPullOrClone "$md_build/$repo"
+        if [[ "${repo}" == "yquake2" ]]; then
+            gitPullOrClone "${md_build}/${repo}"
         else
-            gitPullOrClone "$md_build/$repo" "$url/$repo"
+            gitPullOrClone "${md_build}/${repo}" "${url}/${repo}"
         fi
     done
+
+    # Set Default Config Path(s)
+    sed -e "s|#define CFGDIR \".yq2\"|#define CFGDIR \"ArchyPie/configs/${md_id}\"|g" -i "${md_build}/${md_id}/src/common/header/common.h"
 }
 
 function build_yquake2() {
@@ -54,22 +57,22 @@ function build_yquake2() {
         'yquake2'
     )
     for dir in "${dirs[@]}"; do
-        make -C "$md_build/$dir" clean
-        make -C "$md_build/$dir"
+        make -C "${md_build}/${dir}" clean
+        make -C "${md_build}/${dir}"
     done
-    md_ret_require="$md_build/yquake2/release/quake2"
+    md_ret_require="${md_build}/${md_id}/release/quake2"
 }
 
 function install_yquake2() {
     md_ret_files=(
+        'yquake2/LICENSE'
+        'yquake2/README.md'
         'yquake2/release/baseq2'
         'yquake2/release/q2ded'
         'yquake2/release/quake2'
         'yquake2/release/ref_gl1.so'
         'yquake2/release/ref_gl3.so'
         'yquake2/release/ref_soft.so'
-        'yquake2/LICENSE'
-        'yquake2/README.md'
     )
     local dirs=(
         'ctf'
@@ -77,62 +80,84 @@ function install_yquake2() {
         'xatrix'
     )
     for dir in "${dirs[@]}"; do
-        install -Dm644 "$md_build/$dir/release/game.so" -t "$md_inst/$dir"
-    done
-}
-
-function _add_games_yquake2() {
-    local cmd="$1"
-    local game
-    local pak
-    declare -A games=(
-        ['baseq2/pak0']="Quake II"
-        ['xatrix/pak0']="Quake II: Mission Pack 1: The Reckoning"
-        ['rogue/pak0']="Quake II: Mission Pack 2: Ground Zero"
-        ['ctf/pak0']="Quake II: Third Wave Capture The Flag"
-    )
-    for game in "${!games[@]}"; do
-        pak="$romdir/ports/quake2/$game.pak"
-        if [[ -f "$pak" ]]; then
-            addPort "$md_id" "quake2" "${games[$game]}" "$cmd" "${game%%/*}"
-        fi
+        install -Dm644 "${md_build}/${dir}/release/game.so" -t "${md_inst}/${dir}"
     done
 }
 
 function _game_data_yquake2() {
-    if [[ ! -f "$romdir/ports/quake2/baseq2/pak1.pak" && ! -f "$romdir/ports/quake2/baseq2/pak0.pak" ]]; then
-        # get shareware game data
-        downloadAndExtract "https://deponie.yamagi.org/quake2/idstuff/q2-314-demo-x86.exe" "$romdir/ports/quake2/baseq2" -j -LL
-        # remove files that are likely to cause conflicts or unwanted default settings
-        local unwanted
-        for unwanted in $(find "$romdir/ports/quake2" -maxdepth 2 -name "*.so" -o -name "*.cfg" -o -name "*.dll" -o -name "*.exe"); do
-            rm -f "$unwanted"
-        done
+    local portname
+    portname="quake2"
+
+    if [[ ! -f "${romdir}/ports/${portname}/baseq2/pak1.pak" ]] && [[ ! -f "${romdir}/ports/${portname}/baseq2/pak0.pak" ]]; then
+        downloadAndExtract "https://deponie.yamagi.org/quake2/idstuff/q2-314-demo-x86.exe" "${romdir}/ports/${portname}/baseq2" -j -LL
+        # Remove Files That Are Not Required
+        local file
+        while read -r file; do
+            rm -f "${file}"
+        done < <(find "${romdir}/ports/${portname}" -maxdepth 2 -name "*.*" ! -name "*.pak")
     fi
 
-    chown -R "$user:$user" "$romdir/ports/quake2"
+    chown -R "${user}:${user}" "${romdir}/ports/${portname}"
+}
+
+function _add_games_yquake2() {
+    local cmd="$1"
+    local dir
+    local game
+    local portname
+    declare -A games=(
+        ['baseq2/pak0.pak']="Quake II"
+        ['ctf/pak0.pak']="Quake II: Third Wave Capture The Flag"
+        ['rogue/pak0.pak']="Quake II: Mission Pack 2: Ground Zero"
+        ['xatrix/pak0.pak']="Quake II: Mission Pack 1: The Reckoning"
+    )
+
+    # Create .sh Files For Each Game Found. Uppercase Filenames Will Be Converted to Lowercase.
+    for game in "${!games[@]}"; do
+        portname="quake2"
+        dir="${romdir}/ports/${portname}/${game%/*}"
+        if [[ "${md_mode}" == "install" ]]; then
+            pushd "${dir}" || return
+            perl-rename 'y/A-Z/a-z/' [^.-]{*,*/*}
+            popd || return
+        fi
+        if [[ -f "${dir}/${game##*/}" ]]; then
+            addPort "${md_id}" "${portname}" "${games[$game]}" "${cmd}" "${game%%/*}"
+        fi
+    done
 }
 
 function configure_yquake2() {
-    local params=()
+    local portname
+    portname="quake2"
 
-    if isPlatform "gl3"; then
+    if [[ "$md_mode" == "install" ]]; then
+        local dirs=(
+            'baseq2'
+            'ctf'
+            'rogue'
+            'xatrix'
+        )
+        mkRomDir "ports/${portname}"
+        for dir in "${dirs[@]}"; do
+            mkRomDir "ports/${portname}/${dir}"
+        done
+        _game_data_yquake2
+    fi
+
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${portname}/${md_id}/"
+
+    local params=("-datadir ${romdir}/ports/${portname}" "+set game %ROM%")
+
+    if isPlatform "gles3"; then
         params+=("+set vid_renderer gl3")
     elif isPlatform "gl" || isPlatform "mesa"; then
         params+=("+set vid_renderer gl1")
+    elif isPlatform "kms"; then
+        params+=("+set r_mode -1" "+set r_customwidth %XRES%" "+set r_customheight %YRES%" "+set r_vsync 1")
     else
         params+=("+set vid_renderer soft")
     fi
 
-    if isPlatform "kms"; then
-        params+=("+set r_mode -1" "+set r_customwidth %XRES%" "+set r_customheight %YRES%" "+set r_vsync 1")
-    fi
-
-    mkRomDir "ports/quake2"
-
-    moveConfigDir "$home/.yq2" "$md_conf_root/quake2/yquake2"
-
-    [[ "$md_mode" == "install" ]] && _game_data_yquake2
-
-    _add_games_yquake2 "$md_inst/quake2 -datadir $romdir/ports/quake2 ${params[*]} +set game %ROM%"
+    _add_games_yquake2 "${md_inst}/${portname} ${params[*]}"
 }
