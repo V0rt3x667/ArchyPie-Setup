823a95e7ff4e09c3aa3ee82bbbf2fc97a4f6f72c
V0rt3x667 <al-g0l@outlook.com>
2022-12-27 11:42:06 +0000

    Update uqm.sh

--- scriptmodules/ports/uqm.sh
+++ scriptmodules/ports/uqm.sh
@@ -5,7 +5,7 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="uqm"
-rp_module_desc="The Ur-Quan Masters - Star Control 2 Port"
+rp_module_desc="The Ur-Quan Masters: Star Control 2 Port"
 rp_module_licence="GPL2 https://sourceforce.net/p/sc2/uqm/ci/master/tree/sc2/COPYING?format=raw"
 rp_module_repo="file https://sourceforge.net/projects/sc2/files/UQM/0.8/uqm-0.8.0-src.tgz"
 rp_module_section="opt"
@@ -13,11 +13,9 @@ rp_module_section="opt"
 function depends_uqm() {
     local depends=(
         'imagemagick'
-        'libglvnd'
-        'libmikmod' 
+        'libmikmod'
         'libogg'
-        'libvorbis' 
-        'openal'
+        'libvorbis'
         'sdl2_image'
         'sdl2'
         'zlib'
@@ -26,22 +24,25 @@ function depends_uqm() {
 }
 
 function sources_uqm() {
-    downloadAndExtract "$md_repo_url" "$md_build" --strip-components 1
+    downloadAndExtract "${md_repo_url}" "${md_build}" --strip-components 1
     local ver="0.8.0"
     local url="https://sourceforge.net/projects/sc2/files/UQM/0.8"
     local files=(
-        uqm-"$ver"-content.uqm
-        uqm-"$ver"-voice.uqm
-        uqm-"$ver"-3domusic.uqm
+        "uqm-${ver}-content.uqm"
+        "uqm-${ver}-voice.uqm"
+        "uqm-${ver}-3domusic.uqm"
     )
     for file in "${files[@]}"; do
-        if [[ "$file" == uqm-$ver-content.uqm ]]; then
-            curl --create-dirs -sSL "$url/$file" --output "$md_build/content/packages/$file"
-        else 
-            curl --create-dirs -sSL "$url/$file" --output "$md_build/content/addons/$file"
+        if [[ "${file}" == "uqm-${ver}-content.uqm" ]]; then
+            curl --create-dirs -sSL "${url}/${file}" --output "${md_build}/content/packages/${file}"
+        else
+            curl --create-dirs -sSL "${url}/${file}" --output "${md_build}/content/addons/${file}"
         fi
     done
-    chmod -R 755 "$md_build/content"
+    chmod -R 755 "${md_build}/content"
+
+    # Set Default Config Path(s)
+    sed -e "s|#define USERDIR \"~/.uqm/\"|#define USERDIR \"~/ArchyPie/configs/${md_id}/\"|g" -i "${md_build}/src/config_unix.h.in"
 }
 
 function build_uqm() {
@@ -57,18 +58,18 @@ function build_uqm() {
         CHOICE_joystick_VALUE='enabled'
         CHOICE_ioformat_VALUE='stdio_zip'
         CHOICE_threadlib_VALUE='sdl'
-        INPUT_install_prefix_VALUE="$md_inst"
-        INPUT_install_bindir_VALUE="$md_inst/bin"
-        INPUT_install_libdir_VALUE="$md_inst/lib"
-        INPUT_install_sharedir_VALUE="$md_inst/share"
+        INPUT_install_prefix_VALUE="${md_inst}"
+        INPUT_install_bindir_VALUE="${md_inst}/bin"
+        INPUT_install_libdir_VALUE="${md_inst}/lib"
+        INPUT_install_sharedir_VALUE="${md_inst}/share"
     )
     for string in "${strings[@]}"; do
-        printf "%s\n" "$string" >> "$md_build/config.state"
+        printf "%s\n" "${string}" >> "${md_build}/config.state"
     done
 
     ./build.sh uqm reprocess_config
     ./build.sh uqm
-    md_ret_require="$md_build/uqm"
+    md_ret_require="${md_build}/${md_id}"
 }
 
 function install_uqm() {
@@ -76,9 +77,7 @@ function install_uqm() {
 }
 
 function configure_uqm() {
-    addPort "$md_id" "uqm" "Ur-quan Masters" "$md_inst/bin/uqm -f"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${md_id}/"
 
-    [[ "$md_mode" == "remove" ]] && return
-
-    moveConfigDir "$home/.uqm" "$md_conf_root/uqm"
+    addPort "${md_id}" "${md_id}" "The Ur-Quan Masters" "${md_inst}/bin/${md_id} -f"
 }
