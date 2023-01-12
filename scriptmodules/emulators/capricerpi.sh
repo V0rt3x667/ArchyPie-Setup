3d7f30c3f7061c21659a298fb49b891e96263b3b
V0rt3x667 <al-g0l@outlook.com>
2023-01-12 17:44:47 +0000

    Update capricerpi.sh

--- scriptmodules/emulators/capricerpi.sh
+++ scriptmodules/emulators/capricerpi.sh
@@ -5,19 +5,19 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="capricerpi"
-rp_module_desc="CapriceRPI - Amstrad CPC 464 & 6128 Emulator"
-rp_module_help="ROM Extensions: .cdt .cpc .dsk\n\nCopy your Amstrad CPC games to $romdir/amstradcpc"
+rp_module_desc="CapriceRPI: Amstrad CPC 464 & 6128 Emulator"
+rp_module_help="ROM Extensions: .cdt .cpc .dsk\n\nCopy Amstrad CPC Games To: ${romdir}/amstradcpc"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/KaosOverride/CapriceRPI/master/COPYING.txt"
-rp_module_repo="git https://github.com/KaosOverride/CapriceRPI.git master"
+rp_module_repo="git https://github.com/KaosOverride/CapriceRPI master"
 rp_module_section="opt"
 rp_module_flags="!all rpi"
 
 function depends_capricerpi() {
     local depends=(
         'libpng'
-        'sdl'
         'sdl_image'
         'sdl_ttf'
+        'sdl12-compat'
         'zlib'
     )
     getDepends "${depends[@]}"
@@ -25,23 +25,30 @@ function depends_capricerpi() {
 
 function sources_capricerpi() {
     gitPullOrClone
-    sed "s|-lpng12|-lpng|" -i "$md_inst/src/makefile"
+
+    sed -e "s|-lpng12|-lpng|g" -i "${md_inst}/src/makefile"
 }
 
 function build_capricerpi() {
     make -C src clean
     make -C src RELEASE=TRUE
-    md_ret_require="$md_build/src/capriceRPI"
+    md_ret_require="${md_build}/src/capriceRPI"
 }
 
 function install_capricerpi() {
-    cp -Rv "$md_build/"{README*.txt,COPYING.txt} "$md_inst/"
-    cp -Rv "$md_build/src/capriceRPI" "$md_inst/"
+    md_ret_files=(
+        'COPYING.txt'
+        'src/capriceRPI'
+    )
+    cp "${md_build}/README for this port.txt" "${md_inst}/README.txt"
 }
 
 function configure_capricerpi() {
-    mkRomDir "amstradcpc"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "amstradcpc"
+    fi
 
-    addEmulator 0 "$md_id" "amstradcpc" "$md_inst/capriceRPI %ROM%"
+    addEmulator 0 "${md_id}" "amstradcpc" "${md_inst}/capriceRPI %ROM%"
+
     addSystem "amstradcpc"
 }
