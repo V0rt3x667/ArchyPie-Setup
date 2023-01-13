94d80ff1a2fda7cf9808e1875cf21ea1eadf0684
V0rt3x667 <al-g0l@outlook.com>
2023-01-13 19:53:50 +0000

    Update dolphin.sh

--- scriptmodules/emulators/dolphin.sh
+++ scriptmodules/emulators/dolphin.sh
@@ -5,8 +5,8 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="dolphin"
-rp_module_desc="Dolphin - Nintendo Gamecube, Wii & Triforce Emulator"
-rp_module_help="ROM Extensions: .gcm .iso .wbfs .ciso .gcz .rvz .wad .wbfs\n\nCopy Your Gamecube ROMs to: $romdir/gc and Wii ROMs to $romdir/wii"
+rp_module_desc="Dolphin: Nintendo Gamecube & Wii Emulator"
+rp_module_help="ROM Extensions: .gcm .iso .wbfs .ciso .gcz .rvz .wad .wbfs\n\nCopy Gamecube ROMs To: ${romdir}/gc\n\nCopy Wii ROMs To ${romdir}/wii"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/dolphin-emu/dolphin/master/COPYING"
 rp_module_repo="git https://github.com/dolphin-emu/dolphin master"
 rp_module_section="exp"
@@ -32,20 +32,29 @@ function depends_dolphin() {
 
 function sources_dolphin() {
     gitPullOrClone
+
+    # Set Default Config Path(s)
+    sed -e "s|#define DOLPHIN_DATA_DIR \"dolphin-emu\"|#define DOLPHIN_DATA_DIR \"dolphin\"|g" -i "${md_build}/Source/Core/Common/CommonPaths.h"
 }
 
 function build_dolphin() {
+    local params=()
+    ! isPlatform "x11" && params=('-DENABLE_X11="OFF"')
+
     cmake . \
         -Bbuild \
         -GNinja \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$md_inst" \
-        -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON \
-        -DUSE_SHARED_ENET=ON \
+        -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -DENABLE_ANALYTICS="OFF" \
+        -DENABLE_AUTOUPDATE="OFF" \
+        -DUSE_SHARED_ENET="ON" \
+        "${params[@]}" \
         -Wno-dev
     ninja -C build clean
     ninja -C build
-    md_ret_require="$md_build/build/Binaries/dolphin-emu"
+    md_ret_require="${md_build}/build/Binaries/${md_id}-emu"
 }
 
 function install_dolphin() {
@@ -53,25 +62,33 @@ function install_dolphin() {
 }
 
 function configure_dolphin() {
-    mkRomDir "gc"
-    mkRomDir "wii"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/gc/${md_id}"
 
-    moveConfigDir "$home/.dolphin-emu" "$md_conf_root/gc"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "gc"
+        mkRomDir "wii"
 
-    if [[ ! -f "$md_conf_root/gc/Config/Dolphin.ini" ]]; then
-        mkdir -p "$md_conf_root/gc/Config"
-        cat >"$md_conf_root/gc/Config/Dolphin.ini" <<_EOF_
-[Display]
-FullscreenResolution = Auto
-Fullscreen = True
-_EOF_
-        chown -R "${user}:${user}" "$md_conf_root/gc/Config"
+        mkUserDir "${biosdir}/gc"
+        mkUserDir "$md_conf_root/gc/${md_id}/Config"
+
+        local config
+
+        config="$(mktemp)"
+        iniConfig " = " "" "${config}"
+
+        # Set Fullscreen By Default
+        echo "[Display]" > "${config}"
+        iniSet "FullscreenResolution" "Auto"
+        iniSet "Fullscreen" "True"
+
+        copyDefaultConfig "${config}" "${md_conf_root}/gc/${md_id}/Config/Dolphin.ini"
+        rm "${config}"
     fi
 
-    addEmulator 1 "$md_id" "gc" "$md_inst/bin/dolphin-emu-nogui -e %ROM%"
-    addEmulator 0 "$md_id-gui" "gc" "$md_inst/bin/dolphin-emu -b -e %ROM%"
-    addEmulator 1 "$md_id" "wii" "$md_inst/bin/dolphin-emu-nogui -e %ROM%"
-    addEmulator 0 "$md_id-gui" "wii" "$md_inst/bin/dolphin-emu -b -e %ROM%"
+    addEmulator 1 "${md_id}" "gc" "$md_inst/bin/${md_id}-emu-nogui -u ${md_conf_root}/gc/${md_id} -e %ROM%"
+    addEmulator 0 "${md_id}-gui" "gc" "$md_inst/bin/${md_id}-emu -u ${md_conf_root}/gc/${md_id} -b -e %ROM%"
+    addEmulator 1 "${md_id}" "wii" "$md_inst/bin/${md_id}-emu-nogui -u ${md_conf_root}/gc/${md_id} -e %ROM%"
+    addEmulator 0 "${md_id}-gui" "wii" "$md_inst/bin/${md_id}-emu -u ${md_conf_root}/gc/${md_id} -b -e %ROM%"
 
     addSystem "gc"
     addSystem "wii"
