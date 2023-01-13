9dc9cf4f669633cc75883e18703b3350f2476762
V0rt3x667 <al-g0l@outlook.com>
2023-01-13 01:43:33 +0000

    Update hypseus.sh
    Switch to SDL2 gamepad mapping by default.

--- scriptmodules/emulators/hypseus.sh
+++ scriptmodules/emulators/hypseus.sh
@@ -5,10 +5,10 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="hypseus"
-rp_module_desc="Hypseus Singe - Super Multiple Arcade Laserdisc Emulator"
-rp_module_help="ROM Extension: .daphne\n\nCopy Your Laserdisc ROMs to $romdir/daphne"
+rp_module_desc="Hypseus Singe: Super Multiple Arcade Laserdisc Emulator"
+rp_module_help="ROM Extension: .daphne\n\nCopy Laserdisc ROMs To: ${romdir}/daphne"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/DirtBagXon/hypseus-singe/master/LICENSE"
-rp_module_repo="git https://github.com/DirtBagXon/hypseus-singe.git :_get_branch_hypseus"
+rp_module_repo="git https://github.com/DirtBagXon/hypseus-singe :_get_branch_hypseus"
 rp_module_section="main"
 rp_module_flags=""
 
@@ -16,7 +16,7 @@ function _get_branch_hypseus() {
     if isPlatform "rpi"; then
         RetroPie
     else
-        download https://api.github.com/repos/DirtBagXon/hypseus-singe/releases/latest - | grep -m 1 tag_name | cut -d\" -f4
+        download "https://api.github.com/repos/DirtBagXon/${md_id}-singe/releases/latest" - | grep -m 1 tag_name | cut -d\" -f4
     fi
 }
 
@@ -26,9 +26,10 @@ function depends_hypseus() {
         'libmpeg2'
         'libogg'
         'libvorbis'
-        'sdl2'
+        'ninja'
         'sdl2_image'
         'sdl2_ttf'
+        'sdl2'
         'zlib'
     )
     getDepends "${depends[@]}"
@@ -39,60 +40,59 @@ function sources_hypseus() {
 }
 
 function build_hypseus() {
-    # Does not currently build with Ninja.
+    # Not Currently Building With Ninja
     rpSwap on 1024
     cmake . \
-        -Ssrc \
         -Bbuild \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$md_inst" \
-        -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON \
+        -Ssrc \
+        -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
         -Wno-dev
     make -C build clean
     make -C build
     rpSwap off
 
-    cp build/hypseus hypseus.bin
-    md_ret_require="hypseus.bin"
+    cp "build/${md_id}" "${md_id}.bin"
+    md_ret_require="${md_build}/${md_id}.bin"
 }
 
 function install_hypseus() {
     md_ret_files=(
-        'sound'
-        'pics'
         'fonts'
         'hypseus.bin'
         'LICENSE'
+        'pics'
+        'sound'
     )
 }
 
 function configure_hypseus() {
-    mkRomDir "daphne"
-    mkRomDir "daphne/roms"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/daphne/${md_id}"
 
-    addEmulator 0 "$md_id" "daphne" "$md_inst/hypseus.sh %ROM%"
-    addSystem "daphne"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "daphne"
+        mkRomDir "daphne/roms"
 
-    [[ "$md_mode" == "remove" ]] && return
+        local dirs=(
+            'logs'
+            'ram'
+            'screenshots'
+        )
+        for dir in "${dirs[@]}"; do
+            mkUserDir "${md_conf_root}/daphne/${md_id}/${dir}"
+            ln -snf "${md_conf_root}/daphne/${md_id}/${dir}" "${md_inst}/${dir}"
+        done
 
-    mkUserDir "$md_conf_root/daphne"
+        copyDefaultConfig "${md_data}/hypinput.ini" "${md_conf_root}/daphne/${md_id}/hypinput.ini"
 
-    local dir
-    for dir in ram logs screenshots; do
-        mkUserDir "$md_conf_root/daphne/$dir"
-        ln -snf "$md_conf_root/daphne/$dir" "$md_inst/$dir"
-    done
+        ln -snf "${romdir}/daphne/roms" "${md_inst}/roms"
+        ln -snf "${romdir}/daphne/roms" "${md_inst}/singe"
+        ln -sf "${md_conf_root}/daphne/${md_id}/hypinput.ini" "${md_inst}/hypinput.ini"
 
-    copyDefaultConfig "$md_data/hypinput.ini" "$md_conf_root/daphne/hypinput.ini"
+        local common_args="-framefile \"\$dir/\$name.txt\" -homedir \"${md_inst}\" -fullscreen -gamepad \$params"
 
-    ln -snf "$romdir/daphne/roms" "$md_inst/roms"
-    ln -snf "$romdir/daphne/roms" "$md_inst/singe"
-
-    ln -sf "$md_conf_root/daphne/hypinput.ini" "$md_inst/hypinput.ini"
-
-    local common_args="-framefile \"\$dir/\$name.txt\" -homedir \"$md_inst\" -fullscreen \$params"
-
-    cat >"$md_inst/hypseus.sh" <<_EOF_
+        cat >"${md_inst}/${md_id}.sh" <<_EOF_
 #!/bin/bash
 dir="\$1"
 name="\${dir##*/}"
@@ -103,11 +103,15 @@ if [[ -f "\$dir/\$name.commands" ]]; then
 fi
 
 if [[ -f "\$dir/\$name.singe" ]]; then
-    "$md_inst/hypseus.bin" singe vldp -retropath -manymouse -script "\$dir/\$name.singe" $common_args
+    "${md_inst}/${md_id}.bin" singe vldp -retropath -manymouse -script "\$dir/\$name.singe" ${common_args}
 else
-    "$md_inst/hypseus.bin" "\$name" vldp $common_args
+    "${md_inst}/${md_id}.bin" "\$name" vldp ${common_args}
 fi
 _EOF_
-    chmod +x "$md_inst/hypseus.sh"
-    mkdir -p "$md_inst/framefile"
+        chmod +x "${md_inst}/${md_id}.sh"
+        mkdir -p "${md_inst}/framefile"
+    fi
+
+    addEmulator 1 "${md_id}" "daphne" "${md_inst}/${md_id}.sh %ROM%"
+    addSystem "daphne"
 }
