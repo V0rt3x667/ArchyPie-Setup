87b50935f1c22ccc6a179484c600a3ac12e94983
V0rt3x667 <al-g0l@outlook.com>
2023-01-14 22:33:39 +0000

    Update dosbox.sh

--- scriptmodules/emulators/dosbox.sh
+++ scriptmodules/emulators/dosbox.sh
@@ -5,102 +5,86 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="dosbox"
-rp_module_desc="DOSBox - MS-DOS Emulator"
-rp_module_help="ROM Extensions: .bat .com .exe .sh .conf\n\nCopy your DOS games to $romdir/pc"
+rp_module_desc="DOSBox: MS-DOS Emulator"
+rp_module_help="ROM Extensions: .bat .com .conf .exe .sh\n\nCopy DOS Games To: ${romdir}/pc"
 rp_module_licence="GPL2 https://sourceforge.net/p/dosbox/code-0/HEAD/tree/dosbox/trunk/COPYING"
-rp_module_repo="svn https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk - 4465"
+rp_module_repo="svn https://svn.code.sf.net/p/dosbox/code-0/dosbox/trunk"
 rp_module_section="opt"
-rp_module_flags="!mali"
+rp_module_flags=""
 
 function depends_dosbox() {
     local depends=(
         'alsa-lib'
         'alsa-utils'
         'libpng'
+        'sdl_net'
+        'sdl_sound'
+        'sdl12-compat'
+        'subversion'
         'zlib'
-        "$@"
     )
-    [[ "$md_id" == "dosbox" ]] && depends+=('sdl' 'sdl_net' 'sdl_sound')
     isPlatform "rpi" && depends+=('timidity++')
     getDepends "${depends[@]}"
 }
 
 function sources_dosbox() {
-    local revision="$1"
-    [[ -z "$revision" ]] && revision="4465"
+    svn checkout "${md_repo_url}" "${md_build}"
 
-    svn checkout "$md_repo_url" "$md_build" -r "$revision"
+    # Set Default Config Path(s)
+    sed -e "s|in = \"~/.dosbox\";|in = \"~/ArchyPie/configs/${md_id}\";|g" -i "${md_build}/src/misc/cross.cpp"
 }
 
 function build_dosbox() {
     local params=()
 
-    ! isPlatform "x11" && params+=(--disable-opengl)
-    # add or override params from calling function
-    params+=("$@")
+    ! isPlatform "x11" && params+=('--disable-opengl')
 
     ./autogen.sh
-    ./configure --prefix="$md_inst" "${params[@]}"
-    if isPlatform "arm"; then
-        # enable dynamic recompilation for armv4
-        sed -i 's|/\* #undef C_DYNREC \*/|#define C_DYNREC 1|' config.h
-        sed -i 's/C_TARGETCPU.*/C_TARGETCPU ARMV7LE/g' config.h
-        sed -i 's|/\* #undef C_UNALIGNED_MEMORY \*/|#define C_UNALIGNED_MEMORY 1|' config.h
-    fi
+    ./configure --prefix="${md_inst}" "${params[@]}"
     make clean
     make
-    md_ret_require="$md_build/src/dosbox"
+    md_ret_require=("${md_build}/src/${md_id}")
 }
 
 function install_dosbox() {
     make install
-    md_ret_require="$md_inst/bin/dosbox"
+    md_ret_require=("${md_inst}/bin/${md_id}")
 }
 
 function configure_dosbox() {
-    local def=0
-    local launcher_name="+Start DOSBox.sh"
-    local needs_synth=0
-    local config_dir="$home/.$md_id"
-    case "$md_id" in
-        dosbox-sdl2)
-            launcher_name="+Start DOSBox-SDL2.sh"
-            ;;
-        dosbox)
-            def=1
-            # needs software synth for midi; limit to Pi for now
-            isPlatform "rpi" && needs_synth=1
-            ;;
-        dosbox-staging)
-            launcher_name="+Start DOSBox-Staging.sh"
-            config_dir="$home/.config/dosbox"
-            ;;
-        dosbox-x)
-            launcher_name="+Start DOSBox-X.sh"
-            config_dir="$home/.config/dosbox-x"
-            ;;
-        *)
-            return 1
-            ;;
-    esac
+    if [[ "${md_mode}" == "install" ]]; then
+        local def=0
+        local launcher_name="+Start DOSBox.sh"
+        local needs_synth=0
+        local config_dir="${arpdir}/${md_id}"
+        case "${md_id}" in
+            dosbox)
+                def=1
+                # Use Software Synth For MIDI
+                isPlatform "rpi" && needs_synth=1
+                ;;
+            dosbox-staging)
+                launcher_name="+Start DOSBox-Staging.sh"
+                #config_dir="$home/.config/dosbox"
+                ;;
+            dosbox-x)
+                launcher_name="+Start DOSBox-X.sh"
+                #config_dir="$home/.config/dosbox-x"
+                ;;
+            *)
+                return 1
+                ;;
+        esac
 
-    mkRomDir "pc"
+        mkRomDir "pc"
 
-    moveConfigDir "$config_dir" "$md_conf_root/pc"
-
-    addEmulator "$def" "$md_id" "pc" "bash $romdir/pc/${launcher_name// /\\ } %ROM%"
-    addSystem "pc"
-
-    rm -f "$romdir/pc/$launcher_name"
-    [[ "$md_mode" == "remove" ]] && return
-
-    cat > "$romdir/pc/$launcher_name" << _EOF_
+        cat > "${romdir}/pc/${launcher_name}" << _EOF_
 #!/bin/bash
 
-[[ ! -n "\$(aconnect -o | grep -e TiMidity -e FluidSynth)" ]] && needs_synth="$needs_synth"
+[[ ! -n "\$(aconnect -o | grep -e TiMidity -e FluidSynth)" ]] && needs_synth="${needs_synth}"
 
 function midi_synth() {
-    [[ "\$needs_synth" != "1" ]] && return
+    [[ "\${needs_synth}" != "1" ]] && return
 
     case "\$1" in
         "start")
@@ -121,7 +105,7 @@ function midi_synth() {
 
 params=("\$@")
 if [[ -z "\${params[0]}" ]]; then
-    params=(-c "@MOUNT C $romdir/pc -freesize 1024" -c "@C:")
+    params=(-c "@MOUNT C ${romdir}/pc -freesize 1024" -c "@C:")
 elif [[ "\${params[0]}" == *.sh ]]; then
     midi_synth start
     bash "\${params[@]}"
@@ -133,32 +117,37 @@ else
     params+=(-exit)
 fi
 
-# fullscreen when running in X
-[[ -n "\$DISPLAY" ]] && params+=(-fullscreen)
-
 midi_synth start
-if [[ "$md_id" == "dosbox-x" ]]; then
-    "$md_inst/bin/dosbox-x" "\${params[@]}"
+if [[ "${md_id}" == "dosbox-x" ]]; then
+    "${md_inst}/bin/dosbox-x" -fullscreen "\${params[@]}"
 else
-    "$md_inst/bin/dosbox" "\${params[@]}"
+    "${md_inst}/bin/dosbox" -fullscreen "\${params[@]}"
 fi
 midi_synth stop
 _EOF_
-    chmod +x "$romdir/pc/$launcher_name"
-    chown "${user}:${user}" "$romdir/pc/$launcher_name"
+        chmod +x "${romdir}/pc/${launcher_name}"
+        chown "${user}:${user}" "${romdir}/pc/${launcher_name}"
 
-    if [[ "$md_id" == "dosbox" || "$md_id" == "dosbox-sdl2" ]]; then
-        local config_path=$(su "${user}" -c "\"$md_inst/bin/dosbox\" -printconf")
-        if [[ -f "$config_path" ]]; then
-            iniConfig " = " "" "$config_path"
-            iniSet "usescancodes" "false"
-            iniSet "core" "dynamic"
-            iniSet "cycles" "max"
-            iniSet "scaler" "none"
-            if isPlatform "rpi" || [[ -n "$(aconnect -o | grep -e TiMidity -e FluidSynth)" ]]; then
-                iniSet "mididevice" "alsa"
-                iniSet "midiconfig" "128:0"
+        if [[ "${md_id}" == "dosbox" ]]; then
+            local config_path
+            config_path=$(su "${user}" -c "\"${md_inst}/bin/dosbox\" -printconf")
+            if [[ -f "${config_path}" ]]; then
+                iniConfig " = " "" "${config_path}"
+                iniSet "usescancodes" "false"
+                iniSet "core" "dynamic"
+                iniSet "cycles" "max"
+                iniSet "scaler" "none"
+                if isPlatform "rpi" || [[ -n "$(aconnect -o | grep -e TiMidity -e FluidSynth)" ]]; then
+                    iniSet "mididevice" "alsa"
+                    iniSet "midiconfig" "128:0"
+                fi
             fi
         fi
     fi
+
+    moveConfigDir "${config_dir}" "${md_conf_root}/pc"
+
+    addEmulator "${def}" "${md_id}" "pc" "bash ${romdir}/pc/${launcher_name// /\\ } %ROM%"
+
+    addSystem "pc"
 }
