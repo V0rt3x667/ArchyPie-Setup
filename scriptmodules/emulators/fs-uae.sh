054874afa1981d887db4e71ddb43dfb0ffbdf182
V0rt3x667 <al-g0l@outlook.com>
2023-01-15 18:21:10 +0000

    Update fs-uae.sh

--- scriptmodules/emulators/fs-uae.sh
+++ scriptmodules/emulators/fs-uae.sh
@@ -5,12 +5,12 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="fs-uae"
-rp_module_desc="FS-UAE - Commodore Amiga 500, 500+, 600, 1200, CDTV & CD32 Emulator"
-rp_module_help="ROM Extension: .adf .adz .dms .ipf .zip .lha .iso .cue .bin\n\nCopy Amiga Games to $romdir/amiga\n\nCopy Your CD32 Games to $romdir/cd32\n\nCopy Your CDTV Games to $romdir/cdtv\n\nCopy a required BIOS file (e.g. kick13.rom) to $biosdir/amiga."
+rp_module_desc="FS-UAE: Commodore Amiga 500, 500+, 600, 1200, CDTV & CD32 Emulator"
+rp_module_help="ROM Extension: .adf .adz .bin .chd .cue .dms .ipf .iso .lha .m3u .sh .uae .zip\n\nCopy Amiga Games To: ${romdir}/amiga\n\nCopy CD32 Games To: ${romdir}/cd32\n\nCopy CDTV Games To: ${romdir}/cdtv\n\nCopy BIOS Files:\n\nkick13.rom\nkick20.rom\nkick31.rom\n\nTo: ${biosdir}/amiga"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/FrodeSolheim/fs-uae/master/COPYING"
-rp_module_repo="git https://github.com/FrodeSolheim/fs-uae.git :_get_branch_fs-uae"
+rp_module_repo="git https://github.com/FrodeSolheim/fs-uae :_get_branch_fs-uae"
 rp_module_section="main"
-rp_module_flags="!all !arm x11"
+rp_module_flags="!all x86_64"
 
 function depends_fs-uae() {
     local depends=(
@@ -25,9 +25,10 @@ function depends_fs-uae() {
         'libxi'
         'mesa'
         'openal'
-        'python'
         'python-lhafile'
+        'python-pyqt5'
         'python-rx'
+        'python'
         'sdl2'
         'shared-mime-info'
         'zip'
@@ -37,73 +38,79 @@ function depends_fs-uae() {
 }
 
 function _get_branch_fs-uae() {
-    download https://api.github.com/repos/FrodeSolheim/fs-uae/releases - | grep -m 1 tag_name | cut -d\" -f4
+    download "https://api.github.com/repos/FrodeSolheim/fs-uae/releases" - | grep -m 1 tag_name | cut -d\" -f4
 }
 
 function _get_branch_fs-uae-launcher() {
-    download https://api.github.com/repos/FrodeSolheim/fs-uae-launcher/releases - | grep -m 1 tag_name | cut -d\" -f4
+    download "https://api.github.com/repos/FrodeSolheim/fs-uae-launcher/releases" - | grep -m 1 tag_name | cut -d\" -f4
 }
 
 function _get_branch_capsimg() {
-    download https://api.github.com/repos/FrodeSolheim/capsimg/releases - | grep -m 1 tag_name | cut -d\" -f4
+    download "https://api.github.com/repos/FrodeSolheim/capsimg/releases" - | grep -m 1 tag_name | cut -d\" -f4
 }
 
 function sources_fs-uae() {
     gitPullOrClone
-    _sources_fs-uae-launcher
+
     _sources_capsimg
+    _sources_fs-uae-launcher
 }
 
 function _sources_fs-uae-launcher() {
+    local tag
     tag="$(_get_branch_fs-uae-launcher)"
-    gitPullOrClone "$md_build/launcher" "https://github.com/FrodeSolheim/fs-uae-launcher" "$tag"
-    sed "s|/usr/local|$md_inst|g" -i "$md_build/launcher/bootstrap"
 
+    gitPullOrClone "${md_build}/launcher" "https://github.com/FrodeSolheim/fs-uae-launcher" "${tag}"
+
+    # Set Installation Prefix
+    sed -e "s|/usr/local|${md_inst}|g" -i "${md_build}/launcher/bootstrap"
 }
 
 function _sources_capsimg() {
+    local tag
     tag="$(_get_branch_capsimg)"
-    gitPullOrClone "$md_build/capsimg" "https://github.com/FrodeSolheim/capsimg" "$tag"
+
+    gitPullOrClone "${md_build}/capsimg" "https://github.com/FrodeSolheim/capsimg" "${tag}"
 }
 
 function _build_capsimg() {
-    cd "$md_build/capsimg/CAPSImg" || exit
+    cd "${md_build}/capsimg/CAPSImg" || exit
     chmod a+x ./bootstrap.sh
     ./bootstrap.sh
     ./configure
     make clean
     make
     mv libcapsimage.so.5.1 capsimg.so
-    md_ret_require="$md_build/capsimg/CAPSImg/capsimg.so"
+    md_ret_require="${md_build}/capsimg/CAPSImg/capsimg.so"
 }
 
 function _build_fs-uae-launcher() {
-    cd "$md_build/launcher" || exit
+    cd "${md_build}/launcher" || exit
     ./bootstrap
     ./update-version
     make clean
     make
-    md_ret_require="$md_build/launcher/fs-uae-launcher"
+    md_ret_require="${md_build}/launcher/${md_id}-launcher"
 }
 
 function build_fs-uae() {
     _build_capsimg
     _build_fs-uae-launcher
 
-    cd "$md_build" || exit
+    cd "${md_build}" || exit
     ./bootstrap
-    ./configure --prefix="$md_inst"
+    ./configure --prefix="${md_inst}"
     make clean
     make
-    md_ret_require="$md_build/fs-uae"
+    md_ret_require="${md_build}/${md_id}"
 }
 
 function _install_capsimg() {
-    install -Dm644 "$md_build/capsimg/CAPSImg/capsimg.so" "$md_inst/bin/"
+    install -Dm644 "${md_build}/capsimg/CAPSImg/capsimg.so" "${md_inst}/bin/"
 }
 
 function _install_fs-uae-launcher() {
-    make -C "$md_build/launcher" install
+    make -C "${md_build}/launcher" install
 }
 
 function install_fs-uae() {
@@ -113,28 +120,31 @@ function install_fs-uae() {
 }
 
 function configure_fs-uae() {
-    if [[ "$md_mode" == "install" ]]; then
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/amiga/${md_id}"
+
+    # Required For 'fs-uae-launcher'
+    moveConfigDir "${md_conf_root}/amiga/${md_id}/Kickstarts" "${biosdir}/amiga/"
+
+    if [[ "${md_mode}" == "install" ]]; then
         mkRomDir "amiga"
         mkRomDir "cd32"
         mkRomDir "cdtv"
 
-        mkUserDir "$biosdir/amiga"
-        mkUserDir "$biosdir/amiga/workbench"
+        mkUserDir "${biosdir}/amiga"
+        mkUserDir "${biosdir}/amiga/workbench"
 
-        install -Dm755 "$md_data/fs-uae.sh" "$md_inst/bin"
-    fi
+        # Copy Launcher Script
+        install -Dm755 "${md_data}/${md_id}.sh" "${md_inst}/bin"
 
-    moveConfigDir "$arpiedir/emulators/$md_id" "$md_conf_root/amiga/$md_id"
-    moveConfigDir "$md_conf_root/amiga/$md_id/Kickstarts" "$biosdir/amiga/"
+        # Set Default Config File
+        local config
+        config="$(mktemp)"
+        iniConfig " = " "" "${config}"
 
-    if [[ "$md_mode" == "install" ]]; then
-        # Copy Default Config File
-        local config="$(mktemp)"
-        iniConfig " = " "" "$config"
-        iniSet "base_dir" "$arpiedir/emulators/$md_id"
-        iniSet "logs_dir" "$arpiedir/emulators/$md_id"
-        iniSet "cache_dir" "$arpiedir/emulators/$md_id"
-        iniSet "kickstarts_dir" "$biosdir/amiga"
+        iniSet "base_dir" "${arpdir}/${md_id}"
+        iniSet "logs_dir" "${arpdir}/${md_id}"
+        iniSet "cache_dir" "${arpdir}/${md_id}"
+        iniSet "kickstarts_dir" "${biosdir}/amiga"
         iniSet "fullscreen" "1"
         iniSet "keep_aspect" "1"
         iniSet "video_sync" "Auto"
@@ -142,25 +152,27 @@ function configure_fs-uae() {
         iniSet "fsaa" "0"
         iniSet "scanlines" "0"
         iniSet "floppy_drive_speed" "100"
-        copyDefaultConfig "$config" "$md_conf_root/amiga/$md_id/Config.fs-uae"
-        rm "$config"
 
+        copyDefaultConfig "${config}" "${arpdir}/${md_id}/Config.fs-uae"
+        rm "${config}"
+
+        # Copy 'fs-uae-launcher' Script
         local script="+Start FS-UAE.sh"
-        cat > "$romdir/amiga/$script" << _EOF_
+        cat > "${romdir}/amiga/${script}" << _EOF_
 #!/bin/bash
-FS_UAE_BASE_DIR="$arpiedir/emulators/$md_id" "$md_inst/bin/fs-uae-launcher"
+FS_UAE_BASE_DIR="${arpdir}/${md_id}" "${md_inst}/bin/${md_id}-launcher"
 _EOF_
-        chmod a+x "$romdir/amiga/$script"
-        chown "${user}:${user}" "$romdir/amiga/$script"
+        chmod a+x "${romdir}/amiga/${script}"
+        chown "${user}:${user}" "${romdir}/amiga/${script}"
     fi
 
-    addEmulator 0 "$md_id-a1200" "amiga" "$md_inst/bin/fs-uae.sh %ROM% A1200"
-    addEmulator 0 "$md_id-a500plus" "amiga" "$md_inst/bin/fs-uae.sh %ROM% A500+"
-    addEmulator 0 "$md_id-a600" "amiga" "$md_inst/bin/fs-uae.sh %ROM% A600"
-    addEmulator 0 "$md_id-whdload" "amiga" "$md_inst/bin/fs-uae.sh %ROM% WHDLOAD"
-    addEmulator 1 "$md_id-a500" "amiga" "$md_inst/bin/fs-uae.sh %ROM% A500"
-    addEmulator 1 "$md_id-cd32" "cd32" "$md_inst/bin/fs-uae.sh %ROM% CD32"
-    addEmulator 1 "$md_id-cdtv" "cdtv" "$md_inst/bin/fs-uae.sh %ROM% CDTV"
+    addEmulator 0 "${md_id}-a1200" "amiga" "${md_inst}/bin/${md_id}.sh %ROM% A1200"
+    addEmulator 0 "${md_id}-a500plus" "amiga" "${md_inst}/bin/${md_id}.sh %ROM% A500+"
+    addEmulator 0 "${md_id}-a600" "amiga" "${md_inst}/bin/${md_id}.sh %ROM% A600"
+    addEmulator 0 "${md_id}-whdload" "amiga" "${md_inst}/bin/${md_id}.sh %ROM% WHDLOAD"
+    addEmulator 1 "${md_id}-a500" "amiga" "${md_inst}/bin/${md_id}.sh %ROM% A500"
+    addEmulator 1 "${md_id}-cd32" "cd32" "${md_inst}/bin/${md_id}.sh %ROM% CD32"
+    addEmulator 1 "${md_id}-cdtv" "cdtv" "${md_inst}/bin/${md_id}.sh %ROM% CDTV"
 
     addSystem "amiga"
     addSystem "cd32"
--- scriptmodules/emulators/fs-uae/fs-uae.sh
+++ scriptmodules/emulators/fs-uae/fs-uae.sh
@@ -8,68 +8,69 @@ ROM="$1"
 MODEL="$2"
 
 rootdir="/opt/archypie"
-datadir="$HOME/ArchyPie"
-romdir="$datadir/roms"
-arpiedir="$HOME/.config/archypie/emulators/fs-uae"
+datadir="${HOME}/ArchyPie"
+romdir="${datadir}/roms"
+biosdir="${datadir}/BIOS"
+arpdir="${datadir}/configs/fs-uae"
 
-source "$rootdir/lib/archivefuncs.sh"
+source "${rootdir}/lib/archivefuncs.sh"
 
 function launch_amiga() {
-    case "$MODEL" in
+    case "${MODEL}" in
         CD32)
-            "$rootdir/emulators/fs-uae/bin/fs-uae" \
-            --base_dir="$arpiedir" \
+            "${rootdir}/emulators/fs-uae/bin/fs-uae" \
+            --base_dir="${arpdir}" \
             --fullscreen \
-            --amiga_model="$MODEL" \
-            --cdrom_drive_0="$ROM" \
-            --cdroms_dir="$romdir/cd32" \
-            --save_states_dir="$romdir/cd32" \
-            --kickstarts_dir="$biosdir/amiga"
+            --amiga_model="${MODEL}" \
+            --cdrom_drive_0="${ROM}" \
+            --cdroms_dir="${romdir}/cd32" \
+            --save_states_dir="${romdir}/cd32" \
+            --kickstarts_dir="${biosdir}/amiga"
             ;;
         CDTV)
-            "$rootdir/emulators/fs-uae/bin/fs-uae" \
-            --base_dir="$arpiedir" \
+            "${rootdir}/emulators/fs-uae/bin/fs-uae" \
+            --base_dir="${arpdir}" \
             --fullscreen \
-            --amiga_model="$MODEL" \
-            --cdrom_drive_0="$ROM" \
-            --cdroms_dir="$romdir/cdtv" \
-            --save_states_dir="$romdir/cdtv" \
-            --kickstarts_dir="$biosdir/amiga"
+            --amiga_model="${MODEL}" \
+            --cdrom_drive_0="${ROM}" \
+            --cdroms_dir="${romdir}/cdtv" \
+            --save_states_dir="${romdir}/cdtv" \
+            --kickstarts_dir="${biosdir}/amiga"
             ;;
         WHDLOAD)
-            "$rootdir/emulators/fs-uae/bin/fs-uae-launcher" \
-            --base-dir="$arpiedir" \
+            "${rootdir}/emulators/fs-uae/bin/fs-uae-launcher" \
+            --base-dir="${arpdir}" \
             --fullscreen \
             --no-gui \
-            --floppies_dir="$biosdir/amiga/workbench" \
-            --kickstarts_dir="$biosdir/amiga" \
-            "$ROM"
+            --floppies_dir="${biosdir}/amiga/workbench" \
+            --kickstarts_dir="${biosdir}/amiga"
+            "${ROM}"
             ;;
         A500|A500+|A600|A1200)
-            "$rootdir/emulators/fs-uae/bin/fs-uae" \
-            --base_dir="$arpiedir" \
+            "${rootdir}/emulators/fs-uae/bin/fs-uae" \
+            --base_dir="${arpdir}" \
             --fullscreen \
-            --amiga_model="$MODEL" \
-            --floppy_drive_0="$ROM" \
+            --amiga_model="${MODEL}" \
+            --floppy_drive_0="${ROM}" \
             "${floppy_images[@]}" \
-            --floppies_dir="$romdir/amiga" \
-            --save_states_dir="$romdir/amiga" \
-            --kickstarts_dir="$biosdir/amiga"
+            --floppies_dir="${romdir}/amiga" \
+            --save_states_dir="${romdir}/amiga" \
+            --kickstarts_dir="${biosdir}/amiga"
             ;;
     esac
 }
 
 function check_arch_files() {
-    case "$MODEL" in
+    case "${MODEL}" in
         CD32|CDTV|WHDLOAD)
             launch_amiga
             ;;
         A500|A500+|A600|A1200)
-            archiveExtract "$ROM" ".adf .adz .dms .ipf"
+            archiveExtract "${ROM}" ".adf .adz .dms .ipf"
             # Check for Successful Extraction
             if [[ $? == 0 ]]; then
                 ROM="${arch_files[0]}"
-                romdir="$arch_dir"
+                romdir="${arch_dir}"
                 floppy_images=()
                 for i in "${!arch_files[@]}"; do
                     floppy_images+=("--floppy_image_$i=${arch_files[$i]}")
