41dd131a90fea7fdbb31fc4bea1644b3017ac666
V0rt3x667 <al-g0l@outlook.com>
2023-01-15 11:43:17 +0000

    Update frotz.sh

--- scriptmodules/emulators/frotz.sh
+++ scriptmodules/emulators/frotz.sh
@@ -5,15 +5,15 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="frotz"
-rp_module_desc="Frotz - Interpreter for Infocom & Z-Machine Games"
-rp_module_help="ROM Extensions: .dat .zip .z1 .z2 .z3 .z4 .z5 .z6 .z7 .z8\n\nCopy your Infocom games to $romdir/zmachine"
+rp_module_desc="Frotz: Interpreter For Infocom & Z-Machine Games"
+rp_module_help="ROM Extensions: .dat .z1 .z2 .z3 .z4 .z5 .z6 .z7 .z8 .zip\n\nCopy Infocom Games To: ${romdir}/zmachine"
 rp_module_licence="GPL2 https://gitlab.com/DavidGriffith/frotz/raw/master/COPYING"
 rp_module_section="opt"
-rp_module_repo="git https://gitlab.com/DavidGriffith/frotz.git :_get_branch_frotz"
+rp_module_repo="git https://gitlab.com/DavidGriffith/frotz :_get_branch_frotz"
 rp_module_flags=""
 
 function _get_branch_frotz() {
-    download https://gitlab.com/api/v4/projects/DavidGriffith%2Ffrotz/releases - | grep -m 1 tag_name | cut -d\" -f8
+    download "https://gitlab.com/api/v4/projects/DavidGriffith%2Ffrotz/releases" - | grep -m 1 tag_name | cut -d\" -f8
 }
 
 function depends_frotz() {
@@ -26,8 +26,8 @@ function depends_frotz() {
         'libsamplerate'
         'libsndfile'
         'libvorbis'
-        'sdl2'
         'sdl2_mixer'
+        'sdl2'
         'zlib'
     )
     getDepends "${depends[@]}"
@@ -38,35 +38,44 @@ function sources_frotz() {
 }
 
 function build_frotz() {
-    make PREFIX="$md_inst" SYSCONFDIR="$HOME/.config/frotz" sdl
+    make PREFIX="${md_inst}" SYSCONFDIR="${arpdir}/${md_id}" sdl
+    md_ret_require=("${md_build}/sfrotz")
 }
 
 function install_frotz() {
-     make PREFIX="$md_inst" install_sdl
+    make PREFIX="${md_inst}" install_sdl
+    cp "${md_build}/doc/frotz.conf-big" "${md_inst}/share/"
+    md_ret_require=("${md_inst}/bin/sfrotz")
 }
 
-function game_data_frotz() {
-    local dest="$romdir/zmachine"
-    if [[ ! -f "$dest/zork1.dat" ]]; then
-        mkUserDir "$dest"
-        local temp="$(mktemp -d)"
+function _game_data_frotz() {
+    local dest="${romdir}/zmachine"
+    if [[ ! -f "${dest}/zork1.dat" ]]; then
+        mkUserDir "${dest}"
+        local temp
+        temp="$(mktemp -d)"
         local file
         for file in zork1 zork2 zork3; do
-            downloadAndExtract "$__archive_url/${file}.zip" "$temp" -L
-            cp "$temp/data/${file}.dat" "$dest"
-            rm -rf "$temp"
+            downloadAndExtract "${__archive_url}/${file}.zip" "${temp}" -L
+            cp "${temp}/data/${file}.dat" "${dest}"
+            rm -rf "${temp}"
         done
-        rm -rf "$temp"
-        chown -R "${user}:${user}" "$romdir/zmachine"
+        rm -rf "${temp}"
+        chown -R "${user}:${user}" "${romdir}/zmachine"
     fi
 }
 
 function configure_frotz() {
-    mkRomDir "zmachine"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/zmachine/${md_id}/"
 
-    # CON: to stop runcommand from redirecting stdout to log
-    addEmulator 1 "$md_id" "zmachine" "CON:pushd $romdir/zmachine; $md_inst/bin/sfrotz -F %ROM%; popd"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "zmachine"
+        copyDefaultConfig "${md_inst}/share/${md_id}.conf-big" "${arpdir}/${md_id}/${md_id}.conf"
+        _game_data_frotz
+    fi
+
+    # CON: Stop 'runcommand' From Redirecting 'stdout' To Log
+    addEmulator 1 "${md_id}" "zmachine" "CON:pushd ${romdir}/zmachine; ${md_inst}/bin/sfrotz -F %ROM%; popd"
+
     addSystem "zmachine"
-
-    [[ "$md_mode" == "install" ]] && game_data_frotz
 }
