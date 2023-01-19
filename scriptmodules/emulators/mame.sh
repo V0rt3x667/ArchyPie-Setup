535f96b5009778d193ceab369d225407b5ab8f21
V0rt3x667 <al-g0l@outlook.com>
2023-01-19 14:54:41 +0000

    Update mame.sh

--- scriptmodules/emulators/mame.sh
+++ scriptmodules/emulators/mame.sh
@@ -5,25 +5,24 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="mame"
-rp_module_desc="MAME - Arcade Machine & Computer Emulator (Latest Version)"
-rp_module_help="ROM Extensions: .zip .7z\n\nCopy your MAME roms to either $romdir/mame or\n$romdir/arcade"
+rp_module_desc="MAME: Arcade Machine & Computer Emulator (Latest Version)"
+rp_module_help="ROM Extensions: .7z .zip\n\nCopy MAME ROMs To Either: ${romdir}/mame\n\n${romdir}/arcade"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/mamedev/mame/master/COPYING"
-rp_module_repo="git https://github.com/mamedev/mame.git :_get_branch_mame"
-rp_module_section="exp"
-rp_module_flags="!mali"
+rp_module_repo="git https://github.com/mamedev/mame :_get_branch_mame"
+rp_module_section="main"
+rp_module_flags=""
 
 function _get_branch_mame() {
-    download https://api.github.com/repos/mamedev/mame/releases/latest - | grep -m 1 tag_name | cut -d\" -f4
+    download "https://api.github.com/repos/mamedev/${md_id}/releases/latest" - | grep -m 1 tag_name | cut -d\" -f4
 }
 
 function depends_mame() {
     local depends=(
-        'clang'
         'flac'
         'glm'
         'libpulse'
         'libutf8proc'
-        'libxinerama'
+        'libx11'
         'lua53'
         'nasm'
         'portaudio'
@@ -33,20 +32,27 @@ function depends_mame() {
         'qt5-base'
         'rapidjson'
         'sdl2_ttf'
+        'sdl2'
     )
+    isPlatform "x11" && params+=('libxinerama')
     getDepends "${depends[@]}"
 }
 
 function sources_mame() {
     gitPullOrClone
+
+    # Set Default Config Path(s)
+    sed -e "s|# SDL_INI_PATH = .;\$HOME/.mame/;ini;|SDL_INI_PATH = \$HOME/ArchyPie/configs/${md_id}/|g" -i "${md_build}/makefile"
+
     # Use System Libraries
-    sed -e 's|\# USE_SYSTEM_LIB|USE_SYSTEM_LIB|g' -i makefile
+    sed -e "s|\# USE_SYSTEM_LIB|USE_SYSTEM_LIB|g" -i "${md_build}/makefile"
+
     # Except for ASIO
-    sed -e 's|USE_SYSTEM_LIB_ASIO|\# USE_SYSTEM_LIB_ASIO|g' -i makefile
+    sed -e "s|USE_SYSTEM_LIB_ASIO|\# USE_SYSTEM_LIB_ASIO|g" -i "${md_build}/makefile"
 }
 
 function build_mame() {
-    # More memory is required for 64bit platforms
+    # More Memory Is Required For 64bit Platforms
     if isPlatform "64bit"; then
         rpSwap on 8192
     else
@@ -56,24 +62,33 @@ function build_mame() {
     export CFLAGS+=" -I/usr/include/lua5.3/"
     export CXXFLAGS+=" -I/usr/include/lua5.3/"
 
-    # Hack to force linking to lua5.3
+    # Force Linking To lua5.3
     mkdir lib
     ln -s /usr/lib/liblua5.3.so lib/liblua.so
     export LDFLAGS+=" -L${PWD}/lib"
 
-    # Compile MAME
-    local params=(NOWERROR=1 PYTHON_EXECUTABLE=python OPTIMIZE=2)
-    make "${params[@]}" CC="clang" CXX="clang++"
-    strip mame
+    local params=(
+        'LTO=0'
+        'NOWERROR=1'
+        'OPTIMIZE=2'
+        'PYTHON_EXECUTABLE=python'
+    )
+    # ! isPlatform "x11" && params+=('NO_X11=1') Breaks Linking
+    # Error: '/usr/bin/ld: /usr/lib/libX11.so.6: error adding symbols: DSO missing from command line'
+
+    make clean
+    make "${params[@]}"
 
     rpSwap off
-    md_ret_require="$md_build/mame"
+
+    md_ret_require="${md_build}/${md_id}"
 }
 
 function install_mame() {
     md_ret_files=(
         'artwork'
         'bgfx'
+        'COPYING'
         'ctrlr'
         'docs'
         'hash'
@@ -85,88 +100,98 @@ function install_mame() {
         'roms'
         'samples'
         'uismall.bdf'
-        'COPYING'
     )
 }
 
 function configure_mame() {
-    local system="mame"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${md_id}/"
 
-    if [[ "$md_mode" == "install" ]]; then
+    if [[ "${md_mode}" == "install" ]]; then
         mkRomDir "arcade"
-        mkRomDir "$system"
+        mkRomDir "${md_id}"
 
-        # Create required MAME directories underneath the ROM directory
-        local mame_sub_dir
-        for mame_sub_dir in artwork cfg comments diff inp nvram samples scores snap sta; do
-            mkRomDir "$system/$mame_sub_dir"
+        # Create BIOS Directory
+        mkUserDir "${biosdir}/${md_id}"
+
+        # Create MAME Directories
+        local dirs=(
+            'artwork'
+            'cfg'
+            'comments'
+            'diff'
+            'inp'
+            'nvram'
+            'samples'
+            'scores'
+            'snap'
+            'sta'
+        )
+        for dir in "${dirs[@]}"; do
+            mkRomDir "${md_id}/${dir}"
         done
 
-        # Create a BIOS directory, where people will be able to store their BIOS files, separate from ROMs
-        mkUserDir "$biosdir/$system"
+        # Create MAME Config File
+        local config
+        config="$(mktemp)"
 
-        # Create the configuration directory for the MAME ini files
-        moveConfigDir "$home/.mame" "$md_conf_root/$system"
+        iniConfig " " "" "${config}"
+        iniSet "artpath"            "${romdir}/${md_id}/artwork;${romdir}/arcade/artwork"
+        iniSet "ctrlrpath"          "${md_inst}/ctrlr"
+        iniSet "hashpath"           "${md_inst}/hash"
+        iniSet "languagepath"       "${md_inst}/language"
+        iniSet "pluginspath"        "${md_inst}/plugins"
+        iniSet "rompath"            "${romdir}/${md_id};${romdir}/arcade;${biosdir}/${md_id}"
+        iniSet "samplepath"         "${romdir}/${md_id}/samples;${romdir}/arcade/samples"
 
-        # Create new INI files if they do not already exist
-        # Create MAME config file
-        local temp_ini_mame="$(mktemp)"
+        iniSet "cfg_directory"      "${romdir}/${md_id}/cfg"
+        iniSet "comment_directory"  "${romdir}/${md_id}/comments"
+        iniSet "diff_directory"     "${romdir}/${md_id}/diff"
+        iniSet "input_directory"    "${romdir}/${md_id}/inp"
+        iniSet "nvram_directory"    "${romdir}/${md_id}/nvram"
+        iniSet "snapshot_directory" "${romdir}/${md_id}/snap"
+        iniSet "state_directory"    "${romdir}/${md_id}/sta"
 
-        iniConfig " " "" "$temp_ini_mame"
-        iniSet "rompath"            "$romdir/$system;$romdir/arcade;$biosdir/$system"
-        iniSet "hashpath"           "$md_inst/hash"
-        iniSet "samplepath"         "$romdir/$system/samples;$romdir/arcade/samples"
-        iniSet "artpath"            "$romdir/$system/artwork;$romdir/arcade/artwork"
-        iniSet "ctrlrpath"          "$md_inst/ctrlr"
-        iniSet "pluginspath"        "$md_inst/plugins"
-        iniSet "languagepath"       "$md_inst/language"
-
-        iniSet "cfg_directory"      "$romdir/$system/cfg"
-        iniSet "nvram_directory"    "$romdir/$system/nvram"
-        iniSet "input_directory"    "$romdir/$system/inp"
-        iniSet "state_directory"    "$romdir/$system/sta"
-        iniSet "snapshot_directory" "$romdir/$system/snap"
-        iniSet "diff_directory"     "$romdir/$system/diff"
-        iniSet "comment_directory"  "$romdir/$system/comments"
-
-        iniSet "skip_gameinfo" "1"
         iniSet "plugin" "hiscore"
         iniSet "samplerate" "44100"
+        iniSet "skip_gameinfo" "1"
 
-        # Raspberry Pis show improved performance using accelerated mode which enables SDL_RENDERER_TARGETTEXTURE.
-        # On RPI4 it uses OpenGL as a renderer, while on earlier RPIs it uses OpenGLES2 as the renderer. 
-        # X86 Ubuntu by default uses OpenGL as a renderer, but SDL doesn't have target texture enabled as default.
-        # Enabling accel will use target texture on X86 Ubuntu (and likely other X86 Linux platforms).
+        # Raspberry Pis Show Improved Performance Using Accelerated Mode Which Enables 'SDL_RENDERER_TARGETTEXTURE'
         iniSet "video" "accel"
 
-        copyDefaultConfig "$temp_ini_mame" "$md_conf_root/$system/mame.ini"
-        rm "$temp_ini_mame"
+        copyDefaultConfig "${config}" "${md_conf_root}/${md_id}/mame.ini"
+        rm "${config}"
 
-        # Create MAME UI config file
-        local temp_ini_ui="$(mktemp)"
-        iniConfig " " "" "$temp_ini_ui"
-        iniSet "scores_directory" "$romdir/$system/scores"
-        copyDefaultConfig "$temp_ini_ui" "$md_conf_root/$system/ui.ini"
-        rm "$temp_ini_ui"
+        # Create MAME UI Config File
+        local config_ui
+        config_ui="$(mktemp)"
+        iniConfig " " "" "${config_ui}"
+        iniSet "scores_directory" "${romdir}/${md_id}/scores"
+        copyDefaultConfig "${config_ui}" "${md_conf_root}/${md_id}/ui.ini"
+        rm "${config_ui}"
 
-        # Create MAME Plugin config file
-        local temp_ini_plugin="$(mktemp)"
-        iniConfig " " "" "$temp_ini_plugin"
+        # Create MAME Plugin Config File
+        local config_plugin
+        config_plugin="$(mktemp)"
+        iniConfig " " "" "${config_plugin}"
         iniSet "hiscore" "1"
-        copyDefaultConfig "$temp_ini_plugin" "$md_conf_root/$system/plugin.ini"
-        rm "$temp_ini_plugin"
+        copyDefaultConfig "${config_plugin}" "${md_conf_root}/${md_id}/plugin.ini"
+        rm "${config_plugin}"
 
-        # Create MAME Hi Score config file
-        local temp_ini_hiscore="$(mktemp)"
-        iniConfig " " "" "$temp_ini_hiscore"
-        iniSet "hi_path" "$romdir/$system/scores"
-        copyDefaultConfig "$temp_ini_hiscore" "$md_conf_root/$system/hiscore.ini"
-        rm "$temp_ini_hiscore"
+        # Create MAME Hi Score Config File
+        local config_hiscore
+        config_hiscore="$(mktemp)"
+        iniConfig " " "" "${config_hiscore}"
+        iniSet "hi_path" "${romdir}/${md_id}/scores"
+        copyDefaultConfig "${config_hiscore}" "${md_conf_root}/${md_id}/hiscore.ini"
+        rm "${config_hiscore}"
     fi
 
-    addEmulator 0 "$md_id" "arcade" "$md_inst/mame %BASENAME%"
-    addEmulator 1 "$md_id" "$system" "$md_inst/mame %BASENAME%"
+    local params=()
+    isPlatform "wayland" && params+=('-videodriver wayland' '-video opengl')
+
+    addEmulator 0 "${md_id}" "arcade" "${md_inst}/mame ${params[*]} %BASENAME%"
+    addEmulator 1 "${md_id}" "${md_id}" "${md_inst}/mame ${params[*]} %BASENAME%"
 
     addSystem "arcade"
-    addSystem "$system"
+    addSystem "${md_id}"
 }
