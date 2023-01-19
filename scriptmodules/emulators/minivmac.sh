6a7d036a14fb1c36e7b3698ffb580d9b48e09ab9
V0rt3x667 <al-g0l@outlook.com>
2023-01-19 17:04:36 +0000

    Update minivmac.sh

--- scriptmodules/emulators/minivmac.sh
+++ scriptmodules/emulators/minivmac.sh
@@ -5,43 +5,47 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="minivmac"
-rp_module_desc="Mini vMac - Apple Macintosh Plus Emulator"
-rp_module_help="ROM Extensions: .dsk \n\nCopy your Macintosh Plus disks to $romdir/macintosh \n\n You need to copy the Macintosh bios file vMac.ROM into "$biosdir" and System Tools.dsk to $romdir"
+rp_module_desc="Mini vMac: Apple Macintosh Plus Emulator"
+rp_module_help="ROM Extensions: .dsk\n\nCopy Macintosh Plus Games To: ${romdir}/macintosh\n\nCopy Macintosh BIOS File (vMac.ROM) To: ${biosdir}/macintosh"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/vanfanel/minivmac_sdl2/master/COPYING.txt"
 rp_module_repo="file https://www.gryphel.com/d/minivmac/minivmac-36.04/minivmac-36.04.src.tgz"
 rp_module_section="exp"
 rp_module_flags="!all x11"
 
 function depends_minivmac() {
-    getDepends libx11
+    local depends=('libx11')
+    getDepends "${depends[@]}"
 }
 
 function sources_minivmac() {
-    downloadAndExtract "$md_repo_url" "$md_build" --strip-components 1
+    downloadAndExtract "${md_repo_url}" "${md_build}" --strip-components 1
 }
 
 function build_minivmac() {
-    cd $md_build
+    platform=()
+    isPlatform "x86" && isPlatform "64bit" && platform=('lx64')
+    isPlatform "x86" && isPlatform "32bit" && platform=('lx86')
+    isPlatform "arm" && platform=('larm')
+
     gcc ./setup/tool.c -o setup_t
-    ./setup_t -t lx64 > ./setup.sh
+    ./setup_t -t "${platform[@]}" > ./setup.sh
     chmod a+x ./setup.sh
     ./setup.sh
-    # Fix Missing RELOC
-    sed -e 's|$(ObjFiles) -ldl -L/usr/X11R6/lib -lX11|$(ObjFiles) $(LDFLAGS) -ldl -L/usr/X11R6/lib -lX11|' -i "$md_build/Makefile"
     make clean
     make
-    md_ret_require="$md_build/minivmac"
+    md_ret_require="${md_build}/${md_id}"
 }
 
 function install_minivmac() {
-    md_ret_files=('minivmac')
+    md_ret_files=("${md_id}")
 }
 
 function configure_minivmac() {
     mkRomDir "macintosh"
 
-    ln -sf "$biosdir/vMac.ROM" "$md_inst/vMac.ROM"
+    ln -sf "${biosdir}/macintosh/vMac.ROM" "${md_inst}/vMac.ROM"
 
-    addEmulator 1 "$md_id" "macintosh" "pushd $md_inst; $md_inst/minivmac $romdir/macintosh/System\ Tools.dsk %ROM%; popd"
+    addEmulator 0 "${md_id}" "macintosh" "${md_inst}/${md_id} %ROM%"
+
     addSystem "macintosh"
 }
