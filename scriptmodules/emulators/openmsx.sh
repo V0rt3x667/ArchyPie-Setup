6a2f4d6c0f3fc60e160a0df904862cb5775bd203
V0rt3x667 <al-g0l@outlook.com>
2023-01-20 18:38:29 +0000

    Update openmsx.sh

--- scriptmodules/emulators/openmsx.sh
+++ scriptmodules/emulators/openmsx.sh
@@ -5,36 +5,47 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="openmsx"
-rp_module_desc="OpenMSX - Microsoft MSX, MSX2, MSX2+ & TurboR Emulator"
-rp_module_help="ROM Extensions: .cas .col .dsk .mx1 .mx2 .rom .zip\n\nCopy your MSX/MSX2 games to $romdir/msx\nCopy the BIOS files to $biosdir/openmsx"
+rp_module_desc="OpenMSX: Microsoft MSX, MSX2, MSX2+ & TurboR Emulator"
+rp_module_help="ROM Extensions: .cas .col .dsk .mx1 .mx2 .rom .zip\n\nCopy MSX/MSX2 Games To: ${romdir}/msx\nCopy BIOS Files To: ${biosdir}/msx"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/openMSX/openMSX/master/doc/GPL.txt"
-rp_module_repo="git https://github.com/openMSX/openMSX.git :_get_branch_openmsx"
+rp_module_repo="git https://github.com/openMSX/openMSX :_get_branch_openmsx"
 rp_module_section="opt"
 rp_module_flags=""
 
 function _get_branch_openmsx() {
-    download https://api.github.com/repos/openMSX/openMSX/releases/latest - | grep -m 1 tag_name | cut -d\" -f4
+    download "https://api.github.com/repos/openMSX/openMSX/releases/latest" - | grep -m 1 tag_name | cut -d\" -f4
 }
 
 function depends_openmsx() {
     local depends=(
-        'alsa-lib' 
-        'libtheora' 
-        'libvorbis' 
+        'alsa-lib'
+        'libogg'
+        'libpng'
+        'libtheora'
+        'libvorbis'
         'python'
-        'sdl2_ttf' 
+        'sdl2_ttf'
+        'sdl2'
         'tcl'
+        'zlib'
     )
     isPlatform "x11" && depends+=('glew')
+
     getDepends "${depends[@]}"
 }
 
 function sources_openmsx() {
     gitPullOrClone
-    sed -i "s|INSTALL_BASE:=/opt/openMSX|INSTALL_BASE:=$md_inst|" build/custom.mk
-    sed -i "s|SYMLINK_FOR_BINARY:=true|SYMLINK_FOR_BINARY:=false|" build/custom.mk
-    sed -i "s|<SDL_ttf.h>|<SDL2/SDL_ttf.h>|" build/libraries.py
-    echo "LINK_FLAGS:=-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now,-lrt" >> build/custom.mk
+
+    # Set Default Config Path(s)
+    sed -e "s|(\"~/.openMSX\");|(\"~/ArchyPie/configs/${md_id}\");|g" -i "${md_build}/src/file/FileOperations.cc"
+    sed -e "s|-types {hidden d} \".openMSX\"|-types \"ArchyPie/configs/${md_id}\"|g" -i "${md_build}/share/scripts/_osd_menu.tcl"
+
+    # Set Installation Prefix
+    sed -e "s|INSTALL_BASE:=/opt/openMSX|INSTALL_BASE:=${md_inst}|g" -i "${md_build}/build/custom.mk"
+
+    # Do Not Symlink Binary
+    sed -e "s|SYMLINK_FOR_BINARY:=true|SYMLINK_FOR_BINARY:=false|g" -i "${md_build}/build/custom.mk"
 }
 
 function build_openmsx() {
@@ -43,43 +54,48 @@ function build_openmsx() {
     make clean
     make
     rpSwap off
-    md_ret_require="$md_build/derived/openmsx"
+    md_ret_require="${md_build}/derived/${md_id}"
 }
 
 function install_openmsx() {
     make install
-    mkdir -p "$md_inst/share/systemroms/"
-    downloadAndExtract "$__archive_url/openmsxroms.tar.gz" "$md_inst/share/systemroms/"
+    mkdir -p "${md_inst}/share/systemroms/"
+    downloadAndExtract "${__archive_url}/openmsxroms.tar.gz" "${md_inst}/share/systemroms/"
 }
 
 function configure_openmsx() {
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/msx/${md_id}"
+
+    if [[ "${md_mode}" == "install" ]]; then
+        # Set Default Config File
+        local config
+        config="$(mktemp)"
+        echo "$(_default_settings_openmsx)" > "${config}"
+
+        mkUserDir "${arpdir}/${md_id}/share"
+        mkUserDir "${arpdir}/${md_id}/share/scripts"
+        mkUserDir "${arpdir}/${md_id}/share/systemroms"
+
+        moveConfigDir "${arpdir}/${md_id}/share/systemroms" "${biosdir}/${md_id}"
+
+        copyDefaultConfig "${config}" "${md_conf_root}/msx/${md_id}/share/settings.xml"
+        rm "${config}"
+
+        # Copy Launcher Script
+        cp "${md_data}/archypie-init.tcl" "${arpdir}/${md_id}/share/scripts"
+        chown -R "${user}:${user}" "${arpdir}/${md_id}/share/scripts"
+    fi
+
     mkRomDir "msx"
     mkRomDir "msx2"
 
-    addEmulator 0 "$md_id" "msx" "$md_inst/bin/openmsx %ROM%"
-    addEmulator 0 "$md_id-msx2" "msx2" "$md_inst/bin/openmsx -machine 'Boosted_MSX2_EN' %ROM%"
-    addEmulator 0 "$md_id-msx2-plus" "msx2" "$md_inst/bin/openmsx -machine 'Boosted_MSX2+_JP' %ROM%"
-    addEmulator 0 "$md_id-msx-turbor" "msx" "$md_inst/bin/openmsx -machine 'Panasonic_FS-A1GT' %ROM%"
+    addEmulator 1 "${md_id}" "msx" "${md_inst}/bin/${md_id} %ROM%"
+    addEmulator 1 "${md_id}-msx2" "msx2" "${md_inst}/bin/${md_id} -machine 'Boosted_MSX2_EN' %ROM%"
+    addEmulator 0 "${md_id}-msx2-plus" "msx2" "${md_inst}/bin/${md_id} -machine 'Boosted_MSX2+_JP' %ROM%"
+    addEmulator 0 "${md_id}-msx-turbor" "msx" "${md_inst}/bin/${md_id} -machine 'Panasonic_FS-A1GT' %ROM%"
+
     addSystem "msx"
     addSystem "msx2"
-
-    [[ $md_mode == "remove" ]] && return
-
-    # Add a minimal configuration
-    local config="$(mktemp)"
-    echo "$(_default_settings_openmsx)" > "$config"
-
-    mkUserDir "$home/.openMSX/share/scripts"
-    mkUserDir "$home/.openMSX/share/systemroms"
-    moveConfigDir "$home/.openMSX" "$configdir/msx/openmsx"
-    moveConfigDir "$configdir/msx/openmsx/share/systemroms" "$home/ArchyPie/BIOS/openmsx"
-
-    copyDefaultConfig "$config" "$home/.openMSX/share/settings.xml"
-    rm "$config"
-
-    # Add an autostart script, used for joypad configuration
-    cp "$md_data/archypie-init.tcl" "$home/.openMSX/share/scripts"
-    chown -R "${user}:${user}" "$home/.openMSX/share/scripts"
 }
 
 function _default_settings_openmsx() {
@@ -92,10 +108,10 @@ function _default_settings_openmsx() {
 <settings>
   <settings>
     <setting id="default_machine">C-BIOS_MSX</setting>
-    <setting id="osd_disk_path">$romdir/msx</setting>
-    <setting id="osd_rom_path">$romdir/msx</setting>
-    <setting id="osd_tape_path">$romdir/msx</setting>
-    <setting id="osd_hdd_path">$romdir/msx</setting>
+    <setting id="osd_disk_path">${romdir}/msx</setting>
+    <setting id="osd_rom_path">${romdir}/msx</setting>
+    <setting id="osd_tape_path">${romdir}/msx</setting>
+    <setting id="osd_hdd_path">${romdir}/msx</setting>
     <setting id="fullscreen">true</setting>
     <setting id="save_settings_on_exit">false</setting>
 _EOF_
--- scriptmodules/emulators/openmsx/archypie-init.tcl
+++ scriptmodules/emulators/openmsx/archypie-init.tcl
@@ -5,7 +5,7 @@ proc init {} {
     set config_dir [file normalize "$::env(OPENMSX_USER_DATA)/joystick"]
 
     # Sanitize rom name
-    regsub -all {[\?\<\>\\\/:\*\|]} $rom_name "" rom_name
+    regsub -all {[\?\<\>\\\/:\*\|]} ${rom_name} "" rom_name
 
     # Auto-configure the 1st plugged in joystick, but only if present
     # openMSX automatically loads the plugged in joysticks in 'autoplug.tcl'
@@ -16,31 +16,31 @@ proc init {} {
     # Disable the OSD menu box added by osd_menu.tcl
     osd destroy main_menu_pop_up_button
 
-    if { !($rom_name eq "") } {
-        if { [ file exists "$config_dir/game/$rom_name.tcl" ] } {
-            load_config_joystick $rom_name "$config_dir/game/$rom_name.tcl"
+    if { !(${rom_name} eq "") } {
+        if { [ file exists "${config_dir}/game/${rom_name}.tcl" ] } {
+            load_config_joystick $rom_name "${config_dir}/game/${rom_name}.tcl"
             return
         }
     }
 
     if { [catch {exec udevadm info --name=/dev/input/js0 | grep -q "ID_INPUT_JOYSTICK=1"}] == 0 } {
         set path [exec udevadm info --name=/dev/input/js0 --query=name]
-        set joy_name [exec cat /sys/class/$path/device/name]
-        regsub -all {[\?\<\>\\\/:\*\|]} $joy_name "" joy_name$
-        if { [file exists "$config_dir/$joy_name.tcl"] } {
-                load_config_joystick $joy_name "$config_dir/$joy_name.tcl"
+        set joy_name [exec cat /sys/class/${path}/device/name]
+        regsub -all {[\?\<\>\\\/:\*\|]} ${joy_name} "" joy_name$
+        if { [file exists "${config_dir}/${joy_name}.tcl"] } {
+                load_config_joystick ${joy_name} "${config_dir}/${joy_name}.tcl"
         }
     }
 }
 
 proc load_config_joystick { conf_name conf_script } {
-    source "$conf_script"
+    source "${conf_script}"
     # Check for auto configuration function
     if { [info procs "auto_config_joypad"] == "" } {
         return
     }
     auto_config_joypad
-    message "Loaded joystick configuration for '$conf_name'"
+    message "Loaded Joystick Configuration For: '${conf_name}'"
 }
 
 }; # namespace: archypie
