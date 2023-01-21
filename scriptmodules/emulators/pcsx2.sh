f49aea2b814f277e04ca694d0bd5cc11b038adb2
V0rt3x667 <al-g0l@outlook.com>
2023-01-21 16:53:32 +0000

    Update pcsx2.sh

--- scriptmodules/emulators/pcsx2.sh
+++ scriptmodules/emulators/pcsx2.sh
@@ -5,12 +5,12 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="pcsx2"
-rp_module_desc="PCSX2 - Sony PlayStation 2 Emulator"
-rp_module_help="ROM Extensions: .bin .bz2 .chd .cso .dump .gz .ima .img .iso .mdf .z .z2\n\nCopy Your PS2 ROMs to: $romdir/ps2\n\nCopy the required BIOS file to: $biosdir"
+rp_module_desc="PCSX2: Sony PlayStation 2 Emulator"
+rp_module_help="ROM Extensions: .bin .bz2 .chd .cso .dump .gz .ima .img .iso .mdf .z .z2\n\nCopy PS2 ROMs To: ${romdir}/ps2\n\nCopy BIOS Files To: ${biosdir}/ps2"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/PCSX2/pcsx2/master/COPYING.GPLv3"
 rp_module_repo="git https://github.com/PCSX2/pcsx2 master"
 rp_module_section="main"
-rp_module_flags="!all x86"
+rp_module_flags="!all x86_64"
 
 function depends_pcsx2() {
     local depends=(
@@ -28,77 +28,79 @@ function depends_pcsx2() {
         'sdl2'
         'soundtouch'
     )
-
     getDepends "${depends[@]}"
 }
 
 function sources_pcsx2() {
     gitPullOrClone
 
-    applyPatch "$md_data/01_set_default_config_path.patch"
+    # Set Default Config Path(s)
+    applyPatch "${md_data}/01_set_default_config_path.patch"
+
+    # Get Latest "gamecontrollerdb.txt" File
+    curl -sSL "https://github.com/gabomdq/SDL_GameControllerDB/archive/refs/heads/master.zip" | bsdtar xvf - --strip-components=1 -C "${md_build}"
 }
 
 function build_pcsx2() {
-    # For future use.
-    #local params=()
-    #isPlatform wayland && params+=('-DWAYLAND_API=ON')
+    local params=()
+    isPlatform "wayland" && params+=(-DWAYLAND_API="ON")
+    isPlatform "wayland" || isPlatform "kms" && params+=(-DX11_API="OFF")
 
     cmake . \
-        -Bbuild \
-        -GNinja \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$md_inst" \
-        -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON \
-        -DENABLE_TESTS=OFF \
-        -DDISABLE_BUILD_DATE=ON \
-        -DDISABLE_PCSX2_WRAPPER=ON \
-        -DUSE_VTUNE=OFF \
-        -DUSE_SYSTEM_LIBS=ON \
-        -DXDG_STD=ON \
-        -DQT_BUILD=ON \
+        -B"build" \
+        -G"Ninja" \
+        -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -DENABLE_TESTS="OFF" \
+        -DDISABLE_BUILD_DATE="ON" \
+        -DUSE_SYSTEM_LIBS="ON" \
+        -DQT_BUILD="ON" \
+        "${params[@]}" \
         -Wno-dev
     ninja -C build clean
     ninja -C build
-    md_ret_require="$md_build/build/pcsx2-qt/pcsx2-qt"
+    md_ret_require="${md_build}/build/bin/${md_id}-qt"
 }
 
 function install_pcsx2() {
     md_ret_files=(
-        'build/pcsx2-qt/pcsx2-qt'
-        'build/pcsx2-qt/resources'
+        "build/bin/pcsx2-qt"
+        "build/bin/resources"
+        "gamecontrollerdb.txt"
     )
-    ln -svf "$md_inst/pcsx2-qt" "$md_inst/pcsx2"
 }
 
 function configure_pcsx2() {
-    mkRomDir "ps2"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/ps2/${md_id}"
+    
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "ps2"
 
-    mkUserDir "$biosdir/ps2"
-    mkUserDir "$arpiedir/emulators"
-    mkUserDir "$arpiedir/emulators/$md_id"
-    mkUserDir "$arpiedir/emulators/$md_id/inis"
+        mkUserDir "${biosdir}/ps2"
+        mkUserDir "${arpdir}/${md_id}/inis"
 
-    moveConfigDir "$arpiedir/emulators/$md_id" "$md_conf_root/ps2/$md_id"
+        # # Set Default Config File
+        local config
+        config="$(mktemp)"
+        iniConfig " = " "" "${config}"
 
-    addEmulator 1 "$md_id" "ps2" "$md_inst/pcsx2 -nogui -fullscreen %ROM%"
-    addEmulator 0 "$md_id-gui" "ps2" "$md_inst/pcsx2 -fullscreen"
+        echo "[UI]" > "${config}"
+        iniSet "SettingsVersion" "1"
+        iniSet "StartFullscreen" "true"
+        iniSet "HideMouseCursor" "true"
+        iniSet "ConfirmShutdown" "false"
+        echo "[GameList]" >> "${config}"
+        iniSet "Paths" "${romdir}/ps2"
+        echo "[Folders]" >> "${config}"
+        iniSet "Bios" "${biosdir}/ps2"
+
+        copyDefaultConfig "${config}" "${md_conf_root}/ps2/${md_id}/inis/PCSX2.ini"
+        rm "${config}"
+    fi
+
+    addEmulator 1 "${md_id}" "ps2" "${md_inst}/pcsx2-qt -nogui -fullscreen %ROM%"
+    addEmulator 0 "${md_id}-gui" "ps2" "${md_inst}/pcsx2-qt -fullscreen"
 
     addSystem "ps2"
-
-    [[ "$md_mode" == "remove" ]] && return
-
-    # Set default settings.
-    local config="$(mktemp)"
-    iniConfig " = " "" "$config"
-    echo "[UI]" > "$config"
-    iniSet "SettingsVersion" "1"
-    iniSet "StartFullscreen" "true"
-    iniSet "HideMouseCursor" "true"
-    iniSet "ConfirmShutdown" "false"
-    echo "[GameList]" >> "$config"
-    iniSet "Paths" "$romdir/ps2"
-    echo "[Folders]" >> "$config"
-    iniSet "Bios" "$biosdir/ps2"
-    copyDefaultConfig "$config" "$md_conf_root/ps2/$md_id/inis/PCSX2.ini"
-    rm "$config"
 }
