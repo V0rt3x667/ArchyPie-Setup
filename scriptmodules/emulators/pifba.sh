3e01885e86145a7a60564319c0b8f20d3478aa5b
V0rt3x667 <al-g0l@outlook.com>
2023-01-21 17:15:20 +0000

    Update pifba.sh

--- scriptmodules/emulators/pifba.sh
+++ scriptmodules/emulators/pifba.sh
@@ -5,15 +5,21 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="pifba"
-rp_module_desc="PiFBA - Final Burn Alpha Emulator"
-rp_module_help="ROM Extension: .zip\n\nCopy your FBA roms to\n$romdir/fba or\n$romdir/neogeo or\n$romdir/arcade\n\nFor NeoGeo games the neogeo.zip BIOS is required and must be placed in the same directory as your FBA roms."
+rp_module_desc="PiFBA: Final Burn Alpha Emulator (FB Alpha 0.2.96.71)"
+rp_module_help="ROM Extension: .zip\n\nCopy FBA ROMs To One Of:\n${romdir}/fba\n${romdir}/neogeo\n${romdir}/arcade\n\nFor NeoGeo Copy BIOS File (neogeo.zip) To One Of:\n${romdir}/fba\n${romdir}/neogeo\n${romdir}/arcade"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/RetroPie/pifba/master/FBAcapex_src/COPYING"
-rp_module_repo="git https://github.com/RetroPie/pifba.git master"
+rp_module_repo="git https://github.com/RetroPie/pifba master"
 rp_module_section="opt"
 rp_module_flags="!all rpi"
 
 function depends_pifba() {
-    getDepends ffmpeg sdl raspberrypi-firmware
+    local depends=(
+        'ffmpeg'
+        'raspberrypi-firmware'
+        'sdl12-compat'
+        'sdl2'
+    )
+    getDepends "${depends[@]}"
 }
 
 function sources_pifba() {
@@ -24,42 +30,44 @@ function build_pifba() {
     mkdir ".obj"
     make clean
     make
-    md_ret_require="$md_build/fba2x"
+    md_ret_require="${md_build}/fba2x"
 }
 
 function install_pifba() {
-    mkdir -p "$md_inst/"{roms,skin,preview}
+    mkdir -p "${md_inst}"/{roms,skin,preview}
     md_ret_files=(
-        'fba2x'
-        'fba2x.cfg.template'
         'capex.cfg.template'
-        'zipname.fba'
-        'rominfo.fba'
-        'FBACache_windows.zip'
         'fba_029671_clrmame_dat.zip'
+        'fba2x.cfg.template'
+        'fba2x'
+        'FBACache_windows.zip'
+        'rominfo.fba'
+        'zipname.fba'
     )
 }
 
 function configure_pifba() {
-    mkRomDir "arcade"
-    mkRomDir "fba"
-    mkRomDir "neogeo"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "arcade"
+        mkRomDir "fba"
+        mkRomDir "neogeo"
 
-    if [[ "$md_mode" == "install" ]]; then
-        mkUserDir "$md_conf_root/fba"
+        mkUserDir "${md_conf_root}/fba"
 
-        local config
-        for config in fba2x.cfg capex.cfg; do
-            # move old config if it exists
-            moveConfigFile "$md_inst/$config" "$md_conf_root/fba/$config"
-            copyDefaultConfig "$config.template" "$md_conf_root/fba/$config"
+        local configs=(
+            'capex.cfg'
+            'fba2x.cfg'
+        )
+        for config in "${configs[@]}"; do
+            copyDefaultConfig "${config}.template" "${md_conf_root}/fba/${config}"
         done
     fi
 
-    addEmulator 0 "$md_id" "arcade" "$md_inst/fba2x %ROM%"
-    addEmulator 0 "$md_id" "neogeo" "$md_inst/fba2x %ROM%"
-    addEmulator 0 "$md_id" "fba" "$md_inst/fba2x %ROM%"
+    addEmulator 0 "${md_id}" "arcade" "${md_inst}/fba2x %ROM%"
+    addEmulator 0 "${md_id}" "fba" "${md_inst}/fba2x %ROM%"
+    addEmulator 0 "${md_id}" "neogeo" "${md_inst}/fba2x %ROM%"
+
     addSystem "arcade"
-    addSystem "neogeo"
     addSystem "fba"
+    addSystem "neogeo"
 }
