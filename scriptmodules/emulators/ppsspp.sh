96fe16c3d9f342417146e4a58d3b2579590b5785
V0rt3x667 <al-g0l@outlook.com>
2023-01-21 22:00:47 +0000

    Update ppsspp.sh

--- scriptmodules/emulators/ppsspp.sh
+++ scriptmodules/emulators/ppsspp.sh
@@ -5,15 +5,15 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="ppsspp"
-rp_module_desc="PPSSPP - Sony PlayStation Portable Emulator"
-rp_module_help="ROM Extensions: .iso .pbp .cso\n\nCopy your PlayStation Portable roms to $romdir/psp"
+rp_module_desc="PPSSPP: Sony PlayStation Portable Emulator"
+rp_module_help="ROM Extensions: .cso .iso .pbp\n\nCopy PlayStation Portable ROMs To: ${romdir}/psp"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/hrydgard/ppsspp/master/LICENSE.TXT"
-rp_module_repo="git https://github.com/hrydgard/ppsspp.git :_get_branch_ppsspp"
+rp_module_repo="git https://github.com/hrydgard/ppsspp :_get_branch_ppsspp"
 rp_module_section="opt"
 rp_module_flags=""
 
 function _get_branch_ppsspp() {
-    download https://api.github.com/repos/hrydgard/ppsspp/releases/latest - | grep -m 1 tag_name | cut -d\" -f4
+    download "https://api.github.com/repos/hrydgard/${md_id}/releases/latest" - | grep -m 1 tag_name | cut -d\" -f4
 }
 
 function depends_ppsspp() {
@@ -21,50 +21,63 @@ function depends_ppsspp() {
         'cmake'
         'libzip'
         'ninja'
+        'qt5-base'
+        'qt5-tools'
         'sdl2'
         'snappy'
         'zlib'
     )
-    isPlatform "rpi" && depends+=(raspberrypi-firmware)
-    isPlatform "mesa" && depends+=(libglvnd)
+    isPlatform "rpi" && depends+=('raspberrypi-firmware')
+    isPlatform "mesa" && depends+=('libglvnd')
+    isPlatform "x11" || isPlatform "wayland" && depends+=('spirv-tools')
     getDepends "${depends[@]}"
 }
 
 function sources_ppsspp() {
     gitPullOrClone
+
+    # Set Default Config Path(s)
+    if [[ "${md_id}" == "ppsspp" || "${md_id}" == "ppsspp-1.5.4" ]]; then
+        sed -e "s|\"/.config\"|\"/ArchyPie/configs\"|g" -i "${md_build}/UI/NativeApp.cpp"
+    fi
 }
 
 function build_ppsspp() {
-    local ppsspp_binary="PPSSPPSDL"
-    rm -rf CMakeCache.txt CMakeFiles
+    local binary="PPSSPPSDL"
     local params=()
-    if isPlatform "rpi"; then
-        params+=(-DCMAKE_TOOLCHAIN_FILE=cmake/Toolchains/raspberry.armv7.cmake)
-    elif isPlatform "mesa"; then
-        params+=(-DUSING_GLES2=ON -DUSING_EGL=OFF)
+
+    if isPlatform "mesa"; then
+        params+=('-DUSING_GLES2=ON' '-DUSING_EGL=OFF')
     elif isPlatform "mali"; then
-        params+=(-DUSING_GLES2=ON -DUSING_FBDEV=ON)
-        # remove -DGL_GLEXT_PROTOTYPES on odroid-xu to avoid errors due to header prototype differences
-        params+=(-DCMAKE_C_FLAGS="${CFLAGS/-DGL_GLEXT_PROTOTYPES/}")
-        params+=(-DCMAKE_CXX_FLAGS="${CXXFLAGS/-DGL_GLEXT_PROTOTYPES/}")
+        params+=('-DUSING_GLES2=ON' '-DUSING_FBDEV=ON')
     fi
-    if isPlatform "arm" && ! isPlatform "x11"; then
-        params+=(-DARM_NO_VULKAN=ON)
+
+    isPlatform "x86" && isPlatform "32bit" && params+=('-DX86=ON' '-DX86_64=OFF')
+    isPlatform "x86" && isPlatform "64bit" && params+=('-DX86=OFF' '-DX86_64=ON')
+    isPlatform "wayland" && params+=('-DUSING_X11_VULKAN=OFF' '-DUSE_WAYLAND_WSI=ON')
+
+    if [[ "${md_id}" == "lr-ppsspp" ]]; then
+        params+=('-DLIBRETRO=On')
+        binary="lib/ppsspp_libretro.so"
     fi
-    if [[ "$md_id" == "lr-ppsspp" ]]; then
-        params+=(-DLIBRETRO=On)
-        ppsspp_binary="lib/ppsspp_libretro.so"
-    fi
+
     cmake . \
-        -Bbuild \
-        -GNinja \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$md_inst" \
-        -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON \
+        -B"build" \
+        -G"Ninja" \
+        -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -DBUILD_TESTING="OFF" \
+        -DHEADLESS="ON" \
+        -DUSE_SYSTEM_LIBZIP="ON" \
+        -DUSE_SYSTEM_SNAPPY="ON" \
+        -DUSE_SYSTEM_ZSTD="ON" \
+        -DUSING_QT_UI="OFF" \
         "${params[@]}" \
         -Wno-dev
+    ninja -C build clean
     ninja -C build
-    md_ret_require="$md_build/build/$ppsspp_binary"
+    md_ret_require="${md_build}/build/${binary}"
 }
 
 function install_ppsspp() {
@@ -72,25 +85,27 @@ function install_ppsspp() {
 }
 
 function configure_ppsspp() {
-    local extra_params=()
-    if isPlatform "x11"; then
-        extra_params+=(--fullscreen)
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/psp/${md_id}"
+
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "psp"
+        mkUserDir "${md_conf_root}/psp/${md_id}/PSP"
+        ln -snf "${romdir}/psp" "${md_conf_root}/psp/${md_id}/PSP/GAME"
     fi
 
-    mkRomDir "psp"
-    if [[ "$md_mode" == "install" ]]; then
-        moveConfigDir "$home/.config/ppsspp" "$md_conf_root/psp"
-        mkUserDir "$md_conf_root/psp/PSP"
-        ln -snf "$romdir/psp" "$md_conf_root/psp/PSP/GAME"
+    local params=()
+    if isPlatform "x11" || isPlatform "wayland"; then
+        params+=('--fullscreen')
     fi
 
-    addEmulator 1 "$md_id" "psp" "pushd $md_inst; $md_inst/bin/PPSSPPSDL ${extra_params[*]} %ROM%; popd"
+    addEmulator 1 "${md_id}" "psp" "pushd ${md_inst}; ${md_inst}/bin/PPSSPPSDL ${params[*]} %ROM%; popd"
+
     addSystem "psp"
 
     # if we are removing the last remaining psp emu - remove the symlink
-    if [[ "$md_mode" == "remove" ]]; then
-        if [[ -h "$home/.config/ppsspp" && ! -f "$md_conf_root/psp/emulators.cfg" ]]; then
-            rm -f "$home/.config/ppsspp"
-        fi
-    fi
+    # if [[ "$md_mode" == "remove" ]]; then
+    #     if [[ -h "$home/.config/ppsspp" && ! -f "$md_conf_root/psp/emulators.cfg" ]]; then
+    #         rm -f "$home/.config/ppsspp"
+    #     fi
+    # fi
 }
