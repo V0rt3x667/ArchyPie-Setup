41e007f51ffdbe6b6e5f8506642e6db4f9c61917
V0rt3x667 <al-g0l@outlook.com>
2023-01-21 17:32:27 +0000

    Update ppsspp-1.5.4.sh

--- scriptmodules/emulators/ppsspp-1.5.4.sh
+++ scriptmodules/emulators/ppsspp-1.5.4.sh
@@ -5,10 +5,10 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="ppsspp-1.5.4"
-rp_module_desc="PPSSPP v1.5.4 - Sony PlayStation Portable Emulator"
-rp_module_help="ROM Extensions: .iso .pbp .cso\n\nCopy your PlayStation Portable roms to $romdir/psp"
+rp_module_desc="PPSSPP: Sony PlayStation Portable Emulator (v1.5.4)"
+rp_module_help="ROM Extensions: .cso .iso .pbp\n\nCopy PlayStation Portable ROMs To: ${romdir}/psp"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/hrydgard/ppsspp/master/LICENSE.TXT"
-rp_module_repo="git https://github.com/hrydgard/ppsspp.git v1.5.4"
+rp_module_repo="git https://github.com/hrydgard/ppsspp v1.5.4"
 rp_module_section="opt"
 rp_module_flags="!all rpi"
 
