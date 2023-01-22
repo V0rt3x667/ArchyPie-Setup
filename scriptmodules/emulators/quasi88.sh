4062ffb5aa592b40ebb22e0520126bd9443abb27
V0rt3x667 <al-g0l@outlook.com>
2023-01-22 18:56:16 +0000

    Update quasi88.sh

--- scriptmodules/emulators/quasi88.sh
+++ scriptmodules/emulators/quasi88.sh
@@ -5,46 +5,74 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="quasi88"
-rp_module_desc="QUASI88 - NEC PC-8801 Emulator"
-rp_module_help="ROM Extensions: .d88 .88d .cmt .t88\n\nCopy your pc88 games to to $romdir/pc88\n\nCopy bios files FONT.ROM, N88.ROM, N88KNJ1.ROM, N88KNJ2.ROM, and N88SUB.ROM to $biosdir/pc88"
-rp_module_repo="file $__archive_url/quasi88-0.6.4.tgz"
+rp_module_desc="QUASI88: NEC PC-8801 Emulator"
+rp_module_help="ROM Extensions: .88d .cmt .d88 .t88\n\nCopy PC-88 Games To: ${romdir}/pc88\n\nCopy BIOS Files:\nFONT.ROM\nN88.ROM\nN88KNJ1.ROM\nN88KNJ2.ROM\nN88SUB.ROM\nTo ${biosdir}/pc88"
+rp_module_licence="BSD3 https://raw.githubusercontent.com/winterheart/quasi88/master/LICENSE.txt"
+rp_module_repo="git https://github.com/winterheart/quasi88 :_get_branch_quasi88"
 rp_module_section="exp"
-rp_module_flags="!mali !kms"
+rp_module_flags=""
+
+function _get_branch_quasi88() {
+    download "https://api.github.com/repos/winterheart/${md_id}/releases/latest" - | grep -m 1 tag_name | cut -d\" -f4
+}
 
 function depends_quasi88() {
-    getDepends sdl
+    local depends=(
+        'cmake'
+        'ninja'
+        'sdl2'
+    )
+    getDepends "${depends[@]}"
 }
 
 function sources_quasi88() {
-    downloadAndExtract "$md_repo_url" "$md_build" --strip-components 1
-    applyPatch "$md_data/01_Fix_Build_Flags.patch"
+    gitPullOrClone
+
+    # Set Default Config Path(s)
+    sed -e "s|path(env_home) / \".quasi88\"|path(env_home) / \"ArchyPie/configs/${md_id}\"|g" -i "${md_build}/src/file-op.cpp"
 }
 
 function build_quasi88() {
-    make X11_VERSION= SDL_VERSION=1 clean
-    make \
-        LDFLAGS="${LDFLAGS}" \
-        X11_VERSION= \
-        SDL_VERSION=1 \
-        ARCH=linux \
-        SOUND_SDL=1 \
-        USE_OLD_MAME_SOUND=1 \
-        USE_FMGEN=1 \
-        ROMDIR="$biosdir/pc88" \
-        DISKDIR="$romdir/pc88" \
-        TAPEDIR="$romdir/pc88"
-    md_ret_require="$md_build/quasi88.sdl"
+    local params=()
+
+    isPlatform "x11" && params+=('-DENABLE_X11=ON')
+    
+    cmake . \
+        -B"build" \
+        -G"Ninja" \
+        -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -DENABLE_JOYSTICK="SDL" \
+        "${params[@]}" \
+        -Wno-dev
+    ninja -C build clean
+    ninja -C build
+    md_ret_require="${md_build}/build/${md_id}.sdl"
 }
 
 function install_quasi88() {
-    make X11_VERSION= SDL_VERSION=1 BINDIR="$md_inst" install
+    md_ret_files=(
+        'build/quasi88.sdl'
+        'keyconf.rc'
+        'quasi88.rc'
+    )
 }
 
 function configure_quasi88() {
-    mkRomDir "pc88"
-    moveConfigDir "$home/.quasi88" "$md_conf_root/pc88"
-    mkUserDir "$biosdir/pc88"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/pc88/${md_id}"
 
-    addEmulator 1 "$md_id" "pc88" "$md_inst/quasi88.sdl -f6 IMAGE-NEXT1 -f7 IMAGE-NEXT2 -f8 NOWAIT -f9 ROMAJI -f10 NUMLOCK -fullscreen %ROM%"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "pc88"
+        mkUserDir "${biosdir}/pc88"
+
+        # Symlink ROM, Disk & Tape Directories
+        ln -snf "${biosdir}/pc88" "${md_conf_root}/pc88/${md_id}/rom"
+        ln -snf "${romdir}/pc88" "${md_conf_root}/pc88/${md_id}/disk"
+        ln -snf "${romdir}/pc88" "${md_conf_root}/pc88/${md_id}/tape"
+    fi
+
+    addEmulator 1 "${md_id}" "pc88" "${md_inst}/${md_id}.sdl -f6 IMAGE-NEXT1 -f7 IMAGE-NEXT2 -f8 NOWAIT -f9 ROMAJI -f10 NUMLOCK -fullscreen %ROM%"
+
     addSystem "pc88"
 }
