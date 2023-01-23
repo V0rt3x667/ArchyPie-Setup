25b44a0e7e45289391d28cac35d0c127d01f8b1a
V0rt3x667 <al-g0l@outlook.com>
2023-01-23 18:33:58 +0000

    Update sdltrs.sh

--- scriptmodules/emulators/sdltrs.sh
+++ scriptmodules/emulators/sdltrs.sh
@@ -5,15 +5,15 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="sdltrs"
-rp_module_desc="SDLTRS - Radio Shack TRS-80 Model 1, 3, 4 & 4P Emulator"
-rp_module_help="ROM Extension: .dsk\n\nCopy your TRS-80 games to: $romdir/trs-80\n\nCopy the required BIOS file level2.rom, level3.rom, level4.rom or level4p.rom to $biosdir"
+rp_module_desc="SDLTRS: Radio Shack TRS-80 Model 1, 3, 4 & 4P Emulator"
+rp_module_help="ROM Extension: .dsk\n\nCopy TRS-80 Games To: ${romdir}/trs-80\n\nCopy BIOS Files:\nlevel2.rom\nlevel3.rom\nlevel4.rom\nlevel4p.rom\n\nTo: ${biosdir}/trs-80"
 rp_module_section="exp"
 rp_module_licence="BSD https://gitlab.com/jengun/sdltrs/-/raw/master/LICENSE"
-rp_module_repo="git https://gitlab.com/jengun/sdltrs.git :_get_branch_sdltrs"
+rp_module_repo="git https://gitlab.com/jengun/sdltrs :_get_branch_sdltrs"
 rp_module_flags=""
 
 function _get_branch_sdltrs() {
-    download https://gitlab.com/api/v4/projects/12284576/releases - | grep -m 1 tag_name | cut -d\" -f4
+    download "https://gitlab.com/api/v4/projects/12284576/releases" - | grep -m 1 tag_name | cut -d\" -f4
 }
 
 function depends_sdltrs() {
@@ -26,51 +26,59 @@ function depends_sdltrs() {
 
 function sources_sdltrs() {
     gitPullOrClone
+
+    # Set Default Config Path(s)
+    sed -e "s|/.sdltrs.t8c\"|/ArchyPie/configs/${md_id}/sdltrs.t8c\"|g" -i "${md_build}/src/trs_sdl_interface.c"
 }
 
 function build_sdltrs() {
     cmake . \
-        -Bbuild \
-        -GNinja \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$md_inst" \
-        -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON \
+        -B"build" \
+        -G"Ninja" \
+        -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
         -Wno-dev
+    ninja -C build clean
     ninja -C build
-    md_ret_require="$md_build/build/sdltrs"
+    md_ret_require="${md_build}/build/${md_id}"
 }
 
 function install_sdltrs() {
     ninja -C build install/strip
+    md_ret_require="${md_inst}/bin/${md_id}"
 }
 
 function configure_sdltrs() {
-    local common_args
-    mkRomDir "trs-80"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/trs-80/${md_id}/"
 
-    common_args="-fullscreen -nomousepointer -showled"
-    addEmulator 1 "$md_id-model1" "trs-80" "$md_inst/bin/sdltrs $common_args -m1 -romfile $biosdir/level2.rom -disk0 %ROM%"
-    addEmulator 0 "$md_id-model3" "trs-80" "$md_inst/bin/sdltrs $common_args -m3 -romfile3 $biosdir/level3.rom -disk0 %ROM%"
-    addEmulator 0 "$md_id-model4" "trs-80" "$md_inst/bin/sdltrs $common_args -m4 -romfile3 $biosdir/level4.rom -disk0 %ROM%"
-    addEmulator 0 "$md_id-model4p" "trs-80" "$md_inst/bin/sdltrs $common_args -m4p -romfile4p $biosdir/level4p.rom -disk0 %ROM%"
-    addSystem "trs-80"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "trs-80"
+        mkUserDir "${biosdir}/trs-80"
 
-    [[ "$md_mode" == "remove" ]] && return
+        # Create Default Config File
+        local config
+        config="$(mktemp)"
+        iniConfig "=" "" "${config}"
 
-    # Migrate settings from the previous version
-    if [[ -h "$home/sdltrs.t8c" || -f "$home/sdltrs.t8c" ]]; then
-       mv "$(readlink -f "$home/sdltrs.t8c")" "$home/.sdltrs.t8c"
+        iniSet "statedir"   "${romdir}/trs-80"
+        iniSet "diskdir"    "${romdir}/trs-80"
+        iniSet "cassdir"    "${romdir}/trs-80"
+        iniSet "disksetdir" "${romdir}/trs-80"
+        iniSet "harddir"    "${romdir}/trs-80"
+        copyDefaultConfig "${config}" "${md_conf_root}/trs-80/${md_id}/sdltrs.t8c"
     fi
 
-    local config
-    config="$(mktemp)"
-    iniConfig "=" "" "$config"
-    iniSet "statedir"   "$romdir/trs-80"
-    iniSet "diskdir"    "$romdir/trs-80"
-    iniSet "cassdir"    "$romdir/trs-80"
-    iniSet "disksetdir" "$romdir/trs-80"
-    iniSet "harddir"    "$romdir/trs-80"
-    copyDefaultConfig "$config" "$md_conf_root/trs-80/sdltrs.t8c"
+    local params=(
+        '-fullscreen'
+        '-nomousepointer'
+        '-showled'
+    )
 
-    moveConfigFile "$home/.sdltrs.t8c" "$md_conf_root/trs-80/sdltrs.t8c"
+    addEmulator 1 "${md_id}-model1" "trs-80" "${md_inst}/bin/sdltrs ${params[*]} -m1 -romfile ${biosdir}/trs-80/level2.rom -disk0 %ROM%"
+    addEmulator 0 "${md_id}-model3" "trs-80" "${md_inst}/bin/sdltrs ${params[*]} -m3 -romfile3 ${biosdir}/trs-80/level3.rom -disk0 %ROM%"
+    addEmulator 0 "${md_id}-model4" "trs-80" "${md_inst}/bin/sdltrs ${params[*]} -m4 -romfile3 ${biosdir}/trs-80/level4.rom -disk0 %ROM%"
+    addEmulator 0 "${md_id}-model4p" "trs-80" "${md_inst}/bin/sdltrs ${params[*]} -m4p -romfile4p ${biosdir}/trs-80/level4p.rom -disk0 %ROM%"
+
+    addSystem "trs-80"
 }
