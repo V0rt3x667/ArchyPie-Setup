73bfb4dc508e110faa55209c3debc78af9cb2ab1
V0rt3x667 <al-g0l@outlook.com>
2023-01-23 22:39:03 +0000

    Update snes9x.sh

--- scriptmodules/emulators/snes9x.sh
+++ scriptmodules/emulators/snes9x.sh
@@ -5,57 +5,63 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="snes9x"
-rp_module_desc="SNES9X - Nintendo SNES Emulator"
-rp_module_help="ROM Extensions: .bin .smc .sfc .fig .swc .mgd .zip\n\nCopy your SNES roms to $romdir/snes"
+rp_module_desc="SNES9X: Nintendo SNES Emulator"
+rp_module_help="ROM Extensions: .bin .fig .mgd .sfc .smc .swc .zip\n\nCopy SNES ROMs To: ${romdir}/snes"
 rp_module_licence="NONCOM https://raw.githubusercontent.com/snes9xgit/snes9x/master/LICENSE"
-rp_module_repo="git https://github.com/snes9xgit/snes9x master"
+rp_module_repo="git https://github.com/snes9xgit/snes9x :_get_branch_snes9x"
 rp_module_section="main"
-rp_module_flags="!all x86"
+rp_module_flags=""
+
+function _get_branch_snes9x() {
+    download "https://api.github.com/repos/snes9xgit/${md_id}/releases/latest" - | grep -m 1 tag_name | cut -d\" -f4
+}
 
 function depends_snes9x() {
     local depends=(
         'alsa-lib'
         'boost-libs'
+        'cmake'
         'libpulse'
         'libx11'
         'libxv'
+        'meson'
         'minizip'
+        'ninja'
         'portaudio'
-        'sdl2'
         'sdl2_ttf'
+        'sdl2'
     )
     getDepends "${depends[@]}"
 }
 
 function sources_snes9x() {
     gitPullOrClone
+
+    # Set Default Config Path(s)
+    applyPatch "${md_data}/01_set_default_config_path.patch"
 }
 
 function build_snes9x() {
-    cd unix
-    CXXFLAGS+=" -I/usr/include/glslang"
-    autoconf
-    ./configure \
-        --prefix="$md_inst" \
-        --enable-netplay
-    make clean
-    make
-    md_ret_require="$md_build/unix/snes9x"
+    cd gtk || exit
+
+    meson setup -Dprefix="${md_inst}" -Dbuildtype="release" build
+    meson compile -j"${__jobs}" -C build
+    md_ret_require="${md_build}/gtk/build/${md_id}-gtk"
 }
 
 function install_snes9x() {
-    md_ret_files=(
-        'unix/snes9x'
-        'docs'
-        'LICENSE'
-    )
+    ninja -C gtk/build install
+    md_ret_require="${md_inst}/bin/${md_id}-gtk"
 }
 
 function configure_snes9x() {
-    mkRomDir "snes"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/snes/${md_id}"
 
-    moveConfigDir "$home/.config/snes9x" "$md_conf_root/snes/snes9x"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "snes"
+    fi
 
-    addEmulator 1 "$md_id" "snes" "$md_inst/snes9x -fullscreen %ROM%"
+    addEmulator 1 "${md_id}" "snes" "${md_inst}/bin/${md_id}-gtk %ROM%"
+
     addSystem "snes"
 }
