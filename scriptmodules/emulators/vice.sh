d86d0b8712454540fcbe401e49027bceb8dc463d
V0rt3x667 <al-g0l@outlook.com>
2023-01-24 21:33:44 +0000

    Update vice.sh
    I could not get this to build on my wayland machine.

--- scriptmodules/emulators/vice.sh
+++ scriptmodules/emulators/vice.sh
@@ -5,74 +5,89 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="vice"
-rp_module_desc="VICE - Commodore C64, C64DTV, C128, VIC20, PET, PLUS4 & CBM-II Emulator"
-rp_module_help="ROM Extensions: .crt .d64 .g64 .prg .t64 .tap .x64 .zip .vsf\n\nCopy your Commodore 64 games to $romdir/c64"
+rp_module_desc="VICE: Commodore C64, C64DTV, C128, VIC20, PET, PLUS4 & CBM-II Emulator"
+rp_module_help="ROM Extensions: .crt .d64 .g64 .prg .t64 .tap .vsf .x64 .zip\n\nCopy Commodore 64 Games To: ${romdir}/c64"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/VICE-Team/svn-mirror/main/vice/COPYING"
-rp_module_repo="git https://github.com/VICE-Team/svn-mirror.git :_get_branch_vice"
+rp_module_repo="git https://github.com/VICE-Team/svn-mirror :_get_branch_vice"
 rp_module_section="opt"
-rp_module_flags=""
+rp_module_flags="!wayland xwayland"
 
 function _get_branch_vice() {
-    download https://api.github.com/repos/VICE-Team/svn-mirror/releases/latest - | grep -m 1 tag_name | cut -d\" -f4
+    download "https://api.github.com/repos/VICE-Team/svn-mirror/releases/latest" - | grep -m 1 tag_name | cut -d\" -f4
 }
 
 function depends_vice() {
     local depends=(
         'alsa-lib'
         'dos2unix'
-        'ffmpeg'
+        'ffmpeg4.4'
         'flac'
         'libjpeg'
-        'mpg123'
         'libpcap'
         'libpng'
         'libvorbis'
         'libxaw'
+        'mpg123'
         'pciutils'
         'portaudio'
-        'sdl2'
         'sdl2_image'
+        'sdl2'
         'xa'
     )
-    isPlatform "x11" && depends+=('libpulse')
+    isPlatform "x11" || ! isPlatform "wayland" && depends+=('libpulse')
     getDepends "${depends[@]}"
 }
 
 function sources_vice() {
     gitPullOrClone
+
+    # Set Default Config Path(s)
+    sed -e "s|ARCHDEP_XDG_CONFIG_HOME \".config\"|ARCHDEP_XDG_CONFIG_HOME \"ArchyPie/configs/${md_id}\"|g" -i "${md_build}/${md_id}/src/arch/shared/archdep_defs.h"
+
+    sed -e "s|lib64|lib|g" -i "${md_build}/${md_id}/configure.ac"
 }
 
 function build_vice() {
-    cd "$md_build/vice"
+    cd "${md_build}/${md_id}"
+
     local params=(
-        --disable-pdf-docs
-        --enable-ethernet
-        --enable-external-ffmpeg
-        --enable-sdlui2
-        --enable-x64
-        --with-fastsid
-        --without-oss
+        '--disable-pdf-docs'
+        '--enable-ethernet'
+        '--enable-external-ffmpeg'
+        '--enable-sdlui2'
+        '--enable-x64'
+        '--with-fastsid'
+        '--without-oss'
+        '--libdir=/usr/lib'
     )
-    ! isPlatform "x11" && params+=(--disable-catweasel --without-pulse)
+    ! isPlatform "x11" || ! isPlatform "wayland" && params+=('--disable-catweasel' '--without-pulse')
+    
+    export CFLAGS="${CFLAGS} -w -Wl,--allow-multiple-definition"
+    PKG_CONFIG_PATH="/usr/lib/ffmpeg4.4/pkgconfig"
     ./autogen.sh
-    ./configure --prefix="$md_inst" "${params[@]}"
+    ./configure --prefix="${md_inst}" "${params[@]}"
     make clean
     make
-    md_ret_require="$md_build/vice/src/x64"
+    md_ret_require="${md_build}/${md_id}/src/x64"
 }
 
 function install_vice() {
-    cd "$md_build/vice"
+    cd "${md_build}/${md_id}"
     make install
+    md_ret_require="${md_build}/${md_id}/bin/x64"
 }
 
 function configure_vice() {
-    # get a list of supported extensions
-    local exts="$(getPlatformConfig c64_exts)"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/c64"
 
-    # install the vice start script
-    mkdir -p "$md_inst/bin"
-    cat > "$md_inst/bin/vice.sh" << _EOF_
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "c64"
+
+        # Create Launcher Script
+        local exts
+        exts="$(getPlatformConfig c64_exts)"
+
+        cat > "${md_inst}/bin/${md_id}.sh" << _EOF_
 #!/bin/bash
 
 BIN="\${0%/*}/\$1"
@@ -81,70 +96,64 @@ PARAMS=("\${@:3}")
 
 romdir="\${ROM%/*}"
 ext="\${ROM##*.}"
-source "$rootdir/lib/archivefuncs.sh"
+source "${rootdir}/lib/archivefuncs.sh"
 
-archiveExtract "\$ROM" "$exts"
+archiveExtract "\${ROM}" "${exts}"
 
-# check successful extraction and if we have at least one file
+# Check For Successful Extraction
 if [[ \$? == 0 ]]; then
     ROM="\${arch_files[0]}"
-    romdir="\$arch_dir"
+    romdir="\${arch_dir}"
 fi
 
-"\$BIN" -chdir "\$romdir" "\${PARAMS[@]}" "\$ROM"
+"\${BIN}" -chdir "\${romdir}" "\${PARAMS[@]}" "\${ROM}"
 archiveCleanup
 _EOF_
+        chmod +x "${md_inst}/bin/${md_id}.sh"
 
-    chmod +x "$md_inst/bin/vice.sh"
+        # Create Default Configuration File
+        local config
+        config="$(mktemp)"
+        iniConfig "=" "" "${config}"
 
-    mkRomDir "c64"
+        echo "[C64]" > "${config}"
+        if ! isPlatform "x11" || ! isPlatform "wayland"; then
+            iniSet "Mouse" "1"
+            iniSet "VICIIDoubleSize" "0"
+            iniSet "VICIIDoubleScan" "0"
+            iniSet "VICIIFilter" "0"
+            iniSet "VICIIVideoCache" "0"
+            iniSet "SDLWindowWidth" "384"
+            iniSet "SDLWindowHeight" "272"
+        fi
 
-    addEmulator 1 "$md_id-x64" "c64" "$md_inst/bin/vice.sh x64 %ROM%"
-    addEmulator 0 "$md_id-x64dtv" "c64" "$md_inst/bin/vice.sh x64dtv %ROM%"
-    addEmulator 0 "$md_id-x64sc" "c64" "$md_inst/bin/vice.sh x64sc %ROM%"
-    addEmulator 0 "$md_id-x128" "c64" "$md_inst/bin/vice.sh x128 %ROM%"
-    addEmulator 0 "$md_id-xpet" "c64" "$md_inst/bin/vice.sh xpet %ROM%"
-    addEmulator 0 "$md_id-xplus4" "c64" "$md_inst/bin/vice.sh xplus4 %ROM%"
-    addEmulator 0 "$md_id-xvic" "c64" "$md_inst/bin/vice.sh xvic %ROM%"
-    addEmulator 0 "$md_id-xvic-cart" "c64" "$md_inst/bin/vice.sh xvic %ROM% -cartgeneric"
+        if isPlatform "arm"; then
+            iniSet "SidEngine" "0"
+        fi
+
+        if isPlatform "x11" || isPlatform "kms" || isPlatform "wayland"; then
+            iniSet "VICIIFullscreen" "1"
+        fi
+
+        if ! isPlatform "x11"; then
+            iniDel "SDLBitdepth"
+            iniSet "VICIIDoubleSize" "0"
+            iniSet "VICIIDoubleScan" "0"
+            iniSet "SDLWindowWidth" "384"
+            iniSet "SDLWindowHeight" "272"
+        fi
+        copyDefaultConfig "${config}" "${md_conf_root}/c64/sdl-vicerc"
+        rm "${config}"
+    fi
+
+    addEmulator 1 "${md_id}-x64" "c64" "${md_inst}/bin/${md_id}.sh x64 %ROM%"
+    addEmulator 0 "${md_id}-x64dtv" "c64" "${md_inst}/bin/${md_id}.sh x64dtv %ROM%"
+    addEmulator 0 "${md_id}-x64sc" "c64" "${md_inst}/bin/${md_id}.sh x64sc %ROM%"
+    addEmulator 0 "${md_id}-x128" "c64" "${md_inst}/bin/${md_id}.sh x128 %ROM%"
+    addEmulator 0 "${md_id}-xpet" "c64" "${md_inst}/bin/${md_id}.sh xpet %ROM%"
+    addEmulator 0 "${md_id}-xplus4" "c64" "${md_inst}/bin/${md_id}.sh xplus4 %ROM%"
+    addEmulator 0 "${md_id}-xvic" "c64" "${md_inst}/bin/${md_id}.sh xvic %ROM%"
+    addEmulator 0 "${md_id}-xvic-cart" "c64" "${md_inst}/bin/${md_id}.sh xvic %ROM% -cartgeneric"
+
     addSystem "c64"
-
-    [[ "$md_mode" == "remove" ]] && return
-
-    # copy configs and symlink the old and new config folders to $md_conf_root/c64/
-    moveConfigDir "$home/.vice" "$md_conf_root/c64"
-    moveConfigDir "$home/.config/vice" "$md_conf_root/c64"
-
-    local config="$(mktemp)"
-    echo "[C64]" > "$config"
-    iniConfig "=" "" "$config"
-    if ! isPlatform "x11"; then
-        iniSet "Mouse" "1"
-        iniSet "VICIIDoubleSize" "0"
-        iniSet "VICIIDoubleScan" "0"
-        iniSet "VICIIFilter" "0"
-        iniSet "VICIIVideoCache" "0"
-        iniSet "SDLWindowWidth" "384"
-        iniSet "SDLWindowHeight" "272"
-    fi
-
-    if isPlatform "arm"; then
-        iniSet "SidEngine" "0"
-    fi
-
-    if isPlatform "x11" || isPlatform "kms"; then
-        iniSet "VICIIFullscreen" "1"
-    fi
-
-    copyDefaultConfig "$config" "$md_conf_root/c64/sdl-vicerc"
-    rm "$config"
-
-    if ! isPlatform "x11"; then
-        iniConfig "=" "" "$md_conf_root/c64/sdl-vicerc"
-        iniDel "SDLBitdepth"
-        iniSet "VICIIDoubleSize" "0"
-        iniSet "VICIIDoubleScan" "0"
-        iniSet "SDLWindowWidth" "384"
-        iniSet "SDLWindowHeight" "272"
-    fi
 }
