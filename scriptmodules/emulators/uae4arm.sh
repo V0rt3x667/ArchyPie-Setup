822360840911e2cb860d03714be122b6d861c19e
V0rt3x667 <al-g0l@outlook.com>
2023-01-24 17:50:21 +0000

    Update uae4arm.sh

--- scriptmodules/emulators/uae4arm.sh
+++ scriptmodules/emulators/uae4arm.sh
@@ -5,15 +5,30 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="uae4arm"
-rp_module_desc="UAE4ARM (JIT Support) - Commodore Amiga Emulator"
-rp_module_help="ROM Extension: .adf .ipf\n\nCopy your Amiga games to $romdir/amiga\n\nCopy the required BIOS files\nkick13.rom\nkick20.rom\nkick31.rom\nto $biosdir/amiga"
+rp_module_desc="UAE4ARM: Commodore Amiga Emulator"
+rp_module_help="ROM Extension: .adf .ipf\n\nCopy Amiga Games To: ${romdir}/amiga\n\nCopy BIOS Files\n\nkick13.rom\nkick20.rom\nkick31.rom\n\nTo: ${biosdir}/amiga"
 rp_module_licence="GPL2"
-rp_module_repo="git https://github.com/Chips-fr/uae4arm-rpi.git master"
+rp_module_repo="git https://github.com/Chips-fr/uae4arm-rpi master"
 rp_module_section="opt"
-rp_module_flags="!all dispmanx"
+rp_module_flags="!all rpi"
 
 function depends_uae4arm() {
-    getDepends sdl sdl_mixer sdl_image sdl_gfx sdl_ttf guichan libmpg123 libxml2 flac libmpeg2
+    local depends=(
+        'flac'
+        'guichan'
+        'libmpeg2'
+        'libmpg123'
+        'libpng'
+        'libxml2'
+        'sdl_gfx'
+        'sdl_image'
+        'sdl_mixer'
+        'sdl_ttf'
+        'sdl12-compat'
+        'sdl2'
+        'zlib'
+    )
+    getDepends "${depends[@]}"
 }
 
 function sources_uae4arm() {
@@ -22,9 +37,9 @@ function sources_uae4arm() {
 
 function build_uae4arm() {
     make clean
-    CXXFLAGS="" make PLATFORM=rpi2
-    
-    md_ret_require="$md_build/uae4arm"
+    make CXXFLAGS=""
+
+    md_ret_require="${md_build}/${md_id}"
 }
 
 function install_uae4arm() {
@@ -35,60 +50,74 @@ function install_uae4arm() {
 }
 
 function configure_uae4arm() {
-    addEmulator 1 "uae4arm" "amiga" "$md_inst/uae4arm.sh %ROM%"
-    addEmulator 0 "uae4arm-a500" "amiga" "$md_inst/uae4arm.sh %ROM% -config=conf/rp-a500.uae"
-    addEmulator 0 "uae4arm-a1200" "amiga" "$md_inst/uae4arm.sh %ROM% -config=conf/rp-a1200.uae"
-    addSystem "amiga"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/amiga/${md_id}"
 
-    [[ "$md_mode" == "remove" ]] && return
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "amiga"
 
-    mkRomDir "amiga"
+        mkUserDir "${biosdir}/amiga"
 
-    mkUserDir "$md_conf_root/amiga"
-    mkUserDir "$md_conf_root/amiga/uae4arm"
+        # Symlink Configuration, Savestates & Screenshots Directories
+        local dirs=(
+            'conf'
+            'savestates'
+            'screenshots'
+        )
+        for dir in "${dirs[@]}"; do
+            moveConfigDir "${md_inst}/${dir}" "${md_conf_root}/amiga/${md_id}/${dir}"
+        done
 
-    # move config / save folders to $md_conf_root/amiga/uae4arm
-    local dir
-    for dir in conf savestates screenshots; do
-        moveConfigDir "$md_inst/$dir" "$md_conf_root/amiga/uae4arm/$dir"
-    done
+        # Symlink BIOS Directory
+        moveConfigDir "${md_inst}/kickstarts" "${biosdir}/amiga"
 
-    moveConfigDir "$md_inst/kickstarts" "$biosdir/amiga"
-    chown -R "${user}:${user}" "$biosdir/amiga"
+        # Set Default A500 Configuration File
+        local conf
+        conf="$(mktemp)"
+        iniConfig "=" "" "${conf}"
 
-    local conf="$(mktemp)"
-    iniConfig "=" "" "$conf"
-    iniSet "config_description" "RetroPie A500, 68000, OCS, 512KB Chip + 512KB Slow Fast"
-    iniSet "chipmem_size" "1"
-    iniSet "bogomem_size" "2"
-    iniSet "chipset" "ocs"
-    iniSet "cachesize" "0"
-    iniSet "kickstart_rom_file" "\$(FILE_PATH)/kick13.rom"
-    copyDefaultConfig "$conf" "$md_conf_root/amiga/uae4arm/conf/rp-a500.uae"
-    rm "$conf"
+        iniSet "config_description" "ArchyPie A500, 68000, OCS, 512KB Chip + 512KB Slow Fast"
+        iniSet "chipmem_size" "1"
+        iniSet "bogomem_size" "2"
+        iniSet "chipset" "ocs"
+        iniSet "cachesize" "0"
+        iniSet "kickstart_rom_file" "\$(FILE_PATH)/kick13.rom"
 
-    conf="$(mktemp)"
-    iniConfig "=" "" "$conf"
-    iniSet "config_description" "RetroPie A1200, 68EC020, AGA, 2MB Chip"
-    iniSet "chipmem_size" "4"
-    iniSet "finegrain_cpu_speed" "1024"
-    iniSet "cpu_type" "68ec020"
-    iniSet "cpu_model" "68020"
-    iniSet "chipset" "aga"
-    iniSet "cachesize" "0"
-    iniSet "kickstart_rom_file" "\$(FILE_PATH)/kick31.rom"
-    copyDefaultConfig "$conf" "$md_conf_root/amiga/uae4arm/conf/rp-a1200.uae"
-    rm "$conf"
+        copyDefaultConfig "${conf}" "${md_conf_conf}/amiga/${md_id}/conf/rp-a500.uae"
+        rm "${conf}"
 
-    # copy shared uae4arm/amiberry launch script
-    cp "$md_data/uae4arm.sh" "$md_inst/"
-    chmod a+x "$md_inst/uae4arm.sh"
+        # Set Default A1200 Configuration File
+        conf="$(mktemp)"
+        iniConfig "=" "" "${conf}"
 
-    local script="+Start UAE4Arm.sh"
-    cat > "$romdir/amiga/$script" << _EOF_
+        iniSet "config_description" "ArchyPie A1200, 68EC020, AGA, 2MB Chip"
+        iniSet "chipmem_size" "4"
+        iniSet "finegrain_cpu_speed" "1024"
+        iniSet "cpu_type" "68ec020"
+        iniSet "cpu_model" "68020"
+        iniSet "chipset" "aga"
+        iniSet "cachesize" "0"
+        iniSet "kickstart_rom_file" "\$(FILE_PATH)/kick31.rom"
+
+        copyDefaultConfig "${conf}" "${md_conf_conf}/amiga/${md_id}/conf/rp-a1200.uae"
+        rm "${conf}"
+
+        # Use Shared UAE4ARM/Amiberry Launcher Script
+        cp "${md_data}/${md_id}.sh" "${md_inst}/"
+        chmod a+x "${md_inst}/${md_id}.sh"
+
+        # Create EmulationStation Launcher Script
+        local launcher="+Start UAE4Arm.sh"
+        cat > "${romdir}/amiga/${launcher}" << _EOF_
 #!/bin/bash
-"$md_inst/uae4arm.sh"
+"${md_inst}/${md_id}.sh"
 _EOF_
-    chmod a+x "$romdir/amiga/$script"
-    chown "${user}:${user}" "$romdir/amiga/$script"
+        chmod a+x "${romdir}/amiga/${launcher}"
+        chown "${user}:${user}" "${romdir}/amiga/${script}"
+    fi
+
+    addEmulator 1 "${md_id}" "amiga" "${md_inst}/${md_id}.sh %ROM%"
+    addEmulator 0 "${md_id}-a500" "amiga" "${md_inst}/${md_id}.sh %ROM% -config=conf/rp-a500.uae"
+    addEmulator 0 "${md_id}-a1200" "amiga" "${md_inst}/${md_id}.sh %ROM% -config=conf/rp-a1200.uae"
+
+    addSystem "amiga"
 }
