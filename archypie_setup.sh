2aa9f97f7d8633ba7e813e04e90195a7c57b534a
V0rt3x667 <al-g0l@outlook.com>
2023-01-26 01:28:37 +0000

    Update archypie_setup.sh
    Add fail safe to ensure script is run as ./archypie-setup and not sudo archypie-setup. Sudo is invoked from within the script to preserve envar XDG_SESSION_TYPE.

--- archypie_setup.sh
+++ archypie_setup.sh
@@ -4,13 +4,19 @@
 #
 # Please see the LICENSE file at the top-level directory of this distribution.
 
-if [[ "$(id -u)" -ne 0 ]]; then
-    display="${XDG_SESSION_TYPE}"
-    sudo __XDG_SESSION_TYPE="${display}" -E "$0"
-    exit $?
-fi
+function check_sudo() {
+    if [[ "$(id -u)" -ne 0 ]]; then
+        return
+    else
+        echo "The script must be run as './archypie-setup' and not 'sudo ./archypie-setup'. You will still be prompted to enter your sudo password. This change is required so that the environment variable 'XDG_SESSION_TYPE' can be passed to the script. Thank you."
+        exit 1
+    fi
+}
+
+check_sudo
 
 scriptdir="$(dirname "$0")"
 scriptdir="$(cd "${scriptdir}" && pwd)"
 
-"${scriptdir}/archypie_packages.sh" setup gui
+display="${XDG_SESSION_TYPE}"
+sudo __XDG_SESSION_TYPE="${display}" "${scriptdir}/archypie_packages.sh" setup gui
