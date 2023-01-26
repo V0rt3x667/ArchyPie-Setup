6972a9ae64f943076bb4eec3c80afc8596929e67
V0rt3x667 <al-g0l@outlook.com>
2023-01-26 21:36:28 +0000

    Update lr-puae.sh & lr-puae2021.sh

--- scriptmodules/libretrocores/lr-puae.sh
+++ scriptmodules/libretrocores/lr-puae.sh
@@ -6,21 +6,24 @@
 
 rp_module_id="lr-puae"
 rp_module_desc="Commodore Amiga 500, 500+, 600, 1200, 4000, CDTV & CD32 Libretro Core"
-rp_module_help="ROM Extensions: .adf .ipf .lha .uae\n\nCopy Amiga Games to Directory: $romdir/amiga"
+rp_module_help="ROM Extensions: .7z .adf .adz .ccd .chd .cue .dms .fdi .hdf .hdz .info .ipf .iso .lha .m3u .mds .nrg .slave .uae .zip\n\nCopy Amiga Games To: ${romdir}/amiga\n\nCopy BIOS Files:\n\nkick13.rom\nkick20.rom\nkick31.rom\n\nTo: ${biosdir}/amiga"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/PUAE/master/COPYING"
 rp_module_repo="git https://github.com/libretro/libretro-uae master"
 rp_module_section="opt"
 
 function sources_lr-puae() {
     gitPullOrClone
-    _sources_libcapsimage_fs-uae
+
+    _sources_capsimg
 }
 
 function build_lr-puae() {
-    _build_libcapsimage_fs-uae
-    cd "$md_build"
-    make
-    md_ret_require="$md_build/puae_libretro.so"
+    _build_capsimg
+
+    cd "${md_build}" || exit
+    make clean
+    make LDFLAGS="${LDFLAGS} -Wl,-rpath='${md_inst}'"
+    md_ret_require="${md_build}/puae_libretro.so"
 }
 
 function install_lr-puae() {
@@ -28,13 +31,20 @@ function install_lr-puae() {
         'puae_libretro.so'
         'README.md'
         'sources/uae_data'
-        'capsimg/CAPSImg/capsimg.so'
     )
+    if [[ ! -f "${biosdir}amiga/capsimg.so" ]]; then
+        cp "${md_build}/capsimg/CAPSImg/capsimg.so" "${biosdir}/amiga"
+    fi
 }
 
 function configure_lr-puae() {
     mkRomDir "amiga"
-    defaultRAConfig "amiga"
-    addEmulator 1 "$md_id" "amiga" "$md_inst/puae_libretro.so"
+
+    defaultRAConfig "amiga" "system_directory" "${biosdir}/amiga"
+
+    addEmulator 1 "${md_id}" "amiga" "${md_inst}/puae_libretro.so"
+
     addSystem "amiga"
+    addSystem "cd32"
+    addSystem "cdtv"
 }
--- scriptmodules/libretrocores/lr-puae2021.sh
+++ scriptmodules/libretrocores/lr-puae2021.sh
@@ -6,30 +6,45 @@
 
 rp_module_id="lr-puae2021"
 rp_module_desc="Commodore Amiga 500, 500+, 600, 1200, 4000, CDTV & CD32 Libretro Core (v2.6.1)"
-rp_module_help="ROM Extensions: .adf .ipf .lha .uae\n\nCopy Amiga Games to Directory: $romdir/amiga"
+rp_module_help="ROM Extensions: .7z .adf .adz .ccd .chd .cue .dms .fdi .hdf .hdz .info .ipf .iso .lha .m3u .mds .nrg .slave .uae .zip\n\nCopy Amiga Games To: ${romdir}/amiga\n\nCopy BIOS Files:\n\nkick13.rom\nkick20.rom\nkick31.rom\n\nTo: ${biosdir}/amiga"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/PUAE/master/COPYING"
 rp_module_repo="git https://github.com/libretro/libretro-uae 2.6.1"
 rp_module_section="opt"
 
 function sources_lr-puae2021() {
     gitPullOrClone
+
+    _sources_capsimg
 }
 
 function build_lr-puae2021() {
+    _build_capsimg
+
+    cd "${md_build}" || exit
+    make clean
     make
-    md_ret_require="$md_build/puae2021_libretro.so"
+    md_ret_require="${md_build}/puae2021_libretro.so"
 }
 
 function install_lr-puae2021() {
     md_ret_files=(
         'puae2021_libretro.so'
         'README.md'
+        'sources/uae_data'
     )
+    if [[ ! -f "${biosdir}amiga/capsimg.so" ]]; then
+        cp "${md_build}/capsimg/CAPSImg/capsimg.so" "${biosdir}/amiga"
+    fi
 }
 
 function configure_lr-puae2021() {
     mkRomDir "amiga"
-    defaultRAConfig "amiga"
-    addEmulator 1 "lr-puae2021" "amiga" "$md_inst/puae2021_libretro.so"
+
+    defaultRAConfig "amiga" "system_directory" "${biosdir}/amiga"
+
+    addEmulator 1 "${md_id}" "amiga" "${md_inst}/puae2021_libretro.so"
+
     addSystem "amiga"
+    addSystem "cd32"
+    addSystem "cdtv"
 }
