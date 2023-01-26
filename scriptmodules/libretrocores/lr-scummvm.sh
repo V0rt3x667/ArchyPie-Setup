9b7df7b43ea23d719cb47d8c02edcd6cf707da36
V0rt3x667 <al-g0l@outlook.com>
2023-01-26 18:08:56 +0000

    Update lr-scummvm.sh

--- scriptmodules/libretrocores/lr-scummvm.sh
+++ scriptmodules/libretrocores/lr-scummvm.sh
@@ -6,77 +6,81 @@
 
 rp_module_id="lr-scummvm"
 rp_module_desc="ScummVM Libretro Core"
-rp_module_help="Copy your ScummVM games to $romdir/scummvm\n\nThe name of your game directories must be suffixed with '.svm' for direct launch in EmulationStation."
-rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/scummvm/master/COPYING"
-rp_module_repo="git https://github.com/libretro/scummvm master"
+rp_module_help="Copy ScummVM Games To: ${romdir}/scummvm\n\nGame Directories Must Be Suffixed With '.svm' For Direct Launch In EmulationStation."
+rp_module_licence="GPL3 https://raw.githubusercontent.com/libretro/scummvm/main/LICENSE"
+rp_module_repo="git https://github.com/libretro/scummvm main"
 rp_module_section="exp"
+
+function depends_lr-scummvm() {
+    local depends=('zip')
+    getDepends "${depends[@]}"
+}
 
 function sources_lr-scummvm() {
     gitPullOrClone
 }
 
 function build_lr-scummvm() {
-    make clean -C backends/platform/libretro/build
-
-    local params=(HAVE_MT32EMU=1)
-    local platform
-    isPlatform "arm" && platform+="armv"
-    isPlatform "neon" && platform+="neon"
-    [[ -n "$platform" ]] && params+=(platform="$platform")
-
-    make "${params[@]}" -C backends/platform/libretro/build
-    md_ret_require="$md_build/backends/platform/libretro/build/scummvm_libretro.so"
+    make clean
+    make HAVE_MT32EMU=1
+    make datafiles
+    md_ret_require="${md_build}/scummvm_libretro.so"
 }
 
 function install_lr-scummvm() {
     md_ret_files=(
-        "backends/platform/libretro/build/scummvm_libretro.so"
-        "COPYING"
+        "LICENSE"
+        "scummvm_libretro.so"
+        "scummvm.zip"
     )
 }
 
 function configure_lr-scummvm() {
-    addEmulator 0 "$md_id" "scummvm" "$md_inst/romdir-launcher.sh %ROM%"
-    addSystem "scummvm"
-    [[ "$md_mode" == "remove" ]] && return
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "scummvm"
 
-    # ensure rom dir and system retroconfig
-    mkRomDir "scummvm"
-    defaultRAConfig "scummvm"
+        defaultRAConfig "scummvm" "system_directory" "${biosdir}/scummvm"
 
-    # download and extract auxiliary data (theme, extra)
-    downloadAndExtract "https://github.com/libretro/scummvm/raw/master/backends/platform/libretro/aux-data/scummvm.zip" "$biosdir"
-    chown -R "${user}:${user}" "$biosdir/scummvm"
+        # Unpack Data Files To System Directory
+        runCmd unzip -q "${md_inst}/scummvm.zip" -d "${biosdir}"
+        chown -R ${user}:${user} "${biosdir}/scummvm"
 
-    # basic initial configuration (if config file not found)
-    if [[ ! -f "$biosdir/scummvm.ini" ]]; then
-        echo "[scummvm]" > "$biosdir/scummvm.ini"
-        iniConfig "=" "" "$biosdir/scummvm.ini"
-        iniSet "extrapath" "$biosdir/scummvm/extra"
-        iniSet "themepath" "$biosdir/scummvm/theme"
-        iniSet "soundfont" "$biosdir/scummvm/extra/Roland_SC-55.sf2"
-        iniSet "gui_theme" "scummmodern"
+        # Create Default Configuration File
+        local config
+        config="$(mktemp)"
+        iniConfig " = " "" "${config}"
+        
+        echo "[scummvm]" > "${config}"
+        iniSet "extrapath" "${biosdir}/scummvm/extra"
+        iniSet "themepath" "${biosdir}/scummvm/theme"
+        iniSet "soundfont" "${biosdir}/scummvm/extra/Roland_SC-55.sf2"
+        iniSet "gui_theme" "scummremastered"
         iniSet "subtitles" "true"
         iniSet "multi_midi" "true"
         iniSet "gm_device" "fluidsynth"
-        chown "${user}:${user}" "$biosdir/scummvm.ini"
-    fi
 
-    # enable speed hack core option if running in arm platform
-    isPlatform "arm" && setRetroArchCoreOption "scummvm_speed_hack" "enabled"
+        copyDefaultConfig "${config}" "${biosdir}/scummvm/scummvm.ini"
+        rm "${config}"
 
-    # create retroarch launcher for lr-scummvm with support for rom directories
-    # containing svm files inside (for direct game directory launching in ES)
-    cat > "$md_inst/romdir-launcher.sh" << _EOF_
+        # Enable Speed Hack Core Option For ARM Platform
+        isPlatform "arm" && setRetroArchCoreOption "scummvm_speed_hack" "enabled"
+
+        # Create Retroarch Launcher For 'lr-scummvm'
+        cat > "${md_inst}/romdir-launcher.sh" << _EOF_
 #!/usr/bin/env bash
 ROM=\$1; shift
 SVM_FILES=()
-[[ -d \$ROM ]] && mapfile -t SVM_FILES < <(compgen -G "\$ROM/*.svm")
+[[ -d \${ROM} ]] && mapfile -t SVM_FILES < <(compgen -G "\${ROM}/*.svm")
 [[ \${#SVM_FILES[@]} -eq 1 ]] && ROM=\${SVM_FILES[0]}
-$emudir/retroarch/bin/retroarch \\
-    -L "$md_inst/scummvm_libretro.so" \\
-    --config "$md_conf_root/scummvm/retroarch.cfg" \\
-    "\$ROM" "\$@"
+${emudir}/retroarch/bin/retroarch \\
+    -L "${md_inst}/scummvm_libretro.so" \\
+    --config "${md_conf_root}/scummvm/retroarch.cfg" \\
+    "\${ROM}" "\$@"
 _EOF_
-    chmod +x "$md_inst/romdir-launcher.sh"
+        chmod +x "${md_inst}/romdir-launcher.sh"
+    fi
+
+    addEmulator 0 "${md_id}" "scummvm" "${md_inst}/romdir-launcher.sh %ROM%"
+
+    addSystem "scummvm"
 }
