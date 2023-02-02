fe28e4a80fc025cab8c1e94ce3d71992d9025e82
V0rt3x667 <al-g0l@outlook.com>
2023-02-02 21:19:41 +0000

    Update lr-superflappybirds.sh
    Build with clang 14 as gcc 11 is not available in the Arch Linux ARM repos.

--- scriptmodules/libretrocores/lr-superflappybirds.sh
+++ scriptmodules/libretrocores/lr-superflappybirds.sh
@@ -6,7 +6,6 @@
 
 rp_module_id="lr-superflappybirds"
 rp_module_desc="Super Flappy Birds Libretro Core"
-rp_module_help="https://github.com/IgniparousTempest/libretro-superflappybirds/wiki"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/IgniparousTempest/libretro-superflappybirds/master/LICENSE"
 rp_module_repo="git https://github.com/IgniparousTempest/libretro-superflappybirds master"
 rp_module_section="exp"
@@ -14,7 +13,8 @@ rp_module_section="exp"
 function depends_lr-superflappybirds() {
     local depends=(
         'cmake'
-        'gcc11'
+        'clang14'
+        'openmp'
         'ninja'
     )
     getDepends "${depends[@]}"
@@ -26,13 +26,13 @@ function sources_lr-superflappybirds() {
 
 function build_lr-superflappybirds() {
     cmake . \
-        -Bbuild \
-        -GNinja \
+        -B"build" \
+        -G"Ninja" \
+        -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
         -DCMAKE_BUILD_TYPE="Release" \
         -DCMAKE_INSTALL_PREFIX="${md_inst}" \
-        -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
-        -DCMAKE_C_COMPILER="gcc-11" \
-        -DCMAKE_CXX_COMPILER="g++-11" \
+        -DCMAKE_C_COMPILER="/usr/lib/llvm14/bin/clang" \
+        -DCMAKE_CXX_COMPILER="/usr/lib/llvm14/bin/clang++" \
         -Wno-dev
     ninja -C build clean
     ninja -C build
@@ -41,8 +41,8 @@ function build_lr-superflappybirds() {
 
 function install_lr-superflappybirds() {
     md_ret_files=(
-        'build/superflappybirds_libretro.so'
-        'resources'
+        "build/superflappybirds_libretro.so"
+        "resources"
     )
 }
 
@@ -50,7 +50,9 @@ function configure_lr-superflappybirds() {
     local portname
     portname="superflappybirds"
 
-    [[ "${md_mode}" == "install" ]] && mkUserDir "${biosdir}/${portname}"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkUserDir "${biosdir}/${portname}"
+    fi
 
     setConfigRoot "ports"
 
