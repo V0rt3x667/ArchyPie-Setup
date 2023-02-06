825ceca80e4912a2e4d498b58728c05034b2449c
V0rt3x667 <al-g0l@outlook.com>
2023-02-06 21:33:51 +0000

    Update lr-81.sh

--- scriptmodules/libretrocores/lr-81.sh
+++ scriptmodules/libretrocores/lr-81.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-81"
 rp_module_desc="Sinclair ZX81 Libretro Core"
-rp_module_help="ROM Extensions: .p .tzx .t81\n\nCopy your ZX81 roms to $romdir/zx81"
+rp_module_help="ROM Extensions: .p .t81 .tzx\n\nCopy ZX81 ROMs To: ${romdir}/zx81"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/libretro/81-libretro/master/LICENSE"
 rp_module_repo="git https://github.com/libretro/81-libretro master"
 rp_module_section="exp"
@@ -18,7 +18,7 @@ function sources_lr-81() {
 function build_lr-81() {
     make clean
     make -f Makefile.libretro
-    md_ret_require="$md_build/81_libretro.so"
+    md_ret_require="${md_build}/81_libretro.so"
 }
 
 function install_lr-81() {
@@ -30,8 +30,10 @@ function install_lr-81() {
 
 function configure_lr-81() {
     mkRomDir "zx81"
+
     defaultRAConfig "zx81"
 
-    addEmulator 1 "$md_id" "zx81" "$md_inst/81_libretro.so"
+    addEmulator 1 "${md_id}" "zx81" "${md_inst}/81_libretro.so"
+
     addSystem "zx81"
 }
