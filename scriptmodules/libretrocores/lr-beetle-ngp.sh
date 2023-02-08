d002d8137382c808c3a4360ee811f36ef90b6e0d
V0rt3x667 <al-g0l@outlook.com>
2023-02-08 23:50:26 +0000

    Update lr-beetle-ngp.sh

--- scriptmodules/libretrocores/lr-beetle-ngp.sh
+++ scriptmodules/libretrocores/lr-beetle-ngp.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-beetle-ngp"
 rp_module_desc="Neo Geo Pocket & Pocket Color Libretro Core"
-rp_module_help="ROM Extensions: .ngc .ngp .zip\n\nCopy your Neo Geo Pocket roms to $romdir/ngp\n\nCopy your Neo Geo Pocket Color roms to $romdir/ngpc"
+rp_module_help="ROM Extensions: .ngc .ngp .zip\n\nCopy Neo Geo Pocket ROMs To: ${romdir}/ngp\n\nCopy Neo Geo Pocket Color ROMs To: ${romdir}/ngpc"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/beetle-ngp-libretro/master/COPYING"
 rp_module_repo="git https://github.com/libretro/beetle-ngp-libretro master"
 rp_module_section="main"
@@ -18,22 +18,25 @@ function sources_lr-beetle-ngp() {
 function build_lr-beetle-ngp() {
     make clean
     make
-    md_ret_require="$md_build/mednafen_ngp_libretro.so"
+    md_ret_require="${md_build}/mednafen_ngp_libretro.so"
 }
 
 function install_lr-beetle-ngp() {
-    md_ret_files=(
-        'mednafen_ngp_libretro.so'
-    )
+    md_ret_files=('mednafen_ngp_libretro.so')
 }
 
 function configure_lr-beetle-ngp() {
-    local system
-    for system in ngp ngpc; do
-        mkRomDir "$system"
-        defaultRAConfig "$system"
-        addEmulator 1 "$md_id" "$system" "$md_inst/mednafen_ngp_libretro.so"
-        addSystem "$system"
-    done
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "ngp"
+        mkRomDir "ngpc"
+    fi
 
+    defaultRAConfig "ngp"
+    defaultRAConfig "ngpc"
+
+    addEmulator 1 "${md_id}" "ngp" "${md_inst}/mednafen_ngp_libretro.so"
+    addEmulator 1 "${md_id}" "ngpc" "${md_inst}/mednafen_ngp_libretro.so"
+
+    addSystem "ngp"
+    addSystem "ngpc"
 }
