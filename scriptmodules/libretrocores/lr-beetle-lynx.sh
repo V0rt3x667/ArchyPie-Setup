fbcad0da4def055b4c78819d7d45fca03866b95c
V0rt3x667 <al-g0l@outlook.com>
2023-02-08 23:19:47 +0000

    Update lr-beetle-lynx.sh

--- scriptmodules/libretrocores/lr-beetle-lynx.sh
+++ scriptmodules/libretrocores/lr-beetle-lynx.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-beetle-lynx"
 rp_module_desc="Atari Lynx Libretro Core"
-rp_module_help="ROM Extensions: .lnx .zip\n\nCopy your Atari Lynx roms to $romdir/atarilynx\n\nCopy the required BIOS file lynxboot.img to $biosdir"
+rp_module_help="ROM Extensions: .lnx .zip\n\nCopy Atari Lynx ROMs To: ${romdir}/atarilynx\n\nCopy BIOS File (lynxboot.img) To: ${biosdir}/atarilynx"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/beetle-lynx-libretro/master/COPYING"
 rp_module_repo="git https://github.com/libretro/beetle-lynx-libretro master"
 rp_module_section="opt"
@@ -18,19 +18,23 @@ function sources_lr-beetle-lynx() {
 function build_lr-beetle-lynx() {
     make clean
     make
-    md_ret_require="$md_build/mednafen_lynx_libretro.so"
+    md_ret_require="${md_build}/mednafen_lynx_libretro.so"
 }
 
 function install_lr-beetle-lynx() {
-    md_ret_files=(
-        'mednafen_lynx_libretro.so'
-    )
+    md_ret_files=('mednafen_lynx_libretro.so')
 }
 
 function configure_lr-beetle-lynx() {
-    mkRomDir "atarilynx"
-    defaultRAConfig "atarilynx"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "atarilynx"
 
-    addEmulator 0 "$md_id" "atarilynx" "$md_inst/mednafen_lynx_libretro.so"
+        mkUserDir "${biosdir}/atarilynx"
+    fi
+
+    defaultRAConfig "atarilynx" "system_directory" "${biosdir}/atarilynx"
+
+    addEmulator 0 "${md_id}" "atarilynx" "${md_inst}/mednafen_lynx_libretro.so"
+
     addSystem "atarilynx"
 }
