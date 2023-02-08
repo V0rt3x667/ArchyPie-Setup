fe84dfda5abeedd11e9588d445a5da0c835386a5
V0rt3x667 <al-g0l@outlook.com>
2023-02-08 22:45:14 +0000

    Update lr-atari800.sh

--- scriptmodules/libretrocores/lr-atari800.sh
+++ scriptmodules/libretrocores/lr-atari800.sh
@@ -6,19 +6,22 @@
 
 rp_module_id="lr-atari800"
 rp_module_desc="Atari 5200, 400, 800, XL & XE Libretro Core"
-rp_module_help="ROM Extensions: .a52 .bas .bin .car .xex .atr .xfd .dcm .atr.gz .xfd.gz\n\nCopy your Atari800 games to $romdir/atari800\n\nCopy your Atari 5200 roms to $romdir/atari5200 You need to copy the Atari 800/5200 BIOS files (5200.ROM, ATARIBAS.ROM, ATARIOSB.ROM and ATARIXL.ROM) to the folder $biosdir"
+rp_module_help="ROM Extensions: .a52 .atr .bas .bin .car .cas .cdm .com .dcm .xex .xfd .zip\n\nCopy Atari800 Games To: ${romdir}/atari800\n\nCopy Atari 5200 ROMs To: ${romdir}/atari5200Copy Atari800 Games To: ${romdir}/atari800\n\nCopy Atari 5200 ROMs To: ${romdir}/atari5200\n\nCopy BIOS Files:\n\n5200.ROM\nATARIBAS.ROM\nATARIOSB.ROM\nATARIXL.ROM\n\nTo: ${biosdir}\atari800"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/libretro-atari800/master/atari800/COPYING"
 rp_module_repo="git https://github.com/libretro/libretro-atari800 master"
 rp_module_section="main"
 
 function sources_lr-atari800() {
     gitPullOrClone
+
+    # Set Default Config Path(s)
+    applyPatch "${md_data}/01_set_default_config_path.patch"
 }
 
 function build_lr-atari800() {
     make clean
-    CFLAGS+=" -DDEFAULT_CFG_NAME=\\\".lr-atari800.cfg\\\"" make
-    md_ret_require="$md_build/atari800_libretro.so"
+    make
+    md_ret_require="${md_build}/atari800_libretro.so"
 }
 
 function install_lr-atari800() {
@@ -29,17 +32,21 @@ function install_lr-atari800() {
 }
 
 function configure_lr-atari800() {
-    mkRomDir "atari800"
-    mkRomDir "atari5200"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/atari800/${md_id}"
 
-    defaultRAConfig "atari800"
-    defaultRAConfig "atari5200"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "atari800"
+        mkRomDir "atari5200"
 
-    mkUserDir "$md_conf_root/atari800"
-    moveConfigFile "$home/.lr-atari800.cfg" "$md_conf_root/atari800/lr-atari800.cfg"
+        mkUserDir "${biosdir}/atari800"
+    fi
 
-    addEmulator 1 "lr-atari800" "atari800" "$md_inst/atari800_libretro.so"
-    addEmulator 1 "lr-atari800" "atari5200" "$md_inst/atari800_libretro.so"
+    defaultRAConfig "atari800" "system_directory" "${biosdir}/atari800"
+    defaultRAConfig "atari5200" "system_directory" "${biosdir}/atari800"
+
+    addEmulator 1 "lr-atari800" "atari800" "${md_inst}/atari800_libretro.so"
+    addEmulator 1 "lr-atari800" "atari5200" "${md_inst}/atari800_libretro.so"
+
     addSystem "atari800"
     addSystem "atari5200"
 }
