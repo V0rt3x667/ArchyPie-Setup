a25976f377db97e35cf5cc289517be7e1f88437b
V0rt3x667 <al-g0l@outlook.com>
2023-02-11 22:06:53 +0000

    Update lr-bsnes-mercury.sh

--- scriptmodules/libretrocores/lr-bsnes-mercury.sh
+++ scriptmodules/libretrocores/lr-bsnes-mercury.sh
@@ -6,27 +6,34 @@
 
 rp_module_id="lr-bsnes-mercury"
 rp_module_desc="Nintendo SNES Libretro Core"
-rp_module_help="ROM Extensions: .bin .smc .sfc .fig .swc .mgd .zip\n\nCopy Your SNES ROMs to $romdir/snes"
+rp_module_help="ROM Extensions: .bml .sfc .smc .zip\n\nCopy SNES ROMs To: ${romdir}/snes"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/libretro/bsnes-mercury/master/LICENSE"
 rp_module_repo="git https://github.com/libretro/bsnes-mercury master"
 rp_module_section="opt"
 
 function sources_lr-bsnes-mercury() {
     gitPullOrClone
+
+    # Surpress Undefined Reference Errors When Building The Balanced Core
+    sed -e "s|--no-undefined -Wl,||g" -i "${md_build}/Makefile"
 }
 
 function build_lr-bsnes-mercury() {
-    local profile
-    #surpress undefined reference errors when building the balanced core
-    sed -e "s|--no-undefined -Wl,||g" -i ./Makefile
-    for profile in 'accuracy' 'balanced' 'performance'; do
+    local profiles=(
+        'accuracy'
+        'balanced'
+        'performance'
+    )
+
+    for profile in "${profiles[@]}"; do
         make clean
-        make PROFILE="$profile"
+        make PROFILE="${profile}"
     done
+
     md_ret_require=(
-        "$md_build/bsnes_mercury_accuracy_libretro.so"
-        "$md_build/bsnes_mercury_balanced_libretro.so"
-        "$md_build/bsnes_mercury_performance_libretro.so"
+        "${md_build}/bsnes_mercury_accuracy_libretro.so"
+        "${md_build}/bsnes_mercury_balanced_libretro.so"
+        "${md_build}/bsnes_mercury_performance_libretro.so"
     )
 }
 
@@ -40,11 +47,12 @@ function install_lr-bsnes-mercury() {
 
 function configure_lr-bsnes-mercury() {
     mkRomDir "snes"
+
     defaultRAConfig "snes"
 
-    addEmulator 0 "$md_id" "snes" "$md_inst/bsnes_mercury_accuracy_libretro.so"
-    addEmulator 0 "$md_id" "snes" "$md_inst/bsnes_mercury_balanced_libretro.so"
-    addEmulator 0 "$md_id" "snes" "$md_inst/bsnes_mercury_performance_libretro.so"
+    addEmulator 0 "${md_id}" "snes" "${md_inst}/bsnes_mercury_accuracy_libretro.so"
+    addEmulator 0 "${md_id}" "snes" "${md_inst}/bsnes_mercury_balanced_libretro.so"
+    addEmulator 0 "${md_id}" "snes" "${md_inst}/bsnes_mercury_performance_libretro.so"
 
     addSystem "snes"
 }
