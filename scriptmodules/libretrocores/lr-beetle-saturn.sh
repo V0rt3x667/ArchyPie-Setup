5805a1b79c9742b6d57811086903204ee1d5ca3f
V0rt3x667 <al-g0l@outlook.com>
2023-02-11 19:24:09 +0000

    Update lr-beetle-saturn.sh

--- scriptmodules/libretrocores/lr-beetle-saturn.sh
+++ scriptmodules/libretrocores/lr-beetle-saturn.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-beetle-saturn"
 rp_module_desc="Sega Saturn Libretro Core"
-rp_module_help="ROM Extensions: .chd .cue\n\nCopy your Saturn roms to $romdir/saturn\n\nCopy the required BIOS files sega_101.bin / mpr-17933.bin to $biosdir"
+rp_module_help="ROM Extensions: .ccd .chd .cue .m3u .toc\n\nCopy Saturn ROMs To: ${romdir}/saturn\n\nCopy BIOS Files (sega_101.bin & mpr-17933.bin) To: ${biosdir}/saturn"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/beetle-saturn-libretro/master/COPYING"
 rp_module_repo="git https://github.com/libretro/beetle-saturn-libretro master"
 rp_module_section="exp"
@@ -19,19 +19,23 @@ function sources_lr-beetle-saturn() {
 function build_lr-beetle-saturn() {
     make clean
     make
-    md_ret_require="$md_build/mednafen_saturn_libretro.so"
+    md_ret_require="${md_build}/mednafen_saturn_libretro.so"
 }
 
 function install_lr-beetle-saturn() {
-    md_ret_files=(
-        'mednafen_saturn_libretro.so'
-    )
+    md_ret_files=('mednafen_saturn_libretro.so')
 }
 
 function configure_lr-beetle-saturn() {
-    mkRomDir "saturn"
-    defaultRAConfig "saturn"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "saturn"
 
-    addEmulator 1 "$md_id" "saturn" "$md_inst/mednafen_saturn_libretro.so"
+        mkUserDir "${biosdir}/saturn"
+    fi
+
+    defaultRAConfig "saturn" "system_directory" "${biosdir}/saturn"
+
+    addEmulator 1 "${md_id}" "saturn" "${md_inst}/mednafen_saturn_libretro.so"
+
     addSystem "saturn"
 }
