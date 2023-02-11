ca89be2bd53fb3b92ae855651aefb928f94e95e4
V0rt3x667 <al-g0l@outlook.com>
2023-02-11 21:33:16 +0000

    Update lr-blastem.sh

--- scriptmodules/libretrocores/lr-blastem.sh
+++ scriptmodules/libretrocores/lr-blastem.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-blastem"
 rp_module_desc="Sega Mega Drive (Genesis) Libretro Core"
-rp_module_help="ROM Extensions: .md .bin .smd\n\nCopy Your Sega Mega Drive (Genesis) ROMs to $romdir/megadrive or $romdir/genesis\n\n"
+rp_module_help="ROM Extensions: .68k .bin .gen .md .sgd .smd\n\nCopy Sega Mega Drive (Genesis) ROMs To: ${romdir}/megadrive Or ${romdir}/genesis"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/libretro/blastem/master/COPYING"
 rp_module_repo="git https://github.com/libretro/blastem libretro"
 rp_module_section="opt"
@@ -18,19 +18,19 @@ function sources_lr-blastem() {
 function build_lr-blastem() {
     make clean
     make -f Makefile.libretro
-    md_ret_require="$md_build/blastem_libretro.so"
+    md_ret_require="${md_build}/blastem_libretro.so"
 }
 
 function install_lr-blastem() {
-    md_ret_files=(
-        'blastem_libretro.so'
-    )
+    md_ret_files=('blastem_libretro.so')
 }
 
 function configure_lr-blastem() {
     mkRomDir "megadrive"
+
     defaultRAConfig "megadrive"
 
-    addEmulator 0 "$md_id" "megadrive" "$md_inst/blastem_libretro.so"
+    addEmulator 0 "${md_id}" "megadrive" "${md_inst}/blastem_libretro.so"
+
     addSystem "megadrive"
 }
