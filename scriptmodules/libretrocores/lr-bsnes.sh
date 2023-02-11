a407af3abb1e8ac8a360479da1228d0cf00a6d9d
V0rt3x667 <al-g0l@outlook.com>
2023-02-11 23:12:31 +0000

    Update lr-bsnes.sh

--- scriptmodules/libretrocores/lr-bsnes.sh
+++ scriptmodules/libretrocores/lr-bsnes.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-bsnes"
 rp_module_desc="Super Nintendo Entertainment System Libretro Core"
-rp_module_help="ROM Extensions: .bml .smc .sfc .zip\n\nCopy your SNES roms to $romdir/snes"
+rp_module_help="ROM Extensions: .bml .sfc .smc .zip\n\nCopy SNES ROMs To: ${romdir}/snes"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/libretro/bsnes/master/LICENSE.txt"
 rp_module_repo="git https://github.com/libretro/bsnes master"
 rp_module_section="opt"
@@ -20,23 +20,19 @@ function build_lr-bsnes() {
     local params=(target="libretro" build="release" binary="library")
     make -C bsnes clean "${params[@]}"
     make -C bsnes "${params[@]}"
-    md_ret_require="$md_build/bsnes/out/bsnes_libretro.so"
+    md_ret_require="${md_build}/bsnes/out/bsnes_libretro.so"
 }
 
 function install_lr-bsnes() {
-    md_ret_files=(
-        'bsnes/out/bsnes_libretro.so'
-        'LICENSE.txt'
-        'GPLv3.txt'
-        'CREDITS.md'
-        'README.md'
-    )
+    md_ret_files=('bsnes/out/bsnes_libretro.so')
 }
 
 function configure_lr-bsnes() {
     mkRomDir "snes"
+
     defaultRAConfig "snes"
 
-    addEmulator 1 "$md_id" "snes" "$md_inst/bsnes_libretro.so"
+    addEmulator 1 "${md_id}" "snes" "${md_inst}/bsnes_libretro.so"
+
     addSystem "snes"
 }
