0428e39ca43e3bdfa4c3a375cd20e9a521b021c5
V0rt3x667 <al-g0l@outlook.com>
2023-02-11 15:09:50 +0000

    Update lr-beetle-pce-fast.sh

--- scriptmodules/libretrocores/lr-beetle-pce-fast.sh
+++ scriptmodules/libretrocores/lr-beetle-pce-fast.sh
@@ -6,15 +6,10 @@
 
 rp_module_id="lr-beetle-pce-fast"
 rp_module_desc="NEC PC Engine (TurboGrafx-16) Fast Libretro Core"
-rp_module_help="ROM Extensions: .pce .ccd .cue .zip\n\nCopy your NEC PC Engine (TurboGrafx-16) roms to $romdir/pcengine\n\nCopy the required BIOS file syscard3.pce to $biosdir"
+rp_module_help="ROM Extensions: .ccd .chd .cue .pce .sgx .zip\n\nCopy NEC PC Engine (TurboGrafx-16) ROMs To: ${romdir}/pcengine\n\nCopy BIOS File (syscard3.pce) To: ${biosdir}/pcengine"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/beetle-pce-fast-libretro/master/COPYING"
 rp_module_repo="git https://github.com/libretro/beetle-pce-fast-libretro master"
 rp_module_section="main"
-
-function _update_hook_lr-beetle-pce-fast() {
-    # move from old location and update emulators.cfg
-    renameModule "lr-mednafen-pce-fast" "lr-beetle-pce-fast"
-}
 
 function sources_lr-beetle-pce-fast() {
     gitPullOrClone
@@ -23,7 +18,7 @@ function sources_lr-beetle-pce-fast() {
 function build_lr-beetle-pce-fast() {
     make clean
     make
-    md_ret_require="$md_build/mednafen_pce_fast_libretro.so"
+    md_ret_require="${md_build}/mednafen_pce_fast_libretro.so"
 }
 
 function install_lr-beetle-pce-fast() {
@@ -35,8 +30,10 @@ function install_lr-beetle-pce-fast() {
 
 function configure_lr-beetle-pce-fast() {
     mkRomDir "pcengine"
+
     defaultRAConfig "pcengine"
 
-    addEmulator 1 "$md_id" "pcengine" "$md_inst/mednafen_pce_fast_libretro.so"
+    addEmulator 1 "${md_id}" "pcengine" "${md_inst}/mednafen_pce_fast_libretro.so"
+
     addSystem "pcengine"
 }
