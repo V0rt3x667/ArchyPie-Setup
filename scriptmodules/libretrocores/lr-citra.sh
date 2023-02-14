f932b49c779f34bbb35b36fcddb8b2a3809332ab
V0rt3x667 <al-g0l@outlook.com>
2023-02-14 19:03:55 +0000

    Update lr-citra.sh

--- scriptmodules/libretrocores/lr-citra.sh
+++ scriptmodules/libretrocores/lr-citra.sh
@@ -6,15 +6,14 @@
 
 rp_module_id="lr-citra"
 rp_module_desc="Nintendo 3DS Libretro Core"
-rp_module_help="ROM Extensions: .3ds .3dsx .app .cci .cxi\n\nCopy Your Nintendo 3DS ROMs to $romdir/3ds"
+rp_module_help="ROM Extensions: .3ds .3dsx .app .axf .cci .cxi .elf\n\nCopy 3DS ROMs To: ${romdir}/3ds"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/citra/master/license.txt"
 rp_module_repo="git https://github.com/libretro/citra master"
 rp_module_section="opt"
-rp_module_flags=""
+rp_module_flags="!all 64bit"
 
 function depends_lr-citra() {
     local depends=(
-        'boost'
         'ffmpeg'
         'fmt'
         'sdl2'
@@ -24,12 +23,15 @@ function depends_lr-citra() {
 
 function sources_lr-citra() {
     gitPullOrClone
+
+    # Fix Missing Include
+    sed "/#include <vector>/a #include <limits>/" -i "${md_build}/src/common/ring_buffer.h"
 }
 
 function build_lr-citra() {
     make HAVE_FFMPEG_STATIC=0 clean
     make HAVE_FFMPEG_STATIC=0
-    md_ret_require="$md_build/citra_libretro.so"
+    md_ret_require="${md_build}/citra_libretro.so"
 }
 
 function install_lr-citra() {
@@ -41,6 +43,7 @@ function configure_lr-citra() {
 
     defaultRAConfig "3ds"
 
-    addEmulator 0 "$md_id" "3ds" "$md_inst/citra_libretro.so"
+    addEmulator 0 "${md_id}" "3ds" "${md_inst}/citra_libretro.so"
+
     addSystem "3ds"
 }
