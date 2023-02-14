18e76d394f1b7edcfd298efccc4d2f171a64aef6
V0rt3x667 <al-g0l@outlook.com>
2023-02-14 14:23:42 +0000

    Update lr-caprice32.sh

--- scriptmodules/libretrocores/lr-caprice32.sh
+++ scriptmodules/libretrocores/lr-caprice32.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-caprice32"
 rp_module_desc="Amstrad CPC Libretro Core"
-rp_module_help="ROM Extensions: .cdt .cpc .dsk\n\nCopy your Amstrad CPC games to $romdir/amstradcpc"
+rp_module_help="ROM Extensions: .cdt .cpc .dsk\n\nCopy Amstrad CPC Games To: ${romdir}/amstradcpc"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/libretro-cap32/master/cap32/COPYING.txt"
 rp_module_repo="git https://github.com/libretro/libretro-cap32 master"
 rp_module_section="main"
@@ -18,7 +18,7 @@ function sources_lr-caprice32() {
 function build_lr-caprice32() {
     make clean
     make
-    md_ret_require="$md_build/cap32_libretro.so"
+    md_ret_require="${md_build}/cap32_libretro.so"
 }
 
 function install_lr-caprice32() {
@@ -27,6 +27,7 @@ function install_lr-caprice32() {
 
 function configure_lr-caprice32() {
     mkRomDir "amstradcpc"
+
     defaultRAConfig "amstradcpc"
 
     setRetroArchCoreOption "cap32_autorun" "enabled"
@@ -34,6 +35,7 @@ function configure_lr-caprice32() {
     setRetroArchCoreOption "cap32_Ram" "128"
     setRetroArchCoreOption "cap32_combokey" "y"
 
-    addEmulator 1 "$md_id" "amstradcpc" "$md_inst/cap32_libretro.so"
+    addEmulator 1 "${md_id}" "amstradcpc" "${md_inst}/cap32_libretro.so"
+
     addSystem "amstradcpc"
 }
