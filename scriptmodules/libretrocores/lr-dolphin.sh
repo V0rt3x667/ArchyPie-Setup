a0eff55be6a3848ffdc30a6e473ffc442d3ece64
V0rt3x667 <al-g0l@outlook.com>
2023-02-14 20:44:05 +0000

    Update lr-dolphin.sh

--- scriptmodules/libretrocores/lr-dolphin.sh
+++ scriptmodules/libretrocores/lr-dolphin.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-dolphin"
 rp_module_desc="Nintendo Gamecube & Wii Libretro Core"
-rp_module_help="ROM Extensions: .gcm .iso .wbfs .ciso .gcz\n\nCopy your Gamecube roms to $romdir/gc and Wii roms to $romdir/wii"
+rp_module_help="ROM Extensions: .ciso .dff .dol .elf .gcm .gcz .iso .m3u .rvz .tgc .wad .wbfs .wia\n\nCopy Gamecube ROMs To: ${romdir}/gc\nCopy Wii ROMs To: ${romdir}/wii\n\nOPTIONAL: Copy BIOS File (IPL.bin) To:\n${biosdir}/gc/EUR\n${biosdir}/gc/JAP\n${biosdir}/gc/USA"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/dolphin/master/license.txt"
 rp_module_repo="git https://github.com/libretro/dolphin master"
 rp_module_section="exp"
@@ -22,6 +22,7 @@ function depends_lr-dolphin() {
         'mbedtls'
         'miniupnpc'
         'minizip'
+        'ninja'
         'pugixml'
         'qt5-base'
         'sfml'
@@ -35,18 +36,20 @@ function sources_lr-dolphin() {
 
 function build_lr-dolphin() {
     cmake . \
-        -Bbuild \
-        -GNinja \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$md_inst" \
-        -DCMAKE_BUILD_RPATH_USE_ORIGIN=ON \
-        -DLIBRETRO=ON \
-        -DLIBRETRO_STATIC=1 \
-        -DENABLE_LTO=ON \
+        -B"build" \
+        -G"Ninja" \
+        -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -DENABLE_LTO="OFF" \
+        -DENABLE_QT="OFF" \
+        -DENABLE_TESTS="OFF" \
+        -DLIBRETRO_STATIC="ON" \
+        -DLIBRETRO="ON" \
         -Wno-dev
     ninja -C build clean
     ninja -C build
-    md_ret_require="$md_build/build/dolphin_libretro.so"
+    md_ret_require="${md_build}/build/dolphin_libretro.so"
 }
 
 function install_lr-dolphin() {
@@ -57,19 +60,22 @@ function install_lr-dolphin() {
 }
 
 function configure_lr-dolphin() {
-    mkRomDir "gc"
-    mkRomDir "wii"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "gc"
+        mkRomDir "wii"
 
-    defaultRAConfig "gc"
+        mkUserDir "${biosdir}/gc"
+        mkUserDir "${biosdir}/gc/dolphin-emu"
+
+        cp -r "${md_inst}/Sys" "${biosdir}/dolphin-emu"
+        chown -R "${user}:${user}" "${biosdir}/dolphin-emu"
+    fi
+
+    defaultRAConfig "gc" "system_directory" "${biosdir}/gc"
     defaultRAConfig "wii"
 
-    mkUserDir "$biosdir/dolphin-emu"
-
-    cp -r "$md_inst/Sys" "$biosdir/dolphin-emu"
-    chown -R "${user}:${user}" "$biosdir/dolphin-emu"
-
-    addEmulator 0 "$md_id" "gc" "$md_inst/dolphin_libretro.so"
-    addEmulator 0 "$md_id" "wii" "$md_inst/dolphin_libretro.so"
+    addEmulator 0 "${md_id}" "gc" "${md_inst}/dolphin_libretro.so"
+    addEmulator 0 "${md_id}" "wii" "${md_inst}/dolphin_libretro.so"
 
     addSystem "gc"
     addSystem "wii"
