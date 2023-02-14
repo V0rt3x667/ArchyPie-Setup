4049dca4daf2e2dc85cdcf1488791b1df8b7dccf
V0rt3x667 <al-g0l@outlook.com>
2023-02-14 21:16:22 +0000

    Update lr-dosbox-pure.sh

--- scriptmodules/libretrocores/lr-dosbox-pure.sh
+++ scriptmodules/libretrocores/lr-dosbox-pure.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-dosbox-pure"
 rp_module_desc="DOSBox Pure Libretro Core"
-rp_module_help="ROM Extensions: .bat .com .cue .dosz .exe .ins .ima .img .iso .m3u .m3u8 .vhd .zip\n\nCopy your DOS games to $ROMDIR/pc"
+rp_module_help="ROM Extensions: .bat .com .conf .cue .dosz .exe .ima .img .ins .iso .jrc .m3u .m3u8 .tc .vhd .zip\n\nCopy DOS Games To: ${romdir}/pc"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/dosbox-pure/main/LICENSE"
 rp_module_repo="git https://github.com/libretro/dosbox-pure main"
 rp_module_section="exp"
@@ -19,21 +19,23 @@ function sources_lr-dosbox-pure() {
 function build_lr-dosbox-pure() {
     make clean
     make
-    md_ret_require="$md_build/dosbox_pure_libretro.so"
+    md_ret_require="${md_build}/dosbox_pure_libretro.so"
 }
 
 function install_lr-dosbox-pure() {
     md_ret_files=(
-        'LICENSE'
         'dosbox_pure_libretro.so'
+        'LICENSE'
         'README.md'
     )
 }
 
 function configure_lr-dosbox-pure() {
     mkRomDir "pc"
+
     defaultRAConfig "pc"
 
-    addEmulator 0 "$md_id" "pc" "$md_inst/dosbox_pure_libretro.so"
+    addEmulator 0 "${md_id}" "pc" "${md_inst}/dosbox_pure_libretro.so"
+
     addSystem "pc"
 }
