1ec81770240ba414185ab948eeba1e0857d28f5b
V0rt3x667 <al-g0l@outlook.com>
2023-02-14 19:15:58 +0000

    Update lr-desmume.sh

--- scriptmodules/libretrocores/lr-desmume.sh
+++ scriptmodules/libretrocores/lr-desmume.sh
@@ -6,20 +6,16 @@
 
 rp_module_id="lr-desmume"
 rp_module_desc="Nintendo DS Libretro Core"
-rp_module_help="ROM Extensions: .nds .zip\n\nCopy your Nintendo DS roms to $romdir/nds"
+rp_module_help="ROM Extensions: .nds .zip\n\nCopy Nintendo DS ROMs To: ${romdir}/nds"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/desmume/master/desmume/COPYING"
 rp_module_repo="git https://github.com/libretro/desmume master"
 rp_module_section="exp"
 
-function _params_lr-desmume() {
-    local params=()
-    isPlatform "arm" && params+=("platform=unixarmvhardfloat")
-    isPlatform "aarch64" && params+=("DESMUME_JIT=0")
-    echo "${params[@]}"
-}
-
 function depends_lr-desmume() {
-    local depends=('libpcap' 'mesa')
+    local depends=(
+        'libpcap'
+        'mesa'
+    )
     getDepends "${depends[@]}"
 }
 
@@ -28,9 +24,14 @@ function sources_lr-desmume() {
 }
 
 function build_lr-desmume() {
+    local params=()
+    isPlatform "arm" && params+=("platform=unixarmvhardfloat")
+    isPlatform "aarch64" && params+=("DESMUME_JIT=0")
+
     make -C desmume/src/frontend/libretro clean
-    make -C desmume/src/frontend/libretro -f Makefile.libretro "$(_params_lr-desmume)"
-    md_ret_require="$md_build/desmume/src/frontend/libretro/desmume_libretro.so"
+    make -C desmume/src/frontend/libretro -f Makefile.libretro "${params[@]}"
+
+    md_ret_require="${md_build}/desmume/src/frontend/libretro/desmume_libretro.so"
 }
 
 function install_lr-desmume() {
@@ -39,8 +40,10 @@ function install_lr-desmume() {
 
 function configure_lr-desmume() {
     mkRomDir "nds"
+
     defaultRAConfig "nds"
 
-    addEmulator 0 "$md_id" "nds" "$md_inst/desmume_libretro.so"
+    addEmulator 0 "${md_id}" "nds" "${md_inst}/desmume_libretro.so"
+
     addSystem "nds"
 }
