b1be671fd61d39cdf2b9ee311ceb1a19be18c3da
V0rt3x667 <al-g0l@outlook.com>
2023-02-14 19:15:53 +0000

    Update lr-craft.sh

--- scriptmodules/libretrocores/lr-craft.sh
+++ scriptmodules/libretrocores/lr-craft.sh
@@ -5,8 +5,7 @@
 # Please see the LICENSE.md file at the top-level directory of this distribution.
 
 rp_module_id="lr-craft"
-rp_module_desc="Craft (MineCraft Clone) Libretro Core"
-rp_module_help="ROM Extensions: N/A"
+rp_module_desc="Craft (Minecraft Clone) Libretro Core"
 rp_module_licence="MIT https://raw.githubusercontent.com/libretro/Craft/master/LICENSE.md"
 rp_module_repo="git https://github.com/libretro/craft master"
 rp_module_section="exp"
@@ -23,7 +22,7 @@ function sources_lr-craft() {
 function build_lr-craft() {
     make clean
     make
-    md_ret_require="$md_build/craft_libretro.so"
+    md_ret_require="${md_build}/craft_libretro.so"
 }
 
 function install_lr-craft() {
@@ -36,7 +35,7 @@ function install_lr-craft() {
 function configure_lr-craft() {
     setConfigRoot "ports"
 
-    addPort "$md_id" "craft" "Craft" "$md_inst/craft_libretro.so"
-
     defaultRAConfig "craft"
+
+    addPort "${md_id}" "craft" "Craft" "${md_inst}/craft_libretro.so"
 }
