7db5323a370e83c16b0033cf52a7b67288ec5011
V0rt3x667 <al-g0l@outlook.com>
2023-02-15 16:16:43 +0000

    Update lr-fceumm.sh

--- scriptmodules/libretrocores/lr-fceumm.sh
+++ scriptmodules/libretrocores/lr-fceumm.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-fceumm"
 rp_module_desc="Nintendo NES & Famicom Libretro Core"
-rp_module_help="ROM Extensions: .nes .zip\n\nCopy your NES roms to $romdir/nes\n\nFor the Famicom Disk System copy your roms to $romdir/fds\n\nFor the Famicom Disk System copy the required BIOS file disksys.rom to $biosdir"
+rp_module_help="ROM Extensions: .fds .nes .unf .unif .zip\n\nCopy NES ROMs To: ${romdir}/nes\n\nCopy Famicom Disk System Games To: ${romdir}/fds\n\nCopy Famicom Disk System BIOS File (disksys.rom} To: ${biosdir}/fds"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/libretro-fceumm/master/Copying"
 rp_module_repo="git https://github.com/libretro/libretro-fceumm master"
 rp_module_section="main"
@@ -18,28 +18,28 @@ function sources_lr-fceumm() {
 function build_lr-fceumm() {
     make -f Makefile.libretro clean
     make -f Makefile.libretro
-    md_ret_require="$md_build/fceumm_libretro.so"
+
+    md_ret_require="${md_build}/fceumm_libretro.so"
 }
 
 function install_lr-fceumm() {
-    md_ret_files=(
-        'Authors'
-        'changelog.txt'
-        'Copying'
-        'fceumm_libretro.so'
-        'whatsnew.txt'
-        'zzz_todo.txt'
-    )
+    md_ret_files=('fceumm_libretro.so')
 }
 
 function configure_lr-fceumm() {
-    mkRomDir "nes"
-    mkRomDir "fds"
-    defaultRAConfig "nes"
-    defaultRAConfig "fds"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "fds"
+        mkRomDir "nes"
 
-    addEmulator 1 "$md_id" "nes" "$md_inst/fceumm_libretro.so"
-    addEmulator 0 "$md_id" "fds" "$md_inst/fceumm_libretro.so"
-    addSystem "nes"
+        mkUserDir "${biosdir}/fds"
+    fi
+
+    defaultRAConfig "fds" "system_directory" "${biosdir}/fds"
+    defaultRAConfig "nes"
+
+    addEmulator 0 "${md_id}" "fds" "${md_inst}/fceumm_libretro.so"
+    addEmulator 1 "${md_id}" "nes" "${md_inst}/fceumm_libretro.so"
+
     addSystem "fds"
+    addSystem "nes"
 }
