7813dc95cf9c9eee607ab99ea1b9a465256f9109
V0rt3x667 <al-g0l@outlook.com>
2023-02-15 20:59:29 +0000

    Rename lr-beetle-psx
    Rename lr-beetle-psx to lr-beetle-psx-hw. Clarifies that it is the core with both software and hardware acceleration that is being built.

--- scriptmodules/libretrocores/lr-beetle-psx-hw.sh
+++ scriptmodules/libretrocores/lr-beetle-psx-hw.sh
@@ -4,7 +4,7 @@
 #
 # Please see the LICENSE file at the top-level directory of this distribution.
 
-rp_module_id="lr-beetle-psx"
+rp_module_id="lr-beetle-psx-hw"
 rp_module_desc="Sony PlayStation Libretro Core"
 rp_module_help="ROM Extensions: .ccd .chd .cue .exe .m3u .pbp .toc\n\nCopy PlayStation ROMs To: ${romdir}/psx\n\nCopy BIOS Files:\n\nscph5500.bin\nscph5501.bin\nscph5502.bin\n\nTo: ${biosdir}/psx"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/beetle-psx-libretro/master/COPYING"
@@ -12,7 +12,7 @@ rp_module_repo="git https://github.com/libretro/beetle-psx-libretro master"
 rp_module_section="opt"
 rp_module_flags=""
 
-function depends_lr-beetle-psx() {
+function depends_lr-beetle-psx-hw() {
     local depends=(
         'libglvnd'
         'mesa'
@@ -21,21 +21,21 @@ function depends_lr-beetle-psx() {
     getDepends "${depends[@]}"
 }
 
-function sources_lr-beetle-psx() {
+function sources_lr-beetle-psx-hw() {
     gitPullOrClone
 }
 
-function build_lr-beetle-psx() {
+function build_lr-beetle-psx-hw() {
     make clean
     make HAVE_HW=1
     md_ret_require=('mednafen_psx_hw_libretro.so')
 }
 
-function install_lr-beetle-psx() {
+function install_lr-beetle-psx-hw() {
     md_ret_files=('mednafen_psx_hw_libretro.so')
 }
 
-function configure_lr-beetle-psx() {
+function configure_lr-beetle-psx-hw() {
     if [[ "${md_mode}" == "install" ]]; then
         mkRomDir "psx"
 
