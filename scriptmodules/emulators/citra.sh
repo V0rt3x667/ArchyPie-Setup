800c0f251e80a9c654e212731c878014b1c2f204
V0rt3x667 <al-g0l@outlook.com>
2023-02-15 01:11:38 +0000

    Update citra.sh
    Fix location of the config path.

--- scriptmodules/emulators/citra.sh
+++ scriptmodules/emulators/citra.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="citra"
 rp_module_desc="Citra: Nintendo 3DS Emulator"
-rp_module_help="ROM Extensions: .3ds .3dsx .app .axf .cci .cia .cxi .elf\n\nCopy Nintendo 3DS ROMs To: ${romdir}/3ds\n\nNote: .cia ROMs Will Only Work If A 'aes_keys.txt' File Exists In The 'sysdata' Folder"
+rp_module_help="ROM Extensions: .3ds .3dsx .app .axf .cci .cia .cxi .elf\n\nCopy Nintendo 3DS ROMs To: ${romdir}/3ds\n\nNOTE: .cia ROMs Will Only Work If An 'aes_keys.txt' File Exists In The '${arpdir}/citra/sysdata' Folder"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/citra-emu/citra/master/license.txt"
 rp_module_repo="git https://github.com/citra-emu/citra master"
 rp_module_section="main"
@@ -32,7 +32,7 @@ function sources_citra() {
     gitPullOrClone
 
     # Set Default Config Path(s)
-    #applyPatch "${md_data}/01_set_default_config_path.patch"
+    applyPatch "${md_data}/01_set_default_config_path.patch"
 
 }
 
@@ -44,12 +44,13 @@ function build_citra() {
     fi
 
     cmake . \
-        -Bbuild \
-        -GNinja \
+        -B"build" \
+        -G"Ninja" \
         -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
         -DCMAKE_BUILD_TYPE="Release" \
         -DCMAKE_INSTALL_PREFIX="${md_inst}" \
         -DENABLE_FFMPEG_AUDIO_DECODER="ON" \
+        -DENABLE_LTO="OFF" \
         -DENABLE_WEB_SERVICE="OFF" \
         -DUSE_SYSTEM_BOOST="ON" \
         -DUSE_SYSTEM_SDL2="ON" \
@@ -63,14 +64,14 @@ function install_citra() {
 }
 
 function configure_citra() {
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/3ds/${md_id}"
+
     if [[ "${md_mode}" == "install" ]]; then
         mkRomDir "3ds"
     fi
 
-    #moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/3ds/${md_id}"
-
     addEmulator 1 "${md_id}" "3ds" "${md_inst}/bin/${md_id} -f %ROM%"
-    addEmulator 0 "${md_id}-gui" "3ds" "${md_inst}/bin/${md_id}-qt -f %ROM%"
+    addEmulator 0 "${md_id}-gui" "3ds" "${md_inst}/bin/${md_id}-qt %ROM%"
 
     addSystem "3ds"
 }
