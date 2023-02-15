a7e412f319c412fbdb34a529807f85403bb3aec3
V0rt3x667 <al-g0l@outlook.com>
2023-02-15 23:14:41 +0000

    Update lr-fmsx.sh

--- scriptmodules/libretrocores/lr-fmsx.sh
+++ scriptmodules/libretrocores/lr-fmsx.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-fmsx"
 rp_module_desc="Microsoft MSX, MSX2 & MSX2+ Libretro Core"
-rp_module_help="ROM Extensions: .rom .mx1 .mx2 .col .dsk .zip\n\nCopy the fmsx BIOS files to '$biosdir'\n\nCopy your MSX/MSX2 games to $romdir/msx"
+rp_module_help="ROM Extensions: .cas .dsk .fdi .m3u .mx1 .mx2 .rom .zip\n\nCopy MSX Games To: ${romdir}/msx\nCopy MSX2 Games To: ${romdir}/msx2\nCopy Colecovision Games To: ${romdir}/coleco\n\nCopy BIOS Files (DISK.ROM, MSX.ROM, MSX2.ROM, MSX2EXT.ROM, MSX2P.ROM, MSX2PEXT.ROM) To: ${biosdir}/msx"
 rp_module_licence="NONCOM https://raw.githubusercontent.com/libretro/fmsx-libretro/master/LICENSE"
 rp_module_repo="git https://github.com/libretro/fmsx-libretro master"
 rp_module_section="opt"
@@ -18,30 +18,38 @@ function sources_lr-fmsx() {
 function build_lr-fmsx() {
     make clean
     make
-    md_ret_require="$md_build/fmsx_libretro.so"
+    md_ret_require="${md_build}/fmsx_libretro.so"
 }
 
 function install_lr-fmsx() {
     md_ret_files=(
         'fmsx_libretro.so'
-        'README.md'
         'fMSX/ROMs/CARTS.SHA'
+        'README.md'
     )
 }
 
 function configure_lr-fmsx() {
-    mkRomDir "msx"
-    defaultRAConfig "msx"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "msx"
+        mkRomDir "msx2"
 
-    # default to MSX2+ core
+        mkUserDir "${biosdir}/msx"
+
+        # Copy CARTS.SHA To '${biosdir}'
+        cp "${md_inst}/CARTS.SHA" "${biosdir}/msx"
+        chown "${user}:${user}" "${biosdir}/msx/CARTS.SHA"
+    fi
+
+    defaultRAConfig "msx" "system_directory" "${biosdir}/msx"
+    defaultRAConfig "msx2" "system_directory" "${biosdir}/msx"
+
+    # Default To MSX2+ Core
     setRetroArchCoreOption "fmsx_mode" "MSX2+"
 
-    addEmulator 0 "$md_id" "msx" "$md_inst/fmsx_libretro.so"
+    addEmulator 0 "${md_id}" "msx" "${md_inst}/fmsx_libretro.so"
+    addEmulator 0 "${md_id}" "msx2" "${md_inst}/fmsx_libretro.so"
+
     addSystem "msx"
-
-    [[ "$md_mode" == "remove" ]] && return
-
-    # Copy CARTS.SHA to $biosdir
-    cp "$md_inst/CARTS.SHA" "$biosdir/"
-    chown "${user}:${user}" "$biosdir/CARTS.SHA"
+    addSystem "msx2"
 }
