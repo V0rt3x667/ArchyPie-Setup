6760821a1a35cc0e6c8c0aa49188b3727d972925
V0rt3x667 <al-g0l@outlook.com>
2023-02-18 02:06:17 +0000

    Update lr-freechaf.sh

--- scriptmodules/libretrocores/lr-freechaf.sh
+++ scriptmodules/libretrocores/lr-freechaf.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-freechaf"
 rp_module_desc="Fairchild ChannelF Libretro Core"
-rp_module_help="ROM Extensions: .bin .rom\n\nCopy your ChannelF roms to $romdir/channelf\n\nCopy the required BIOS files sl31245.bin and sl31253.bin or sl90025.bin to $biosdir"
+rp_module_help="ROM Extensions: .bin .rom\n\nCopy ChannelF ROMs To: ${romdir}/channelf\n\nCopy BIOS Files (sl31245.bin & sl31253.bin Or sl90025.bin) To: ${biosdir}/channelf"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/libretro/FreeChaF/master/LICENSE"
 rp_module_repo="git https://github.com/libretro/FreeChaF master"
 rp_module_section="exp"
@@ -18,21 +18,23 @@ function sources_lr-freechaf() {
 function build_lr-freechaf() {
     make clean
     make
-    md_ret_require="$md_build/freechaf_libretro.so"
+    md_ret_require="${md_build}/freechaf_libretro.so"
 }
 
 function install_lr-freechaf() {
-    md_ret_files=(
-        'freechaf_libretro.so'
-        'LICENSE'
-        'README.md'
-    )
+    md_ret_files=('freechaf_libretro.so')
 }
 
 function configure_lr-freechaf() {
-    mkRomDir "channelf"
-    defaultRAConfig "channelf"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "channelf"
 
-    addEmulator 1 "$md_id" "channelf" "$md_inst/freechaf_libretro.so"
+        mkUserDir "${biosdir}/channelf"
+    fi
+
+    defaultRAConfig "channelf" "system_directory" "${biosdir}/channelf"
+
+    addEmulator 1 "${md_id}" "channelf" "${md_inst}/freechaf_libretro.so"
+
     addSystem "channelf"
 }
