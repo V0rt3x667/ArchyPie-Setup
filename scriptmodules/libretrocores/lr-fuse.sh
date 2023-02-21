933ad9876c4a862c99e3206c45d062bce29f8087
V0rt3x667 <al-g0l@outlook.com>
2023-02-21 00:06:38 +0000

    Update lr-fuse.sh

--- scriptmodules/libretrocores/lr-fuse.sh
+++ scriptmodules/libretrocores/lr-fuse.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-fuse"
 rp_module_desc="Sinclair ZX Spectrum Libretro Core"
-rp_module_help="ROM Extensions: .sna .szx .z80 .tap .tzx .gz .udi .mgt .img .trd .scl .dsk .zip\n\nCopy your ZX Spectrum games to $romdir/zxspectrum"
+rp_module_help="ROM Extensions: .dsk .rzx .scl .tap .trd .tzx .z80 .zip\n\nCopy ZX Spectrum Games To: ${romdir}/zxspectrum"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/libretro/fuse-libretro/master/LICENSE"
 rp_module_repo="git https://github.com/libretro/fuse-libretro master"
 rp_module_section="main"
@@ -18,24 +18,23 @@ function sources_lr-fuse() {
 function build_lr-fuse() {
     make -f Makefile.libretro clean
     make -f Makefile.libretro
-    md_ret_require="$md_build/fuse_libretro.so"
+
+    md_ret_require="${md_build}/fuse_libretro.so"
 }
 
 function install_lr-fuse() {
-    md_ret_files=(
-        'fuse_libretro.so'
-        'LICENSE'
-        'README.md'
-    )
+    md_ret_files=('fuse_libretro.so')
 }
 
 function configure_lr-fuse() {
     mkRomDir "zxspectrum"
+
     defaultRAConfig "zxspectrum"
 
-    # default to 128k spectrum
+    # Default To 128k Spectrum
     setRetroArchCoreOption "fuse_machine" "Spectrum 128K"
 
-    addEmulator 1 "$md_id" "zxspectrum" "$md_inst/fuse_libretro.so"
+    addEmulator 1 "${md_id}" "zxspectrum" "${md_inst}/fuse_libretro.so"
+
     addSystem "zxspectrum"
 }
