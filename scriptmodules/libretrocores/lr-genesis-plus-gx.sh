cb440c4ce7396cd6a7496f97867282a8d25d8df9
V0rt3x667 <al-g0l@outlook.com>
2023-02-22 17:14:23 +0000

    Update lr-genesis-plus-gx.sh

--- scriptmodules/libretrocores/lr-genesis-plus-gx.sh
+++ scriptmodules/libretrocores/lr-genesis-plus-gx.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-genesis-plus-gx"
 rp_module_desc="Sega Master System, Game Gear, Mega Drive (Genesis), Sega CD & SG-1000 Libretro Core"
-rp_module_help="ROM Extensions: .bin .cue .gen .gg .iso .md .sg .smd .sms .zip\nCopy your Game Gear roms to $romdir/gamegear\nMasterSystem roms to $romdir/mastersystem\nMegadrive / Genesis roms to $romdir/megadrive\nSG-1000 roms to $romdir/sg-1000\nSegaCD roms to $romdir/segacd\nThe Sega CD requires the BIOS files bios_CD_U.bin and bios_CD_E.bin and bios_CD_J.bin copied to $biosdir"
+rp_module_help="ROM Extensions: .68k .bin .bms .chd .cue .gen .gg .iso .m3u .md .mdx .sg .sgd .smd .sms .zip\nCopy Game Gear ROMs To: ${romdir}/gamegear\nCopy MasterSystem ROMs To: ${romdir}/mastersystem\nCopy Megadrive (Genesis) ROMs To: ${romdir}/megadrive\nCopy SG-1000 ROMs To: ${romdir}/sg-1000\nCopy SegaCD ROMs To: ${romdir}/segacd\nCopy Sega CD BIOS Files (bios_CD_U.bin, bios_CD_E.bin & bios_CD_J.bin) To: ${biosdir}/segacd"
 rp_module_licence="NONCOM https://raw.githubusercontent.com/libretro/Genesis-Plus-GX/master/LICENSE.txt"
 rp_module_repo="git https://github.com/libretro/Genesis-Plus-GX master"
 rp_module_section="main"
@@ -18,25 +18,20 @@ function sources_lr-genesis-plus-gx() {
 function build_lr-genesis-plus-gx() {
     make -f Makefile.libretro clean
     make -f Makefile.libretro
-    md_ret_require="$md_build/genesis_plus_gx_libretro.so"
+    md_ret_require="${md_build}/genesis_plus_gx_libretro.so"
 }
 
 function install_lr-genesis-plus-gx() {
-    md_ret_files=(
-        'genesis_plus_gx_libretro.so'
-        'HISTORY.txt'
-        'LICENSE.txt'
-        'README.md'
-    )
+    md_ret_files=('genesis_plus_gx_libretro.so')
 }
 
 function configure_lr-genesis-plus-gx() {
     local system
-    local def
+
     for system in gamegear mastersystem megadrive sg-1000 segacd; do
-        mkRomDir "$system"
-        defaultRAConfig "$system"
-        addEmulator 1 "$md_id" "$system" "$md_inst/genesis_plus_gx_libretro.so"
-        addSystem "$system"
+        mkRomDir "${system}"
+        defaultRAConfig "${system}"
+        addEmulator 1 "${md_id}" "${system}" "${md_inst}/genesis_plus_gx_libretro.so"
+        addSystem "${system}"
     done
 }
