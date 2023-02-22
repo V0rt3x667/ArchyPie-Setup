5947484e5635eef2da6af74a7cf8e0498ecfcaca
V0rt3x667 <al-g0l@outlook.com>
2023-02-22 03:57:55 +0000

    Update lr-gambatte.sh

--- scriptmodules/libretrocores/lr-gambatte.sh
+++ scriptmodules/libretrocores/lr-gambatte.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-gambatte"
 rp_module_desc="Nintendo Gameboy & GameBoy Color Libretro Core"
-rp_module_help="ROM Extensions: .gb .gbc .zip\n\nCopy your GameBoy roms to $romdir/gb\n\nCopy your GameBoy Color roms to $romdir/gbc"
+rp_module_help="ROM Extensions: .dmg .gb .gbc .zip\n\nCopy GameBoy ROMs To: ${romdir}/gb\n\nCopy GameBoy Color ROMs To: ${romdir}/gbc"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/gambatte-libretro/master/COPYING"
 rp_module_repo="git https://github.com/libretro/gambatte-libretro master"
 rp_module_section="main"
@@ -18,31 +18,34 @@ function sources_lr-gambatte() {
 function build_lr-gambatte() {
     make -f Makefile.libretro clean
     make -f Makefile.libretro
-    md_ret_require="$md_build/gambatte_libretro.so"
+    md_ret_require="${md_build}/gambatte_libretro.so"
 }
 
 function install_lr-gambatte() {
-    md_ret_files=(
-        'COPYING'
-        'changelog'
-        'README.md'
-        'gambatte_libretro.so'
-    )
+    md_ret_files=('gambatte_libretro.so')
 }
 
 function configure_lr-gambatte() {
-    # add default green yellow palette for gameboy classic
-    mkUserDir "$biosdir/palettes"
-    cp "$md_data/default.pal" "$biosdir/palettes/"
-    chown "${user}:${user}" "$biosdir/palettes/default.pal"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "gb"
+        mkRomDir "gbc"
+
+        # Add Default Green Yellow Palette For Gameboy Classic
+        mkUserDir "${biosdir}/gb"
+        mkUserDir "${biosdir}/gb/palettes"
+
+        cp "${md_data}/default.pal" "${biosdir}/gb/palettes/"
+        chown "${user}:${user}" "${biosdir}/gb/palettes/default.pal"
+    fi
+
     setRetroArchCoreOption "gambatte_gb_colorization" "custom"
 
-    mkRomDir "gbc"
-    mkRomDir "gb"
-    defaultRAConfig "gb"
+    defaultRAConfig "gb" "system_directory" "${biosdir}/gb"
     defaultRAConfig "gbc"
-    addEmulator 1 "$md_id" "gb" "$md_inst/gambatte_libretro.so"
-    addEmulator 1 "$md_id" "gbc" "$md_inst/gambatte_libretro.so"
+
+    addEmulator 1 "${md_id}" "gb" "${md_inst}/gambatte_libretro.so"
+    addEmulator 1 "${md_id}" "gbc" "${md_inst}/gambatte_libretro.so"
+
     addSystem "gb"
     addSystem "gbc"
 }
