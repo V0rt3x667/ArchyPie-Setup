bb862304d9a5e9988a4e81908b7c38a5a7048834
V0rt3x667 <al-g0l@outlook.com>
2023-02-22 17:21:20 +0000

    Update lr-gw.sh

--- scriptmodules/libretrocores/lr-gw.sh
+++ scriptmodules/libretrocores/lr-gw.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-gw"
 rp_module_desc="Nintendo Game & Watch Libretro Core"
-rp_module_help="ROM Extension: .mgw\n\nCopy your Game and Watch games to $romdir/gameandwatch"
+rp_module_help="ROM Extension: .mgw\n\nCopy Game and Watch ROMs To: ${romdir}/gameandwatch"
 rp_module_licence="ZLIB https://raw.githubusercontent.com/libretro/gw-libretro/master/LICENSE"
 rp_module_repo="git https://github.com/libretro/gw-libretro master"
 rp_module_section="opt"
@@ -18,21 +18,22 @@ function sources_lr-gw() {
 function build_lr-gw() {
     make -f Makefile.libretro clean
     make -f Makefile.libretro
-    md_ret_require="$md_build/gw_libretro.so"
+    md_ret_require="${md_build}/gw_libretro.so"
 }
 
 function install_lr-gw() {
     md_ret_files=(
         'gw_libretro.so'
         'LICENSE'
-        'README.md'
     )
 }
 
 function configure_lr-gw() {
     mkRomDir "gameandwatch"
+
     defaultRAConfig "gameandwatch"
 
-    addEmulator 1 "$md_id" "gameandwatch" "$md_inst/gw_libretro.so"
+    addEmulator 1 "${md_id}" "gameandwatch" "${md_inst}/gw_libretro.so"
+
     addSystem "gameandwatch"
 }
