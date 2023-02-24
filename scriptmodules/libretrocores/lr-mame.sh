d7dd2363b8001abafd916cb1cedc74459af88c1d
V0rt3x667 <al-g0l@outlook.com>
2023-02-24 02:04:12 +0000

    Update lr-mame.sh

--- scriptmodules/libretrocores/lr-mame.sh
+++ scriptmodules/libretrocores/lr-mame.sh
@@ -6,14 +6,14 @@
 
 rp_module_id="lr-mame"
 rp_module_desc="MAME (Latest Version) Libretro Core"
-rp_module_help="ROM Extension: .zip\n\nCopy your MAME roms to either $romdir/mame-libretro or\n$romdir/arcade"
+rp_module_help="ROM Extension: .zip\n\nCopy MAME ROMs To: ${romdir}/mame-libretro\nOr\n${romdir}/arcade"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/mame/master/COPYING"
 rp_module_repo="git https://github.com/libretro/mame master"
 rp_module_section="exp"
 rp_module_flags=""
 
 function _get_params_lr-mame() {
-    local params=(OSD=retro RETRO=1 NOWERROR=1 OS=linux TARGETOS=linux CONFIG=libretro NO_USE_MIDI=1 TARGET=mame PYTHON_EXECUTABLE=python)
+    local params=(CONFIG=libretro NO_USE_MIDI=1 NOWERROR=1 OS=linux OSD=retro PYTHON_EXECUTABLE=python RETRO=1 TARGET=mame TARGETOS=linux)
     isPlatform "64bit" && params+=('PTR64=1')
     echo "${params[@]}"
 }
@@ -30,7 +30,7 @@ function sources_lr-mame() {
 }
 
 function build_lr-mame() {
-    # More memory is required for 64bit platforms
+    # More Memory Is Required For 64bit Platforms
     if isPlatform "64bit"; then
         rpSwap on 8192
     else
@@ -41,23 +41,19 @@ function build_lr-mame() {
     make clean
     make "${params[@]}"
     rpSwap off
-    md_ret_require="$md_build/mamearcade_libretro.so"
+    md_ret_require="${md_build}/mamearcade_libretro.so"
 }
 
 function install_lr-mame() {
-    md_ret_files=(
-        'COPYING'
-        'mamearcade_libretro.so'
-        'README.md'
-    )
+    md_ret_files=('mamearcade_libretro.so')
 }
 
 function configure_lr-mame() {
     local system
     for system in arcade mame-libretro; do
-        mkRomDir "$system"
-        defaultRAConfig "$system"
-        addEmulator 0 "$md_id" "$system" "$md_inst/mamearcade_libretro.so"
-        addSystem "$system"
+        mkRomDir "${system}"
+        defaultRAConfig "${system}"
+        addEmulator 0 "${md_id}" "${system}" "${md_inst}/mamearcade_libretro.so"
+        addSystem "${system}"
     done
 }
