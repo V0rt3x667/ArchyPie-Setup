feed20183b4ad9bf3cdfa535079f869fd201afb1
V0rt3x667 <al-g0l@outlook.com>
2023-02-25 19:12:51 +0000

    Update lr-mame2003-plus.sh

--- scriptmodules/libretrocores/lr-mame2003-plus.sh
+++ scriptmodules/libretrocores/lr-mame2003-plus.sh
@@ -6,31 +6,63 @@
 
 rp_module_id="lr-mame2003-plus"
 rp_module_desc="MAME 0.78 Enhanced Libretro Core"
-rp_module_help="ROM Extension: .zip\n\nCopy your MAME roms to either $romdir/mame-libretro or\n$romdir/arcade"
+rp_module_help="ROM Extension: .zip\n\nCopy MAME ROMs To:\n${romdir}/mame-libretro\nOr\n${romdir}/arcade"
 rp_module_licence="NONCOM https://raw.githubusercontent.com/libretro/mame2003-plus-libretro/master/LICENSE.md"
 rp_module_repo="git https://github.com/libretro/mame2003-plus-libretro master"
 rp_module_section="opt"
-
-function _get_dir_name_lr-mame2003-plus() {
-    echo "mame2003-plus"
-}
-
-function _get_so_name_lr-mame2003-plus() {
-    echo "mame2003_plus"
-}
 
 function sources_lr-mame2003-plus() {
     gitPullOrClone
 }
 
 function build_lr-mame2003-plus() {
-    build_lr-mame2003
+    rpSwap on 750
+    make clean
+    local params=()
+    isPlatform "arm" && params+=("ARM=1")
+    make ARCH="${CFLAGS}" "${params[@]}"
+    rpSwap off
+    md_ret_require="${md_build}/mame2003_plus_libretro.so"
 }
 
 function install_lr-mame2003-plus() {
-    install_lr-mame2003
+    md_ret_files=(
+        'mame2003_plus_libretro.so'
+        'metadata'
+    )
 }
 
 function configure_lr-mame2003-plus() {
-    configure_lr-mame2003
+    if [[ "${md_mode}" == "install" ]]; then
+        local core_name="mame2003-plus"
+        local mame_dir=('arcade' 'mame-libretro')
+        local mame_sub_dir=('cfg' 'ctrlr' 'diff' 'hi' 'memcard' 'nvram')
+        for dir in "${mame_dir[@]}"; do
+            mkRomDir "${dir}"
+            mkRomDir "${dir}/${core_name}"
+            for sub_dir in "${mame_sub_dir[@]}"; do
+            mkRomDir "${dir}/${core_name}/${sub_dir}"
+            done
+        done
+
+        mkUserDir "${biosdir}/${core_name}"
+        mkUserDir "${biosdir}/${core_name}/samples"
+        mkUserDir "${biosdir}/${core_name}/artwork"
+
+        # Copy hiscore.dat, cheat.dat & artwork
+        cp -rv "${md_inst}/metadata/"{cheat.dat,hiscore.dat} "${biosdir}/${core_name}/"
+        cp "${md_inst}/metadata/artwork/"* "${biosdir}/${core_name}/artwork/"
+        chown -R "${user}:${user}" "${biosdir}/${core_name}"
+    fi
+
+    setRetroArchCoreOption "${core_name}_skip_disclaimer" "enabled"
+
+    defaultRAConfig "arcade"
+    defaultRAConfig "mame-libretro"
+
+    addEmulator 0 "${md_id}" "arcade" "${md_inst}/mame2003_plus_libretro.so"
+    addEmulator 1 "${md_id}" "mame-libretro" "${md_inst}/mame2003_plus_libretro.so"
+
+    addSystem "arcade"
+    addSystem "mame-libretro"
 }
