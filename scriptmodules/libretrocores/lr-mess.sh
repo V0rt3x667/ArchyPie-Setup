d8d4ccf910effad89f80aee2f6776d716ba129a0
V0rt3x667 <al-g0l@outlook.com>
2023-02-26 15:52:32 +0000

    Update lr-mess.sh

--- scriptmodules/libretrocores/lr-mess.sh
+++ scriptmodules/libretrocores/lr-mess.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-mess"
 rp_module_desc="MESS (Latest Version) Libretro Core"
-rp_module_help="See wiki for detailed explanation"
+rp_module_help="ROM Extension: .zip"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/mame/master/COPYING"
 rp_module_repo="git https://github.com/libretro/mame master"
 rp_module_section="exp"
@@ -21,7 +21,7 @@ function sources_lr-mess() {
 }
 
 function build_lr-mess() {
-    # More memory is required for 64bit platforms
+    # More Memory Is Required For 64bit Platforms
     if isPlatform "64bit"; then
         rpSwap on 8192
     else
@@ -32,37 +32,36 @@ function build_lr-mess() {
     make clean
     make "${params[@]}"
     rpSwap off
-    md_ret_require="$md_build/mamemess_libretro.so"
+    md_ret_require="${md_build}/mamemess_libretro.so"
 }
 
 function install_lr-mess() {
     md_ret_files=(
-        'COPYING'
         'mamemess_libretro.so'
-        'README.md'
         'hash'
     )
 }
 
 function configure_lr-mess() {
     local module="$1"
-    [[ -z "$module" ]] && module="mamemess_libretro.so"
+    [[ -z "${module}" ]] && module="mamemess_libretro.so"
+
+    if [[ "${md_mode}" == "install" ]]; then
+        mkUserDir "${biosdir}/mame"
+        
+        cp -rv "${md_inst}/hash" "${biosdir}/mame/"
+        chown -R "${user}:${user}" "${biosdir}/mame"
+    fi
 
     local system
-    for system in nes gb coleco arcadia crvision; do
-        mkRomDir "$system"
-        defaultRAConfig "$system"
-        addEmulator 0 "$md_id" "$system" "$md_inst/$module"
-        addSystem "$system"
+    for system in 'arcadia' 'coleco' 'crvision' 'gb' 'nes'; do
+        mkRomDir "${system}"
+        defaultRAConfig "${system}"
+        addEmulator 0 "${md_id}" "${system}" "${md_inst}/${module}"
+        addSystem "${system}"
     done
-
-    [[ "$md_mode" == "remove" ]] && return
 
     setRetroArchCoreOption "mame_softlists_enable" "enabled"
     setRetroArchCoreOption "mame_softlists_auto_media" "enabled"
     setRetroArchCoreOption "mame_boot_from_cli" "enabled"
-
-    mkUserDir "$biosdir/mame"
-    cp -rv "$md_inst/hash" "$biosdir/mame/"
-    chown -R "${user}:${user}" "$biosdir/mame"
 }
