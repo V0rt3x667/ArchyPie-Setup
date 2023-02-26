b6693a584c6a0918fa243286ba3cf2182521f547
V0rt3x667 <al-g0l@outlook.com>
2023-02-26 16:38:24 +0000

    Update lr-mgba.sh

--- scriptmodules/libretrocores/lr-mgba.sh
+++ scriptmodules/libretrocores/lr-mgba.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-mgba"
 rp_module_desc="Nintendo Game Boy, Game Boy Advance & Game Boy Color Libretro Core"
-rp_module_help="ROM Extensions: .gb .gbc .gba .zip\n\nCopy your Game Boy roms to $romdir/gb\nGame Boy Color roms to $romdir/gbc\nGame Boy Advance roms to $romdir/gba\n\nCopy the recommended BIOS files gb_bios.bin, gbc_bios.bin, sgb_bios.bin and gba_bios.bin to $biosdir"
+rp_module_help="ROM Extensions: .gb .gba .gbc .zip\n\nCopy Game Boy ROMs To: ${romdir}/gb\nCopy Game Boy Color ROMs To: ${romdir}/gbc\nCopy Game Boy Advance ROMs To: ${romdir}/gba\n\nOPTIONAL:\nCopy BIOS File (gb_bios.bin) To: ${biosdir}/gb\nCopy BIOS File (gba_bios.bin) To: ${biosdir}/gba\nCopy BIOS File (gbc_bios.bin) To: ${biosdir}/gbc"
 rp_module_licence="MPL2 https://raw.githubusercontent.com/libretro/mgba/master/LICENSE"
 rp_module_repo="git https://github.com/libretro/mgba master"
 rp_module_section="main"
@@ -19,27 +19,32 @@ function sources_lr-mgba() {
 function build_lr-mgba() {
     make -f Makefile.libretro clean
     make -f Makefile.libretro
-    md_ret_require="$md_build/mgba_libretro.so"
+    md_ret_require="${md_build}/mgba_libretro.so"
 }
 
 function install_lr-mgba() {
     md_ret_files=(
-        'mgba_libretro.so'
-        'CHANGES'
         'LICENSE'
-        'README.md'
+        'mgba_libretro.so'
     )
 }
 
 function configure_lr-mgba() {
     local system
     local def
-    for system in gb gbc gba; do
+
+    if [[ "${md_mode}" == "install" ]]; then
+        for system in 'gb' 'gba' 'gbc'; do
+            mkRomDir "${system}"
+            mkUserDir "${biosdir}/${system}"
+        done
+    fi
+
+    for system in 'gb' 'gba' 'gbc'; do
         def=0
-        [[ "$system" == "gba" ]] && def=1
-        mkRomDir "$system"
-        defaultRAConfig "$system"
-        addEmulator "$def" "$md_id" "$system" "$md_inst/mgba_libretro.so"
-        addSystem "$system"
+        [[ "${system}" == "gba" ]] && def=1
+        defaultRAConfig "${system}" "system_directory" "${biosdir}/${system}"
+        addEmulator "${def}" "${md_id}" "${system}" "${md_inst}/mgba_libretro.so"
+        addSystem "${system}"
     done
 }
