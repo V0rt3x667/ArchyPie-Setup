5e0ac9f36047534a996ceeca0603fbaea5875a4d
V0rt3x667 <al-g0l@outlook.com>
2023-02-26 14:34:54 +0000

    Update lr-mame2015.sh

--- scriptmodules/libretrocores/lr-mame2015.sh
+++ scriptmodules/libretrocores/lr-mame2015.sh
@@ -6,15 +6,10 @@
 
 rp_module_id="lr-mame2015"
 rp_module_desc="MAME 0.160 Libretro Core"
-rp_module_help="ROM Extension: .zip\n\nCopy your MAME roms to either $romdir/mame-libretro or\n$romdir/arcade"
+rp_module_help="ROM Extension: .zip\n\nCopy MAME ROMs To: ${romdir}/mame-libretro\nOr\n${romdir}/arcade"
 rp_module_licence="NONCOM https://raw.githubusercontent.com/libretro/mame2015-libretro/master/docs/license.txt"
 rp_module_repo="git https://github.com/libretro/mame2015-libretro master"
 rp_module_section="exp"
-
-function depends_lr-mame2015() {
-    local depends=('python2')
-    getDepends "${depends[@]}"
-}
 
 function sources_lr-mame2015() {
     gitPullOrClone
@@ -25,23 +20,19 @@ function build_lr-mame2015() {
     make clean
     make
     rpSwap off
-    md_ret_require="$md_build/mame2015_libretro.so"
+    md_ret_require="${md_build}/mame2015_libretro.so"
 }
 
 function install_lr-mame2015() {
-    md_ret_files=(
-        'mame2015_libretro.so'
-        'docs/README-original.md'
-        'docs/license.txt'
-    )
+    md_ret_files=('mame2015_libretro.so')
 }
 
 function configure_lr-mame2015() {
     local system
     for system in arcade mame-libretro; do
-        mkRomDir "$system"
-        defaultRAConfig "$system"
-        addEmulator 0 "$md_id" "$system" "$md_inst/mame2015_libretro.so"
-        addSystem "$system"
+        mkRomDir "${system}"
+        defaultRAConfig "${system}"
+        addEmulator 0 "${md_id}" "${system}" "${md_inst}/mame2015_libretro.so"
+        addSystem "${system}"
     done
 }
