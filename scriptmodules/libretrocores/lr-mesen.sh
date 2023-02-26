7e7ff8d69d5fe8fd6c33461e32bb90d0dfedc62f
V0rt3x667 <al-g0l@outlook.com>
2023-02-26 15:34:08 +0000

    Update lr-mesen.sh

--- scriptmodules/libretrocores/lr-mesen.sh
+++ scriptmodules/libretrocores/lr-mesen.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-mesen"
 rp_module_desc="Nintendo NES (Famicom) & Famicom Disk System Libretro Core"
-rp_module_help="ROM Extensions: .nes .fds .unf .unif .zip\n\nCopy your NES roms to $romdir/nes\nFamicom roms to $romdir/fds\nCopy the recommended BIOS file disksys.rom to $biosdir"
+rp_module_help="ROM Extensions: .fds .nes .unf .unif .zip\n\nCopy NES ROMs To: ${romdir}/nes\nCopy Famicom ROMs To: ${romdir}/fds\nCopy BIOS File (disksys.rom) To: ${biosdir}/fds"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/sourmesen/mesen/master/LICENSE"
 rp_module_repo="git https://github.com/libretro/Mesen master"
 rp_module_section="exp"
@@ -19,23 +19,21 @@ function sources_lr-mesen() {
 function build_lr-mesen() {
     make -C Libretro clean
     make -C Libretro
-    md_ret_require="$md_build/Libretro/mesen_libretro.so"
+    md_ret_require="${md_build}/Libretro/mesen_libretro.so"
 }
 
 function install_lr-mesen() {
-    md_ret_files=(
-        'Libretro/mesen_libretro.so'
-        'LICENSE'
-        'README.md'
-    )
+    md_ret_files=('Libretro/mesen_libretro.so')
 }
 
 function configure_lr-mesen() {
     local system
-    for system in "nes" "fds"; do
-        mkRomDir "$system"
-        defaultRAConfig "$system"
-        addEmulator 0 "$md_id" "$system" "$md_inst/mesen_libretro.so"
-        addSystem "$system"
+    for system in "fds" "nes"; do
+        mkRomDir "${system}"
+        addEmulator 0 "${md_id}" "${system}" "${md_inst}/mesen_libretro.so"
+        addSystem "${system}"
     done
+
+    defaultRAConfig "fds" "system_directory" "${biosdir}/fds"
+    defaultRAConfig "nes"
 }
