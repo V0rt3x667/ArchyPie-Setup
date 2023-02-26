24f5b1072bce9b6cf9d72aedbb870618ee6ace0c
V0rt3x667 <al-g0l@outlook.com>
2023-02-26 22:34:51 +0000

    Update lr-mupen64plus-next.sh

--- scriptmodules/libretrocores/lr-mupen64plus-next.sh
+++ scriptmodules/libretrocores/lr-mupen64plus-next.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-mupen64plus-next"
 rp_module_desc="Nintendo N64 Libretro Core"
-rp_module_help="ROM Extensions: .z64 .n64 .v64\n\nCopy your N64 roms to $romdir/n64"
+rp_module_help="ROM Extensions: .bin .n64 .u1 .v64 .z64\n\nCopy N64 ROMs To: ${romdir}/n64"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/mupen64plus-libretro-nx/master/LICENSE"
 rp_module_repo="git https://github.com/libretro/mupen64plus-libretro-nx develop"
 rp_module_section="opt kms=main"
@@ -14,10 +14,10 @@ rp_module_flags=""
 
 function depends_lr-mupen64plus-next() {
     local depends=()
-    isPlatform "x11" && depends+=(glew libglvnd)
-    isPlatform "x86" && depends+=(nasm)
-    isPlatform "rpi" && depends+=(raspberrypi-firmware)
-    isPlatform "mesa" && depends+=(libglvnd)
+    isPlatform "x11" && depends+=('glew' 'libglvnd')
+    isPlatform "x86" && depends+=('nasm')
+    isPlatform "rpi" && depends+=('raspberrypi-firmware')
+    isPlatform "mesa" && depends+=('libglvnd')
     getDepends "${depends[@]}"
 }
 
@@ -45,36 +45,33 @@ function build_lr-mupen64plus-next() {
         params+=(FORCE_GLES=1)
     fi
 
-    # use a custom core name to avoid core option name clashes with lr-mupen64plus
     params+=(CORE_NAME=mupen64plus-next)
     make "${params[@]}" clean
     make "${params[@]}"
 
-    md_ret_require="$md_build/mupen64plus_next_libretro.so"
+    md_ret_require="${md_build}/mupen64plus_next_libretro.so"
 }
 
 function install_lr-mupen64plus-next() {
-    md_ret_files=(
-        'mupen64plus_next_libretro.so'
-        'LICENSE'
-        'README.md'
-    )
+    md_ret_files=('mupen64plus_next_libretro.so')
 }
 
 function configure_lr-mupen64plus-next() {
     mkRomDir "n64"
+
     defaultRAConfig "n64"
 
     if isPlatform "rpi"; then
-        # Disable hybrid upscaling filter (needs better GPU)
+        # Disable Hybrid Upscaling Filter (Needs Better GPU)
         setRetroArchCoreOption "mupen64plus-next-HybridFilter" "False"
-        # Disable overscan/VI emulation (slight performance drain)
+        # Disable Overscan/VI Emulation (Slight Performance Drain)
         setRetroArchCoreOption "mupen64plus-next-EnableOverscan" "Disabled"
-        # Enable Threaded GL calls
+        # Enable Threaded GL Calls
         setRetroArchCoreOption "mupen64plus-next-ThreadedRenderer" "True"
     fi
     setRetroArchCoreOption "mupen64plus-next-EnableNativeResFactor" "1"
 
-    addEmulator 1 "$md_id" "n64" "$md_inst/mupen64plus_next_libretro.so"
+    addEmulator 1 "${md_id}" "n64" "${md_inst}/mupen64plus_next_libretro.so"
+
     addSystem "n64"
 }
