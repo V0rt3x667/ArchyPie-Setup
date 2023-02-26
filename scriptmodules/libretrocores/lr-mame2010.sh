50d6abe75f81e7078231d7e125d92c53e104ced0
V0rt3x667 <al-g0l@outlook.com>
2023-02-26 14:34:46 +0000

    Update lr-mame2010.sh

--- scriptmodules/libretrocores/lr-mame2010.sh
+++ scriptmodules/libretrocores/lr-mame2010.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-mame2010"
 rp_module_desc="MAME 0.139 Libretro Core"
-rp_module_help="ROM Extension: .zip\n\nCopy your MAME roms to either $romdir/mame-libretro or\n$romdir/arcade"
+rp_module_help="ROM Extension: .zip\n\nCopy MAME ROMs To: ${romdir}/mame-libretro\nOr\n${romdir}/arcade"
 rp_module_licence="NONCOM https://raw.githubusercontent.com/libretro/mame2010-libretro/master/docs/license.txt"
 rp_module_repo="git https://github.com/libretro/mame2010-libretro master"
 rp_module_section="opt"
@@ -23,31 +23,27 @@ function build_lr-mame2010() {
     rpSwap on 750
     make clean
     local params=()
-    ! isPlatform "x86" && params+=("VRENDER=soft" "FORCE_DRC_C_BACKEND=1")
-    # ARM_ENABLED flag is only used in osinline.h for the YieldProcessor macro and is needed also for aarch64
+    ! isPlatform "x86" && params+=('VRENDER=soft' 'FORCE_DRC_C_BACKEND=1')
     if isPlatform "arm" || isPlatform "aarch64"; then
-        params+=("ARM_ENABLED=1")
+        params+=('ARM_ENABLED=1')
     fi
-    isPlatform "64bit" && params+=("PTR64=1")
-    make "${params[@]}" ARCHOPTS="$CFLAGS" buildtools
-    make "${params[@]}" ARCHOPTS="$CFLAGS"
+    isPlatform "64bit" && params+=('PTR64=1')
+    make "${params[@]}" ARCHOPTS="${CFLAGS}" buildtools
+    make "${params[@]}" ARCHOPTS="${CFLAGS}"
     rpSwap off
-    md_ret_require="$md_build/mame2010_libretro.so"
+    md_ret_require="${md_build}/mame2010_libretro.so"
 }
 
 function install_lr-mame2010() {
-    md_ret_files=(
-        'mame2010_libretro.so'
-        'README.md'
-    )
+    md_ret_files=('mame2010_libretro.so')
 }
 
 function configure_lr-mame2010() {
     local system
     for system in arcade mame-libretro; do
-        mkRomDir "$system"
-        defaultRAConfig "$system"
-        addEmulator 0 "$md_id" "$system" "$md_inst/mame2010_libretro.so"
-        addSystem "$system"
+        mkRomDir "${system}"
+        defaultRAConfig "${system}"
+        addEmulator 0 "${md_id}" "${system}" "${md_inst}/mame2010_libretro.so"
+        addSystem "${system}"
     done
 }
