b94389666fde471364dde02512fc4960f9f5527a
V0rt3x667 <al-g0l@outlook.com>
2023-02-27 01:08:54 +0000

    Update lr-neocd & Add Neo Geo CD System

--- scriptmodules/libretrocores/lr-neocd.sh
+++ scriptmodules/libretrocores/lr-neocd.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-neocd"
 rp_module_desc="Neo Geo CD Libretro Core"
-rp_module_help="ROM Extension: .chd .cue\n\nCopy Neo Geo CD ROMs To:\n${romdir}/neogeo\n\nCopy BIOS Files (ng-lo.rom & uni-bioscd.rom) To: ${biosdir}/neocd"
+rp_module_help="ROM Extension: .chd .cue\n\nCopy Neo Geo CD ROMs To:\n${romdir}/neocd\n\nCopy BIOS File (neocd_f.rom) Or (uni-bioscd.rom) To: ${biosdir}/neocd"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/libretro/neocd_libretro/master/LICENSE.md"
 rp_module_repo="git https://github.com/libretro/neocd_libretro master"
 rp_module_section="exp"
@@ -22,19 +22,19 @@ function build_lr-neocd() {
 }
 
 function install_lr-neocd() {
-    md_ret_files=(
-        'LICENSE.md'
-        'neocd_libretro.so'
-        'README.md'
-    )
+    md_ret_files=('neocd_libretro.so')
 }
 
 function configure_lr-neocd() {
-    mkRomDir "neogeo"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "neocd"
 
-    defaultRAConfig "neogeo"
+        mkUserDir "${biosdir}/neocd"
+    fi
 
-    addEmulator 0 "${md_id}" "neogeo" "${md_inst}/neocd_libretro.so"
+    defaultRAConfig "neocd"
 
-    addSystem "neogeo"
+    addEmulator 1 "${md_id}" "neocd" "${md_inst}/neocd_libretro.so"
+
+    addSystem "neocd"
 }
