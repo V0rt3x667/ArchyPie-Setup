505f7c0267f4b36e146540f18042d43a3680f1a2
V0rt3x667 <al-g0l@outlook.com>
2023-02-27 21:39:12 +0000

    Update lr-opera.sh

--- scriptmodules/libretrocores/lr-opera.sh
+++ scriptmodules/libretrocores/lr-opera.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-opera"
 rp_module_desc="3DO Interactive Multiplayer Libretro Core"
-rp_module_help="ROM Extension: .cue .chd .iso .zip\n\nCopy your 3do roms to $romdir/3do\n\nCopy the required BIOS file panazf10.bin to $biosdir"
+rp_module_help="ROM Extension: .chd .cue .iso .zip\n\nCopy 3do ROMs To: ${romdir}/3do\n\nCopy BIOS File (panazf10.bin) To ${biosdir}/3do"
 rp_module_licence="LGPL https://raw.githubusercontent.com/libretro/opera-libretro/master/libopera/opera_3do.c"
 rp_module_repo="git https://github.com/libretro/opera-libretro master"
 rp_module_section="exp"
@@ -18,19 +18,23 @@ function sources_lr-opera() {
 function build_lr-opera() {
     make clean
     make
-    md_ret_require="$md_build/opera_libretro.so"
+    md_ret_require="${md_build}/opera_libretro.so"
 }
 
 function install_lr-opera() {
-    md_ret_files=(
-        'opera_libretro.so'
-    )
+    md_ret_files=('opera_libretro.so')
 }
 
 function configure_lr-opera() {
-    mkRomDir "3do"
-    defaultRAConfig "3do"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "3do"
 
-    addEmulator 1 "$md_id" "3do" "$md_inst/opera_libretro.so"
+        mkUserDir "${biosdir}/3do"
+    fi
+
+    defaultRAConfig "3do" "system_directory" "${biosdir}/3do"
+
+    addEmulator 1 "${md_id}" "3do" "${md_inst}/opera_libretro.so"
+
     addSystem "3do"
 }
