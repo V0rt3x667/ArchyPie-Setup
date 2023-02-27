3de1a05302983f6765025c1f404764d0f834cfee
V0rt3x667 <al-g0l@outlook.com>
2023-02-27 21:07:01 +0000

    Update lr-o2em.sh

--- scriptmodules/libretrocores/lr-o2em.sh
+++ scriptmodules/libretrocores/lr-o2em.sh
@@ -5,9 +5,9 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="lr-o2em"
-rp_module_desc="Magnavox Odyssey 2 (Philips VideoPac) Libretro Core"
-rp_module_help="ROM Extensions: .bin .zip\n\nCopy your Odyssey 2 / Videopac roms to $romdir/videopac\n\nCopy the required BIOS file o2rom.bin to $biosdir"
-rp_module_licence="OTHER"
+rp_module_desc="Magnavox Odyssey 2 (Philips VideoPac+) Libretro Core"
+rp_module_help="ROM Extensions: .bin .zip\n\nCopy Odyssey 2 (Videopac+) ROMs To: ${romdir}/videopac\n\nCopy BIOS File (o2rom.bin) To: ${biosdir}/videopac"
+rp_module_licence="OTHER https://raw.githubusercontent.com/libretro/libretro-o2em/master/COPYING"
 rp_module_repo="git https://github.com/libretro/libretro-o2em master"
 rp_module_section="opt"
 
@@ -18,20 +18,26 @@ function sources_lr-o2em() {
 function build_lr-o2em() {
     make clean
     make
-    md_ret_require="$md_build/o2em_libretro.so"
+    md_ret_require="${md_build}/o2em_libretro.so"
 }
 
 function install_lr-o2em() {
     md_ret_files=(
+        'COPYING'
         'o2em_libretro.so'
-        'README.md'
     )
 }
 
 function configure_lr-o2em() {
-    mkRomDir "videopac"
-    defaultRAConfig "videopac"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "videopac"
 
-    addEmulator 1 "$md_id" "videopac" "$md_inst/o2em_libretro.so"
+        mkUserDir "${biosdir}/videopac"
+    fi
+
+    defaultRAConfig "videopac" "system_directory" "${biosdir}/videopac"
+
+    addEmulator 1 "${md_id}" "videopac" "${md_inst}/o2em_libretro.so"
+
     addSystem "videopac"
 }
