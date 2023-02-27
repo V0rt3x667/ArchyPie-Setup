2653066040a2c6cf6e992cdd6c85876a482ee83b
V0rt3x667 <al-g0l@outlook.com>
2023-02-27 16:38:47 +0000

    Update lr-nestopia.sh

--- scriptmodules/libretrocores/lr-nestopia.sh
+++ scriptmodules/libretrocores/lr-nestopia.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-nestopia"
 rp_module_desc="Nintendo Entertainment System Libretro Core"
-rp_module_help="ROM Extensions: .nes .zip\n\nCopy your NES roms to $romdir/nes\n\nFor the Famicom Disk System copy your roms to $romdir/fds\n\nFor the Famicom Disk System copy the required BIOS file disksys.rom to $biosdir"
+rp_module_help="ROM Extensions: .fds .nes .unf .unif .zip\n\nCopy NES ROMs To: ${romdir}/nes\n\nCopy Famicom Disk System ROMs To: ${romdir}/fds\n\nCopy BIOS File (disksys.rom) To: ${biosdir}/fds"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/nestopia/master/COPYING"
 rp_module_repo="git https://github.com/libretro/nestopia master"
 rp_module_section="main"
@@ -16,29 +16,31 @@ function sources_lr-nestopia() {
 }
 
 function build_lr-nestopia() {
-    cd libretro
     rpSwap on 512
-    make clean
-    make
+    make -C libretro clean
+    make -C libretro
     rpSwap off
-    md_ret_require="$md_build/libretro/nestopia_libretro.so"
+    md_ret_require="${md_build}/libretro/nestopia_libretro.so"
 }
 
 function install_lr-nestopia() {
-    md_ret_files=(
-        'libretro/nestopia_libretro.so'
-        'COPYING'
-    )
+    md_ret_files=('libretro/nestopia_libretro.so')
 }
 
 function configure_lr-nestopia() {
-    mkRomDir "nes"
-    mkRomDir "fds"
-    defaultRAConfig "nes"
-    defaultRAConfig "fds"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "fds"
+        mkRomDir "nes"
 
-    addEmulator 0 "$md_id" "nes" "$md_inst/nestopia_libretro.so"
-    addEmulator 1 "$md_id" "fds" "$md_inst/nestopia_libretro.so"
-    addSystem "nes"
+        mkUserDir "${biosdir}/fds"
+    fi
+
+    defaultRAConfig "fds" "system_directory" "${biosdir}/fds"
+    defaultRAConfig "nes"
+
+    addEmulator 1 "${md_id}" "fds" "${md_inst}/nestopia_libretro.so"
+    addEmulator 0 "${md_id}" "nes" "${md_inst}/nestopia_libretro.so"
+
     addSystem "fds"
+    addSystem "nes"
 }
