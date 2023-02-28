374e6fc3870adc945753243f74592db89ff6dd56
V0rt3x667 <al-g0l@outlook.com>
2023-02-28 13:30:01 +0000

    Update lr-ppsspp.sh

--- scriptmodules/libretrocores/lr-ppsspp.sh
+++ scriptmodules/libretrocores/lr-ppsspp.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-ppsspp"
 rp_module_desc="Sony PlayStation Portable Libretro Core"
-rp_module_help="ROM Extensions: .iso .pbp .cso\n\nCopy your PlayStation Portable roms to $romdir/psp"
+rp_module_help="ROM Extensions: .cso .elf .iso .pbp .prx\n\nCopy PlayStation Portable ROMs To: ${romdir}/psp"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/RetroPie/ppsspp/master/LICENSE.TXT"
 rp_module_repo="git https://github.com/hrydgard/ppsspp master"
 rp_module_section="opt"
@@ -18,6 +18,9 @@ function depends_lr-ppsspp() {
 
 function sources_lr-ppsspp() {
     sources_ppsspp
+
+    # Set BIOS Directory
+    sed -e "s|retro_base_dir /= \"PPSSPP\";|retro_base_dir /= \"psp\";|g" -i "${md_build}/libretro/libretro.cpp"
 }
 
 function build_lr-ppsspp() {
@@ -26,33 +29,31 @@ function build_lr-ppsspp() {
 
 function install_lr-ppsspp() {
     md_ret_files=(
-        'build/lib/ppsspp_libretro.so'
         'assets'
+        'build/lib/ppsspp_libretro.so'
     )
 }
 
 function configure_lr-ppsspp() {
-    mkRomDir "psp"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/psp/${md_id}"
+
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "psp"
+
+        mkUserDir "${biosdir}/psp"
+
+        # Copy Assets
+        cp -Rv "${md_inst}/assets/"* "${biosdir}/psp/"
+        chown -R "${user}:${user}" "${biosdir}/psp"
+
+        # The Core Needs A Save File Directory
+        iniConfig " = " "" "${configdir}/psp/retroarch.cfg"
+        iniSet "savefile_directory" "${arpdir}/${md_id}"
+    fi
+
     defaultRAConfig "psp"
 
-    if [[ "$md_mode" == "install" ]]; then
-        mkUserDir "$biosdir/PPSSPP"
-        cp -Rv "$md_inst/assets/"* "$biosdir/PPSSPP/"
-        chown -R "${user}:${user}" "$biosdir/PPSSPP"
+    addEmulator 1 "${md_id}" "psp" "${md_inst}/ppsspp_libretro.so"
 
-        # the core needs a save file directory, use the same folder as standalone 'ppsspp'
-        iniConfig " = " "" "$configdir/psp/retroarch.cfg"
-        iniSet "savefile_directory" "$home/.config/ppsspp"
-        moveConfigDir "$home/.config/ppsspp" "$md_conf_root/psp"
-    fi
-
-    addEmulator 1 "$md_id" "psp" "$md_inst/ppsspp_libretro.so"
     addSystem "psp"
-
-    # if we are removing the last remaining psp emu - remove the symlink
-    if [[ "$md_mode" == "remove" ]]; then
-        if [[ -h "$home/.config/ppsspp" && ! -f "$md_conf_root/psp/emulators.cfg" ]]; then
-            rm -f "$home/.config/ppsspp"
-        fi
-    fi
 }
