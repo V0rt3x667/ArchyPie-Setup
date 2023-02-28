6d05e0deeb03534857601d8eefcec3828807ce28
V0rt3x667 <al-g0l@outlook.com>
2023-02-28 21:36:49 +0000

    Update lr-prosystem.sh

--- scriptmodules/libretrocores/lr-prosystem.sh
+++ scriptmodules/libretrocores/lr-prosystem.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-prosystem"
 rp_module_desc="Atari 7800 Libretro Core"
-rp_module_help="ROM Extensions: .a78 .bin .zip\n\nCopy your Atari 7800 roms to $romdir/atari7800\n\nCopy the optional BIOS file 7800 BIOS (U).rom to $biosdir"
+rp_module_help="ROM Extensions: .a78 .bin .cdf .zip\n\nCopy Atari 7800 ROMs To: ${romdir}/atari7800\n\nOPTIONAL: Copy BIOS File (7800 BIOS (U).rom) To: ${biosdir}/atari7800"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/prosystem-libretro/master/License.txt"
 rp_module_repo="git https://github.com/libretro/prosystem-libretro master"
 rp_module_section="main"
@@ -17,22 +17,24 @@ function sources_lr-prosystem() {
 
 function build_lr-prosystem() {
     make clean
-    CXXFLAGS="$CXXFLAGS -fsigned-char" make
-    md_ret_require="$md_build/prosystem_libretro.so"
+    make
+    md_ret_require="${md_build}/prosystem_libretro.so"
 }
 
 function install_lr-prosystem() {
-    md_ret_files=(
-        'prosystem_libretro.so'
-        'README.md'
-    )
+    md_ret_files=('prosystem_libretro.so')
 }
 
 function configure_lr-prosystem() {
-    mkRomDir "atari7800"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "atari7800"
 
-    defaultRAConfig "atari7800"
+        mkUserDir "${biosdir}/atari7800"
+    fi
 
-    addEmulator 1 "$md_id" "atari7800" "$md_inst/prosystem_libretro.so"
+    defaultRAConfig "atari7800" "system_directory" "${biosdir}/atari7800"
+
+    addEmulator 1 "${md_id}" "atari7800" "${md_inst}/prosystem_libretro.so"
+
     addSystem "atari7800"
 }
