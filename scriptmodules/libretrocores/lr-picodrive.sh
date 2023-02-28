2789188f083e79a1fdcbc53d9136b7281f6a87e6
V0rt3x667 <al-g0l@outlook.com>
2023-02-28 00:42:53 +0000

    Update lr-picodrive.sh

--- scriptmodules/libretrocores/lr-picodrive.sh
+++ scriptmodules/libretrocores/lr-picodrive.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-picodrive"
 rp_module_desc="Sega Master System, Mega Drive (Genesis), Sega CD & Sega 32X Libretro Core"
-rp_module_help="ROM Extensions: .32x .iso .cue .sms .smd .bin .gen .md .sg .zip\n\nCopy your Megadrive / Genesis roms to $romdir/megadrive\nMasterSystem roms to $romdir/mastersystem\nSega 32X roms to $romdir/sega32x and\nSegaCD roms to $romdir/segacd\nThe Sega CD requires the BIOS files us_scd1_9210.bin, eu_mcd1_9210.bin, jp_mcd1_9112.bin copied to $biosdir"
+rp_module_help="ROM Extensions: .32x .68k .bin .chd .cue .gen .gg .iso .m3u .md .pco .sgd .smd .sms .zip\n\nCopy Megadrive (Genesis) ROMs To: ${romdir}/megadrive\nCopy MasterSystem ROMs To: ${romdir}/mastersystem\nCopy Sega 32X ROMs To: ${romdir}/sega32x\nCopy SegaCD ROMs To: ${romdir}/segacd\n\nCopy BIOS Files (bios_CD_E.bin, bios_CD_J.bin & bios_CD_U.bin) To: ${biosdir}/segacd"
 rp_module_licence="NONCOM https://raw.githubusercontent.com/libretro/picodrive/master/COPYING"
 rp_module_repo="git https://github.com/libretro/picodrive master"
 rp_module_section="main"
@@ -22,28 +22,41 @@ function build_lr-picodrive() {
     elif isPlatform "aarch64"; then
         params+=(use_sh2drc=0)
     fi
-    make clean
+    make -f Makefile.libretro clean
     make -f Makefile.libretro "${params[@]}"
-    md_ret_require="$md_build/picodrive_libretro.so"
+    md_ret_require="${md_build}/picodrive_libretro.so"
 }
 
 function install_lr-picodrive() {
     md_ret_files=(
-        'AUTHORS'
         'COPYING'
         'picodrive_libretro.so'
-        'README'
     )
 }
 
 function configure_lr-picodrive() {
-    local system
-    local def
-    for system in megadrive mastersystem segacd sega32x; do
-        def=0
-        mkRomDir "$system"
-        defaultRAConfig "$system"
-        addEmulator $def "$md_id" "$system" "$md_inst/picodrive_libretro.so"
-        addSystem "$system"
+    local systems=(
+        'mastersystem'
+        'megadrive'
+        'sega32x'
+        'segacd'
+    )
+
+    if [[ "${md_mode}" == "install" ]]; then
+        for system in "${systems[@]}"; do
+            mkRomDir "${system}"
+        done
+
+        mkUserDir "${biosdir}/segacd"
+    fi
+
+    defaultRAConfig "mastersystem"
+    defaultRAConfig "megadrive"
+    defaultRAConfig "sega32x"
+    defaultRAConfig "segacd" "system_directory" "${biosdir}/segacd"
+
+    for system in "${systems[@]}"; do
+        addEmulator 0 "${md_id}" "${system}" "${md_inst}/picodrive_libretro.so"
+        addSystem "${system}"
     done
 }
