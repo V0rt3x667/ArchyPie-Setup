98eba141184b01369c6d79a0db6b093fa7f6478a
V0rt3x667 <al-g0l@outlook.com>
2023-03-01 20:54:32 +0000

    Update lr-smsplus-gx.sh

--- scriptmodules/libretrocores/lr-smsplus-gx.sh
+++ scriptmodules/libretrocores/lr-smsplus-gx.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-smsplus-gx"
 rp_module_desc="Sega Master System & Game Gear Libretro Core"
-rp_module_help="ROM Extensions: .gg .sms .bin .zip\nCopy your Game Gear roms to $romdir/gamegear\nMasterSystem roms to $romdir/mastersystem"
+rp_module_help="ROM Extensions: .bin .col .gg .rom .sg .sms .zip\nCopy Game Gear ROMs To: ${romdir}/gamegear\nCopy MasterSystem ROMs To: ${romdir}/mastersystem"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/smsplus-gx/master/docs/license"
 rp_module_repo="git https://github.com/libretro/smsplus-gx master"
 rp_module_section="exp"
@@ -18,23 +18,19 @@ function sources_lr-smsplus-gx() {
 function build_lr-smsplus-gx() {
     make -f Makefile.libretro clean
     make -f Makefile.libretro
-    md_ret_require="$md_build/smsplus_libretro.so"
+    md_ret_require="${md_build}/smsplus_libretro.so"
 }
 
 function install_lr-smsplus-gx() {
-    md_ret_files=(
-        'smsplus_libretro.so'
-        'docs/license'
-        'README.md'
-    )
+    md_ret_files=('smsplus_libretro.so')
 }
 
 function configure_lr-smsplus-gx() {
     local system
     for system in gamegear mastersystem; do
-        mkRomDir "$system"
-        defaultRAConfig "$system"
-        addEmulator 0 "$md_id" "$system" "$md_inst/smsplus_libretro.so"
-        addSystem "$system"
+        mkRomDir "${system}"
+        defaultRAConfig "${system}"
+        addEmulator 0 "${md_id}" "${system}" "${md_inst}/smsplus_libretro.so"
+        addSystem "${system}"
     done
 }
