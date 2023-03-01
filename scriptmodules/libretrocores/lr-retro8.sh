80726d6455720b43982342d013dc1e6aab1de181
V0rt3x667 <al-g0l@outlook.com>
2023-03-01 18:02:20 +0000

    Update lr-retro8.sh

--- scriptmodules/libretrocores/lr-retro8.sh
+++ scriptmodules/libretrocores/lr-retro8.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-retro8"
 rp_module_desc="PICO-8 Fantasy Console Libretro Core"
-rp_module_help="ROM Extensions: .p8 .p8.png .zip\n\nCopy your roms to $romdir/pico8"
+rp_module_help="ROM Extensions: .p8 .p8.png .zip\n\nCopy ROMs To: ${romdir}/pico8"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/libretro/retro8/master/LICENSE"
 rp_module_repo="git https://github.com/libretro/retro8 master"
 rp_module_section="exp"
@@ -18,25 +18,20 @@ function sources_lr-retro8() {
 function build_lr-retro8() {
     make clean
     make
-    md_ret_require="$md_build/retro8_libretro.so"
+    md_ret_require="${md_build}/retro8_libretro.so"
 }
 
 function install_lr-retro8() {
-    md_ret_files=(
-        'retro8_libretro.so'
-        'README.md'
-        'LICENSE'
-    )
+    md_ret_files=('retro8_libretro.so')
 }
 
 function configure_lr-retro8() {
     mkRomDir "pico8"
 
-    addEmulator 1 "$md_id" "pico8" "$md_inst/retro8_libretro.so"
-    addSystem "pico8"
-
-    [[ "$md_mode" == "remove" ]] && return
-
-    # disable retroarch built-in imageviewer so we can run .p8.png files
+    # Disable RetroArch Built-in Image Viewer So We Can Run .p8.png Files
     defaultRAConfig "pico8" "builtin_imageviewer_enable" "false"
+
+    addEmulator 1 "${md_id}" "pico8" "${md_inst}/retro8_libretro.so"
+
+    addSystem "pico8"
 }
