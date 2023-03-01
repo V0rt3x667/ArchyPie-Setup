afde3c6aeedb67b55347e2149ad7dc708f049e43
V0rt3x667 <al-g0l@outlook.com>
2023-03-01 18:40:49 +0000

    Update lr-sameboy.sh

--- scriptmodules/libretrocores/lr-sameboy.sh
+++ scriptmodules/libretrocores/lr-sameboy.sh
@@ -5,15 +5,14 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="lr-sameboy"
-rp_module_desc="Nintendo Game Boy, Game Boy Color & Super Game Boy Libretro Core"
-rp_module_help="ROM Extensions: .gb .gbc .zip\n\nCopy Your Game Boy ROMs to $romdir/gb\nGame Boy Color ROMs to $romdir/gbc\nCopy the recommended BIOS files gb_bios.bin and gbc_bios.bin to $biosdir"
+rp_module_desc="Nintendo Game Boy & Game Boy Color Core"
+rp_module_help="ROM Extensions: .gb .gbc .zip\n\nCopy Game Boy ROMs To: ${romdir}/gb\nCopy Game Boy Color ROMs To: ${romdir}/gbc\nOPTIONAL:\n\nCopy BIOS File (gb_bios.bin) To: ${biosdir}/gb\nCopy BIOS File (gbc_bios.bin) To: ${biosdir}/gbc"
 rp_module_licence="MIT https://raw.githubusercontent.com/libretro/SameBoy/buildbot/LICENSE"
 rp_module_repo="git https://github.com/libretro/SameBoy buildbot"
 rp_module_section="opt"
 
 function depends_lr-sameboy() {
     local depends=(
-        'clang'
         'glibc'
         'libgl'
         'libglvnd'
@@ -28,9 +27,9 @@ function sources_lr-sameboy() {
 }
 
 function build_lr-sameboy() {
-    make clean
+    make -C libretro clean
     make -C libretro CONF=release
-    md_ret_require="$md_build/libretro/sameboy_libretro.so"
+    md_ret_require="${md_build}/libretro/sameboy_libretro.so"
 }
 
 function install_lr-sameboy() {
@@ -41,15 +40,18 @@ function install_lr-sameboy() {
 }
 
 function configure_lr-sameboy() {
-    local system
-    local def
+    local systems=('gb' 'gbc')
 
-    for system in gb gbc; do
-        def=0
-        mkRomDir "$system"
-        defaultRAConfig "$system"
-        addEmulator "$def" "$md_id" "$system" "$md_inst/sameboy_libretro.so"
-        addSystem "$system"
+    if [[ "${md_mode}" == "install" ]]; then
+        for system in "${systems[@]}"; do
+            mkRomDir "${system}"
+            mkUserDir "${biosdir}/${system}"
+        done
+    fi
+
+    for system in "${systems[@]}"; do
+        defaultRAConfig "${system}" "system_directory" "${biosdir}/${system}"
+        addEmulator 0 "${md_id}" "${system}" "${md_inst}/sameboy_libretro.so"
+        addSystem "${system}"
     done
 }
-
