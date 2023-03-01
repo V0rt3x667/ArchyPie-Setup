43b5afc40859b5153c6e6bddc5927b1971415a13
V0rt3x667 <al-g0l@outlook.com>
2023-03-01 17:43:21 +0000

    Update lr-quicknes.sh

--- scriptmodules/libretrocores/lr-quicknes.sh
+++ scriptmodules/libretrocores/lr-quicknes.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-quicknes"
 rp_module_desc="Nintendo Entertainment System Libretro Core"
-rp_module_help="ROM Extensions: .nes .zip\n\nCopy your NES roms to $romdir/nes"
+rp_module_help="ROM Extensions: .nes .zip\n\nCopy NES ROMs To: ${romdir}/nes"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/QuickNES_Core/master/LICENSE"
 rp_module_repo="git https://github.com/libretro/QuickNES_Core master"
 rp_module_section="opt"
@@ -18,19 +18,19 @@ function sources_lr-quicknes() {
 function build_lr-quicknes() {
     make clean
     make
-    md_ret_require="$md_build/quicknes_libretro.so"
+    md_ret_require="${md_build}/quicknes_libretro.so"
 }
 
 function install_lr-quicknes() {
-    md_ret_files=(
-        'quicknes_libretro.so'
-    )
+    md_ret_files=('quicknes_libretro.so')
 }
 
 function configure_lr-quicknes() {
     mkRomDir "nes"
+
     defaultRAConfig "nes"
 
-    addEmulator 0 "$md_id" "nes" "$md_inst/quicknes_libretro.so"
+    addEmulator 0 "${md_id}" "nes" "${md_inst}/quicknes_libretro.so"
+
     addSystem "nes"
 }
