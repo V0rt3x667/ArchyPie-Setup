0319467b4576e5d07be32caa493249de5e7243f0
V0rt3x667 <al-g0l@outlook.com>
2023-03-01 01:58:40 +0000

    Update lr-px68k.sh

--- scriptmodules/libretrocores/lr-px68k.sh
+++ scriptmodules/libretrocores/lr-px68k.sh
@@ -6,35 +6,39 @@
 
 rp_module_id="lr-px68k"
 rp_module_desc="Sharp X68000 Libretro Core"
-rp_module_help="You need to copy a X68000 bios file (iplrom30.dat, iplromco.dat, iplrom.dat, or iplromxv.dat), and the font file (cgrom.dat or cgrom.tmp) to $biosdir/keropi. Use F12 to access the in emulator menu."
+rp_module_help="ROM Extensions: .2hd .88d .cmd .d88 .dim .dup .hdf .hdm .img .m3u .xdf .zip\n\nCopy X68000 Games To: ${romdir}/x68000\n\nCopy BIOS Files (cgrom.dat & iplrom.dat) To: ${biosdir}/x68000\n\nOPTIONAL: Copy BIOS Files (iplrom30.dat, iplromco.dat & iplromxv.dat) To: ${biosdir}/x68000"
+rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/px68k-libretro/master/COPYING"
 rp_module_repo="git https://github.com/libretro/px68k-libretro master"
 rp_module_section="exp"
 rp_module_flags=""
 
 function sources_lr-px68k() {
     gitPullOrClone
+
+    # Set BIOS Directory
+    sed -e "s|sprintf(retro_system_conf, \"%s%ckeropi\", RETRO_DIR, SLASH);|sprintf(retro_system_conf, \"%s%cx68000\", RETRO_DIR, SLASH);|g" -i "${md_build}/libretro.c"
 }
 
 function build_lr-px68k() {
     make clean
     make
-    md_ret_require="$md_build/px68k_libretro.so"
+    md_ret_require="${md_build}/px68k_libretro.so"
 }
 
 function install_lr-px68k() {
-    md_ret_files=(
-        'px68k_libretro.so'
-        'README.MD'
-        'readme.txt'
-    )
+    md_ret_files=('px68k_libretro.so')
 }
 
 function configure_lr-px68k() {
-    mkRomDir "x68000"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "x68000"
+
+        mkUserDir "${biosdir}/x68000"
+    fi
+
     defaultRAConfig "x68000"
 
-    mkUserDir "$biosdir/keropi"
+    addEmulator 1 "${md_id}" "x68000" "${md_inst}/px68k_libretro.so"
 
-    addEmulator 1 "$md_id" "x68000" "$md_inst/px68k_libretro.so"
     addSystem "x68000"
 }
