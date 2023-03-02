22649308bf288bf3bb4b4245aba8a097b910158a
V0rt3x667 <al-g0l@outlook.com>
2023-03-02 22:10:42 +0000

    Update lr-theodore.sh

--- scriptmodules/libretrocores/lr-theodore.sh
+++ scriptmodules/libretrocores/lr-theodore.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-theodore"
 rp_module_desc="Thomson TO7, TO7/70, TO8, TO8D, TO9, TO9+, MO5 & MO6 Libretro Core"
-rp_module_help="ROM Extensions: *.fd, *.sap, *.k7, *.m5, *.m7, *.rom\n\nAdd your game files in $romdir/moto"
+rp_module_help="ROM Extensions: .fd .k7 .m5 .m7 .rom .sap .zip\n\nCopy Thomson Games To: ${romdir}/moto"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/Zlika/theodore/master/LICENSE"
 rp_module_repo="git https://github.com/Zlika/theodore master"
 rp_module_section="exp"
@@ -23,18 +23,15 @@ function build_lr-theodore() {
 }
 
 function install_lr-theodore() {
-    md_ret_files=(
-        'theodore_libretro.so'
-        'README.md'
-        'README-FR.md'
-        'LICENSE'
-    )
+    md_ret_files=('theodore_libretro.so')
 }
 
 function configure_lr-theodore() {
     mkRomDir "moto"
+
     defaultRAConfig "moto"
 
-    addEmulator 1 "$md_id" "moto" "$md_inst/theodore_libretro.so"
+    addEmulator 1 "${md_id}" "moto" "${md_inst}/theodore_libretro.so"
+
     addSystem "moto"
 }
