a52552a60fb66d76f0cba0ff93949f04856f9e3d
V0rt3x667 <al-g0l@outlook.com>
2023-03-02 13:00:06 +0000

    Update lr-snes9x.sh

--- scriptmodules/libretrocores/lr-snes9x.sh
+++ scriptmodules/libretrocores/lr-snes9x.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-snes9x"
 rp_module_desc="Nintendo SNES Libretro Core"
-rp_module_help="ROM Extensions: .bin .smc .sfc .fig .swc .mgd .zip\n\nCopy your SNES roms to $romdir/snes"
+rp_module_help="ROM Extensions: .bs .fig .sfc .smc .st .swc .zip\n\nCopy SNES ROMs To: ${romdir}/snes"
 rp_module_licence="NONCOM https://raw.githubusercontent.com/libretro/snes9x/master/LICENSE"
 rp_module_repo="git https://github.com/libretro/snes9x master"
 rp_module_section="opt armv8=main x86=main"
@@ -19,26 +19,23 @@ function build_lr-snes9x() {
     local params=()
     isPlatform "arm" && params+=(platform="armv")
 
-    cd libretro
-    make "${params[@]}" clean
-    # temporarily disable distcc due to segfaults with cross compiler and lto
-    DISTCC_HOSTS="" make "${params[@]}"
-    md_ret_require="$md_build/libretro/snes9x_libretro.so"
+    make -C libretro "${params[@]}" clean
+    make -C libretro "${params[@]}"
+    md_ret_require="${md_build}/libretro/snes9x_libretro.so"
 }
 
 function install_lr-snes9x() {
-    md_ret_files=(
-        'libretro/snes9x_libretro.so'
-        'docs'
-    )
+    md_ret_files=('libretro/snes9x_libretro.so')
 }
 
 function configure_lr-snes9x() {
     mkRomDir "snes"
+
     defaultRAConfig "snes"
 
     local def=0
     ! isPlatform "armv7" && def=1
-    addEmulator $def "$md_id" "snes" "$md_inst/snes9x_libretro.so"
+    addEmulator ${def} "${md_id}" "snes" "${md_inst}/snes9x_libretro.so"
+
     addSystem "snes"
 }
