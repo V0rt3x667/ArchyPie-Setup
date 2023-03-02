7ef29c52d23a13a4059c7c2b87fa997a5e5dcbc2
V0rt3x667 <al-g0l@outlook.com>
2023-03-02 22:34:47 +0000

    Update lr-tic80.sh

--- scriptmodules/libretrocores/lr-tic80.sh
+++ scriptmodules/libretrocores/lr-tic80.sh
@@ -6,13 +6,17 @@
 
 rp_module_id="lr-tic80"
 rp_module_desc="TIC-80 Fantasy Computer Libretro Core"
-rp_module_help="ROM Extensions: .tic .zip\n\nCopy your roms to $romdir/tic80\n\n"
+rp_module_help="ROM Extensions: .tic .zip\n\nCopy TIC-80 Games To: ${romdir}/tic80"
 rp_module_licence="MIT https://raw.githubusercontent.com/libretro/TIC-80/master/LICENSE"
 rp_module_repo="git https://github.com/libretro/TIC-80 master"
 rp_module_section="exp"
 
 function depends_lr-tic80() {
-    getDepends cmake
+    local depends=(
+        'cmake'
+        'ninja'
+    )
+    getDepends "${depends[@]}"
 }
 
 function sources_lr-tic80() {
@@ -20,26 +24,38 @@ function sources_lr-tic80() {
 }
 
 function build_lr-tic80() {
-    rm -rf archypie
-    mkdir -p archypie
-    cd archypie
-    cmake -DBUILD_PLAYER=OFF -DBUILD_SOKOL=OFF -DBUILD_SDL=OFF -DBUILD_DEMO_CARTS=OFF -DBUILD_LIBRETRO=ON ../core
-    make
-    md_ret_require="$md_build/archypie/lib/tic80_libretro.so"
+    cmake . \
+        -B"build" \
+        -G"Ninja" \
+        -S"core" \
+        -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -DBUILD_DEMO_CARTS=OFF \
+        -DBUILD_LIBRETRO=ON \
+        -DBUILD_PLAYER=OFF \
+        -DBUILD_SDL=OFF \
+        -DBUILD_SOKOL=OFF \
+        -Wno-dev
+    ninja -C build clean
+    ninja -C build
+    md_ret_require="${md_build}/build/lib/tic80_libretro.so"
 }
 
 
 function install_lr-tic80() {
     md_ret_files=(
-        'archypie/lib/tic80_libretro.so'
-        'README.md'
+        'build/lib/tic80_libretro.so'
         'LICENSE'
     )
 }
 
 function configure_lr-tic80() {
     mkRomDir "tic80"
+
     defaultRAConfig "tic80"
-    addEmulator 1 "$md_id" "tic80" "$md_inst/tic80_libretro.so"
+
+    addEmulator 1 "${md_id}" "tic80" "${md_inst}/tic80_libretro.so"
+
     addSystem "tic80" "TIC-80"
 }
