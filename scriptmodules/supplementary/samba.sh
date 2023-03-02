efb34a8f357e1f61ea8625d873e449dac5d460b6
V0rt3x667 <al-g0l@outlook.com>
2023-03-02 19:42:52 +0000

    Update samba.sh

--- scriptmodules/supplementary/samba.sh
+++ scriptmodules/supplementary/samba.sh
@@ -14,19 +14,19 @@ function depends_samba() {
 
 function remove_share_samba() {
     local name="$1"
-    [[ -z "$name" || ! -f /etc/samba/smb.conf ]] && return
-    sed -i "/^\[$name\]/,/^force user/d" /etc/samba/smb.conf
+    [[ -z "${name}" || ! -f /etc/samba/smb.conf ]] && return
+    sed -i "/^\[${name}\]/,/^force user/d" /etc/samba/smb.conf
 }
 
 function add_share_samba() {
     local name="$1"
     local path="$2"
-    [[ -z "$name" || -z "$path" ]] && return
-    remove_share_samba "$name"
+    [[ -z "${name}" || -z "${path}" ]] && return
+    remove_share_samba "${name}"
     cat >>/etc/samba/smb.conf <<_EOF_
 [$1]
-comment = $name
-path = "$path"
+comment = ${name}
+path = "${path}"
 writeable = yes
 guest ok = yes
 create mask = 0644
@@ -36,15 +36,16 @@ _EOF_
 }
 
 function restart_samba() {
-  systemctl restart smb
+    systemctl -q is-active smbd.service && systemctl -q reload-or-restart smbd.service
 }
 
 function install_shares_samba() {
     [[ -f /etc/samba/smb.conf ]] && cp /etc/samba/smb.conf /etc/samba/smb.conf.bak
-    add_share_samba "roms" "$romdir"
-    add_share_samba "bios" "$home/ArchyPie/BIOS"
-    add_share_samba "configs" "$configdir"
-    isPlatform "rpi" && add_share_samba "splashscreens" "$datadir/splashscreens"
+    add_share_samba "roms" "${romdir}"
+    add_share_samba "bios" "${home}/ArchyPie/BIOS"
+    add_share_samba "configs" "${configdir}"
+    # Will Be Added In The Future
+    # isPlatform "rpi" && add_share_samba "splashscreens" "${datadir}/splashscreens"
     restart_samba
 }
 
@@ -54,17 +55,17 @@ function remove_shares_samba() {
         'configs'
         'roms'
     )
-    isPlatform "rpi" && names+=('splashscreens')
+    # isPlatform "rpi" && names+=('splashscreens')
 
     for name in "${names[@]}"; do
-        remove_share_samba "$name"
+        remove_share_samba "${name}"
     done
     restart_samba
 }
 
 function gui_samba() {
     while true; do
-        local cmd=(dialog --backtitle "$__backtitle" --menu "Choose an option" 22 76 16)
+        local cmd=(dialog --backtitle "${__backtitle}" --menu "Choose An Option" 22 76 16)
         local options=(
             1 "Install ArchyPie Samba Shares"
             2 "Remove ArchyPie Samba Shares"
@@ -72,26 +73,27 @@ function gui_samba() {
             4 "Restart Samba Service"
             5 "Remove Samba & Configuration"
         )
-        local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+        local choice
+        choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
         if [[ -n "${choice}" ]]; then
             case "${choice}" in
                 1)
-                    rp_callModule "$md_id" depends
-                    rp_callModule "$md_id" install_shares
-                    printMsgs "dialog" "Installed and Enabled Shares"
+                    rp_callModule "${md_id}" depends
+                    rp_callModule "${md_id}" install_shares
+                    printMsgs "dialog" "Installed & Enabled Shares"
                     ;;
                 2)
-                    rp_callModule "$md_id" remove_shares
+                    rp_callModule "${md_id}" remove_shares
                     printMsgs "dialog" "Removed Shares"
                     ;;
                 3)
                     editFile /etc/samba/smb.conf
                     ;;
                 4)
-                    rp_callModule "$md_id" restart
+                    rp_callModule "${md_id}" restart
                     ;;
                 5)
-                    rp_callModule "$md_id" depends remove
+                    rp_callModule "${md_id}" depends remove
                     printMsgs "dialog" "Removed Samba Service"
                     ;;
             esac
