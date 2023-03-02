ca696aae4634279516235c1fb9012f78e80f933e
V0rt3x667 <al-g0l@outlook.com>
2023-03-02 21:01:36 +0000

    Update lr-swanstation.sh

--- scriptmodules/libretrocores/lr-swanstation.sh
+++ scriptmodules/libretrocores/lr-swanstation.sh
@@ -6,40 +6,41 @@
 
 rp_module_id="lr-swanstation"
 rp_module_desc="Sony Playstation Libretro Core"
-rp_module_help="ROM Extensions: .exe .cue .bin .chd .psf .m3u .pbp\n\nCopy your PlayStation roms to $romdir/psx\n\nCopy compatble BIOS files to $biosdir"
+rp_module_help="ROM Extensions: .bin .chd .cue .ecm .exe .img .iso .m3u .mds .pbp .psexe .psf\n\nCopy PlayStation ROMs To: ${romdir}/psx\n\nCopy BIOS Files:\n\nscph5500.bin\nscph5501.bin\nscph5502.bin\n\nTo: ${biosdir}/psx"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/libretro/swanstation/main/LICENSE"
 rp_module_section="exp"
 rp_module_repo="git https://github.com/libretro/swanstation main"
-rp_module_flags="!all arm aarch64 64bit"
+rp_module_flags=""
 
 function sources_lr-swanstation() {
     gitPullOrClone
 }
 
 function build_lr-swanstation() {
-    cmake . -Wno-dev
-    make clean
-    make
-    md_ret_require="$md_build/swanstation_libretro.so"
+    cmake . \
+        -B"build" \
+        -G"Ninja" \
+        -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -Wno-dev
+    ninja -C build clean
+    ninja -C build
+    md_ret_require="${md_build}/build/swanstation_libretro.so"
 }
 
 function install_lr-swanstation() {
-    md_ret_files=('swanstation_libretro.so')
+    md_ret_files=('build/swanstation_libretro.so')
 }
 
 function configure_lr-swanstation() {
     mkRomDir "psx"
-    defaultRAConfig "psx"
 
-    if isPlatform "gles" && ! isPlatform "gles3"; then
-        # Hardware renderer not supported on GLES2 devices
-        setRetroArchCoreOption "swanstation_GPU.Renderer" "Software"
-    fi
+    isPlatform "rpi" && setRetroArchCoreOption "swanstation_GPU.Renderer" "Software"
 
-    # Pi 4 has occasional slowdown with hardware rendering
-    # e.g. Gran Turismo 2 (Arcade) race start
-    isPlatform "rpi4" && setRetroArchCoreOption "swanstation_GPU.Renderer" "Software"
+    defaultRAConfig "psx" "system_directory" "${biosdir}/psx"
 
-    addEmulator 0 "$md_id" "psx" "$md_inst/swanstation_libretro.so" 
+    addEmulator 0 "${md_id}" "psx" "${md_inst}/swanstation_libretro.so" 
+
     addSystem "psx"
 }
