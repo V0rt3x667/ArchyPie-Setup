ad2d144054ffafb84b9d5df4a9edf1bcb1eb2ac2
V0rt3x667 <al-g0l@outlook.com>
2023-03-02 13:00:14 +0000

    Update lr-snes9x2010.sh

--- scriptmodules/libretrocores/lr-snes9x2010.sh
+++ scriptmodules/libretrocores/lr-snes9x2010.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-snes9x2010"
 rp_module_desc="Nintendo SNES 1.52 Libretro Core"
-rp_module_help="ROM Extensions: .bin .smc .sfc .fig .swc .mgd .zip\n\nCopy your SNES roms to $romdir/snes"
+rp_module_help="ROM Extensions: .bs .fig .sfc .smc .st .swc .zip\n\nCopy SNES ROMs To: ${romdir}/snes"
 rp_module_licence="NONCOM https://raw.githubusercontent.com/libretro/snes9x2010/master/docs/snes9x-license.txt"
 rp_module_repo="git https://github.com/libretro/snes9x2010 master"
 rp_module_section="opt arm=main"
@@ -20,27 +20,26 @@ function build_lr-snes9x2010() {
     local platform=""
     isPlatform "arm" && platform+="armv"
     isPlatform "neon" && platform+="neon"
-    if [[ -n "$platform" ]]; then
-        make -f Makefile.libretro platform="$platform"
+    if [[ -n "${platform}" ]]; then
+        make -f Makefile.libretro platform="${platform}"
     else
         make -f Makefile.libretro
     fi
-    md_ret_require="$md_build/snes9x2010_libretro.so"
+    md_ret_require="${md_build}/snes9x2010_libretro.so"
 }
 
 function install_lr-snes9x2010() {
-    md_ret_files=(
-        'snes9x2010_libretro.so'
-        'docs'
-    )
+    md_ret_files=('snes9x2010_libretro.so')
 }
 
 function configure_lr-snes9x2010() {
     mkRomDir "snes"
+
     defaultRAConfig "snes"
 
     local def=0
     isPlatform "armv7" && def=1
-    addEmulator $def "$md_id" "snes" "$md_inst/snes9x2010_libretro.so"
+    addEmulator ${def} "${md_id}" "snes" "${md_inst}/snes9x2010_libretro.so"
+
     addSystem "snes"
 }
