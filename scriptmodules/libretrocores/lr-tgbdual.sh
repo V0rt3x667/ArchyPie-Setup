00898feba7019f8f3d0041c0032e77a3889cd04b
V0rt3x667 <al-g0l@outlook.com>
2023-03-02 21:28:07 +0000

    Update lr-tgbdual.sh

--- scriptmodules/libretrocores/lr-tgbdual.sh
+++ scriptmodules/libretrocores/lr-tgbdual.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-tgbdual"
 rp_module_desc="Nintendo Gameboy & Gameboy Color Libretro Core"
-rp_module_help="ROM Extensions: .gb .gbc .zip\n\nCopy your GameBoy roms to $romdir/gb\n\nCopy your GameBoy Color roms to $romdir/gbc"
+rp_module_help="ROM Extensions: .cgb .dmg .gb .gbc .sgb .zip\n\nCopy GameBoy ROMs To: ${romdir}/gb\n\nCopy GameBoy Color ROMs To: ${romdir}/gbc"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/tgbdual-libretro/master/docs/COPYING-2.0.txt"
 rp_module_repo="git https://github.com/libretro/tgbdual-libretro master"
 rp_module_section="opt"
@@ -18,26 +18,26 @@ function sources_lr-tgbdual() {
 function build_lr-tgbdual() {
     make clean
     make
-    md_ret_require="$md_build/tgbdual_libretro.so"
+    md_ret_require="${md_build}/tgbdual_libretro.so"
 }
 
 function install_lr-tgbdual() {
-    md_ret_files=(
-        'tgbdual_libretro.so'
-    )
+    md_ret_files=('tgbdual_libretro.so')
 }
 
 function configure_lr-tgbdual() {
     mkRomDir "gbc"
     mkRomDir "gb"
+
+    # Enable Dual Link By Default
+    setRetroArchCoreOption "tgbdual_gblink_enable" "enabled"
+
     defaultRAConfig "gb"
     defaultRAConfig "gbc"
 
-    # enable dual / link by default
-    setRetroArchCoreOption "tgbdual_gblink_enable" "enabled"
+    addEmulator 0 "${md_id}" "gb" "${md_inst}/tgbdual_libretro.so"
+    addEmulator 0 "${md_id}" "gbc" "${md_inst}/tgbdual_libretro.so"
 
-    addEmulator 0 "$md_id" "gb" "$md_inst/tgbdual_libretro.so"
-    addEmulator 0 "$md_id" "gbc" "$md_inst/tgbdual_libretro.so"
     addSystem "gb"
     addSystem "gbc"
 }
