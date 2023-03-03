311062c88dd2efce1176e77e3c2f908a815ca913
V0rt3x667 <al-g0l@outlook.com>
2023-03-03 02:55:35 +0000

    Update lr-virtualjaguar.sh

--- scriptmodules/libretrocores/lr-virtualjaguar.sh
+++ scriptmodules/libretrocores/lr-virtualjaguar.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-virtualjaguar"
 rp_module_desc="Atari Jaguar Libretro Core"
-rp_module_help="ROM Extensions: .j64 .jag .zip\n\nCopy your Atari Jaguar roms to $romdir/atarijaguar"
+rp_module_help="ROM Extensions: .abs .bin .cof .j64 .jag .prg .rom .zip\n\nCopy Atari Jaguar ROMs To: ${romdir}/atarijaguar"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/libretro/virtualjaguar-libretro/master/docs/GPLv3"
 rp_module_repo="git https://github.com/libretro/virtualjaguar-libretro master"
 rp_module_section="exp"
@@ -19,20 +19,19 @@ function sources_lr-virtualjaguar() {
 function build_lr-virtualjaguar() {
     make clean
     make
-    md_ret_require="$md_build/virtualjaguar_libretro.so"
+    md_ret_require="${md_build}/virtualjaguar_libretro.so"
 }
 
 function install_lr-virtualjaguar() {
-    md_ret_files=(
-        'virtualjaguar_libretro.so'
-        'README.md'
-    )
+    md_ret_files=('virtualjaguar_libretro.so')
 }
 
 function configure_lr-virtualjaguar() {
     mkRomDir "atarijaguar"
+
     defaultRAConfig "atarijaguar"
 
-    addEmulator 1 "$md_id" "atarijaguar" "$md_inst/virtualjaguar_libretro.so"
+    addEmulator 1 "${md_id}" "atarijaguar" "${md_inst}/virtualjaguar_libretro.so"
+
     addSystem "atarijaguar"
 }
