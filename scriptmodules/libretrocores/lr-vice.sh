8b75585ab82d642709268d85dfe42b6ff67c0b08
V0rt3x667 <al-g0l@outlook.com>
2023-03-03 02:41:17 +0000

    Update lr-vice.sh

--- scriptmodules/libretrocores/lr-vice.sh
+++ scriptmodules/libretrocores/lr-vice.sh
@@ -6,14 +6,14 @@
 
 rp_module_id="lr-vice"
 rp_module_desc="Commodore C64, C128, PET, Plus4 & VIC 20 Libretro Core"
-rp_module_help="ROM Extensions: .cmd .crt .d64 .d71 .d80 .d81 .g64 .m3u .prg .t64 .tap .x64 .zip .vsf\n\nCopy your games to $romdir/c64"
+rp_module_help="ROM Extensions: .cmd .crt .d64 .d71 .d80 .d81 .g64 .m3u .prg .t64 .tap .vsf .x64 .zip\n\nCopy C64 Games To: ${romdir}/c64"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/vice-libretro/master/vice/COPYING"
 rp_module_repo="git https://github.com/libretro/vice-libretro master"
 rp_module_section="opt"
 rp_module_flags=""
 
 function _get_targets_lr-vice() {
-    echo x64 x64dtv x64sc x128 xpet xplus4 xvic
+    echo x128 x64 x64dtv x64sc xpet xplus4 xvic
 }
 
 function sources_lr-vice() {
@@ -21,20 +21,20 @@ function sources_lr-vice() {
 }
 
 function build_lr-vice() {
-    mkdir -p "$md_build/cores"
+    mkdir -p "${md_build}/cores"
     local target
     for target in $(_get_targets_lr-vice); do
         make clean
-        make EMUTYPE="$target"
-        cp "$md_build/vice_${target}_libretro.so" "cores/"
-        md_ret_require+=("$md_build/cores/vice_${target}_libretro.so")
+        make EMUTYPE="${target}"
+        cp "${md_build}/vice_${target}_libretro.so" "cores/"
+        md_ret_require+=("${md_build}/cores/vice_${target}_libretro.so")
     done
 }
 
 function install_lr-vice() {
     md_ret_files=(
-        'vice/data'
         'vice/COPYING'
+        'vice/data'
     )
     local target
     for target in $(_get_targets_lr-vice); do
@@ -44,7 +44,10 @@ function install_lr-vice() {
 
 function configure_lr-vice() {
     mkRomDir "c64"
+
     defaultRAConfig "c64"
+
+    isPlatform "arm" && setRetroArchCoreOption "vice_sid_engine" "FastSID"
 
     local target
     local name
@@ -52,21 +55,12 @@ function configure_lr-vice() {
     for target in $(_get_targets_lr-vice); do
         def=0
         name="-${target}"
-        if [[ "$target" == "x64" ]]; then
+        if [[ "${target}" == "x64" ]]; then
             name=""
             def=1
         fi
-        addEmulator "$def" "$md_id${name}" "c64" "$md_inst/vice_${target}_libretro.so"
+        addEmulator "${def}" "${md_id}${name}" "c64" "${md_inst}/vice_${target}_libretro.so"
     done
-    
+
     addSystem "c64"
-
-    [[ "$md_mode" == "remove" ]] && return
-    
-    if isPlatform "arm"; then
-        setRetroArchCoreOption "vice_sid_engine" "FastSID"
-    fi
-
-    cp -R "$md_inst/data" "$biosdir"
-    chown -R "${user}:${user}" "$biosdir/data"
 }
