8e1b0685080969c0dc2d9868573f3eff6fc162ba
V0rt3x667 <al-g0l@outlook.com>
2023-03-03 11:47:31 +0000

    Update lr-x1.sh

--- scriptmodules/libretrocores/lr-x1.sh
+++ scriptmodules/libretrocores/lr-x1.sh
@@ -6,31 +6,37 @@
 
 rp_module_id="lr-x1"
 rp_module_desc="Sharp X1 Libretro Core"
-rp_module_help="ROM Extensions: .dx1 .zip .2d .2hd .tfd .d88 .88d .hdm .xdf .dup .cmd\n\nCopy your X1 roms to $romdir/x1\n\nCopy the required BIOS files IPLROM.X1 and IPLROM.X1T to $biosdir"
+rp_module_help="ROM Extensions: .2d .2hd .88d .cmd .d88 .dup .dx1 .hdm .tap .tfd .xdf .zip .zip\n\nCopy X1 ROMs To: ${romdir}/x1\n\nCopy BIOS Files (iplrom.x1 & iplrom.x1t) To: ${biosdir}/x1"
 rp_module_repo="git https://github.com/r-type/xmil-libretro master"
 rp_module_section="exp"
 
 function sources_lr-x1() {
     gitPullOrClone
+
+    # Set BIOS Directory
+    sed -e "s|%s%cxmil|%s%cx1|g" -i "${md_build}/libretro/libretro.c"
 }
 
 function build_lr-x1() {
-    cd libretro
-    make clean
-    make
-    md_ret_require="$md_build/libretro/x1_libretro.so"
+    make -C libretro clean
+    make -C libretro
+    md_ret_require="${md_build}/libretro/x1_libretro.so"
 }
 
 function install_lr-x1() {
-    md_ret_files=(
-        'libretro/x1_libretro.so'
-    )
+    md_ret_files=('libretro/x1_libretro.so')
 }
 
 function configure_lr-x1() {
-    mkRomDir "x1"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "x1"
+
+        mkUserDir "${biosdir}/x1"
+    fi
+
     defaultRAConfig "x1"
 
-    addEmulator 1 "$md_id" "x1" "$md_inst/x1_libretro.so"
+    addEmulator 1 "${md_id}" "x1" "${md_inst}/x1_libretro.so"
+
     addSystem "x1"
 }
