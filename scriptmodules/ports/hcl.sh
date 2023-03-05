68d0e9ff1ac54644d01cfbc988416e63ee440b7e
V0rt3x667 <al-g0l@outlook.com>
2023-03-05 17:07:07 +0000

    Add Hydra Castle Labyrinth

---
+++ scriptmodules/ports/hcl.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+# This file is part of the ArchyPie project.
+#
+# Please see the LICENSE file at the top-level directory of this distribution.
+
+rp_module_id="hcl"
+rp_module_desc="Hydra Castle Labyrinth: A Metroidvania Game"
+rp_module_licence="GPL2 https://raw.githubusercontent.com/ptitSeb/hydracastlelabyrinth/master/LICENSE"
+rp_module_repo="git https://github.com/ptitSeb/hydracastlelabyrinth master"
+rp_module_section="opt"
+
+function depends_hcl() {
+    local depends=(
+        'cmake'
+        'ninja'
+        'sdl2_mixer'
+        'sdl2'
+    )
+    getDepends "${depends[@]}"
+}
+
+function sources_hcl() {
+    gitPullOrClone
+
+    # Set Default Config Path(s)
+    sed -e "s|\"/.hydracastlelabyrinth/\"|\"/ArchyPie/configs/${md_id}/\"|g" -i "${md_build}/src/game.c" "${md_build}/src/ini.c"
+    sed -e "s|\"/.hydracastlelabyrinth\"|\"/ArchyPie/configs/${md_id}\"|g" -i "${md_build}/src/main.c"
+}
+
+function build_hcl() {
+    cmake . \
+        -B"build" \
+        -G"Ninja" \
+        -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -DUSE_SDL2="ON" \
+        -Wno-dev
+    ninja -C build clean
+    ninja -C build
+    md_ret_require="${md_build}/build/${md_id}"
+}
+
+function install_hcl() {
+    md_ret_files=(
+        'build/hcl'
+        'data'
+    )
+}
+
+function configure_hcl() {
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${md_id}/"
+
+    addPort "${md_id}" "${md_id}" "Hydra Castle Labyrinth" "pushd ${md_inst}; ${md_inst}/${md_id} -d; popd"
+}
