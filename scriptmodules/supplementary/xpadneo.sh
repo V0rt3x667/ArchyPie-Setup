0c9bb67c7bebf559787272a24c8034b149e5cf72
V0rt3x667 <al-g0l@outlook.com>
2023-03-07 19:43:18 +0000

    Update xpadneo.sh

--- scriptmodules/supplementary/xpadneo.sh
+++ scriptmodules/supplementary/xpadneo.sh
@@ -7,25 +7,32 @@
 rp_module_id="xpadneo"
 rp_module_desc="xpadneo - Advanced Linux Driver for Xbox One Wireless Gamepads"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/atar-axis/xpadneo/master/LICENSE"
-rp_module_repo="git https://github.com/atar-axis/xpadneo.git v0.9.3"
+rp_module_repo="git https://github.com/atar-axis/xpadneo.git v0.9.5"
 rp_module_section="driver"
 rp_module_flags="nobin"
 
 function _version_xpadneo() {
-    cat "$md_inst/VERSION"
+    cat "${md_inst}/VERSION"
 }
 
 function depends_xpadneo() {
-    local depends=(dkms rsync linux-headers dkms bluez bluez-utils)
+    local depends=(
+        'bluez-utils'
+        'bluez'
+        'dkms'
+        'linux-headers'
+        'rsync'
+    )
     getDepends "${depends[@]}"
 }
 
 function sources_xpadneo() {
     gitPullOrClone
-    rsync -a --delete "$md_build/hid-xpadneo/" "$md_inst/"
-    cp "$md_build/VERSION" "$md_inst/"
-    local version="$(_version_xpadneo)"
-    sed "s/@DO_NOT_CHANGE@/$version/g" "$md_inst/dkms.conf.in" > "$md_inst/dkms.conf"
+    rsync -a --delete "${md_build}/hid-xpadneo/" "${md_inst}/"
+    cp "${md_build}/VERSION" "${md_inst}/"
+    local version
+    version="$(_version_xpadneo)"
+    sed "s/@DO_NOT_CHANGE@/${version}/g" "${md_inst}/dkms.conf.in" > "${md_inst}/dkms.conf"
 }
 
 function build_xpadneo() {
@@ -37,7 +44,7 @@ function remove_xpadneo() {
 }
 
 function configure_xpadneo() {
-    [[ "$md_mode" == "remove" ]] && return
+    [[ "${md_mode}" == "remove" ]] && return
 
     dkmsManager reload hid-xpadneo "$(_version_xpadneo)"
 }
