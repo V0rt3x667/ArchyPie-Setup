182a1dc81f85d031ebdb9d50d6f837916fb132f9
V0rt3x667 <al-g0l@outlook.com>
2023-03-07 21:51:58 +0000

    Update virtualgamepad.sh

--- scriptmodules/supplementary/virtualgamepad.sh
+++ scriptmodules/supplementary/virtualgamepad.sh
@@ -5,14 +5,18 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="virtualgamepad"
-rp_module_desc="Virtual Gamepad for Smartphone"
+rp_module_desc="Node Virtual Gamepads: Turns Your Smartphone Into A Virtual Gamepad"
 rp_module_licence="MIT https://raw.githubusercontent.com/miroof/node-virtual-gamepads/master/LICENSE"
 rp_module_repo="git https://github.com/miroof/node-virtual-gamepads.git master"
 rp_module_section="exp"
-rp_module_flags="noinstclean nobin"
+rp_module_flags="nobin noinstclean"
 
 function depends_virtualgamepad() {
-    getDepends nodejs npm
+    local depends=(
+        'nodejs'
+        'npm'
+    )
+    getDepends "${depends[@]}"
 }
 
 function remove_virtualgamepad() {
@@ -21,19 +25,19 @@ function remove_virtualgamepad() {
 }
 
 function sources_virtualgamepad() {
-    gitPullOrClone "$md_inst"
-    chown -R "${user}:${user}" "$md_inst"
+    gitPullOrClone "${md_inst}"
+    chown -R "${user}:${user}" "${md_inst}"
 }
 
 function install_virtualgamepad() {
     npm install pm2 -g --unsafe-perm
-    cd "$md_inst"
-    sudo -u ${user} npm install
-    sudo -u ${user} npm install ref
+    cd "${md_inst}" || exit
+    sudo -u "${user}" npm install
+    sudo -u "${user}" npm install ref
 }
 
 function configure_virtualgamepad() {
-    [[ "$md_mode" == "remove" ]] && return
+    [[ "${md_mode}" == "remove" ]] && return
     pm2 start main.js
     pm2 startup
     pm2 save
