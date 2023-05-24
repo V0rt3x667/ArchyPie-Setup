80db59a59e9c57d3050ecbe48122297c637809e5
V0rt3x667 <al-g0l@outlook.com>
2023-05-24 23:43:02 +0100

    Update lr-flycast.sh & Add New Systems
    Build from upstream repo.
    
    Add Sega Naomi, Naomi 2 & Sammy Atomiswave as separate systems.

--- scriptmodules/libretrocores/lr-flycast.sh
+++ scriptmodules/libretrocores/lr-flycast.sh
@@ -5,61 +5,82 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="lr-flycast"
-rp_module_desc="Sega Dreamcast, Naomi, Naomi 2 & Atomiswave Libretro Core"
-rp_module_help="ROM Extensions: .7z .bin .cdi .chd .cue .dat .elf .gdi .lst .m3u .zip\n\nCopy Dreamcast & Naomi ROMs To: ${romdir}/dreamcast\n\nCopy Dreamcast BIOS Files (dc_boot.bin & dc_flash.bin) To: ${biosdir}/dreamcast/dc\n\nCopy Naomi & Atomiswave BIOS Files (awbios.zip & naomi.zip) To: ${biosdir}/dreamcast/dc/"
-rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/flycast/master/LICENSE"
-rp_module_repo="git https://github.com/libretro/flycast master"
+rp_module_desc="Sega Dreamcast, Naomi, Naomi 2 & Sammy Atomiswave Libretro Core"
+rp_module_help="ROM Extensions: .7z .bin .cdi .chd .cue .dat .elf .gdi .lst .m3u .zip \n\nCopy Dreamcast ROMs To: ${romdir}/dreamcast \n\nCopy Atomiswave ROMs To: ${romdir}/atomiswave \n\nCopy Naomi ROMs To: ${romdir}/naomi \n\nCopy Naomi2 ROMs To: ${romdir}/naomi2 \n\nCopy Dreamcast BIOS File (dc_boot.bin) To: ${biosdir}/dreamcast/dc \n\nCopy Naomi, Naomi2 & Atomiswave BIOS Files (awbios.zip, naomi.zip & naomi2.zip) To: ${biosdir}/dreamcast/dc/"
+rp_module_licence="GPL2 https://raw.githubusercontent.com/flyinghead/flycast/master/LICENSE"
+rp_module_repo="git https://github.com/flyinghead/flycast master"
 rp_module_section="opt"
 rp_module_flags=""
 
 function depends_lr-flycast() {
-    local depends=('zlib')
+    local depends=(
+        'cmake'
+        'ninja'
+        'libzip'
+        'zlib'
+    )
+    isPlatform "x11" && depends+=('libgl')
 
     getDepends "${depends[@]}"
 }
 
 function sources_lr-flycast() {
     gitPullOrClone
-
-    # Prevent ArchyPie C/CXXFLAGS From Being Overwritten
-    sed -e "s|^CFLAGS[[:blank:]]*:=[[:blank:]]*|#CFLAGS :=|g" -i "${md_build}/Makefile"
-    sed -e "s|^CXXFLAGS[[:blank:]]*:=[[:blank:]]*|#CXXFLAGS :=|g" -i "${md_build}/Makefile"
-    sed -e "s|^OPTFLAGS[[:blank:]]*:=[[:blank:]]*-O3|OPTFLAGS := -O2|g" -i "${md_build}/Makefile"
 }
 
 function build_lr-flycast() {
-    local params=('HAVE_LTCG=0')
+    local params=()
 
-    isPlatform "aarch64" && params+=('WITH_DYNAREC=arm64' 'HOST_CPU_FLAGS=-DTARGET_LINUX_ARMv8')
-    isPlatform "arm" && params+=('WITH_DYNAREC=arm')
-    ! isPlatform "x86" && params+=('HAVE_GENERIC_JIT=0')
-    isPlatform "vulkan" && params+=('HAVE_VULKAN=1') || params+=('HAVE_VULKAN=0')
+    isPlatform "gles" && ! isPlatform "gles3" && params+=("-DUSE_GLES2=ON")
+    isPlatform "gles3" && params+=("-DUSE_GLES=ON")
+    ! isPlatform "x86" && params+=("-DUSE_VULKAN=OFF")
 
-    make "${params[@]}" clean
-    make "${params[@]}"
+    cmake . \
+        -B"build" \
+        -G"Ninja" \
+        -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -DLIBRETRO="ON" \
+        -DUSE_HOST_LIBZIP="ON" \
+        "${params[@]}" \
+        -Wno-dev
+    ninja -C build clean
+    ninja -C build
 
-    md_ret_require="${md_build}/flycast_libretro.so"
+    md_ret_require="${md_build}/build/flycast_libretro.so"
 }
 
 function install_lr-flycast() {
-    md_ret_files=('flycast_libretro.so')
+    md_ret_files=('build/flycast_libretro.so')
 }
 
 function configure_lr-flycast() {
+    local systems=(
+        'arcade'
+        'atomiswave'
+        'dreamcast'
+        'naomi'
+        'naomi2'
+    )
+
     if [[ "${md_mode}" == "install" ]]; then
-        mkRomDir "arcade"
-        mkRomDir "dreamcast"
+        for system in "${systems[@]}"; do
+            mkRomDir "${system}"
+        done
 
         mkUserDir "${biosdir}/dreamcast"
         mkUserDir "${biosdir}/dreamcast/dc"
     fi
 
-    defaultRAConfig "arcade" "system_directory" "${biosdir}/dreamcast"
-    defaultRAConfig "dreamcast" "system_directory" "${biosdir}/dreamcast"
+    for system in "${systems[@]}"; do
+        local def=1
+        if [[ "${system}" == "arcade" ]]; then
+            def=0
+        fi
 
-    addEmulator 0 "${md_id}" "arcade" "${md_inst}/flycast_libretro.so"
-    addEmulator 1 "${md_id}" "dreamcast" "${md_inst}/flycast_libretro.so"
-
-    addSystem "arcade"
-    addSystem "dreamcast"
+        defaultRAConfig "${system}" "system_directory" "${biosdir}/dreamcast"
+        addEmulator "${def}" "${md_id}" "${system}" "${md_inst}/flycast_libretro.so"
+        addSystem "${system}"
+    done
 }
