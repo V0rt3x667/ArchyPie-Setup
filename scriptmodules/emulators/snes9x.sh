567ef493328a49ffdc0482df960eae4ee97ce0aa
V0rt3x667 <al-g0l@outlook.com>
2023-11-17 18:16:42 +0000

    Update snes9x.sh
    * Fix patch
    * Update dependencies & build options
    * Switch to Clang & LLD for compilation
    * Create default config file

--- scriptmodules/emulators/snes9x.sh
+++ scriptmodules/emulators/snes9x.sh
@@ -8,7 +8,7 @@ rp_module_id="snes9x"
 rp_module_desc="SNES9X: Nintendo SNES Emulator"
 rp_module_help="ROM Extensions: .bin .fig .mgd .sfc .smc .swc .zip\n\nCopy SNES ROMs To: ${romdir}/snes"
 rp_module_licence="NONCOM https://raw.githubusercontent.com/snes9xgit/snes9x/master/LICENSE"
-rp_module_repo="git https://github.com/snes9xgit/snes9x :_get_branch_snes9x"
+rp_module_repo="git https://github.com/snes9xgit/snes9x master" #:_get_branch_snes9x Not able To Build From Release Tag 1.62.3
 rp_module_section="main"
 rp_module_flags=""
 
@@ -19,18 +19,25 @@ function _get_branch_snes9x() {
 function depends_snes9x() {
     local depends=(
         'alsa-lib'
-        'boost-libs'
+        'cairo'
+        'clang'
         'cmake'
+        'glslang'
+        'gtkmm3'
+        'libepoxy'
+        'libpng'
         'libpulse'
         'libx11'
         'libxv'
-        'meson'
+        'lld'
         'minizip'
         'ninja'
         'portaudio'
-        'sdl2_ttf'
+        'python'
         'sdl2'
+        'zlib'
     )
+    isPlatform "x11" && depends+=('libxrandr')
     getDepends "${depends[@]}"
 }
 
@@ -42,15 +49,27 @@ function sources_snes9x() {
 }
 
 function build_snes9x() {
-    cd gtk || exit
-
-    meson setup -Dprefix="${md_inst}" -Dbuildtype="release" build
-    meson compile -j"${__jobs}" -C build
-    md_ret_require="${md_build}/gtk/build/${md_id}-gtk"
+    cmake . \
+        -B"build" \
+        -G"Ninja" \
+        -S"gtk" \
+        -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
+        -DCMAKE_BUILD_TYPE="Release" \
+        -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -DCMAKE_C_COMPILER="clang" \
+        -DCMAKE_CXX_COMPILER="clang++" \
+        -DCMAKE_EXE_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DCMAKE_MODULE_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DCMAKE_SHARED_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DUSE_OSS="OFF" \
+        -Wno-dev
+    ninja -C build clean
+    ninja -C build
+    md_ret_require="${md_build}/build/${md_id}-gtk"
 }
 
 function install_snes9x() {
-    ninja -C gtk/build install
+    ninja -C build install/strip
     md_ret_require="${md_inst}/bin/${md_id}-gtk"
 }
 
@@ -59,6 +78,34 @@ function configure_snes9x() {
 
     if [[ "${md_mode}" == "install" ]]; then
         mkRomDir "snes"
+
+        # Create Snes9x Directories
+        local dirs=(
+            'cheats'
+            'patchs'
+            'saves'
+            'SRAM'
+        )
+        for dir in "${dirs[@]}"; do
+            mkUserDir "${md_conf_root}/snes/${md_id}/${dir}"
+        done
+
+        # Set Default Config File
+        local config
+        config="$(mktemp)"
+        iniConfig " = " "" "${config}"
+
+        echo "[Display]" > "${config}"
+        iniSet "FullscreenOnOpen" "true"
+        echo "[Files]" >> "${config}"
+        iniSet "CheatDirectory"     "${md_conf_root}/snes/${md_id}/cheats"
+        iniSet "PatchDirectory"     "${md_conf_root}/snes/${md_id}/patchs"
+        iniSet "SRAMDirectory"      "${md_conf_root}/snes/${md_id}/SRAM"
+        iniSet "SaveStateDirectory" "${md_conf_root}/snes/${md_id}/saves"
+
+        copyDefaultConfig "${config}" "${md_conf_root}/snes/${md_id}/snes9x.conf"
+        rm "${config}"
+
     fi
 
     addEmulator 1 "${md_id}" "snes" "${md_inst}/bin/${md_id}-gtk %ROM%"
