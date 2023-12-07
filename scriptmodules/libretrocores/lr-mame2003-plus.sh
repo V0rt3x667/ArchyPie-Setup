eff834c06b09761dd7a60e142731072b896c86b2
V0rt3x667 <al-g0l@outlook.com>
2023-12-07 19:52:56 +0000

    Update lr-mame2003-plus.sh
    * Tidy up script

--- scriptmodules/libretrocores/lr-mame2003-plus.sh
+++ scriptmodules/libretrocores/lr-mame2003-plus.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-mame2003-plus"
 rp_module_desc="MAME 0.78 Enhanced Libretro Core"
-rp_module_help="ROM Extension: .zip\n\nCopy MAME ROMs To:\n${romdir}/mame-libretro\nOr\n${romdir}/arcade"
+rp_module_help="ROM Extension: .zip\n\nCopy MAME ROMs To Either:\n${romdir}/mame-libretro\n${romdir}/arcade"
 rp_module_licence="NONCOM https://raw.githubusercontent.com/libretro/mame2003-plus-libretro/master/LICENSE.md"
 rp_module_repo="git https://github.com/libretro/mame2003-plus-libretro master"
 rp_module_section="opt"
@@ -17,9 +17,9 @@ function sources_lr-mame2003-plus() {
 
 function build_lr-mame2003-plus() {
     rpSwap on 750
-    make clean
     local params=()
     isPlatform "arm" && params+=("ARM=1")
+    make clean
     make ARCH="${CFLAGS}" "${params[@]}"
     rpSwap off
     md_ret_require="${md_build}/mame2003_plus_libretro.so"
@@ -33,36 +33,39 @@ function install_lr-mame2003-plus() {
 }
 
 function configure_lr-mame2003-plus() {
+    local systems=(
+        'arcade'
+        'mame-libretro'
+    )
+
     if [[ "${md_mode}" == "install" ]]; then
         local core_name="mame2003-plus"
-        local mame_dir=('arcade' 'mame-libretro')
-        local mame_sub_dir=('cfg' 'ctrlr' 'diff' 'hi' 'memcard' 'nvram')
-        for dir in "${mame_dir[@]}"; do
-            mkRomDir "${dir}"
-            mkRomDir "${dir}/${core_name}"
-            for sub_dir in "${mame_sub_dir[@]}"; do
-            mkRomDir "${dir}/${core_name}/${sub_dir}"
-            done
+
+        for system in "${systems[@]}"; do
+            mkRomDir "${system}"
+            defaultRAConfig "${system}"
         done
 
-        mkUserDir "${biosdir}/${core_name}"
-        mkUserDir "${biosdir}/${core_name}/samples"
-        mkUserDir "${biosdir}/${core_name}/artwork"
+        # Create BIOS Directory
+        mkUserDir "${biosdir}/mame-libretro/${core_name}/"{artwork,samples}
 
-        # Copy hiscore.dat, cheat.dat & artwork
-        cp -rv "${md_inst}/metadata/"{cheat.dat,hiscore.dat} "${biosdir}/${core_name}/"
-        cp "${md_inst}/metadata/artwork/"* "${biosdir}/${core_name}/artwork/"
-        chown -R "${user}:${user}" "${biosdir}/${core_name}"
+        # Copy 'hiscore.dat', 'cheat.dat' & 'artwork'
+        cp -rv "${md_inst}/metadata/"{cheat.dat,hiscore.dat} "${biosdir}/mame-libretro/${core_name}/"
+        cp "${md_inst}/metadata/artwork/"* "${biosdir}/mame-libretro/${core_name}/artwork/"
+        chown -R "${user}:${user}" "${biosdir}/mame-libretro/${core_name}"
+
+        # Symlink BIOS Directory To 'arcade' So Assets Can Be Shared
+        ln -snf "${biosdir}/mame-libretro/${core_name}" "${biosdir}/arcade/${core_name}"
+
+        setRetroArchCoreOption "${core_name}_skip_disclaimer" "enabled"
     fi
 
-    setRetroArchCoreOption "${core_name}_skip_disclaimer" "enabled"
-
-    defaultRAConfig "arcade"
-    defaultRAConfig "mame-libretro"
-
-    addEmulator 0 "${md_id}" "arcade" "${md_inst}/mame2003_plus_libretro.so"
-    addEmulator 1 "${md_id}" "mame-libretro" "${md_inst}/mame2003_plus_libretro.so"
-
-    addSystem "arcade"
-    addSystem "mame-libretro"
+    for system in "${systems[@]}"; do
+        local def=1
+        if [[ "${system}" == "arcade" ]]; then
+            def=0
+        fi
+        addEmulator "${def}" "${md_id}" "${system}" "${md_inst}/mame2003_plus_libretro.so"
+        addSystem "${system}"
+    done
 }
