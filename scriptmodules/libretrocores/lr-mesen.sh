2dd88ad2eddcc1c203d5590c671b7be6226c076b
V0rt3x667 <al-g0l@outlook.com>
2023-12-08 20:29:52 +0000

    Update lr-mesen.sh
    * Tidy up script

--- scriptmodules/libretrocores/lr-mesen.sh
+++ scriptmodules/libretrocores/lr-mesen.sh
@@ -6,8 +6,8 @@
 
 rp_module_id="lr-mesen"
 rp_module_desc="Nintendo NES (Famicom) & Famicom Disk System Libretro Core"
-rp_module_help="ROM Extensions: .fds .nes .unf .unif .zip\n\nCopy NES ROMs To: ${romdir}/nes\nCopy Famicom ROMs To: ${romdir}/fds\nCopy BIOS File (disksys.rom) To: ${biosdir}/fds"
-rp_module_licence="GPL3 https://raw.githubusercontent.com/sourmesen/mesen/master/LICENSE"
+rp_module_help="ROM Extensions: .7z .fds .nes .unf .unif .zip\n\nCopy NES ROMs To: ${romdir}/nes\n\nCopy Famicom ROMs To: ${romdir}/fds\n\nCopy BIOS File: disksys.rom To: ${biosdir}/fds\n\nCopy Famicom Disk System BIOS File: disksys.rom To: ${biosdir}/fds\n\nOPTIONAL: Copy HD Packs & Custom Palettes To The Relevant BIOS Directory"
+rp_module_licence="GPL3 https://raw.githubusercontent.com/libretro/Mesen/master/LICENSE"
 rp_module_repo="git https://github.com/libretro/Mesen master"
 rp_module_section="exp"
 rp_module_flags=""
@@ -27,13 +27,21 @@ function install_lr-mesen() {
 }
 
 function configure_lr-mesen() {
-    local system
-    for system in "fds" "nes"; do
-        mkRomDir "${system}"
+    local systems=(
+        'fds'
+        'nes'
+    )
+
+    if [[ "${md_mode}" == "install" ]]; then
+        for system in "${systems[@]}"; do
+            mkRomDir "${system}"
+            mkUserDir "${biosdir}/${system}"
+            defaultRAConfig "${system}"
+        done
+    fi
+
+    for system in "${systems[@]}"; do
         addEmulator 0 "${md_id}" "${system}" "${md_inst}/mesen_libretro.so"
         addSystem "${system}"
     done
-
-    defaultRAConfig "fds" "system_directory" "${biosdir}/fds"
-    defaultRAConfig "nes"
 }
