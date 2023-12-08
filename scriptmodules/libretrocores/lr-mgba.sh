531936c7631f669367cf828e7ca72453aeeddf9b
V0rt3x667 <al-g0l@outlook.com>
2023-12-08 20:40:40 +0000

    Update lr-mgba.sh
    * Tidy up script

--- scriptmodules/libretrocores/lr-mgba.sh
+++ scriptmodules/libretrocores/lr-mgba.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-mgba"
 rp_module_desc="Nintendo Game Boy, Game Boy Advance & Game Boy Color Libretro Core"
-rp_module_help="ROM Extensions: .gb .gba .gbc .zip\n\nCopy Game Boy ROMs To: ${romdir}/gb\nCopy Game Boy Color ROMs To: ${romdir}/gbc\nCopy Game Boy Advance ROMs To: ${romdir}/gba\n\nOPTIONAL:\nCopy BIOS File (gb_bios.bin) To: ${biosdir}/gb\nCopy BIOS File (gba_bios.bin) To: ${biosdir}/gba\nCopy BIOS File (gbc_bios.bin) To: ${biosdir}/gbc"
+rp_module_help="ROM Extensions: .gb .gba .gbc .zip\n\nCopy Game Boy ROMs To: ${romdir}/gb\nCopy Game Boy Color ROMs To: ${romdir}/gbc\nCopy Game Boy Advance ROMs To: ${romdir}/gba\n\nOPTIONAL: Copy BIOS File: gb_bios.bin To: ${biosdir}/gb\n\nCopy BIOS File: gba_bios.bin To: ${biosdir}/gba\n\nCopy BIOS File: gbc_bios.bin To: ${biosdir}/gbc"
 rp_module_licence="MPL2 https://raw.githubusercontent.com/libretro/mgba/master/LICENSE"
 rp_module_repo="git https://github.com/libretro/mgba master"
 rp_module_section="main"
@@ -30,20 +30,25 @@ function install_lr-mgba() {
 }
 
 function configure_lr-mgba() {
-    local system
-    local def
+    local systems=(
+        'gb'
+        'gba'
+        'gbc'
+    )
 
     if [[ "${md_mode}" == "install" ]]; then
-        for system in 'gb' 'gba' 'gbc'; do
+        for system in "${systems[@]}"; do
             mkRomDir "${system}"
             mkUserDir "${biosdir}/${system}"
+            defaultRAConfig "${system}"
         done
     fi
 
-    for system in 'gb' 'gba' 'gbc'; do
-        def=0
-        [[ "${system}" == "gba" ]] && def=1
-        defaultRAConfig "${system}" "system_directory" "${biosdir}/${system}"
+    for system in "${systems[@]}"; do
+        local def=0
+        if [[ "${system}" == "gba" ]]; then
+            def=1
+        fi
         addEmulator "${def}" "${md_id}" "${system}" "${md_inst}/mgba_libretro.so"
         addSystem "${system}"
     done
