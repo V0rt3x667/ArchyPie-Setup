661b3811c96d1edc5a3bdbf79b0c39393ef2d3d2
V0rt3x667 <al-g0l@outlook.com>
2023-12-08 19:32:01 +0000

    Update lr-puae.sh
    * Tidy up script

--- scriptmodules/libretrocores/lr-puae.sh
+++ scriptmodules/libretrocores/lr-puae.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-puae"
 rp_module_desc="Commodore Amiga 500, 500+, 600, 1200, 4000, CDTV & CD32 Libretro Core"
-rp_module_help="ROM Extensions: .7z .adf .adz .ccd .chd .cue .dms .fdi .hdf .hdz .info .ipf .iso .lha .m3u .mds .nrg .slave .uae .zip \n\nCopy Amiga Games To: ${romdir}/amiga \nCopy CD32 Games To: ${romdir}/amigacd32 \nCopy CDTV Games To: ${romdir}/amigacdtv \n\nCopy BIOS Files: \n\nkick34005.A500 \nkick40063.A600 \nkick40068.A1200 \nkick40060.CD32 \nkick34005.CDTV \n\nTo: ${biosdir}/amiga"
+rp_module_help="ROM Extensions: .7z .adf .adz .ccd .chd .cue .dms .fdi .hdf .hdz .info .ipf .iso .lha .m3u .mds .nrg .slave .uae .zip\n\nCopy Amiga Games To: ${romdir}/amiga\nCopy CD32 Games To: ${romdir}/amigacd32\nCopy CDTV Games To: ${romdir}/amigacdtv\n\nCopy BIOS Files: kick34005.A500, kick40063.A600, kick40068.A1200, kick40060.CD32 & kick34005.CDTV To: ${biosdir}/amiga"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/PUAE/master/COPYING"
 rp_module_repo="git https://github.com/libretro/libretro-uae master"
 rp_module_section="opt"
@@ -32,6 +32,9 @@ function install_lr-puae() {
 }
 
 function configure_lr-puae() {
+    local core="${1}"
+    [[ -z "${core}" ]] && core="puae_libretro.so"
+
     local systems=(
         'amiga'
         'amigacd32'
@@ -41,30 +44,39 @@ function configure_lr-puae() {
     if [[ "${md_mode}" == "install" ]]; then
         for system in "${systems[@]}"; do
             mkRomDir "${system}"
-            defaultRAConfig "${system}" "system_directory" "${biosdir}/amiga"
         done
 
+        # Create BIOS Directory
         mkUserDir "${biosdir}/amiga"
 
         # Copy CAPs Image & Floppy Disk Audio Files To BIOS Directory
         install -Dm644 "${md_inst}/capsimg.so" -t "${biosdir}/amiga/"
         cp -r "${md_inst}/uae_data" -t "${biosdir}/amiga/"
+        chown -R "${user}:${user}" "${biosdir}/amiga"
+
+        # Symlink Supported Systems' BIOS Dirs To 'amiga'
+        for system in "${systems[@]}"; do
+            if [[ "${system}" != "amiga" ]]; then
+                ln -snf "${biosdir}/amiga" "${biosdir}/${system}"
+            fi
+        done
 
         # Force CDTV System
         local config="${md_conf_root}/amigacdtv/retroarch-core-options.cfg"
-        iniConfig " = " '"' "${config}"
-        iniSet "puae_model" "CDTV"
-        chown "${user}:${user}" "${config}"
-
-        # Add CDTV Overide To 'retroarch.cfg', 'defaultRAConfig' Can Only Be Called Once
-        local raconfig="${md_conf_root}/amigacdtv/retroarch.cfg"
-        iniConfig " = " '"' "${raconfig}"
-        iniSet "core_options_path" "${config}"
-        chown "${user}:${user}" "${raconfig}"
+        for system in "${systems[@]}"; do
+            if [[ "${system}" == "amigacdtv" ]]; then
+                defaultRAConfig  "${system}" "core_options_path" "${config}"
+                iniConfig " = " '"' "${config}"
+                iniSet "puae_model" "CDTV" "${config}"
+                chown "${user}:${user}" "${config}"
+            else
+                defaultRAConfig "${system}"
+            fi
+        done
     fi
 
     for system in "${systems[@]}"; do
-        addEmulator 1 "${md_id}" "${system}" "${md_inst}/puae_libretro.so"
+        addEmulator 1 "${md_id}" "${system}" "${md_inst}/${core}"
         addSystem "${system}"
     done
 }
