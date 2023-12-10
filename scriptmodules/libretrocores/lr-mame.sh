9795f50b49cf30beff36cc58cdd0228c2a952219
V0rt3x667 <al-g0l@outlook.com>
2023-12-10 13:10:35 +0000

    Update lr-mame.sh
    * Tidy up and add other supported systems to the script

--- scriptmodules/libretrocores/lr-mame.sh
+++ scriptmodules/libretrocores/lr-mame.sh
@@ -6,14 +6,24 @@
 
 rp_module_id="lr-mame"
 rp_module_desc="MAME (Latest Version) Libretro Core"
-rp_module_help="ROM Extension: .zip\n\nCopy MAME ROMs To: ${romdir}/mame-libretro\nOr\n${romdir}/arcade"
+rp_module_help="ROM Extension: .zip\n\nCopy MAME ROMs To Either: ${romdir}/mame-libretro\n${romdir}/arcade\n\nIf You Wish To Have Seperate Systems For Sega Titan Video (ST-V), Naomi, Naomi 2 & Sammy Atomiswave Copy Their ROMs To:\n${romdir}/segastv\n${romdir}/naomi\n${romdir}/naomi2\n${romdir}/atomiswave"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/mame/master/COPYING"
 rp_module_repo="git https://github.com/libretro/mame master"
 rp_module_section="exp"
 rp_module_flags=""
 
 function _get_params_lr-mame() {
-    local params=(CONFIG=libretro NO_USE_MIDI=1 NOWERROR=1 OS=linux OSD=retro PYTHON_EXECUTABLE=python RETRO=1 TARGET=mame TARGETOS=linux)
+    local params=(
+        'CONFIG=libretro'
+        'NO_USE_MIDI=1'
+        'NOWERROR=1'
+        'OS=linux'
+        'OSD=retro'
+        'PYTHON_EXECUTABLE=python'
+        'RETRO=1'
+        'TARGET=mame'
+        'TARGETOS=linux'
+    )
     isPlatform "64bit" && params+=('PTR64=1')
     echo "${params[@]}"
 }
@@ -49,10 +59,30 @@ function install_lr-mame() {
 }
 
 function configure_lr-mame() {
-    local system
-    for system in arcade mame-libretro; do
-        mkRomDir "${system}"
-        defaultRAConfig "${system}"
+    local systems=(
+        'arcade'
+        'atomiswave'
+        'mame-libretro'
+        'naomi'
+        'naomi2'
+        'segastv'
+    )
+
+    if [[ "${md_mode}" == "install" ]]; then
+        for system in "${systems[@]}"; do
+            mkRomDir "${system}"
+            mkUserDir "${biosdir}/${system}"
+            defaultRAConfig "${system}"
+        done
+
+        # Symlink Supported Systems BIOS Dirs To 'mame'
+        mkUserDir "${biosdir}/mame"
+        for system in "${systems[@]}"; do
+            ln -snf "${biosdir}/mame" "${biosdir}/${system}/mame"
+        done
+    fi
+
+    for system in "${systems[@]}"; do
         addEmulator 0 "${md_id}" "${system}" "${md_inst}/mamearcade_libretro.so"
         addSystem "${system}"
     done
