a7a7a3c4a4a5dd259c6aa1722b331c770fc8131d
V0rt3x667 <al-g0l@outlook.com>
2023-12-12 01:15:53 +0000

    Update lr-pcsx-rearmed.sh
    * Tidy up script

--- scriptmodules/libretrocores/lr-pcsx-rearmed.sh
+++ scriptmodules/libretrocores/lr-pcsx-rearmed.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-pcsx-rearmed"
 rp_module_desc="Sony PlayStation Libretro Core"
-rp_module_help="ROM Extensions: .bin .cbn .chd .cue .img .m3u .mdf .pbp .toc\n\nCopy PSX ROMs To: ${romdir}/psx\n\nCopy BIOS Files:\n\nscph5500.bin\nscph5501.bin\nscph5502.bin\n\nTo: ${biosdir}/psx"
+rp_module_help="ROM Extensions: .bin .cbn .chd .cue .img .m3u .mdf .pbp .toc\n\nCopy PSX ROMs To: ${romdir}/psx\n\nCopy BIOS Files: scph5500.bin, scph5501.bin & scph5502.bin To: ${biosdir}/psx"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/pcsx_rearmed/master/COPYING"
 rp_module_repo="git https://github.com/libretro/pcsx_rearmed master"
 rp_module_section="opt arm=main aarch64=main"
@@ -22,15 +22,15 @@ function sources_lr-pcsx-rearmed() {
 }
 
 function build_lr-pcsx-rearmed() {
-    local params=(THREAD_RENDERING=0)
+    local params=('THREAD_RENDERING=0')
 
     if isPlatform "arm"; then
-        params+=(ARCH=arm DYNAREC=ari64)
+        params+=('ARCH=arm' 'DYNAREC=ari64')
     elif isPlatform "aarch64"; then
-        params+=(ARCH=aarch64 DYNAREC=ari64)
+        params+=('ARCH=aarch64' 'DYNAREC=ari64')
     fi
     if isPlatform "neon"; then
-        params+=(HAVE_NEON=1 HAVE_NEON_ASM=1 BUILTIN_GPU=neon)
+        params+=('HAVE_NEON=1' 'HAVE_NEON_ASM=1' 'BUILTIN_GPU=neon')
     fi
 
     make -f Makefile.libretro "${params[@]}" clean
@@ -45,11 +45,9 @@ function install_lr-pcsx-rearmed() {
 function configure_lr-pcsx-rearmed() {
     if [[ "${md_mode}" == "install" ]]; then
         mkRomDir "psx"
-
         mkUserDir "${biosdir}/psx"
+        defaultRAConfig "psx"
     fi
-
-    defaultRAConfig "psx" "system_directory" "${biosdir}/psx"
 
     addEmulator 1 "${md_id}" "psx" "${md_inst}/pcsx_rearmed_libretro.so"
 
