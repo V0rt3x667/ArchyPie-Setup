6e30c3957335c9bfda7a969ac6c72ae7c7431b3c
V0rt3x667 <al-g0l@outlook.com>
2023-12-12 20:49:57 +0000

    Update lr-prosystem.sh
    * Tidy up script

--- scriptmodules/libretrocores/lr-prosystem.sh
+++ scriptmodules/libretrocores/lr-prosystem.sh
@@ -5,8 +5,8 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="lr-prosystem"
-rp_module_desc="Atari 7800 Libretro Core"
-rp_module_help="ROM Extensions: .a78 .bin .cdf .zip\n\nCopy Atari 7800 ROMs To: ${romdir}/atari7800\n\nOPTIONAL: Copy BIOS File (7800 BIOS (U).rom) To: ${biosdir}/atari7800"
+rp_module_desc="Atari 7800 ProSystem Libretro Core"
+rp_module_help="ROM Extensions: .a78 .bin .cdf .zip\n\nCopy Atari 7800 ROMs To: ${romdir}/atari7800\n\nOPTIONAL: Copy BIOS Files: 7800 BIOS (E).rom & 7800 BIOS (U).rom To: ${biosdir}/atari7800"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/prosystem-libretro/master/License.txt"
 rp_module_repo="git https://github.com/libretro/prosystem-libretro master"
 rp_module_section="main"
@@ -28,11 +28,9 @@ function install_lr-prosystem() {
 function configure_lr-prosystem() {
     if [[ "${md_mode}" == "install" ]]; then
         mkRomDir "atari7800"
-
         mkUserDir "${biosdir}/atari7800"
+        defaultRAConfig "atari7800"
     fi
-
-    defaultRAConfig "atari7800" "system_directory" "${biosdir}/atari7800"
 
     addEmulator 1 "${md_id}" "atari7800" "${md_inst}/prosystem_libretro.so"
 
