e8888d3f419cb71ed83bb03d09ae9f974ce29f82
V0rt3x667 <al-g0l@outlook.com>
2023-12-12 02:01:05 +0000

    Update lr-play.sh
    * Tidy up script

--- scriptmodules/libretrocores/lr-play.sh
+++ scriptmodules/libretrocores/lr-play.sh
@@ -15,11 +15,13 @@ rp_module_flags="!all 64bit"
 function depends_lr-play() {
     local depends=(
         'bzip2'
+        'glang'
         'glew'
         'glibc'
         'icu'
         'libgl'
         'libglvnd'
+        'lld'
         'zlib'
     )
     getDepends "${depends[@]}"
@@ -40,10 +42,16 @@ function build_lr-play() {
         -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
         -DCMAKE_BUILD_TYPE="Release" \
         -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -DCMAKE_C_COMPILER="clang" \
+        -DCMAKE_CXX_COMPILER="clang++" \
+        -DCMAKE_EXE_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DCMAKE_MODULE_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DCMAKE_SHARED_LINKER_FLAGS_INIT="-fuse-ld=lld" \
         -DBUILD_LIBRETRO_CORE="ON" \
         -DBUILD_PLAY="OFF" \
         -DBUILD_TESTS="OFF" \
         -DENABLE_AMAZON_S3="OFF" \
+        -DWITH_SYSTEM_ZLIB="ON" \
         -Wno-dev
     ninja -C build clean
     ninja -C build
@@ -60,9 +68,10 @@ function install_lr-play() {
 function configure_lr-play() {
     moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/ps2/${md_id}"
 
-    mkRomDir "ps2"
-
-    defaultRAConfig "ps2"
+    if [[ "${md_mode}" == "install" ]]; then
+        mkRomDir "ps2"
+        defaultRAConfig "ps2"
+    fi
 
     addEmulator 0 "${md_id}" "ps2" "${md_inst}/play_libretro.so"
 
