568d15b1afda1380a2eaa02dead0e7a4f4f93662
V0rt3x667 <al-g0l@outlook.com>
2023-12-13 20:36:12 +0000

    Update lr-swanstation.sh
    * Fix build
    * Tidy up script

--- scriptmodules/libretrocores/lr-swanstation.sh
+++ scriptmodules/libretrocores/lr-swanstation.sh
@@ -6,10 +6,10 @@
 
 rp_module_id="lr-swanstation"
 rp_module_desc="Sony Playstation Libretro Core"
-rp_module_help="ROM Extensions: .bin .chd .cue .ecm .exe .img .iso .m3u .mds .pbp .psexe .psf\n\nCopy PlayStation ROMs To: ${romdir}/psx\n\nCopy BIOS Files:\n\nscph5500.bin\nscph5501.bin\nscph5502.bin\n\nTo: ${biosdir}/psx"
+rp_module_help="ROM Extensions: .bin .chd .cue .ecm .exe .img .iso .m3u .mds .pbp .psexe .psf\n\nCopy PlayStation ROMs To: ${romdir}/psx\n\nCopy BIOS Files: scph5500.bin, scph5501.bin & scph5502.bin To: ${biosdir}/psx"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/libretro/swanstation/main/LICENSE"
-rp_module_section="exp"
 rp_module_repo="git https://github.com/libretro/swanstation main"
+rp_module_section="exp"
 rp_module_flags=""
 
 function depends_lr-swanstation() {
@@ -31,6 +31,12 @@ function build_lr-swanstation() {
         -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
         -DCMAKE_BUILD_TYPE="Release" \
         -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -DCMAKE_C_COMPILER="clang" \
+        -DCMAKE_CXX_COMPILER="clang++" \
+        -DCMAKE_EXE_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DCMAKE_MODULE_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DCMAKE_SHARED_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DCMAKE_CXX_FLAGS="${CXXFLAGS} -Wno-enum-constexpr-conversion" \
         -Wno-dev
     ninja -C build clean
     ninja -C build
@@ -42,15 +48,13 @@ function install_lr-swanstation() {
 }
 
 function configure_lr-swanstation() {
-        if [[ "${md_mode}" == "install" ]]; then
+    if [[ "${md_mode}" == "install" ]]; then
         mkRomDir "psx"
-
         mkUserDir "${biosdir}/psx"
+        defaultRAConfig "psx"
+
+        isPlatform "rpi2" && setRetroArchCoreOption "swanstation_GPU.Renderer" "Software"
     fi
-
-    isPlatform "rpi" && setRetroArchCoreOption "swanstation_GPU.Renderer" "Software"
-
-    defaultRAConfig "psx" "system_directory" "${biosdir}/psx"
 
     addEmulator 0 "${md_id}" "psx" "${md_inst}/swanstation_libretro.so" 
 
