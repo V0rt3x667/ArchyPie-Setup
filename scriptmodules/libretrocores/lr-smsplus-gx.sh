0bb19d07b283c0f170da13748e23c448e42a8ee3
V0rt3x667 <al-g0l@outlook.com>
2023-12-13 01:55:55 +0000

    Update lr-smsplus-gx.sh
    * Add coleco as a supported system
    * Tidy up script

--- scriptmodules/libretrocores/lr-smsplus-gx.sh
+++ scriptmodules/libretrocores/lr-smsplus-gx.sh
@@ -5,8 +5,8 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="lr-smsplus-gx"
-rp_module_desc="Sega Master System & Game Gear Libretro Core"
-rp_module_help="ROM Extensions: .bin .col .gg .rom .sg .sms .zip\nCopy Game Gear ROMs To: ${romdir}/gamegear\nCopy MasterSystem ROMs To: ${romdir}/mastersystem"
+rp_module_desc="Sega Master System, Game Gear & Coleco ColecoVision Libretro Core"
+rp_module_help="ROM Extensions: .bin .col .gg .rom .sg .sms .zip\nCopy Game Gear ROMs To: ${romdir}/gamegear\n\nCopy MasterSystem ROMs To: ${romdir}/mastersystem\n\nCopy ColecoVision ROMs To: ${romdir}/coleco\n\nOPTIONAL: Copy BIOS File: bios.sms To: ${biosdir}/mastersystem\nCopy BIOS File: BIOS.col To: ${biosdir}/coleco"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/smsplus-gx/master/docs/license"
 rp_module_repo="git https://github.com/libretro/smsplus-gx master"
 rp_module_section="exp"
@@ -26,10 +26,21 @@ function install_lr-smsplus-gx() {
 }
 
 function configure_lr-smsplus-gx() {
-    local system
-    for system in gamegear mastersystem; do
-        mkRomDir "${system}"
-        defaultRAConfig "${system}"
+    local systems=(
+        'coleco'
+        'gamegear'
+        'mastersystem'
+    )
+
+    if [[ "${md_mode}" == "install" ]]; then
+        for system in "${systems[@]}"; do
+            mkRomDir "${system}"
+            mkUserDir "${biosdir}/${system}"
+            defaultRAConfig "${system}"
+        done
+    fi
+
+    for system in "${systems[@]}"; do
         addEmulator 0 "${md_id}" "${system}" "${md_inst}/smsplus_libretro.so"
         addSystem "${system}"
     done
