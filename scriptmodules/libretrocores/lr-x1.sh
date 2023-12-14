ae359d53b49c41be82c2462e15def206124ccf31
V0rt3x667 <al-g0l@outlook.com>
2023-12-14 23:24:34 +0000

    Update lr-x1.sh
    * Remove hardcoded BIOS directory
    * Fix issue with temp files
    * Tidy up script

--- scriptmodules/libretrocores/lr-x1.sh
+++ scriptmodules/libretrocores/lr-x1.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="lr-x1"
 rp_module_desc="Sharp X1 Libretro Core"
-rp_module_help="ROM Extensions: .2d .2hd .88d .cmd .d88 .dup .dx1 .hdm .tap .tfd .xdf .zip .zip\n\nCopy X1 ROMs To: ${romdir}/x1\n\nCopy BIOS Files (iplrom.x1 & iplrom.x1t) To: ${biosdir}/x1"
+rp_module_help="ROM Extensions: .2d .2hd .88d .cmd .d88 .dup .dx1 .hdm .tap .tfd .xdf .zip .zip\n\nCopy X1 ROMs To: ${romdir}/x1\n\nCopy BIOS Files: IPLROM.X1 & IPLROM.X1T To: ${biosdir}/x1"
 rp_module_licence="BSD https://raw.githubusercontent.com/libretro/xmil-libretro/master/LICENSE"
 rp_module_repo="git https://github.com/libretro/xmil-libretro master"
 rp_module_section="exp"
@@ -14,13 +14,16 @@ rp_module_section="exp"
 function sources_lr-x1() {
     gitPullOrClone
 
-    # Set BIOS Directory
-    sed -e "s|%s%cxmil|%s%cx1|g" -i "${md_build}/libretro/libretro.c"
+    # Remove Hardcoded BIOS Directory
+    sed -e "s|\"%s%cxmil\\\0\",RETRO_DIR,slash|\"%s\",RETRO_DIR|g" -i "${md_build}/libretro/libretro.c"
+
+    # Prevent Temp Files From Being Created In The Current Directory
+    sed -e "s|curpath\[MAX_PATH\] = \"./\";|curpath\[MAX_PATH\] = \"${md_inst}\";|g" -i "${md_build}/libretro/dosio.c"
 }
 
 function build_lr-x1() {
-    make -C libretro clean
-    make -C libretro
+    make -C libretro -f Makefile.libretro clean
+    make -C libretro -f Makefile.libretro
     md_ret_require="${md_build}/libretro/x1_libretro.so"
 }
 
@@ -31,11 +34,9 @@ function install_lr-x1() {
 function configure_lr-x1() {
     if [[ "${md_mode}" == "install" ]]; then
         mkRomDir "x1"
-
         mkUserDir "${biosdir}/x1"
+        defaultRAConfig "x1"
     fi
-
-    defaultRAConfig "x1"
 
     addEmulator 1 "${md_id}" "x1" "${md_inst}/x1_libretro.so"
 
