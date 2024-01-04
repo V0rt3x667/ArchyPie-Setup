a34fca8c636ee424b842b7291bad0bfc58c6a565
V0rt3x667 <al-g0l@outlook.com>
2024-01-04 01:11:42 +0000

    Update duckstation.sh
    * Switch to Clang
    * Fix patch for config directory
    * Create default config file
    * Update build options & dependencies

--- scriptmodules/emulators/duckstation.sh
+++ scriptmodules/emulators/duckstation.sh
@@ -6,7 +6,7 @@
 
 rp_module_id="duckstation"
 rp_module_desc="DuckStation: Sony PlayStation Emulator"
-rp_module_help="ROM Extensions: .bin .chd .cue .img\n\nCopy PSX ROMs To: ${romdir}/psx\n\nCopy BIOS File(s):\n\nps-30a\nps-30e\nps-30j\nscph5500.bin\nscph5501.bin\nscph5502.bin\nTo: ${biosdir}/psx"
+rp_module_help="ROM Extensions: .bin .chd .cue .img\n\nCopy PSX ROMs To: ${romdir}/psx\n\nCopy BIOS Files: scph5500.bin, scph5501.bin & scph5502.bin To: ${biosdir}/psx"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/stenzek/duckstation/master/LICENSE"
 rp_module_section="main"
 rp_module_repo="git https://github.com/stenzek/duckstation master"
@@ -14,17 +14,26 @@ rp_module_flags="!all arm x86_64"
 
 function depends_duckstation() {
     local depends=(
+        'clang'
         'cmake'
         'curl'
-        'extra-cmake-modules'
         'libxkbcommon'
+        'lld'
         'ninja'
         'qt6-base'
+        'qt6-svg'
         'qt6-tools'
         'sdl2'
     )
     isPlatform "kms" && depends+=('libdrm')
-    isPlatform "x11" && depends+=('xorg-xrandr')
+    isPlatform "x11" && depends+=(
+        'extra-cmake-modules'
+        'libx11'
+        'qt6-wayland'
+        'wayland-protocols'
+        'wayland'
+        'xorg-xrandr'
+    )
     getDepends "${depends[@]}"
 }
 
@@ -40,8 +49,9 @@ function sources_duckstation() {
 
 function build_duckstation() {
     local params=()
-    isPlatform "kms" && params+=(-DUSE_DRMKMS="ON" -DUSE_X11="OFF")
-    isPlatform "x11" && params+=(-DUSE_WAYLAND="ON")
+    isPlatform "kms" && params+=(-DENABLE_WAYLAND="OFF" -DENABLE_X11="OFF")
+    ! isPlatform "vulkan" && params+=(-DENABLE_VULKAN="OFF")
+    isPlatform "x11" && params+=(-DENABLE_WAYLAND="ON" -DENABLE_X11="ON")
 
     cmake . \
         -B"build" \
@@ -49,9 +59,13 @@ function build_duckstation() {
         -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
         -DCMAKE_BUILD_TYPE="Release" \
         -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -DCMAKE_C_COMPILER="clang" \
+        -DCMAKE_CXX_COMPILER="clang++" \
+        -DCMAKE_EXE_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DCMAKE_MODULE_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DCMAKE_SHARED_LINKER_FLAGS_INIT="-fuse-ld=lld" \
         -DBUILD_NOGUI_FRONTEND="OFF" \
         -DBUILD_QT_FRONTEND="ON" \
-        -DUSE_SDL2="ON" \
         "${params[@]}" \
         -Wno-dev
     ninja -C build clean
@@ -76,6 +90,22 @@ function configure_duckstation() {
         mkRomDir "psx"
         mkUserDir "${biosdir}/psx"
         ln -sf "${biosdir}/psx" "${md_conf_root}/psx/${md_id}/bios"
+
+        # Create Default Configuration File
+        local config
+        config="$(mktemp)"
+
+        iniConfig " = " "" "${config}"
+
+        # Set ROM Path For GUI
+        echo "[GameList]" >> "${config}"
+        iniSet "RecursivePaths" "${romdir}/psx"
+        # Disable AutoUpdater
+        echo "[AutoUpdater]" >> "${config}"
+        iniSet "CheckAtStartup" "false"
+
+        copyDefaultConfig "${config}" "${arpdir}/${md_id}/settings.ini"
+        rm "${config}"
     fi
 
     addEmulator 1 "${md_id}" "psx" "${md_inst}/${md_id}-qt -nogui -batch -fullscreen %ROM%"
