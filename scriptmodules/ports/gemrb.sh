e67ddad14f248a4d9545852540734facaac6fb31
V0rt3x667 <al-g0l@outlook.com>
2024-01-12 15:43:21 +0000

    Update gemrb.sh
    * Switch to Clang
    * Only add launchers for games that exist
    * Update build options & dependencies

--- scriptmodules/ports/gemrb.sh
+++ scriptmodules/ports/gemrb.sh
@@ -9,7 +9,7 @@ rp_module_desc="GemRB: Port of Bioware's Infinity Engine"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/gemrb/gemrb/master/COPYING"
 rp_module_repo="git https://github.com/gemrb/gemrb :_get_branch_gemrb"
 rp_module_section="exp"
-rp_module_flags="!mali"
+rp_module_flags=""
 
 function _get_branch_gemrb() {
     download "https://api.github.com/repos/gemrb/gemrb/releases/latest" - | grep -m 1 tag_name | cut -d\" -f4
@@ -17,15 +17,19 @@ function _get_branch_gemrb() {
 
 function depends_gemrb() {
     local depends=(
+        'clang'
         'cmake'
+        'freetype2'
         'glew'
         'libpng'
         'libvorbis'
+        'lld'
         'ninja'
         'openal'
         'python'
         'sdl2'
         'vlc'
+        'zlib'
     )
     getDepends "${depends[@]}"
 }
@@ -36,15 +40,20 @@ function sources_gemrb() {
 
 function build_gemrb() {
     local params
-    isPlatform "gl" && params+=('-DOPENGL_BACKEND=OpenGL')
-    
+    isPlatform "gl" && params+=(-DOPENGL_BACKEND="OpenGL")
+    isPlatform "rpi" && params+=(-DDISABLE_VIDEOCORE="ON")
+
     cmake . \
         -B"build" \
         -G"Ninja" \
         -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
         -DCMAKE_BUILD_TYPE="Release" \
         -DCMAKE_INSTALL_PREFIX="${md_inst}" \
-        -DFREETYPE_INCLUDE_DIRS="/usr/include/freetype2/" \
+        -DCMAKE_C_COMPILER="clang" \
+        -DCMAKE_CXX_COMPILER="clang++" \
+        -DCMAKE_EXE_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DCMAKE_MODULE_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DCMAKE_SHARED_LINKER_FLAGS_INIT="-fuse-ld=lld" \
         -DSDL_BACKEND="SDL2" \
         -DUSE_SDLMIXER="OFF" \
         "${params[@]}" \
@@ -58,7 +67,35 @@ function install_gemrb() {
     ninja -C build install/strip
 }
 
+function _add_games_gemrb() {
+    local cmd="${1}"
+    local dir
+    local game
+    local portname
+
+    declare -A games=(
+        ['baldursgate1/baldur.ini']="Baldur's Gate"
+        ['baldursgate2/baldur.ini']="Baldur's Gate II: Shadows of Amn"
+        ['icewind1/icewind.ini']="Icewind Dale"
+        ['icewind2/icewind2.ini']="Icewind Dale II"
+        ['planescape/torment.ini']="Planescape Torment"
+    )
+
+    for game in "${!games[@]}"; do
+        portname="gemrb"
+        dir="${romdir}/ports/${portname}/${game%%/*}"
+        # Convert Uppercase Filenames To Lowercase
+        [[ "${md_mode}" == "install" ]] && changeFileCase "${dir}"
+        # Create Launch Scripts For Each Game Found
+        if [[ -f "${dir}/${game##*/}" ]]; then
+            addPort "${md_id}" "${portname}" "${games[${game}]}" "${cmd} -C ${md_conf_root}/${md_id}/%ROM%/GemRB.cfg" "${game%%/*}"
+        fi
+    done
+}
+
 function configure_gemrb() {
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${md_id}/"
+
     if [[ "${md_mode}" == "install" ]]; then
         local dirs=(
             'baldursgate1'
@@ -72,8 +109,6 @@ function configure_gemrb() {
             mkUserDir "${arpdir}/${md_id}/${dir}"
         done
     fi
-
-    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${md_id}/"
 
     # Create Baldurs Gate 1 Configuration
     cat > "${md_conf_root}/${md_id}/baldursgate1/GemRB.cfg" << _EOF_
@@ -90,7 +125,12 @@ DrawFPS=0
 CaseSensitive=1
 GamePath=${romdir}/ports/${md_id}/baldursgate1/
 CD1=${romdir}/ports/${md_id}/baldursgate1/
-CachePath=${romdir}/ports/${md_id}/baldursgate1/cache/
+CD2=${romdir}/ports/${md_id}/baldursgate1/
+CD3=${romdir}/ports/${md_id}/baldursgate1/
+CD4=${romdir}/ports/${md_id}/baldursgate1/
+CD5=${romdir}/ports/${md_id}/baldursgate1/
+CD6=${romdir}/ports/${md_id}/baldursgate1/
+CachePath=${romdir}/ports/${md_id}/baldursgate1/cache
 _EOF_
 
     # Create Baldurs Gate 2 Configuration
@@ -130,9 +170,9 @@ GUIEnhancements = 15
 DrawFPS=0
 CaseSensitive=1
 GamePath=${romdir}/ports/${md_id}/icewind1/
-CD1=${romdir}/ports/${md_id}/icewind1/Data/
-CD2=${romdir}/ports/${md_id}/icewind1/CD2/Data/
-CD3=${romdir}/ports/${md_id}/icewind1/CD3/Data/
+CD1=${romdir}/ports/${md_id}/icewind1/data/
+CD2=${romdir}/ports/${md_id}/icewind1/cd2/data/
+CD3=${romdir}/ports/${md_id}/icewind1/cd3/data/
 CachePath=${romdir}/ports/${md_id}/icewind1/cache/
 _EOF_
 
@@ -169,14 +209,14 @@ DrawFPS=0
 CaseSensitive=1
 GamePath=${romdir}/ports/${md_id}/planescape/
 CD1=${romdir}/ports/${md_id}/planescape/data/
+CD2=${romdir}/ports/${md_id}/planescape/data/
+CD3=${romdir}/ports/${md_id}/planescape/data/
+CD4=${romdir}/ports/${md_id}/planescape/data/
+CD5=${romdir}/ports/${md_id}/planescape/data/
 CachePath=${romdir}/${md_id}/ports/planescape/cache/
 _EOF_
 
     chown -R "${user}:${user}" "${md_conf_root}/${md_id}"
 
-    addPort "${md_id}" "baldursgate1" "Baldur's Gate" "${md_inst}/bin/${md_id} -C ${md_conf_root}/${md_id}/baldursgate1/GemRB.cfg"
-    addPort "${md_id}" "baldursgate2" "Baldur's Gate II: Shadows of Amn" "${md_inst}/bin/${md_id} -C ${md_conf_root}/${md_id}/baldursgate2/GemRB.cfg"
-    addPort "${md_id}" "icewind1" "Icewind Dale" "${md_inst}/bin/${md_id} -C ${md_conf_root}/${md_id}/icewind1/GemRB.cfg"
-    addPort "${md_id}" "icewind2" "Icewind Dale II" "${md_inst}/bin/${md_id} -C ${md_conf_root}/${md_id}/icewind2/GemRB.cfg"
-    addPort "${md_id}" "planescape" "Planescape Torment" "${md_inst}/bin/${md_id} -C ${md_conf_root}/${md_id}/planescape/GemRB.cfg"
+    _add_games_gemrb "${md_inst}/bin/${md_id}"
 }
