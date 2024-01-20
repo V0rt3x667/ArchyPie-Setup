b4c02310ccd81273055696a3a222a0e0faf3e148
V0rt3x667 <al-g0l@outlook.com>
2024-01-20 14:56:37 +0000

    Update ioquake3.sh
    * Update dependencies
    * Add help section
    * Build option added to search rom path, no need for symlinks anymore
    * Use changeFileCase function to rename files

--- scriptmodules/ports/ioquake3.sh
+++ scriptmodules/ports/ioquake3.sh
@@ -6,16 +6,23 @@
 
 rp_module_id="ioquake3"
 rp_module_desc="ioquake3: Quake 3 Arena Port"
+rp_module_help="ROM Extensions: .pak\n\nCopy Quake III Arena Files To: ${romdir}/ports/quake3/baseq3\n\nCopy Quake III Team Fortress Files To: ${romdir}/ports/quake3/missionpack"
 rp_module_licence="GPL2 https://github.com/ioquake/ioq3/blob/master/COPYING.txt"
 rp_module_repo="git https://github.com/ioquake/ioq3 main"
 rp_module_section="opt"
-rp_module_flags="!rpi"
+rp_module_flags=""
 
 function depends_ioquake3() {
     local depends=(
+        'curl'
+        'libogg'
+        'libvorbis'
         'mesa'
-        'perl-rename'
+        'openal'
+        'opus'
+        'opusfile'
         'sdl2'
+        'zlib'
     )
     getDepends "${depends[@]}"
 }
@@ -29,7 +36,7 @@ function sources_ioquake3() {
 
 function build_ioquake3() {
     make clean
-    make COPYDIR="${md_inst}" USE_INTERNAL_LIBS=0
+    make COPYDIR="${md_inst}" USE_INTERNAL_LIBS=0 DEFAULT_BASEDIR="${romdir}/ports/quake3"
     md_ret_require="${md_build}/build/release-linux-$(_arch_${md_id})/${md_id}.$(_arch_${md_id})"
 }
 
@@ -38,7 +45,7 @@ function _arch_ioquake3() {
 }
 
 function install_ioquake3() {
-    make COPYDIR="${md_inst}" USE_INTERNAL_LIBS=0 copyfiles
+    make COPYDIR="${md_inst}" USE_INTERNAL_LIBS=0 DEFAULT_BASEDIR="${romdir}/ports/quake3" copyfiles
 }
 
 function _add_games_ioquake3() {
@@ -51,17 +58,14 @@ function _add_games_ioquake3() {
         ['baseq3/pak0.pk3']="Quake III Arena"
         ['missionpack/pak0.pk3']="Quake III: Team Arena"
     )
-    portname="quake3"
 
-    # Create .sh Files For Each Game Found. Uppercase Filenames Will Be Converted to Lowercase
     for game in "${!games[@]}"; do
-        dir="${romdir}/ports/${portname}"
-        if [[ "${md_mode}" == "install" ]]; then
-            pushd "${dir}/${game%%/*}" || return
-            perl-rename 'y/A-Z/a-z/' [^.-]{*,*/*}
-            popd || return
-        fi
-        if [[ -f "${dir}/${game}" ]]; then
+        portname="quake3"
+        dir="${romdir}/ports/${portname}/${game%%/*}"
+        # Convert Uppercase Filenames To Lowercase
+        [[ "${md_mode}" == "install" ]] && changeFileCase "${dir}"
+        # Create Launch Scripts For Each Game Found
+        if [[ -f "${dir}/${game##*/}" ]]; then
             addPort "${md_id}" "${portname}" "${games[${game}]}" "${cmd}" "${game%%/*}"
         fi
     done
@@ -71,19 +75,25 @@ function configure_ioquake3() {
     local portname
     portname="quake3"
 
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${portname}/${md_id}/"
+
     if [[ "${md_mode}" == "install" ]]; then
+        local dirs=(
+            'baseq3'
+            'missionpack'
+        )
+
         mkRomDir "ports/${portname}"
-        mkRomDir "ports/${portname}/baseq3"
-        mkRomDir "ports/${portname}/missionpack"
+
+        for dir in "${dirs[@]}"; do
+            mkRomDir "ports/${portname}/${dir}"
+        done
 
         _game_data_quake3
     fi
 
-    moveConfigDir "${md_inst}/baseq3" "${romdir}/ports/${portname}/baseq3"
-    moveConfigDir "${md_inst}/missionpack" "${romdir}/ports/${portname}/missionpack"
-    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${portname}/${md_id}/"
-
     local launcher=("${md_inst}/${md_id}.$(_arch_${md_id}) +set fs_game %ROM%")
+
     isPlatform "mesa" && launcher+=("+set cl_renderer opengl1")
     isPlatform "kms" && launcher+=("+set r_mode -1" "+set r_customwidth %XRES%" "+set r_customheight %YRES%" "+set r_swapInterval 1")
     isPlatform "x11" && launcher+=("+set r_mode -2" "+set cl_renderer opengl2" "+set r_fullscreen 1")
