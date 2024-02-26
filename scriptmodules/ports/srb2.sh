8a7d359736304dccd0f4d715281258e97b510953
V0rt3x667 <al-g0l@outlook.com>
2024-02-26 18:53:41 +0000

    Update srb2.sh
    * Switch to building with clang
    * Update build options & dependencies

--- scriptmodules/ports/srb2.sh
+++ scriptmodules/ports/srb2.sh
@@ -16,14 +16,18 @@ function _get_branch_srb2() {
 
 function depends_srb2() {
     local depends=(
+        'clang'
         'cmake'
         'curl'
         'libgme'
         'libopenmpt'
         'libpng'
+        'libupnp'
+        'lld'
         'ninja'
         'sdl2_mixer'
         'sdl2'
+        'zlib'
     )
     getDepends "${depends[@]}"
 }
@@ -35,49 +39,66 @@ function sources_srb2() {
     ver="${branch//./}"
 
     gitPullOrClone
+
+    # Download Assets
     downloadAndExtract "https://github.com/STJr/${md_id}/releases/download/${branch}/${md_id}-v${ver##*_}-Full.zip" "${md_build}/assets/installer"
 
     # Set Default Config Path(s)
     sed -e "s|DEFAULTDIR \".srb2\"|DEFAULTDIR \"${md_id}\"|g" -i "${md_build}/src/doomdef.h"
-
-    # Fix Bug #500 "https://github.com/STJr/SRB2/issues/500"
-    sed -e "s|set(CMAKE_C_FLAGS \${CMAKE_C_FLAGS} -Wno-trigraphs)|set(CMAKE_C_FLAGS \"\${CMAKE_C_FLAGS} -Wno-trigraphs\")|g" -i "${md_build}/src/CMakeLists.txt"
 }
 
 function build_srb2() {
+    local branch
+    branch="$(_get_branch_srb2)"
+
     cmake . \
         -B"build" \
         -G"Ninja" \
         -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
         -DCMAKE_BUILD_TYPE="Release" \
         -DCMAKE_INSTALL_PREFIX="${md_inst}" \
-        -DSRB2_ASSET_HASHED="srb2.pk3;player.dta;music.dta;zones.pk3" \
+        -DCMAKE_C_COMPILER="clang" \
+        -DCMAKE_CXX_COMPILER="clang++" \
+        -DCMAKE_EXE_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DCMAKE_MODULE_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DCMAKE_SHARED_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DBUILD_TESTING="OFF" \
+        -DSRB2_CONFIG_ENABLE_TESTS="OFF" \
         -Wno-dev
     ninja -C build clean
     ninja -C build
-    md_ret_require="${md_build}/build/bin/lsdlsrb2"
+    md_ret_require="${md_build}/build/bin/lsdlsrb2_${branch}"
 }
 
 function install_srb2() {
-    # Copy And Dereference, So We Get A 'srb2' Binary Rather Than A Symlink To 'lsdlsrb2'
-    cp -L 'build/bin/lsdlsrb2' "${md_inst}/${md_id}"
+    local branch
+    branch="$(_get_branch_srb2)"
 
     md_ret_files=(
         'assets/installer/music.dta'
+        'assets/installer/patch.pk3'
         'assets/installer/player.dta'
         'assets/installer/srb2.pk3'
         'assets/installer/zones.pk3'
-        'assets/LICENSE.txt'
         'assets/README.txt'
     )
+
+    # Rename Binary To 'srb2'
+    cp "build/bin/lsdlsrb2_${branch}" "${md_inst}/srb2"
 }
 
 function configure_srb2() {
+    local portname
+    portname="srb2"
+
     moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${md_id}/"
 
+    local launcher_prefix="SRB2WADDIR=${md_inst}"
+    local params=()
+
     if isPlatform "x11"; then
-        addPort "${md_id}" "${md_id}" "Sonic Robo Blast 2" "${md_inst}/${md_id} -home ${md_conf_root} -opengl"
-    else
-        addPort "${md_id}" "${md_id}" "Sonic Robo Blast 2" "${md_inst}/${md_id} -home ${md_conf_root}"
+        params+=("-opengl")
     fi
+
+    addPort "${md_id}" "${portname}" "Sonic Robo Blast 2" "${launcher_prefix} ${md_inst}/${md_id} -home ${md_conf_root} ${params[*]}"
 }
