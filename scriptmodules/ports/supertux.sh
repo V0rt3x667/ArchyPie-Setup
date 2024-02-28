84f0b30ab33c529a741f28b4940024a80abee1df
V0rt3x667 <al-g0l@outlook.com>
2024-02-28 02:27:08 +0000

    Update supertux.sh
    * Switch to building with clang
    * Update dependencies & build options

--- scriptmodules/ports/supertux.sh
+++ scriptmodules/ports/supertux.sh
@@ -9,7 +9,7 @@ rp_module_desc="SuperTux: Classic 2D Jump'n'Run Sidescroller Game"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/SuperTux/supertux/master/LICENSE.txt"
 rp_module_repo="git https://github.com/SuperTux/supertux :_get_branch_supertux"
 rp_module_section="opt"
-rp_module_flags="!mali"
+rp_module_flags=""
 
 function _get_branch_supertux() {
     download "https://api.github.com/repos/supertux/supertux/releases/latest" - | grep -m 1 tag_name | cut -d\" -f4
@@ -19,12 +19,17 @@ function depends_supertux() {
     local depends=(
         'boost-libs'
         'boost'
+        'clang'
         'cmake'
         'curl'
+        'doxygen'
         'freetype2'
+        'fribidi'
         'glew'
+        'harfbuzz'
         'libraqm'
         'libvorbis'
+        'lld'
         'mesa'
         'ninja'
         'openal'
@@ -32,6 +37,7 @@ function depends_supertux() {
         'physfs'
         'sdl2_image'
         'sdl2'
+        'zlib'
     )
     getDepends "${depends[@]}"
 }
@@ -39,19 +45,28 @@ function depends_supertux() {
 function sources_supertux() {
     gitPullOrClone
 
-    # Fix Build On GCC12
+    # Fix Build
     sed "1i#include <memory>" -i "${md_build}/external/partio_zip/zip_manager.cpp"
 }
 
 function build_supertux() {
+    local params=()
+
+    isPlatform "arm" && params+=("-DENABLE_OPENGLES2")
+
     cmake . \
         -B"build" \
         -G"Ninja" \
         -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
         -DCMAKE_BUILD_TYPE="Release" \
         -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -DCMAKE_C_COMPILER="clang" \
+        -DCMAKE_CXX_COMPILER="clang++" \
+        -DCMAKE_EXE_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DCMAKE_MODULE_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DCMAKE_SHARED_LINKER_FLAGS_INIT="-fuse-ld=lld" \
         -DINSTALL_SUBDIR_BIN="bin" \
-        -DUSE_SYSTEM_PHYSFS="ON" \
+        "${params[@]}" \
         -Wno-dev
     ninja -C build clean
     ninja -C build
@@ -63,7 +78,10 @@ function install_supertux() {
 }
 
 function configure_supertux() {
+    local portname
+    portname="supertux"
+
     moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${md_id}/"
 
-    addPort "${md_id}" "${md_id}" "SuperTux" "${md_inst}/bin/supertux2 --userdir ${md_conf_root}/${md_id}/ --fullscreen"
+    addPort "${md_id}" "${portname}" "SuperTux" "${md_inst}/bin/supertux2 --userdir ${md_conf_root}/${md_id}/ --fullscreen"
 }
