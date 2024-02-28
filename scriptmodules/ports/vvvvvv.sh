1b9912bf5025e8af6b1e0f16bf08a8df82f45ef2
V0rt3x667 <al-g0l@outlook.com>
2024-02-28 12:57:16 +0000

    Update vvvvvv.sh
    * Build from release tag
    * Use clang to build
    * Use system libs for physfs & tinyxml2
    * Add missing folders
    * Update build options & dependencies
    * Remove patch file

--- scriptmodules/ports/vvvvvv.sh
+++ scriptmodules/ports/vvvvvv.sh
@@ -8,15 +8,23 @@ rp_module_id="vvvvvv"
 rp_module_desc="VVVVVV: A 2D Puzzle Game"
 rp_module_help="Copy 'data.zip' From A Retail Or Make And Play Edition Of VVVVVV To: ${romdir}/ports/vvvvvv"
 rp_module_licence="NONCOM https://raw.githubusercontent.com/TerryCavanagh/VVVVVV/master/LICENSE.md"
-rp_module_repo="git https://github.com/TerryCavanagh/VVVVVV master"
+rp_module_repo="git https://github.com/TerryCavanagh/VVVVVV :_get_branch_vvvvvv"
 rp_module_section="opt"
+
+function _get_branch_vvvvvv() {
+    download "https://api.github.com/repos/TerryCavanagh/VVVVVV/releases/latest" - | grep -m 1 tag_name | cut -d\" -f4
+}
 
 function depends_vvvvvv() {
     local depends=(
+        'clang'
         'cmake'
+        'faudio'
+        'lld'
         'ninja'
-        'sdl2_mixer'
+        'physfs'
         'sdl2'
+        'tinyxml2'
     )
     getDepends "${depends[@]}"
 }
@@ -25,7 +33,7 @@ function sources_vvvvvv() {
     gitPullOrClone
 
     # Set Default Config Path(s)
-    applyPatch "${md_data}/01_set_default_config_path.patch"
+    sed -e "s|prefDir = PHYSFS_getPrefDir(\"distractionware\", \"VVVVVV\");|prefDir = \"/opt/archypie/configs/ports/vvvvvv/\";|g" -i "${md_build}/desktop_version/src/FileSystemUtils.cpp"
 
     # Set Fullscreen By Default
     sed -e "s|_this->fullscreen = false;|_this->fullscreen = true;|g" -i "${md_build}/desktop_version/src/Screen.cpp"
@@ -35,7 +43,6 @@ function sources_vvvvvv() {
 }
 
 function build_vvvvvv() {
-    rpSwap on 1500
     cmake . \
         -B"build" \
         -G"Ninja" \
@@ -43,29 +50,40 @@ function build_vvvvvv() {
         -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
         -DCMAKE_BUILD_TYPE="Release" \
         -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -DCMAKE_C_COMPILER="clang" \
+        -DCMAKE_CXX_COMPILER="clang++" \
+        -DCMAKE_EXE_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DCMAKE_MODULE_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DCMAKE_SHARED_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DBUNDLE_DEPENDENCIES="OFF" \
         -Wno-dev
     ninja -C build clean
     ninja -C build
-    rpSwap off
     md_ret_require="${md_build}/build/VVVVVV"
 }
 
 function install_vvvvvv() {
     md_ret_files=(
         'build/VVVVVV'
+        'desktop_version/fonts'
+        'desktop_version/lang'
         'gamecontrollerdb.txt'
         'LICENSE.md'
     )
 }
 
 function configure_vvvvvv() {
+    local portname
+    portname="vvvvvv"
+
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${md_id}/"
+
     if [[ "${md_mode}" == "install" ]]; then
         mkRomDir "/ports/${md_id}"
+
         # Symlink Game Data
         ln -snf "${romdir}/ports/${md_id}/data.zip" "${md_inst}/data.zip"
     fi
 
-    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/${md_id}"
-
-    addPort "${md_id}" "${md_id}" "VVVVVV" "${md_inst}/VVVVVV"
+    addPort "${md_id}" "${portname}" "VVVVVV" "${md_inst}/VVVVVV"
 }
