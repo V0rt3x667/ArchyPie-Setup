697deecf8f7661cbf9ba72f06e725360144a5121
V0rt3x667 <al-g0l@outlook.com>
2024-03-04 23:24:43 +0000

    Update quasi88.sh
    * Switch to building with clang
    * Add some of the changes from upstream commit: 963af0d

--- scriptmodules/emulators/quasi88.sh
+++ scriptmodules/emulators/quasi88.sh
@@ -6,8 +6,8 @@
 
 rp_module_id="quasi88"
 rp_module_desc="QUASI88: NEC PC-8801 Emulator"
-rp_module_help="ROM Extensions: .88d .cmt .d88 .t88\n\nCopy PC-88 Games To: ${romdir}/pc88\n\nCopy BIOS Files:\nFONT.ROM\nN88.ROM\nN88KNJ1.ROM\nN88KNJ2.ROM\nN88SUB.ROM\nTo ${biosdir}/pc88"
-rp_module_licence="BSD3 https://raw.githubusercontent.com/winterheart/quasi88/master/LICENSE.txt"
+rp_module_help="ROM Extensions: .88d .cmt .d88 .t88\n\nCopy PC-88 Games To: ${romdir}/pc88\n\nCopy BIOS Files:\nFONT.ROM\nN88.ROM\nN88KNJ1.ROM\nN88KNJ2.ROM\nN88SUB.ROM\nTo: ${biosdir}/pc88"
+rp_module_licence="BSD https://raw.githubusercontent.com/winterheart/quasi88/master/LICENSE.txt"
 rp_module_repo="git https://github.com/winterheart/quasi88 :_get_branch_quasi88"
 rp_module_section="exp"
 rp_module_flags=""
@@ -18,7 +18,9 @@ function _get_branch_quasi88() {
 
 function depends_quasi88() {
     local depends=(
+        'clang'
         'cmake'
+        'lld'
         'ninja'
         'sdl2'
     )
@@ -33,18 +35,21 @@ function sources_quasi88() {
 }
 
 function build_quasi88() {
-    local params=()
-
-    isPlatform "x11" && params+=('-DENABLE_X11=ON')
-    
     cmake . \
         -B"build" \
         -G"Ninja" \
         -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
         -DCMAKE_BUILD_TYPE="Release" \
         -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -DCMAKE_C_COMPILER="clang" \
+        -DCMAKE_CXX_COMPILER="clang++" \
+        -DCMAKE_EXE_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DCMAKE_MODULE_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DCMAKE_SHARED_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DDISKDIR="${romdir}/pc88" \
         -DENABLE_JOYSTICK="SDL" \
-        "${params[@]}" \
+        -DROMDIR="${biosdir}/pc88" \
+        -DTAPEDIR="${romdir}/pc88" \
         -Wno-dev
     ninja -C build clean
     ninja -C build
@@ -60,16 +65,29 @@ function install_quasi88() {
 }
 
 function configure_quasi88() {
-    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/pc88/${md_id}"
+    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/pc88/${md_id}/"
 
     if [[ "${md_mode}" == "install" ]]; then
         mkRomDir "pc88"
         mkUserDir "${biosdir}/pc88"
 
-        # Symlink ROM, Disk & Tape Directories
-        ln -snf "${biosdir}/pc88" "${md_conf_root}/pc88/${md_id}/rom"
-        ln -snf "${romdir}/pc88" "${md_conf_root}/pc88/${md_id}/disk"
-        ln -snf "${romdir}/pc88" "${md_conf_root}/pc88/${md_id}/tape"
+        # Create Default Config File
+        local conf="${md_conf_root}/pc88/quasi88.ini"
+
+        if [[ ! -f "${conf}" ]]; then
+            cat > "${conf}" <<_EOF_
+-auto_mouse
+-nostatus
+-double
+-english
+-use_joy
+-verbose 1
+-romdir "${biosdir}/pc88"
+-diskdir "${romdir}/pc88"
+-tapedir "${romdir}/pc88"
+_EOF_
+            chown -R "${user}:${user}" "${conf}"
+        fi
     fi
 
     addEmulator 1 "${md_id}" "pc88" "${md_inst}/${md_id}.sdl -f6 IMAGE-NEXT1 -f7 IMAGE-NEXT2 -f8 NOWAIT -f9 ROMAJI -f10 NUMLOCK -fullscreen %ROM%"
