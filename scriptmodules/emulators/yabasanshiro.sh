6dd2c189a22f38d732e5a9462cb39540e27be47a
V0rt3x667 <al-g0l@outlook.com>
2024-05-26 00:29:25 +0100

    Update yabasanshiro.sh
    * Add upstream fix for building on arm64
    * Build with clang

--- scriptmodules/emulators/yabasanshiro.sh
+++ scriptmodules/emulators/yabasanshiro.sh
@@ -5,29 +5,26 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="yabasanshiro"
-rp_module_desc="Yaba Sanshiro: SEGA Saturn Emulator "
+rp_module_desc="Yaba Sanshiro: SEGA Saturn Emulator"
 rp_module_help="ROM Extensions: .cue .chd\n\nCopy SEGA Saturn ROMs To: ${romdir}/saturn"
-rp_module_licence="GPL2 https://github.com/devmiyax/yabause/blob/master/LICENSE"
+rp_module_licence="GPL2 https://raw.githubusercontent.com/devmiyax/yabause/master/LICENSE"
 rp_module_repo="git https://github.com/devmiyax/yabause pi4-1-9-0"
 rp_module_section="exp"
-rp_module_flags="!all rpi4"
-
-# function _get_branch_yabasanshiro() {
-#     local branch
-
-#     isPlatform "rpi4" && branch="pi4-1-9-0"
-#     echo "${branch}"
-# }
+rp_module_flags="!all rpi"
 
 function depends_yabasanshiro() {
     local depends=(
         'boost-libs'
-        'cmake' 
+        'clang'
+        'cmake'
+        'doxygen'
         'libsecret'
+        'lld'
         'ninja'
         'openssl'
         'protobuf'
         'sdl2'
+        'zlib'
     )
     getDepends "${depends[@]}"
 }
@@ -38,8 +35,13 @@ function sources_yabasanshiro() {
 
 function build_yabasanshiro() {
     local params=()
-    isPlatform "rpi4" && params+=('-DYAB_PORTS=retro_arena' '-DYAB_WANT_DYNAREC_DEVMIYAX=ON' '-DYAB_WANT_ARM7=ON' '-DCMAKE_TOOLCHAIN_FILE=../yabause/src/retro_arena/pi4.cmake')
-    isPlatform "vulkan" && params+=('-DYAB_WANT_VULKAN=ON')
+
+    isPlatform "32bit" && params+=(-DCMAKE_SYSTEM_PROCESSOR="armv7-a")
+    isPlatform "64bit" && params+=(-DCMAKE_SYSTEM_PROCESSOR="aarch64")
+    isPlatform "vulkan" && params+=(-DYAB_WANT_VULKAN="ON")
+
+    export CFLAGS="${CFLAGS} -D_POSIX_C_SOURCE=199309L -D__PI4__ -D__RETORO_ARENA__"
+    export CXXFLAGS="${CXXFLAGS} -D__PI4__ -D__RETORO_ARENA_"
 
     cmake . \
         -B"build" \
@@ -48,7 +50,16 @@ function build_yabasanshiro() {
         -DCMAKE_BUILD_RPATH_USE_ORIGIN="ON" \
         -DCMAKE_BUILD_TYPE="Release" \
         -DCMAKE_INSTALL_PREFIX="${md_inst}" \
+        -DCMAKE_C_COMPILER="clang" \
+        -DCMAKE_CXX_COMPILER="clang++" \
+        -DCMAKE_EXE_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DCMAKE_MODULE_LINKER_FLAGS_INIT="-fuse-ld=lld" \
+        -DCMAKE_SHARED_LINKER_FLAGS_INIT="-fuse-ld=lld" \
         -DGIT_EXECUTABLE="/usr/bin/git" \
+        -DUSE_EGL="ON" \
+        -DYAB_PORTS="retro_arena" \
+        -DYAB_WANT_ARM7="ON" \
+        -DYAB_WANT_DYNAREC_DEVMIYAX="ON" \
         -DYAB_WANT_OPENAL="OFF" \
         "${params[@]}" \
         -Wno-dev
@@ -60,7 +71,6 @@ function build_yabasanshiro() {
 function install_yabasanshiro() {
     ninja -C build install/strip
 }
-
 
 function configure_yabasanshiro() {
     moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/saturn/${md_id}"
