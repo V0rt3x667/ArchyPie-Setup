01f5a6a6e96bf0bca90164c55d69f86dbd072353
V0rt3x667 <al-g0l@outlook.com>
2024-05-25 19:54:13 +0100

    Update pegasus-fe.sh
    * Update as per upstream

--- scriptmodules/supplementary/pegasus-fe.sh
+++ scriptmodules/supplementary/pegasus-fe.sh
@@ -5,7 +5,8 @@
 # Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="pegasus-fe"
-rp_module_desc="Pegasus: A Cross Platform Graphical Frontend (Latest Alpha Release)"
+rp_module_desc="Pegasus: A Cross Platform Graphical Frontend"
+rp_module_help="Pegasus is a cross platform, customizable graphical frontend for launching emulators & managing your game collection."
 rp_module_licence="GPL3 https://raw.githubusercontent.com/mmatyas/pegasus-frontend/master/LICENSE.md"
 rp_module_repo="git https://github.com/mmatyas/pegasus-frontend master"
 rp_module_section="exp"
@@ -15,7 +16,6 @@ function depends_pegasus-fe() {
     local depends=(
         'gst-libav'
         'gst-plugins-good'
-        'jq'
         'polkit'
         'qt5-declarative'
         'qt5-gamepad'
@@ -32,18 +32,88 @@ function depends_pegasus-fe() {
 
 function sources_pegasus-fe() {
     gitPullOrClone
+
+    # On KMS Apply A Patch To Fix Launching Games
+    isPlatform "kms" && applyPatch "${md_build}/etc/rpi4/kms_launch_fix.diff"
 }
 
 function build_pegasus-fe() {
     qmake . \
-        USE_SDL_GAMEPAD=1
+        INSTALLDIR="${md_inst}" \
+        QMAKE_CXXFLAGS+="${__cxxflags}" \
+        QMAKE_LIBS_LIBDL=-ldl \
+        USE_SDL_GAMEPAD=1 \
+        USE_SDL_POWER=1
     make clean
     make
+
     md_ret_require="${md_build}/src/app/${md_id}"
 }
 
 function install_pegasus-fe() {
-    md_ret_files=("src/app/${md_id}")
+    make install
+    _add_launcher_pegasus-fe
+}
+
+function _add_launcher_pegasus-fe() {
+    cat > /usr/bin/pegasus-fe << _EOF_
+#!/bin/bash
+
+if [[ \$(id -u) -eq 0 ]]; then
+    echo "Pegasus should not be run as root. If you used 'sudo pegasus-fe' please run without sudo."
+    exit 1
+fi
+_EOF_
+
+    # On KMS Platforms Add Additional Setup Commands
+    if isPlatform "kms"; then
+        cat >> /usr/bin/pegasus-fe << _EOF_
+# KMS Setup
+export QT_QPA_EGLFS_FORCE888=1  # Improve Gradients
+export QT_QPA_EGLFS_KMS_ATOMIC=1  # Use The Atomic DRM API On Pi 4
+export QT_QPA_PLATFORM=eglfs
+export QT_QPA_QT_QPA_EGLFS_INTEGRATION=eglfs_kms
+
+# Find The Right DRI Card
+for i in \$(find /sys/devices/platform -name "card?"); do
+    node=\${i:0-1}
+    case "\${i}" in
+        *gpu*)  card=\${node} ;;
+    esac
+done
+
+echo Using DRI Card At /dev/dri/card\${card}
+file="/tmp/pegasus_\$\$.eglfs.json"
+echo "{ \"device\": \"/dev/dri/card\${card}\" }" > "\${file}"
+export QT_QPA_EGLFS_KMS_CONFIG="\${file}"
+_EOF_
+    fi
+
+    cat >> /usr/bin/pegasus-fe << _EOF_
+clear
+"${md_inst}/pegasus-fe" "\${@}"
+
+rm -f "/tmp/pegasus_\$\$.eglfs.json"
+_EOF_
+
+    chmod +x /usr/bin/pegasus-fe
+}
+
+function _update_themes_pegasus-fe() {
+    echo Installing Themes
+    declare themes=(
+        "mmatyas/pegasus-theme-9999999-in-1"
+        "mmatyas/pegasus-theme-es2-simple"
+        "mmatyas/pegasus-theme-flixnet"
+        "mmatyas/pegasus-theme-secretary"
+    )
+    local theme
+    pushd "${home}/.config/pegasus-frontend/themes" || exit
+    for theme in "${themes[@]}"; do
+        local path=${theme//"mmatyas/pegasus-theme-"/}
+        gitPullOrClone "${path}" "https://github.com/${theme}"
+    done
+    popd || exit
 }
 
 function remove_pegasus-fe() {
@@ -51,32 +121,14 @@ function remove_pegasus-fe() {
 }
 
 function configure_pegasus-fe() {
-    moveConfigDir "${home}/.config/pegasus-frontend" "${md_conf_root}/all/${md_id}"
+    moveConfigDir "${home}/.config/pegasus-frontend" "${md_conf_root}/all/pegasus-fe"
 
     if [[ "${md_mode}" == "install" ]]; then
         # Create External Directories
         mkUserDir "${md_conf_root}/all/pegasus-fe/scripts"
         mkUserDir "${md_conf_root}/all/pegasus-fe/themes"
 
-        # Create Launcher Script
-        cat > /usr/bin/pegasus-fe << _EOF_
-#!/bin/bash
-
-if [[ \$(id -u) -eq 0 ]]; then
-    echo "Pegasus should not be run as root. If you used 'sudo pegasus-fe' please run without sudo."
-    exit 1
-fi
-
-# Save Current TTY/VT Number For Use With X So It Can Be Launched On The Correct TTY
-tty=\$(tty)
-export TTY="\${tty:8:1}"
-
-export QT_QPA_EGLFS_FORCE888=1  # Improve Gradients
-export QT_QPA_EGLFS_KMS_ATOMIC=1  # Use The Atomic DRM API On Pi 4
-
-clear
-"${md_inst}/pegasus-fe" "\$@"
-_EOF_
-        chmod +x /usr/bin/pegasus-fe
+        # Install Themes
+        _update_themes_pegasus-fe
     fi
 }
