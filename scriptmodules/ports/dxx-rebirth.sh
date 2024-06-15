40e9647182121f7c7dc5e03756a199c9526e6d8b
V0rt3x667 <al-g0l@outlook.com>
2024-06-15 01:42:04 +0100

    Update dxx-rebirth.sh
    * Add upstream changes for downloading shareware data
    * Minor formatting updates

--- scriptmodules/ports/dxx-rebirth.sh
+++ scriptmodules/ports/dxx-rebirth.sh
@@ -31,27 +31,28 @@ function sources_dxx-rebirth() {
     gitPullOrClone
 
     # Set Default Config Path(s)
-    sed -e "s|\"\t$HOME/.d\" DXX_NAME_NUMBER \"x-rebirth\n\"|\"\t$HOME/ArchyPie/configs/dxx\" DXX_NAME_NUMBER \"-rebirth\n\"|g" -i "${md_build}/similar/main/inferno.cpp"
+    sed -e "s|\"\t${HOME}/.d\" DXX_NAME_NUMBER \"x-rebirth\n\"|\"\t${HOME}/ArchyPie/configs/dxx\" DXX_NAME_NUMBER \"-rebirth\n\"|g" -i "${md_build}/similar/main/inferno.cpp"
     sed -e "s|\"~/.d\" DESCENT_PATH_NUMBER \"x-rebirth/\"|\"~/ArchyPie/configs/dxx\" DESCENT_PATH_NUMBER \"-rebirth/\"|g" -i "${md_build}/similar/misc/physfsx.cpp"
 }
 
 function build_dxx-rebirth() {
     local params=()
-    isPlatform "arm" && params+=('words_need_alignment=yes')
+    isPlatform "arm" && params+=('words_need_alignment=1')
     if isPlatform "rpi"; then
-        params+=('raspberrypi=yes')
-    elif isPlatform "mesa"; then
         params+=(
-            'opengl=yes'
+            'opengl=1'
             'opengles=no'
             'raspberrypi=mesa'
-            'sdl2=yes'
+            'sdl2=1'
         )
     else
-        params+=('opengl=yes' 'sdl2=yes')
+        params+=(
+            'opengl=1'
+            'opengles=0'
+            'sdl2=1'
+        )
     fi
 
-    export CPPFLAGS+=" -D_HAVE_STDINT_H"
     scons -c
     scons "${params[@]}" prefix="${md_inst}" -j"${__jobs}"
 
@@ -62,9 +63,9 @@ function build_dxx-rebirth() {
 }
 
 function install_dxx-rebirth() {
-    mv -f "${md_build}/d1x-rebirth/INSTALL.txt" "${md_build}/d1x-rebirth/D1X-INSTALL.txt"
+    mv -f "${md_build}/d1x-rebirth/INSTALL.txt"       "${md_build}/d1x-rebirth/D1X-INSTALL.txt"
     mv -f "${md_build}/d1x-rebirth/RELEASE-NOTES.txt" "${md_build}/d1x-rebirth/D1X-RELEASE-NOTES.txt"
-    mv -f "${md_build}/d2x-rebirth/INSTALL.txt" "${md_build}/d2x-rebirth/D2X-INSTALL.txt"
+    mv -f "${md_build}/d2x-rebirth/INSTALL.txt"       "${md_build}/d2x-rebirth/D2X-INSTALL.txt"
     mv -f "${md_build}/d2x-rebirth/RELEASE-NOTES.txt" "${md_build}/d2x-rebirth/D2X-RELEASE-NOTES.txt"
 
     md_ret_files=(
@@ -83,16 +84,18 @@ function install_dxx-rebirth() {
 }
 
 function _game_data_dxx-rebirth() {
-    local D1X_HIGH_TEXTURE_URL="https://www.dxx-rebirth.com/download/dxx/res/d1xr-hires.dxa"
-    local D1X_OGG_URL="https://www.dxx-rebirth.com/download/dxx/res/d1xr-sc55-music.dxa"
-    local D1X_SHARE_URL="https://www.dxx-rebirth.com/download/dxx/content/descent-pc-shareware.zip"
-    local D2X_OGG_URL="https://www.dxx-rebirth.com/download/dxx/res/d2xr-sc55-music.dxa"
-    local D2X_SHARE_URL="https://www.dxx-rebirth.com/download/dxx/content/descent2-pc-demo.zip"
+    local base_url="${__archive_url}/descent"
+    local D1X_SHARE_URL="${base_url}/descent-pc-shareware.zip"
+    local D2X_SHARE_URL="${base_url}/descent2-pc-demo.zip"
+    local D1X_HIGH_TEXTURE_URL="${base_url}/d1xr-hires.dxa"
+    local D1X_OGG_URL="${base_url}/d1xr-sc55-music.dxa"
+    local D2X_OGG_URL="${base_url}/d2xr-sc55-music.dxa"
+
     local dest_d1="${romdir}/ports/descent1"
     local dest_d2="${romdir}/ports/descent2"
 
     # Download, Unpack & Install Descent Shareware Files
-    if [[ ! -f "${dest_d1}/descent.hog" ]]; then
+    if [[ -z "$(find "${dest_d1}" -maxdepth 1 -iname descent.hog)" ]]; then
         downloadAndExtract "${D1X_SHARE_URL}" "${dest_d1}"
     fi
 
@@ -107,7 +110,7 @@ function _game_data_dxx-rebirth() {
     fi
 
     # Download, Unpack & Install Descent 2 Shareware Files
-    if [[ ! -f "${dest_d2}/D2DEMO.HOG" ]]; then
+    if [[ -z "$(find "${dest_d2}" -maxdepth 1 \( -iname D2DEMO.HOG -o -iname DESCENT2.HOG \))" ]]; then
         downloadAndExtract "${D2X_SHARE_URL}" "${dest_d2}"
     fi
 
@@ -120,32 +123,31 @@ function _game_data_dxx-rebirth() {
 }
 
 function configure_dxx-rebirth() {
-    local vers=(
-        '1'
-        '2'
-    )
+    local config
+	local ver
+	local portname="Descent Rebirth"
+	
+	for ver in 1 2; do
+	    [[ "${ver}" -eq 2 ]] && portname="Descent 2 Rebirth"
 
-    for ver in "${vers[@]}"; do
         moveConfigDir "${arpdir}/dxx${ver}-rebirth" "${md_conf_root}/descent${ver}/"
-    done
 
-    if [[ "${md_mode}" == "install" ]]; then
-        for ver in "${vers[@]}"; do
+        if [[ "${md_mode}" == "install" ]]; then
             mkRomDir "ports/descent${ver}"
+            
             # Set VSync For 'kms' Platform
             if isPlatform "kms"; then
                 config="$(mktemp)"
-                iniConfig '=' '' "${config}"
+                iniConfig "=" '' "${config}"
                 iniSet "VSync" "1"
                 copyDefaultConfig "${config}" "${md_conf_root}/descent${ver}/descent.cfg"
                 rm "${config}"
             fi
-        done
 
-        # Install Shareware & Data Files
-        _game_data_dxx-rebirth
-    fi
+            # Install Shareware & Data Files
+            _game_data_dxx-rebirth
+        fi
 
-    addPort "${md_id}" "descent1" "Descent Rebirth" "${md_inst}/d1x-rebirth -hogdir ${romdir}/ports/descent1"
-    addPort "${md_id}" "descent2" "Descent II Rebirth" "${md_inst}/d2x-rebirth -hogdir ${romdir}/ports/descent2"
+        addPort "${md_id}" "descent${ver}" "${portname}" "${md_inst}/d${ver}x-rebirth -hogdir ${romdir}/ports/descent${ver}"
+    done
 }
