1a4a889e08d843afea930c7c9940a09c058a5a68
V0rt3x667 <al-g0l@outlook.com>
2024-06-21 19:10:36 +0100

    Update supermodel.sh
    * Fix config file location
    * Add a launcher script

--- scriptmodules/emulators/supermodel.sh
+++ scriptmodules/emulators/supermodel.sh
@@ -10,61 +10,30 @@ rp_module_help="ROM Extension: .zip\n\nCopy Model 3 ROMs To: ${romdir}/model3"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/trzy/Supermodel/master/Docs/LICENSE.txt"
 rp_module_repo="git https://github.com/trzy/Supermodel master"
 rp_module_section="exp"
-rp_module_flags="!all x86_64"
+rp_module_flags="!all x86"
 
 function depends_supermodel() {
-    local depends=(
-        'glu'
-        'sdl2_net'
-        'sdl2'
-        'zlib'
-    )
-    getDepends "${depends[@]}"
+    depends_supermodel-sinden
 }
 
 function sources_supermodel() {
     gitPullOrClone
 
     # Set Default Config Path(s)
-    sed -e "s|/.config|/ArchyPie/configs|g" -i "${md_build}/Src/OSD/Unix/FileSystemPath.cpp"
-    sed -e "s|/.local/share|/ArchyPie/configs|g" -i "${md_build}/Src/OSD/Unix/FileSystemPath.cpp"
+    applyPatch "${md_data}/01_set_default_config_path.patch"
 }
 
 function build_supermodel() {
     make -f Makefiles/Makefile.UNIX clean
-    make -f Makefiles/Makefile.UNIX NET_BOARD=1
+    make -f Makefiles/Makefile.UNIX NET_BOARD=1 VERBOSE=1 ARCH="" OPT="${__default_cflags}"
+
     md_ret_require="${md_build}/bin/${md_id}"
 }
 
 function install_supermodel() {
-    md_ret_files=(
-        'Assets'
-        'bin/supermodel'
-        'Config'
-    )
+    install_supermodel-sinden
 }
 
 function configure_supermodel() {
-    moveConfigDir "${arpdir}/${md_id}" "${md_conf_root}/model3/${md_id}/"
-
-    if [[ "${md_mode}" == "install" ]]; then
-        mkRomDir "arcade"
-        mkRomDir "model3"
-
-        mkUserDir "${md_conf_root}/model3/${md_id}/Config"
-
-        # Copy Default Config Files
-        cp "${md_inst}/Config/Supermodel.ini" "${md_conf_root}/model3/${md_id}/Config"
-        cp "${md_inst}/Config/Games.xml" "${md_conf_root}/model3/${md_id}/Config"
-        chown -R "${user}:${user}" "${md_conf_root}/model3/${md_id}/Config"
-
-        # Symlink NVRAM Folder
-        moveConfigDir "${md_inst}/NVRAM" "${md_conf_root}/model3/${md_id}/NVRAM/"
-    fi
-
-    addEmulator 0 "${md_id}" "arcade" "pushd ${md_inst}; ${md_inst}/${md_id} -fullscreen %ROM%; popd"
-    addEmulator 1 "${md_id}" "model3" "pushd ${md_inst}; ${md_inst}/${md_id} -fullscreen %ROM%; popd"
-
-    addSystem "arcade"
-    addSystem "model3"
+    configure_supermodel-sinden
 }
