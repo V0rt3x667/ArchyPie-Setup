0339a8c58fd22acd009bbbcf7243b581b6678fc9
V0rt3x667 <al-g0l@outlook.com>
2026-02-19 15:24:51 +0000

    resetromdirs.sh cosmetic changes

--- scriptmodules/supplementary/resetromdirs.sh
+++ scriptmodules/supplementary/resetromdirs.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
 
-# This file is part of the ArchyPie project.
+#     ________   ______    ______   ___   ___   __  __            ______   ________  ______      
+#    /_______/\ /_____/\  /_____/\ /__/\ /__/\ /_/\/_/\          /_____/\ /_______/\/_____/\     
+#    \::: _  \ \\:::_ \ \ \:::__\/ \::\ \\  \ \\ \ \ \ \  _______\:::_ \ \\__.::._\/\::::_\/_    
+#     \::(_)  \ \\:(_) ) )_\:\ \  __\::\/_\ .\ \\:\_\ \ \/______/\\:(_) \ \  \::\ \  \:\/___/\   
+#      \:: __  \ \\: __ `\ \\:\ \/_/\\:: ___::\ \\::::_\/\__::::\/ \: ___\/  _\::\ \__\::___\/_  
+#       \:.\ \  \ \\ \ `\ \ \\:\_\ \ \\: \ \\::\ \ \::\ \           \ \ \   /__\::\__/\\:\____/\ 
+#        \__\/\__\/ \_\/ \_\/ \_____\/ \__\/ \::\/  \__\/            \_\/   \________\/ \_____\/ 
 #
-# Please see the LICENSE file at the top-level directory of this distribution.
+#    This file is part of the ArchyPie Project.
+#
+#    Please see the LICENSE file at the top-level directory of this distribution.
 
 rp_module_id="resetromdirs"
-rp_module_desc="Reset Ownership & Permissions Of The ArchyPie ROM & BIOS Directories"
+rp_module_desc="Reset Ownership & Permissions of the ArchyPie ROM & BIOS Directories"
 rp_module_section="config"
 
 function gui_resetromdirs() {
-    printHeading "Resetting ${romdir} Ownership & Permissions"
-
-    mkUserDir "${romdir}"
-    mkUserDir "${biosdir}"
-
-    chown -R "${__user}":"${__group}" "${romdir}"
-    chown -R "${__user}":"${__group}" "${biosdir}"
-
-    chmod -R ug+rwx "${romdir}"
-    chmod -R ug+rwx "${biosdir}"
+    printHeading "Resetting $romdir ownership & permissions"
+    mkUserDir "$romdir"
+    mkUserDir "$biosdir"
+    chown -R "$__user":"$__group" "$romdir"
+    chown -R "$__user":"$__group" "$biosdir"
+    chmod -R ug+rwX "$romdir"
+    chmod -R ug+rwX "$biosdir"
 }
+
